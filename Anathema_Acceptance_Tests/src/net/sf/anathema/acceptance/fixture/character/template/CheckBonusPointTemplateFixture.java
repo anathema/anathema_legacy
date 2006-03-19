27c73fba67c50744f79ece37e1b4199358d4ccd8
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-03-19 17:25:15 +0000

    Tests for the Solar Bonus Point Template

---
+++ Anathema_Acceptance_Tests/src/net/sf/anathema/acceptance/fixture/character/template/CheckBonusPointTemplateFixture.java
@@ -0,0 +1,50 @@
+package net.sf.anathema.acceptance.fixture.character.template;
+
+import net.sf.anathema.character.generic.template.experience.ICurrentRatingCosts;
+
+public class CheckBonusPointTemplateFixture extends AbstractTemplateColumnFixture {
+
+  public int getUnfavoredAttributePointCost() {
+    return getTemplate().getBonusPointCosts().getAttributeCosts(false).getRatingCosts(1);
+  }
+
+  public int getFavoredAttributePointCost() {
+    return getTemplate().getBonusPointCosts().getAttributeCosts(true).getRatingCosts(1);
+  }
+
+  public int getFavoredAbilityPointCost() {
+    return getTemplate().getBonusPointCosts().getAbilityCosts(true).getRatingCosts(1);
+  }
+
+  public int getUnfavoredAbilityPointCost() {
+    return getTemplate().getBonusPointCosts().getAbilityCosts(false).getRatingCosts(1);
+  }
+
+  public int getUnfavoredSpecialtyDotsPerPoint() {
+    return getTemplate().getBonusPointCosts().getDefaultSpecialtyDotsPerPoint();
+  }
+
+  public int getFavoredSpecialtyDotsPerPoint() {
+    return getTemplate().getBonusPointCosts().getFavoredSpecialtyDotsPerPoint();
+  }
+
+  public int getLowBackgroundCost() {
+    return getTemplate().getBonusPointCosts().getBackgroundBonusPointCost().getRatingCosts(1);
+  }
+
+  public int getHighBackgroundCost() {
+    return getTemplate().getBonusPointCosts().getBackgroundBonusPointCost().getRatingCosts(4);
+  }
+
+  public int getVirtueCost() {
+    return getTemplate().getBonusPointCosts().getVirtueCosts().getRatingCosts(1);
+  }
+
+  public int getWillpowerCost() {
+    return getTemplate().getBonusPointCosts().getWillpowerCosts();
+  }
+
+  public int getEssenceCost() {
+    return getTemplate().getBonusPointCosts().getEssenceCost();
+  }
+}
--- Anathema_Acceptance_Tests/src/net/sf/anathema/acceptance/fixture/character/template/CheckMagicTemplateFixture.java
+++ Anathema_Acceptance_Tests/src/net/sf/anathema/acceptance/fixture/character/template/CheckMagicTemplateFixture.java
@@ -1,8 +1,36 @@
 package net.sf.anathema.acceptance.fixture.character.template;
 
+import net.sf.anathema.character.generic.impl.rules.ExaltedRuleSet;
+import net.sf.anathema.character.generic.magic.spells.CircleType;
+
 public class CheckMagicTemplateFixture extends AbstractTemplateColumnFixture {
 
   public String getFavoringTraitType() {
     return getTemplate().getMagicTemplate().getFavoringTraitType().getId();
+  }
+
+  public String getMaximumSorceryCircle() {
+    return getMaximumCircle(getTemplate().getMagicTemplate().getSpellMagic().getSorceryCircles()).getId();
+  }
+
+  public String getMaximumNecromancyCircle() {
+    return getMaximumCircle(getTemplate().getMagicTemplate().getSpellMagic().getNecromancyCircles()).getId();
+  }
+
+  private CircleType getMaximumCircle(CircleType[] circles) {
+    CircleType highestCircle = circles[0];
+    for (CircleType circle : circles) {
+      highestCircle = circle.compareTo(highestCircle) > 0 ? circle : highestCircle;
+    }
+    return highestCircle;
+  }
+
+  public String getMartialArtsLevel() {
+    return getTemplate().getMagicTemplate().getCharmTemplate().getMartialArtsLevel().getId();
+  }
+
+  public String getCharmType() {
+    return getTemplate().getMagicTemplate().getCharmTemplate().getCharms(ExaltedRuleSet.CoreRules)[0].getCharacterType()
+        .getId();
   }
 }
