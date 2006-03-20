0ba88fa46ee37d4496e1751b573611a0c2a4b291
Urs Reupke <ursreupke@gmail.com>
2006-03-20 16:45:22 +0000

    Removed all undue references to AbilityType.values()

--- Character_Main/src/net/sf/anathema/character/model/traits/ICoreTraitConfiguration.java
+++ Character_Main/src/net/sf/anathema/character/model/traits/ICoreTraitConfiguration.java
@@ -2,6 +2,7 @@ package net.sf.anathema.character.model.traits;
 
 import net.sf.anathema.character.generic.traits.groups.IIdentifiedTraitTypeGroup;
 import net.sf.anathema.character.generic.traits.types.AbilityType;
+import net.sf.anathema.character.library.trait.IFavorableTrait;
 import net.sf.anathema.character.library.trait.ITraitCollection;
 import net.sf.anathema.character.model.background.IBackgroundConfiguration;
 import net.sf.anathema.lib.util.IIdentificate;
@@ -13,6 +14,8 @@ public interface ICoreTraitConfiguration extends ITraitCollection {
   public IIdentifiedTraitTypeGroup[] getAttributeTypeGroups();
 
   public IIdentifiedTraitTypeGroup[] getAbilityTypeGroups();
+
+  public IFavorableTrait[] getAllAbilities();
 
   public IIdentificate getAbilityGroupId(AbilityType abilityType);
 }
