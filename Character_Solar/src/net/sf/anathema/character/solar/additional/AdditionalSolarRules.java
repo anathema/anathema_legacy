d6cec12d241bba6cc4ac8d7e6ee06de7a72375ab
Urs Reupke <ursreupke@gmail.com>
2006-03-20 12:49:11 +0000

    Alien charm rules moved to charmtemplate. Eclipse caste is fully funtional.

--- Character_Solar/src/net/sf/anathema/character/solar/additional/AdditionalSolarRules.java
+++ Character_Solar/src/net/sf/anathema/character/solar/additional/AdditionalSolarRules.java
@@ -2,21 +2,11 @@ package net.sf.anathema.character.solar.additional;
 
 import java.util.ArrayList;
 
-import net.sf.anathema.character.generic.caste.ICasteType;
 import net.sf.anathema.character.generic.impl.additional.DefaultAdditionalRules;
-import net.sf.anathema.character.solar.caste.SolarCaste;
 
 public class AdditionalSolarRules extends DefaultAdditionalRules {
 
   public AdditionalSolarRules() {
     super(new ArrayList<String>());
-  }
-
-  @Override
-  public boolean isAllowedAlienCharms(ICasteType type) {
-    if (type == SolarCaste.Eclipse) {
-      return true;
-    }
-    return super.isAllowedAlienCharms(type);
   }
 }
