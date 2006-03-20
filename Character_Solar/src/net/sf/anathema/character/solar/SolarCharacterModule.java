46bf3ae0887dea64b2b748775a0a4387f7f612ed
Urs Reupke <ursreupke@gmail.com>
2006-03-20 12:58:20 +0000

    XML-Solars: Removed Solar template classes

--- Character_Solar/src/net/sf/anathema/character/solar/SolarCharacterModule.java
+++ Character_Solar/src/net/sf/anathema/character/solar/SolarCharacterModule.java
@@ -6,20 +6,15 @@ import net.sf.anathema.character.generic.framework.additionaltemplate.model.IAdd
 import net.sf.anathema.character.generic.framework.additionaltemplate.persistence.IAdditionalPersisterFactory;
 import net.sf.anathema.character.generic.framework.module.CharacterGenericsModuleAdapter;
 import net.sf.anathema.character.generic.impl.caste.CasteCollection;
-import net.sf.anathema.character.generic.impl.magic.persistence.CharmCache;
 import net.sf.anathema.character.generic.magic.charms.special.ISpecialCharm;
-import net.sf.anathema.character.generic.template.ITemplateRegistry;
 import net.sf.anathema.character.generic.type.CharacterType;
-import net.sf.anathema.character.solar.additional.AdditionalSolarRules;
 import net.sf.anathema.character.solar.caste.SolarCaste;
 import net.sf.anathema.character.solar.reporting.SolarVoidStateReportTemplate;
 import net.sf.anathema.character.solar.template.ISolarSpecialCharms;
-import net.sf.anathema.character.solar.template.SolarTemplate;
 import net.sf.anathema.character.solar.virtueflaw.SolarVirtueFlawModelFactory;
 import net.sf.anathema.character.solar.virtueflaw.SolarVirtueFlawPersisterFactory;
 import net.sf.anathema.character.solar.virtueflaw.SolarVirtueFlawTemplate;
 import net.sf.anathema.character.solar.virtueflaw.SolarVirtueFlawViewFactory;
-import net.sf.anathema.lib.exception.PersistenceException;
 import net.sf.anathema.lib.logging.Logger;
 import net.sf.anathema.lib.registry.IRegistry;
 import net.sf.anathema.lib.resources.IResources;
@@ -27,7 +22,6 @@ import net.sf.anathema.lib.resources.IResources;
 public class SolarCharacterModule extends CharacterGenericsModuleAdapter {
 
   private final Logger logger = Logger.getLogger(SolarCharacterModule.class);
-  private final AdditionalSolarRules additionalSolarRules = new AdditionalSolarRules();
 
   @Override
   public void registerCommonData(ICharacterGenerics characterGenerics) {
@@ -45,17 +39,6 @@ public class SolarCharacterModule extends CharacterGenericsModuleAdapter {
   @Override
   public void addCharacterTemplates(ICharacterGenerics characterGenerics) {
     registerParsedTemplate(characterGenerics, "template/Solar.template"); //$NON-NLS-1$
-    // CharmCache charmProvider = CharmCache.getInstance();
-    // registerSolarTemplate(characterGenerics.getTemplateRegistry(), charmProvider);
-  }
-
-  private void registerSolarTemplate(ITemplateRegistry templateRegistry, CharmCache charmProvider) {
-    try {
-      templateRegistry.register(new SolarTemplate(charmProvider, additionalSolarRules));
-    }
-    catch (PersistenceException exception) {
-      logger.error("Solar Charms not found", exception); //$NON-NLS-1$
-    }
   }
 
   @Override
