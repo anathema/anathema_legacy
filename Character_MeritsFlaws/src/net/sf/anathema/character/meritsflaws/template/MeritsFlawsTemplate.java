93b4bf66409b4f408b0372d090ffaaea9a56885b
Urs Reupke <ursreupke@gmail.com>
2006-04-02 10:46:44 +0000

    Introduced "GlobalAdditionalTemplate" with edition support flag

--- Character_MeritsFlaws/src/net/sf/anathema/character/meritsflaws/template/MeritsFlawsTemplate.java
+++ Character_MeritsFlaws/src/net/sf/anathema/character/meritsflaws/template/MeritsFlawsTemplate.java
@@ -1,9 +1,11 @@
 package net.sf.anathema.character.meritsflaws.template;
 
-import net.sf.anathema.character.generic.template.additional.IAdditionalTemplate;
+import net.sf.anathema.character.generic.rules.IEditionVisitor;
+import net.sf.anathema.character.generic.rules.IExaltedEdition;
+import net.sf.anathema.character.generic.template.additional.IGlobalAdditionalTemplate;
 import net.sf.anathema.lib.util.Identificate;
 
-public class MeritsFlawsTemplate extends Identificate implements IAdditionalTemplate {
+public class MeritsFlawsTemplate extends Identificate implements IGlobalAdditionalTemplate {
 
   public static final String ID = "MeritsAndFlaws"; //$NON-NLS-1$
 
@@ -13,5 +15,19 @@ public class MeritsFlawsTemplate extends Identificate implements IAdditionalTemp
 
   public int getMaximalBonusPointGain() {
     return 10;
+  }
+
+  public boolean supportsEdition(IExaltedEdition edition) {
+    final boolean[] supported = new boolean[1];
+    edition.accept(new IEditionVisitor() {
+      public void visitFirstEdition(IExaltedEdition visitedEdition) {
+        supported[0] = true;
+      }
+
+      public void visitSecondEdition(IExaltedEdition visitedEdition) {
+        supported[0] = false;
+      }
+    });
+    return supported[0];
   }
 }
