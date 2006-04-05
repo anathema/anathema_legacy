be3852dfc806fd8259bf8966e8bae1f2362061df
Urs Reupke <ursreupke@gmail.com>
2006-04-05 19:56:37 +0000

    RemovableTraitView / IRemovableIntValueView moved to Character Library.

---
+++ Character_Library/src/net/sf/anathema/character/library/intvalue/IRemovableIntValueView.java
@@ -0,0 +1,8 @@
+package net.sf.anathema.character.library.intvalue;
+
+import net.sf.anathema.character.library.removableentry.presenter.IRemovableEntryView;
+import net.sf.anathema.framework.value.IIntValueView;
+
+public interface IRemovableIntValueView extends IIntValueView, IRemovableEntryView {
+  // Nothing to do
+}
