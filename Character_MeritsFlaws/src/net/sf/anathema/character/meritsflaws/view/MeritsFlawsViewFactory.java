262a23a5df2f0038ba11c9c18ad4f02cad8f3dcd
Urs Reupke <ursreupke@gmail.com>
2006-04-06 17:54:49 +0000

    CharacterType parameter for ViewFactory "CreateView"

--- Character_MeritsFlaws/src/net/sf/anathema/character/meritsflaws/view/MeritsFlawsViewFactory.java
+++ Character_MeritsFlaws/src/net/sf/anathema/character/meritsflaws/view/MeritsFlawsViewFactory.java
@@ -2,6 +2,7 @@ package net.sf.anathema.character.meritsflaws.view;
 
 import net.sf.anathema.character.generic.additionaltemplate.IAdditionalModel;
 import net.sf.anathema.character.generic.framework.additionaltemplate.IAdditionalViewFactory;
+import net.sf.anathema.character.generic.type.CharacterType;
 import net.sf.anathema.character.meritsflaws.presenter.IMeritsFlawsAdditionalModel;
 import net.sf.anathema.character.meritsflaws.presenter.MeritsFlawsPresenter;
 import net.sf.anathema.character.meritsflaws.presenter.MeritsFlawsViewProperties;
@@ -10,7 +11,7 @@ import net.sf.anathema.lib.resources.IResources;
 
 public class MeritsFlawsViewFactory implements IAdditionalViewFactory {
 
-  public ISimpleTabView createView(IAdditionalModel model, IResources resources) {
+  public ISimpleTabView createView(IAdditionalModel model, IResources resources, CharacterType type) {
     IMeritsFlawsAdditionalModel meritsFlawsModel = (IMeritsFlawsAdditionalModel) model;
     MeritsFlawsViewProperties properties = new MeritsFlawsViewProperties(
         meritsFlawsModel.getMeritsFlawsModel(),
