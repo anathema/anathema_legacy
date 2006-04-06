2ca40cf3103a48492629ba917899d48b9ac35153
Urs Reupke <ursreupke@gmail.com>
2006-04-06 17:55:09 +0000

    CharacterType parameter for ViewFactory "CreateView"

--- Character_Sidereal/src/net/sf/anathema/character/sidereal/colleges/SiderealCollegeViewFactory.java
+++ Character_Sidereal/src/net/sf/anathema/character/sidereal/colleges/SiderealCollegeViewFactory.java
@@ -2,6 +2,7 @@ package net.sf.anathema.character.sidereal.colleges;
 
 import net.sf.anathema.character.generic.additionaltemplate.IAdditionalModel;
 import net.sf.anathema.character.generic.framework.additionaltemplate.IAdditionalViewFactory;
+import net.sf.anathema.character.generic.type.CharacterType;
 import net.sf.anathema.character.sidereal.colleges.presenter.ISiderealCollegeModel;
 import net.sf.anathema.character.sidereal.colleges.presenter.ISiderealCollegeViewProperties;
 import net.sf.anathema.character.sidereal.colleges.presenter.SiderealCollegePresenter;
@@ -14,7 +15,7 @@ import net.sf.anathema.lib.resources.IResources;
 
 public class SiderealCollegeViewFactory implements IAdditionalViewFactory {
 
-  public ISimpleTabView createView(IAdditionalModel model, IResources resources) {
+  public ISimpleTabView createView(IAdditionalModel model, IResources resources, CharacterType type) {
     ISiderealCollegeViewProperties properties = new SiderealCollegeViewProperties(resources);
     SiderealCollegeCreationOverview creationOverview = new SiderealCollegeCreationOverview(properties);
     SiderealCollegeExperiencedOverview experiencedOverview = new SiderealCollegeExperiencedOverview(properties);
