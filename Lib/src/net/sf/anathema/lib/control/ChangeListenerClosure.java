79616421fe8d582e8d27e2c163f71f75cdd41b77
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-09 20:15:11 +0000

    GenericControl<IChangeListener> replaced with ChangeControl

--- Lib/src/net/sf/anathema/lib/control/ChangeListenerClosure.java
+++
@@ -1,8 +0,0 @@
-package net.sf.anathema.lib.control;
-
-
-public final class ChangeListenerClosure implements IClosure<IChangeListener> {
-  public void execute(IChangeListener input) {
-    input.changeOccured();
-  }
-}
--- Lib/src/net/sf/anathema/lib/control/ValueChangeEventClosure.java
+++
@@ -1,8 +0,0 @@
-package net.sf.anathema.lib.control;
-
-
-public final class ValueChangeEventClosure implements IClosure<IChangeListener> {
-  public void execute(IChangeListener input) {
-    input.changeOccured();
-  }
-}
