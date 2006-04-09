1feeb226af34e0f27a752df8745983a436807333
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-09 20:08:23 +0000

    ChangeControl uses IChangeListener

--- Character_Generic_Framework/src/net/sf/anathema/character/generic/framework/additionaltemplate/model/IAdditionalModelFactory.java
+++ Character_Generic_Framework/src/net/sf/anathema/character/generic/framework/additionaltemplate/model/IAdditionalModelFactory.java
@@ -1,14 +1,13 @@
 package net.sf.anathema.character.generic.framework.additionaltemplate.model;
 
-import javax.swing.event.ChangeListener;
-
 import net.sf.anathema.character.generic.additionaltemplate.IAdditionalModel;
 import net.sf.anathema.character.generic.template.additional.IAdditionalTemplate;
+import net.sf.anathema.lib.control.IChangeListener;
 
 public interface IAdditionalModelFactory {
 
   public IAdditionalModel createModel(
       IAdditionalTemplate additionalTemplate,
       ICharacterModelContext context,
-      ChangeListener[] listeners);
+      IChangeListener[] listeners);
 }
