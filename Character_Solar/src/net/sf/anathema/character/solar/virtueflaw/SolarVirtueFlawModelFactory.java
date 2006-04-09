4a42051ca0c2d54cddf58ac6d712ad996e81dbd6
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-09 20:09:18 +0000

    ChangeControl uses IChangeListener

--- Character_Solar/src/net/sf/anathema/character/solar/virtueflaw/SolarVirtueFlawModelFactory.java
+++ Character_Solar/src/net/sf/anathema/character/solar/virtueflaw/SolarVirtueFlawModelFactory.java
@@ -1,21 +1,20 @@
 package net.sf.anathema.character.solar.virtueflaw;
 
-import javax.swing.event.ChangeListener;
-
 import net.sf.anathema.character.generic.additionaltemplate.IAdditionalModel;
 import net.sf.anathema.character.generic.framework.additionaltemplate.model.IAdditionalModelFactory;
 import net.sf.anathema.character.generic.framework.additionaltemplate.model.ICharacterModelContext;
 import net.sf.anathema.character.generic.template.additional.IAdditionalTemplate;
 import net.sf.anathema.character.solar.virtueflaw.model.SolarVirtueFlawModel;
+import net.sf.anathema.lib.control.IChangeListener;
 
 public class SolarVirtueFlawModelFactory implements IAdditionalModelFactory {
 
   public IAdditionalModel createModel(
       IAdditionalTemplate additionalTemplate,
       ICharacterModelContext context,
-      ChangeListener[] listeners) {
+      IChangeListener[] listeners) {
     SolarVirtueFlawModel model = new SolarVirtueFlawModel(context, additionalTemplate);
-    for (ChangeListener listener : listeners) {
+    for (IChangeListener listener : listeners) {
       model.addBonusPointsChangeListener(listener);
     }
     return model;
