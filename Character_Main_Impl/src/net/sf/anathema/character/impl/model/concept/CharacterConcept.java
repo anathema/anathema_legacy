b8dfc5ac1ad15b5c4f942bf99724109b53bcbf8d
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-09 19:24:14 +0000

    Merge CharacterConcept classes

--- Character_Main_Impl/src/net/sf/anathema/character/impl/model/concept/CharacterConcept.java
+++ Character_Main_Impl/src/net/sf/anathema/character/impl/model/concept/CharacterConcept.java
@@ -1,14 +1,28 @@
 package net.sf.anathema.character.impl.model.concept;
 
+import net.sf.anathema.character.generic.caste.ICasteType;
 import net.sf.anathema.character.impl.model.TypedDescription;
+import net.sf.anathema.character.model.ICharacterConcept;
 import net.sf.anathema.character.model.ITypedDescription;
 import net.sf.anathema.character.model.nature.INatureType;
+import net.sf.anathema.lib.workflow.textualdescription.ISimpleTextualDescription;
+import net.sf.anathema.lib.workflow.textualdescription.model.SimpleTextualDescription;
 
-public class CharacterConcept extends AbstractCharacterConcept {
+public class CharacterConcept implements ICharacterConcept {
 
   private final ITypedDescription<INatureType> nature = new TypedDescription<INatureType>();
+  private final ITypedDescription<ICasteType> caste = new Caste();
+  private final ISimpleTextualDescription concept = new SimpleTextualDescription();
 
   public ITypedDescription<INatureType> getNature() {
     return nature;
+  }
+
+  public ITypedDescription<ICasteType> getCaste() {
+    return caste;
+  }
+
+  public ISimpleTextualDescription getConcept() {
+    return concept;
   }
 }
