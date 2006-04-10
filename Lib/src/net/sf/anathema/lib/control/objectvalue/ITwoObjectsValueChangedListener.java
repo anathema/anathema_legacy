26dfd388a6c1a57962e1743132876b3e017aaf5d
Urs Reupke <ursreupke@gmail.com>
2006-04-10 19:13:36 +0000

    Cleanup

--- Lib/src/net/sf/anathema/lib/control/objectvalue/ITwoObjectsValueChangedListener.java
+++ Lib/src/net/sf/anathema/lib/control/objectvalue/ITwoObjectsValueChangedListener.java
@@ -1,6 +1,5 @@
 package net.sf.anathema.lib.control.objectvalue;
 
-
 public interface ITwoObjectsValueChangedListener {
 
   public void valueChanged(Object oldValue1, Object oldValue2, Object newValue1, Object newValue2);
