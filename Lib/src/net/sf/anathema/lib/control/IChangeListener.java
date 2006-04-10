c784eaa7ace4fec73779bbb33adb220fe2840c12
Urs Reupke <ursreupke@gmail.com>
2006-04-10 19:29:51 +0000

    Some more cleanuo

--- Lib/src/net/sf/anathema/lib/control/IChangeListener.java
+++ Lib/src/net/sf/anathema/lib/control/IChangeListener.java
@@ -1,8 +1,8 @@
 package net.sf.anathema.lib.control;
+
 import java.util.EventListener;
 
 public interface IChangeListener extends EventListener {
-  
+
   public void changeOccured();
-  
 }
--- Lib/src/net/sf/anathema/lib/control/IExceptionThrowingSimpleBlock.java
+++
@@ -1,6 +0,0 @@
-package net.sf.anathema.lib.control;
-
-public interface IExceptionThrowingSimpleBlock {
-
-  public void perform() throws Exception;
-}
