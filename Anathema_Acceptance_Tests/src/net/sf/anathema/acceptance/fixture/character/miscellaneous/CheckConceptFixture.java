f7f0bf36844930976fec7fae87739db5e3911227
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-11 14:23:50 +0000

    Nature Model separate from Concept

--- Anathema_Acceptance_Tests/src/net/sf/anathema/acceptance/fixture/character/miscellaneous/CheckConceptFixture.java
+++ Anathema_Acceptance_Tests/src/net/sf/anathema/acceptance/fixture/character/miscellaneous/CheckConceptFixture.java
@@ -3,6 +3,7 @@ package net.sf.anathema.acceptance.fixture.character.miscellaneous;
 import net.sf.anathema.acceptance.fixture.character.util.AbstractCharacterColumnFixture;
 import net.sf.anathema.character.generic.caste.ICasteType;
 import net.sf.anathema.character.model.concept.ICharacterConcept;
+import net.sf.anathema.character.model.concept.INature;
 import net.sf.anathema.character.model.concept.INatureType;
 
 public class CheckConceptFixture extends AbstractCharacterColumnFixture {
@@ -10,15 +11,15 @@ public class CheckConceptFixture extends AbstractCharacterColumnFixture {
   public String concept() {
     return getCharacterConcept().getConcept().getText();
   }
-  
+
   public String nature() {
-    INatureType type = getCharacterConcept().getNature().getType();
+    INatureType type = ((INature) getCharacterConcept().getWillpowerRegainingConcept()).getDescription().getType();
     if (type == null) {
       return null;
     }
     return type.getId();
   }
-  
+
   public String caste() {
     ICasteType caste = getCharacterConcept().getCaste().getType();
     if (caste == null) {
