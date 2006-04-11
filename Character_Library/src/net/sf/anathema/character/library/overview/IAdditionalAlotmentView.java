ee8493637ae3caa656c46e444b0221f6e8d30b0f
Urs Reupke <ursreupke@gmail.com>
2006-04-11 21:16:56 +0000

    Extracted IStyleableView

--- Character_Library/src/net/sf/anathema/character/library/overview/IAdditionalAlotmentView.java
+++ Character_Library/src/net/sf/anathema/character/library/overview/IAdditionalAlotmentView.java
@@ -1,11 +1,10 @@
 package net.sf.anathema.character.library.overview;
 
-import net.sf.anathema.lib.workflow.labelledvalue.ILabelledAlotmentView;
+import net.sf.anathema.lib.workflow.labelledvalue.IStyleableView;
 
-public interface IAdditionalAlotmentView extends ILabelledAlotmentView {
+public interface IAdditionalAlotmentView extends IStyleableView {
 
   public void setAlotment(int alotment, int additionalAlotment);
 
   public void setValue(int value, int additionalValue);
-
 }
