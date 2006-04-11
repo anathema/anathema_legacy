03194593e4611983f5c0acc9beccf8cf66356bb3
Urs Reupke <ursreupke@gmail.com>
2006-04-11 19:38:04 +0000

    Backgrounds use Subpresenter

--- Anathema_Acceptance_Tests/src/net/sf/anathema/acceptance/fixture/character/costs/CheckBackgroundPointsFixture.java
+++ Anathema_Acceptance_Tests/src/net/sf/anathema/acceptance/fixture/character/costs/CheckBackgroundPointsFixture.java
@@ -3,6 +3,6 @@ package net.sf.anathema.acceptance.fixture.character.costs;
 public class CheckBackgroundPointsFixture extends AbstractCheckPointsFixture {
 
   public int getBackgroundDotsSpent() {
-    return createManagement().getBackgroundDotsSpent();
-  }  
+    return createManagement().getBackgroundModel().getValue();
+  }
 }
