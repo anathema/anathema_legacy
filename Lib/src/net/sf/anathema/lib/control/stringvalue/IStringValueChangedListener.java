d113baf45432cba29221a6dbd115511e2a1008a6
Urs Reupke <ursreupke@gmail.com>
2006-04-11 18:36:42 +0000

    Deleted StringValueControl

--- Lib/src/net/sf/anathema/lib/control/stringvalue/IStringValueChangedListener.java
+++
@@ -1,8 +0,0 @@
-package net.sf.anathema.lib.control.stringvalue;
-
-import net.sf.anathema.lib.control.objectvalue.IObjectValueChangedListener;
-
-public interface IStringValueChangedListener extends IObjectValueChangedListener<String>{
-  
-  public void valueChanged(String newValue);
-}
