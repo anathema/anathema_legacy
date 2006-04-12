983264ade348acb6cdfb05302ef33aa6fe8de8c7
Urs Reupke <ursreupke@gmail.com>
2006-04-12 15:08:12 +0000

    No more static access to TextualPresentation

--- Lib_Workflow/src/net/sf/anathema/lib/workflow/textualdescription/TextualPresentation.java
+++ Lib_Workflow/src/net/sf/anathema/lib/workflow/textualdescription/TextualPresentation.java
@@ -1,16 +1,10 @@
 package net.sf.anathema.lib.workflow.textualdescription;
 
-import net.disy.commons.core.exception.UnreachableCodeReachedException;
 import net.sf.anathema.lib.control.objectvalue.IObjectValueChangedListener;
 
-// FIXME: Have calling classes extend TextualPresentation instead of making static calls
 public class TextualPresentation {
 
-  private TextualPresentation() {
-    throw new UnreachableCodeReachedException();
-  }
-
-  public static void initView(final ITextView textView, final ISimpleTextualDescription textualDescription) {
+  public void initView(final ITextView textView, final ISimpleTextualDescription textualDescription) {
     textView.addTextChangedListener(new IObjectValueChangedListener<String>() {
       public void valueChanged(String newValue) {
         textualDescription.setText(newValue);
