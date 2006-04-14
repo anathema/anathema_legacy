e77fd0a0d76fe9364ca0470dd62643efa50ea2a0
Urs Reupke <ursreupke@gmail.com>
2006-04-14 08:47:43 +0000

    Second Edition Combo Rules done

---
+++ Character_Generic/src/net/sf/anathema/character/generic/magic/IExtendedCharmData.java
@@ -0,0 +1,10 @@
+package net.sf.anathema.character.generic.magic;
+
+import net.sf.anathema.lib.util.IIdentificate;
+import net.sf.anathema.lib.util.Identificate;
+
+public interface IExtendedCharmData extends ICharmData {
+  public static final IIdentificate COMBO_OK_ATTRIBUTE = new Identificate("Combo-Ok"); //$NON-NLS-1$
+  public static final IIdentificate COMBO_BASIC_ATTRIBUTE = new Identificate("Combo-Basic"); //$NON-NLS-1$
+
+}
