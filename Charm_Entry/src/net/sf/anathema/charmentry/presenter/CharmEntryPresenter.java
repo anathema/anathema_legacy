926488d49675cd53df8c9f369bf92610ca384879
Urs Reupke <ursreupke@gmail.com>
2006-04-15 14:17:29 +0000

    Keywords persisted

--- Charm_Entry/src/net/sf/anathema/charmentry/presenter/CharmEntryPresenter.java
+++ Charm_Entry/src/net/sf/anathema/charmentry/presenter/CharmEntryPresenter.java
@@ -47,7 +47,7 @@ public class CharmEntryPresenter {
         IConfigurableCharmData charmData = model.getCharmData();
         CharacterType characterType = charmData.getCharacterType();
         try {
-          CharmCache.getInstance().addCharm(charmData);
+          CharmCache.getInstance().addCharm(charmData, model.getKeywordModel().getEntries());
           new CharmEntryPropertiesPersister().addPropertyInternal(characterType, charmData.getId(), charmData.getName());
           basicDataPresenter.charmAdded(charmData);
           keywordPresenter.charmAdded(charmData);
