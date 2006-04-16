2ac9120954eb9599131f8acb645241596c17ee99
Urs Reupke <ursreupke@gmail.com>
2006-04-16 16:20:26 +0000

    Check attribute instead of instanceof

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/MartialArtsUtilities.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/MartialArtsUtilities.java
@@ -1,11 +1,14 @@
 package net.sf.anathema.character.generic.impl.magic;
 
 import net.sf.anathema.character.generic.magic.ICharm;
-import net.sf.anathema.character.generic.magic.IMartialArtsCharm;
+import net.sf.anathema.character.generic.traits.types.AbilityType;
+import net.sf.anathema.lib.util.Identificate;
 
 public class MartialArtsUtilities {
 
+  private static final Identificate MARTIAL_ARTS = new Identificate(AbilityType.MartialArts.getId());
+
   public static boolean isMartialArtsCharm(ICharm charm) {
-    return charm instanceof IMartialArtsCharm;
+    return charm.hasAttribute(MARTIAL_ARTS);
   }
 }
