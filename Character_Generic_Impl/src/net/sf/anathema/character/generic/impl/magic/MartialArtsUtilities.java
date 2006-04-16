33f63c9d7ff354b05b31c36f0046b6b167b2ec58
Urs Reupke <ursreupke@gmail.com>
2006-04-16 16:58:49 +0000

    "hasLevel" in Utilities

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/MartialArtsUtilities.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/MartialArtsUtilities.java
@@ -2,6 +2,7 @@ package net.sf.anathema.character.generic.impl.magic;
 
 import net.sf.anathema.character.generic.magic.ICharm;
 import net.sf.anathema.character.generic.magic.IMartialArtsCharm;
+import net.sf.anathema.character.generic.magic.charms.MartialArtsLevel;
 import net.sf.anathema.character.generic.traits.types.AbilityType;
 import net.sf.anathema.lib.util.Identificate;
 
@@ -9,11 +10,35 @@ public class MartialArtsUtilities {
 
   private static final Identificate MARTIAL_ARTS = new Identificate(AbilityType.MartialArts.getId());
 
+  // private static final Identificate MORTAL_LEVEL = new Identificate(MartialArtsLevel.Mortal.getId());
+  // private static final Identificate TERRESTRIAL_LEVEL = new Identificate(MartialArtsLevel.Terrestrial.getId());
+  // private static final Identificate CELESTIAL_LEVEL = new Identificate(MartialArtsLevel.Celestial.getId());
+  // private static final Identificate SIDEREAL_LEVEL = new Identificate(MartialArtsLevel.Sidereal.getId());
+
   public static boolean isMartialArtsCharm(ICharm charm) {
     return charm.hasAttribute(MARTIAL_ARTS);
   }
 
   public static boolean isFormCharm(ICharm charm) {
     return charm.hasAttribute(IMartialArtsCharm.FORM_ATTRIBUTE);
+  }
+
+  public static MartialArtsLevel getLevel(ICharm charm) {
+    if (!isMartialArtsCharm(charm)) {
+      return null;
+    }
+    for (MartialArtsLevel level : MartialArtsLevel.values()) {
+      if (charm.hasAttribute(new Identificate(level.name()))) {
+        return level;
+      }
+    }
+    throw new IllegalStateException("Martial Arts Charm without level: " + charm.getId()); //$NON-NLS-1$
+  }
+
+  public static boolean hasLevel(MartialArtsLevel level, ICharm charm) {
+    if (!isMartialArtsCharm(charm)) {
+      return false;
+    }
+    return charm.hasAttribute(new Identificate(level.name()));
   }
 }
--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/test/MartialArtsUtilitiesTest.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/test/MartialArtsUtilitiesTest.java
@@ -2,6 +2,7 @@ package net.sf.anathema.character.generic.impl.magic.test;
 
 import net.sf.anathema.character.generic.impl.magic.MartialArtsUtilities;
 import net.sf.anathema.character.generic.magic.ICharm;
+import net.sf.anathema.character.generic.magic.charms.MartialArtsLevel;
 import net.sf.anathema.lib.testing.BasicTestCase;
 import net.sf.anathema.lib.util.IIdentificate;
 
@@ -25,5 +26,25 @@ public class MartialArtsUtilitiesTest extends BasicTestCase {
       }
     };
     assertTrue(MartialArtsUtilities.isFormCharm(charm));
+  }
+
+  public void testGetMartialArtsLevel() throws Exception {
+    DummyMartialArtsCharm charm = new DummyMartialArtsCharm("Dummy") { //$NON-NLS-1$
+      @Override
+      public boolean hasAttribute(IIdentificate attribute) {
+        return attribute.getId().equals("MartialArts") || attribute.getId().equals("Terrestrial"); //$NON-NLS-1$ //$NON-NLS-2$
+      }
+    };
+    assertEquals(MartialArtsLevel.Terrestrial, MartialArtsUtilities.getLevel(charm));
+  }
+
+  public void testHasMartialArtsLevel() throws Exception {
+    DummyMartialArtsCharm charm = new DummyMartialArtsCharm("Dummy") { //$NON-NLS-1$
+      @Override
+      public boolean hasAttribute(IIdentificate attribute) {
+        return attribute.getId().equals("MartialArts") || attribute.getId().equals("Terrestrial"); //$NON-NLS-1$ //$NON-NLS-2$
+      }
+    };
+    assertTrue(MartialArtsUtilities.hasLevel(MartialArtsLevel.Terrestrial, charm));
   }
 }
