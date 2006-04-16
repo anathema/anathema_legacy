9d8de8e9e49de3e3b6e5e5e213ca27b6839bb879
Urs Reupke <ursreupke@gmail.com>
2006-04-16 16:31:13 +0000

    Form-Charm-Checking in MartialArtsUtilities

--- Character_Generic/src/net/sf/anathema/character/generic/magic/IMartialArtsCharm.java
+++ Character_Generic/src/net/sf/anathema/character/generic/magic/IMartialArtsCharm.java
@@ -1,10 +1,11 @@
 package net.sf.anathema.character.generic.magic;
 
 import net.sf.anathema.character.generic.magic.charms.MartialArtsLevel;
+import net.sf.anathema.lib.util.IIdentificate;
+import net.sf.anathema.lib.util.Identificate;
 
 public interface IMartialArtsCharm extends ICharm {
-  
+  public static final IIdentificate FORM_ATTRIBUTE = new Identificate("Form"); //$NON-NLS-1$
+
   public MartialArtsLevel getLevel();
-  
-  public boolean isFormCharm();
 }
