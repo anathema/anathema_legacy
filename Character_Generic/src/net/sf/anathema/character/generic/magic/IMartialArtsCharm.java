37b0c4fabfa48f1a02e1ea06b89a12a2772ca02a
Urs Reupke <ursreupke@gmail.com>
2006-04-16 17:30:37 +0000

    getLevel removed from Martial Arts Charm

--- Character_Generic/src/net/sf/anathema/character/generic/magic/IMartialArtsCharm.java
+++ Character_Generic/src/net/sf/anathema/character/generic/magic/IMartialArtsCharm.java
@@ -1,11 +1,12 @@
 package net.sf.anathema.character.generic.magic;
 
-import net.sf.anathema.character.generic.magic.charms.MartialArtsLevel;
 import net.sf.anathema.lib.util.IIdentificate;
 import net.sf.anathema.lib.util.Identificate;
 
 public interface IMartialArtsCharm extends ICharm {
   public static final IIdentificate FORM_ATTRIBUTE = new Identificate("Form"); //$NON-NLS-1$
+  public static final String ALLOWS_CELESTIAL_ATTRIBUTE = "AllowsCelestial"; //$NON-NLS-1$
+  public static final IIdentificate UNRESTRICTED_ATTRIBUTE = new Identificate("Unrestricted"); //$NON-NLS-1$
+  public static final IIdentificate NO_STYLE_ATTRIBUTE = new Identificate("NoStyle"); //$NON-NLS-1$
 
-  public MartialArtsLevel getLevel();
 }
