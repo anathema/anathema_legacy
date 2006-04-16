b50693ff844381c00dda6fd1fb17d91a55b82f6f
Urs Reupke <ursreupke@gmail.com>
2006-04-16 18:34:40 +0000

    Errors resolved. Removed abstractcharmtree

--- Character_Generic/src/net/sf/anathema/character/generic/magic/charms/ICharmTree.java
+++ Character_Generic/src/net/sf/anathema/character/generic/magic/charms/ICharmTree.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import net.sf.anathema.character.generic.magic.ICharm;
 
-public interface ICharmTree<C extends ICharm> {
+public interface ICharmTree {
 
   public ICharmGroup[] getAllCharmGroups();
 
-  public C[] getAllCharms();
+  public ICharm[] getAllCharms();
 
-  public List<C> getAllCharmsForGroup(String id);
+  public List<ICharm> getAllCharmsForGroup(String id);
 
-  public C getCharmByID(String id);
+  public ICharm getCharmByID(String id);
 }
