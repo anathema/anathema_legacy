277316f6663be6ca066e2e1debf4d806c1fd11ff
Urs Reupke <ursreupke@gmail.com>
2006-04-21 15:20:36 +0000

    Report! Report! SeriesReport! (Page numbers, line)

--- Campaign_Plot/src/net/sf/anathema/campaign/reporting/MultiColumnSeriesReport.java
+++ Campaign_Plot/src/net/sf/anathema/campaign/reporting/MultiColumnSeriesReport.java
@@ -17,13 +17,17 @@ import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
 import com.lowagie.text.Font;
 import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
 import com.lowagie.text.Rectangle;
 import com.lowagie.text.TextElementArray;
+import com.lowagie.text.pdf.ColumnText;
 import com.lowagie.text.pdf.MultiColumnText;
 import com.lowagie.text.pdf.PdfAction;
+import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfOutline;
 import com.lowagie.text.pdf.PdfPageEventHelper;
 import com.lowagie.text.pdf.PdfPageLabels;
+import com.lowagie.text.pdf.PdfTemplate;
 import com.lowagie.text.pdf.PdfWriter;
 
 public class MultiColumnSeriesReport implements IITextReport {
@@ -42,6 +46,11 @@ public class MultiColumnSeriesReport implements IITextReport {
       @Override
       public void onGenericTag(PdfWriter currentWriter, Document currentDocument, Rectangle rect, String text) {
         tableOfContents.put(text, currentWriter.getPageNumber());
+      }
+
+      @Override
+      public void onEndPage(PdfWriter currentWriter, Document currentDocument) {
+        printPageNumber(currentWriter, currentDocument, String.valueOf(currentWriter.getPageNumber()));
       }
     });
     PdfOutline rootOutline = writer.getDirectContent().getRootOutline();
@@ -114,8 +123,30 @@ public class MultiColumnSeriesReport implements IITextReport {
     document.newPage();
   }
 
+  private void printPageNumber(PdfWriter writer, Document document, String pageNumber) {
+    PdfContentByte directContent = writer.getDirectContent();
+    PdfTemplate template = directContent.createTemplate(document.getPageSize().width(), document.getPageSize().height());
+    template.moveTo(document.left(), document.bottom() - 11);
+    template.lineTo(document.right(), document.bottom() - 11);
+    template.stroke();
+    directContent.addTemplate(template, 0, 0);
+    ColumnText.showTextAligned(directContent, Element.ALIGN_CENTER, new Phrase(
+        pageNumber,
+        reportUtils.createDefaultFont(8, Font.NORMAL)), document.getPageSize().width() / 2, document.bottom() - 20, 0);
+  }
+
   private void createTableOfContents(Document document, PdfWriter writer, String seriesTitle) throws DocumentException {
-    int lastContentPage = writer.getPageNumber();
+    final int lastContentPage = writer.getPageNumber();
+    writer.setPageEvent(null);
+    writer.setPageEvent(new PdfPageEventHelper() {
+      @Override
+      public void onEndPage(PdfWriter currentWriter, Document currentDocument) {
+        printPageNumber(
+            currentWriter,
+            currentDocument,
+            new RomanNumber(currentWriter.getPageNumber() - lastContentPage).getLowerCaseRoman());
+      }
+    });
     Paragraph titleParagraph = reportUtils.createNewParagraph(seriesTitle, Element.ALIGN_CENTER, Font.BOLD);
     titleParagraph.font().setSize(15);
     document.add(titleParagraph);
@@ -141,10 +172,10 @@ public class MultiColumnSeriesReport implements IITextReport {
     int tocPageLength = totalPages - lastContentPage;
     int reorder[] = new int[totalPages];
     for (int index = 0; index < tocPageLength; ++index) {
-      reorder[index] = lastContentPage + index+1;
+      reorder[index] = lastContentPage + index + 1;
     }
     for (int index = 0; index < lastContentPage; ++index) {
-      reorder[tocPageLength + index] = index+1;
+      reorder[tocPageLength + index] = index + 1;
     }
     writer.reorderPages(reorder);
     PdfPageLabels pageLabels = new PdfPageLabels();
