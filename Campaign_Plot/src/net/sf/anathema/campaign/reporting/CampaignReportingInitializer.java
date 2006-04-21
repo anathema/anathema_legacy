309b048573b9c63d02641c9d0e3c23f3f2452839
Urs Reupke <ursreupke@gmail.com>
2006-04-21 15:24:48 +0000

    Removed simple series report

--- Campaign_Plot/src/net/sf/anathema/campaign/reporting/CampaignReportingInitializer.java
+++ Campaign_Plot/src/net/sf/anathema/campaign/reporting/CampaignReportingInitializer.java
@@ -6,7 +6,6 @@ public class CampaignReportingInitializer {
 
   public void initReporting(IReportRegistry reportRegistry) {
     reportRegistry.addReport(new NoteReport());
-    reportRegistry.addReport(new SeriesReport());
     reportRegistry.addReport(new MultiColumnSeriesReport());
   }
 }
