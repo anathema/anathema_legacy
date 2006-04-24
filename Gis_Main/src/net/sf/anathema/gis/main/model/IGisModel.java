3f463fdf05f4f7bdc01dff6cb01011068a2c69d8
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-24 12:49:58 +0000

    SketchLayer einfügbar

--- Gis_Main/src/net/sf/anathema/gis/main/model/IGisModel.java
+++ Gis_Main/src/net/sf/anathema/gis/main/model/IGisModel.java
@@ -1,11 +1,12 @@
 package net.sf.anathema.gis.main.model;
 
+import net.sf.anathema.framework.itemdata.IItemData;
 import net.sf.anathema.gis.main.model.layerfactory.IStandardLayerFactory;
 import de.disy.gis.gisterm.map.IMapModel;
 
-public interface IGisModel {
+public interface IGisModel extends IItemData {
 
   public IMapModel getMapModel();
-  
+
   public IStandardLayerFactory getStandardLayerFactory();
 }
