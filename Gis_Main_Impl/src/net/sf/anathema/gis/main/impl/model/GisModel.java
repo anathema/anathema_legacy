f93b8ee3662c2ba415aef012e7c531fcdc7d946b
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-24 11:59:48 +0000

    Wenigstens Tiff-Bilder tun jetzt ohne eine Exception

--- Gis_Main_Impl/src/net/sf/anathema/gis/main/impl/model/GisModel.java
+++ Gis_Main_Impl/src/net/sf/anathema/gis/main/impl/model/GisModel.java
@@ -1,5 +1,7 @@
 package net.sf.anathema.gis.main.impl.model;
 
+import java.io.File;
+
 import net.sf.anathema.gis.main.model.IGisModel;
 import net.sf.anathema.gis.main.model.layerfactory.IStandardLayerFactory;
 import de.disy.gis.gisterm.map.BasicMapModel;
@@ -8,7 +10,11 @@ import de.disy.gis.gisterm.map.IMapModel;
 public class GisModel implements IGisModel {
   
   private final IMapModel mapModel = new BasicMapModel();
-  private final StandardLayerFactory standardLayerFactory = new StandardLayerFactory();
+  private final StandardLayerFactory standardLayerFactory;
+  
+  public GisModel(File repositoryFile) {
+    standardLayerFactory  = new StandardLayerFactory(repositoryFile);
+  }
 
   public IMapModel getMapModel() {
     return mapModel;
--- Gis_Main_Impl/src/net/sf/anathema/gis/main/impl/model/StandardLayerFactory.java
+++ Gis_Main_Impl/src/net/sf/anathema/gis/main/impl/model/StandardLayerFactory.java
@@ -1,53 +1,86 @@
 package net.sf.anathema.gis.main.impl.model;
 
 import gis.gisterm.gcore.GenericLayer;
-import gis.gisterm.gcore.WorldBox;
 import gis.gisterm.map.RasterCatalogLayer;
 import gis.gisterm.map.layer.datasource.RasterCatalogLayerDataProvider;
 
 import java.io.File;
 
+import javax.swing.JPopupMenu;
+
+import net.disy.commons.core.progress.NullProgressMonitor;
 import net.sf.anathema.gis.main.model.layerfactory.IStandardLayerFactory;
 import net.sf.anathema.gis.main.model.layerfactory.LayerCreationException;
-import de.disy.gis.gisterm.image.FileWorldImageTile;
-import de.disy.gis.gisterm.image.IWorldImageTile;
-import de.disy.gis.gisterm.imagecatalog.ImageCatalog;
-import de.disy.gis.gisterm.imagecatalog.ImageCatalogFactory;
 import de.disy.gis.gisterm.imagecatalog.ImageCatalogQuery;
+import de.disy.gis.gisterm.imagecatalog.layer.IImageCatalogLayerCreationStrategy;
+import de.disy.gis.gisterm.imagecatalog.layer.IImageCatalogProperties;
+import de.disy.gis.gisterm.imagecatalog.layer.ImageCatalogLayerCreationStrategy;
 import de.disy.gis.gisterm.map.scale.IScaleRange;
+import de.disy.gis.gisterm.map.theme.ITheme;
+import de.disy.gis.gisterm.pro.map.layer.ILayerPopupFactoryExtension;
+import de.disy.gis.gisterm.pro.map.layer.popup.IMapLayerPopupMenuContext;
 
 public class StandardLayerFactory implements IStandardLayerFactory {
+  private final File repositoryFolder;
+  private final ILayerPopupFactoryExtension rasterLayerPopupMenuFactory = new ILayerPopupFactoryExtension() {
+    public JPopupMenu createPopupMenu(ITheme theme, IMapLayerPopupMenuContext menuContext) {
+      return null;
+    }
+  };
+
+  public StandardLayerFactory(File repositoryFolder) {
+    this.repositoryFolder = repositoryFolder;
+    String gisDataPath = new File("../Anathema/gisdata/").getAbsolutePath(); //$NON-NLS-1$
+    System.setProperty("ANATHEMA_GISDATA", gisDataPath); //$NON-NLS-1$
+  }
 
   public GenericLayer createXeriarRasterLayer() throws LayerCreationException {
-    //return createPngXeria();
-    return createJpegXeria();
-  }
-
-  private GenericLayer createPngXeria() throws LayerCreationException {
     try {
-      return ImageCatalogFactory.createUnmonitoredImageCatalogLayer(new File("../Anathema/gisdata/Xeria PNG/"));
-    }
-    catch (Exception e) {
-      throw new LayerCreationException(e);
-    }
-  }
-
-  private GenericLayer createJpegXeria() throws LayerCreationException {
-    try {
-      RasterCatalogLayer catalogLayer = new RasterCatalogLayer();
-      RasterCatalogLayerDataProvider rasterDataProvider = catalogLayer.getRasterCatalogLayerDataProvider();
-      ImageCatalogQuery query = new ImageCatalogQuery(new File("../Anathema/gisdata/Creation Xeria.icat").toURL()
-          .toExternalForm(), IScaleRange.MIN_VALUE, IScaleRange.MAX_VALUE);
-      File xeriaFile = new File("../Anathema/gisdata/Xeria/creation-large-1-7.jpg"); //$NON-NLS-1$
-      FileWorldImageTile imageTile = new FileWorldImageTile(xeriaFile, new WorldBox(1.52, 7024.22, 11708.55, 0));
-      IWorldImageTile[] worldTiles = new IWorldImageTile[] { imageTile };
-      query.setCatalog(new ImageCatalog(worldTiles));
-      rasterDataProvider.setQuery(query);
-      catalogLayer.setName("Xeriar");
-      return catalogLayer;
+      File xeriarFile = new File(repositoryFolder, "gisdata/Xeriar.dbf"); //$NON-NLS-1$
+      return createXeria(xeriarFile);
     }
     catch (Exception e) {
       throw new LayerCreationException("An error occured loading the Xeriar raster layer.", e); //$NON-NLS-1$
     }
+  }
+
+  private GenericLayer createXeria(final File dbfFile) throws Exception {
+    RasterCatalogLayer catalogLayer = new RasterCatalogLayer() {
+      @Override
+      public void legendLayerChanged() {
+        if (getLayerPanel() != null) {
+          getLayerPanel().setPopupMenuFactory(rasterLayerPopupMenuFactory);
+        }
+        super.legendLayerChanged();
+      }
+    };
+    RasterCatalogLayerDataProvider rasterDataProvider = catalogLayer.getRasterCatalogLayerDataProvider();
+    IImageCatalogProperties properties = new IImageCatalogProperties() {
+      public String getAbsoluteImagePath() {
+        return dbfFile.getAbsolutePath();
+      }
+
+      public String getCatalogName() {
+        return "Creation Xeriar";
+      }
+
+      public double getInitialMaxScale() {
+        return IScaleRange.MAX_VALUE;
+      }
+
+      public double getInitialMinScale() {
+        return IScaleRange.MIN_VALUE;
+      }
+
+      public String getMmlSaveString() {
+        throw new UnsupportedOperationException();
+      }
+    };
+    IImageCatalogLayerCreationStrategy layerCreationStrategy = new ImageCatalogLayerCreationStrategy(properties);
+    layerCreationStrategy.initialize(NullProgressMonitor.getInstance());
+    ImageCatalogQuery query = new ImageCatalogQuery(layerCreationStrategy);
+    rasterDataProvider.setQuery(query);
+    catalogLayer.setName(properties.getCatalogName());
+    return catalogLayer;
   }
 }
