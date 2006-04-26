e65611c50418f6799c89052d2a8f0237c4d04426
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-26 14:41:01 +0000

    Aufräumen ITextualDescription

--- Campaign_Music/src/net/sf/anathema/campaign/music/model/IMusicDatabase.java
+++ Campaign_Music/src/net/sf/anathema/campaign/music/model/IMusicDatabase.java
@@ -4,7 +4,7 @@ import net.sf.anathema.campaign.music.model.libary.ILibraryControl;
 import net.sf.anathema.campaign.music.model.selection.IMusicSelectionModel;
 import net.sf.anathema.campaign.music.presenter.IMusicSearchControl;
 import net.sf.anathema.campaign.music.presenter.selection.player.IMusicPlayerModel;
-import net.sf.anathema.framework.itemdata.IItemData;
+import net.sf.anathema.framework.itemdata.model.IItemData;
 
 public interface IMusicDatabase extends IItemData {
 
