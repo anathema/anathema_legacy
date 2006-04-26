cbc260c48a9e6b1bf131c8cbefe6ad3c8a7ba149
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-26 14:40:43 +0000

    Aufräumen ITextualDescription

--- Development_Test_Character/src/net/sf/anathema/test/character/equipment/item/EquipmentItemCreationTest.java
+++ Development_Test_Character/src/net/sf/anathema/test/character/equipment/item/EquipmentItemCreationTest.java
@@ -1,7 +1,7 @@
 package net.sf.anathema.test.character.equipment.item;
 
 import net.sf.anathema.character.equipment.impl.model.EquipmentItemData;
-import net.sf.anathema.framework.itemdata.IBasicItemData;
+import net.sf.anathema.framework.itemdata.model.IBasicItemData;
 import net.sf.anathema.test.platform.itemdata.AbstractBasicItemDataTest;
 
 public class EquipmentItemCreationTest extends AbstractBasicItemDataTest {
