9ed2a35a2a47634dcb3b102386e5c6f6fc6f88d3
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-26 20:08:58 +0000

    Reste von Equipment

--- Development_Test_Character/src/net/sf/anathema/test/character/equipment/item/AddEquipmentStatisticsWizardDemo.java
+++ Development_Test_Character/src/net/sf/anathema/test/character/equipment/item/AddEquipmentStatisticsWizardDemo.java
@@ -2,8 +2,8 @@ package net.sf.anathema.test.character.equipment.item;
 
 import net.disy.commons.swing.dialog.wizard.WizardDialog;
 import net.sf.anathema.character.equipment.impl.wizard.AddEquipmentStatisticWizard;
-import net.sf.anathema.character.equipment.impl.wizard.AddEquipmentStatsticsProperties;
 import net.sf.anathema.character.equipment.impl.wizard.model.AddEquipmentStatisticsWizardModel;
+import net.sf.anathema.character.equipment.impl.wizard.properties.AddEquipmentStatsticsProperties;
 import net.sf.anathema.framework.resources.AnathemaResources;
 import de.jdemo.extensions.SwingDemoCase;
 
