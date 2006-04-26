f5e9d19a0107f41c761f1152f5a2369218e08032
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-26 15:07:20 +0000

    View für Description

--- Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/view/EquimentItemDataView.java
+++ Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/view/EquimentItemDataView.java
@@ -3,13 +3,18 @@ package net.sf.anathema.character.equipment.impl.view;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 
+import net.disy.commons.swing.layout.grid.GridDialogLayout;
 import net.sf.anathema.character.equipment.view.IEquipmentItemDataView;
 
 public class EquimentItemDataView implements IEquipmentItemDataView {
 
-  private JPanel content = new JPanel();
+  private JPanel content = new JPanel(new GridDialogLayout(1, false));
 
   public JComponent getComponent() {
     return content;
+  }
+
+  public void fillDescriptionPanel(JComponent descriptionPanel) {
+    content.add(descriptionPanel);
   }
 }
