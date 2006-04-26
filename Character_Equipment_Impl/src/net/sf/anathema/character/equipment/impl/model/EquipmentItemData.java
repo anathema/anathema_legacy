2607847fcad8572358e08884d1f8ad686486cb77
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-26 13:23:19 +0000

    first share Equipment-Kram

---
+++ Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/model/EquipmentItemData.java
@@ -0,0 +1,8 @@
+package net.sf.anathema.character.equipment.impl.model;
+
+import net.sf.anathema.character.equipment.model.IEquipmentItemData;
+import net.sf.anathema.framework.itemdata.BasicItemData;
+
+public class EquipmentItemData extends BasicItemData implements IEquipmentItemData {
+
+}
