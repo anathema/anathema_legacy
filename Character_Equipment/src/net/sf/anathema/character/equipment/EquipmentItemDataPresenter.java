03a91acf8da4448cdbcf046fbe2c1fb5c12f5669
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-26 13:43:52 +0000

    Demo-Struktur für Character_Equipment

---
+++ Character_Equipment/src/net/sf/anathema/character/equipment/EquipmentItemDataPresenter.java
@@ -0,0 +1,18 @@
+package net.sf.anathema.character.equipment;
+
+import net.sf.anathema.character.equipment.model.IEquipmentItemData;
+import net.sf.anathema.character.equipment.view.IEquipmentItemDataView;
+import net.sf.anathema.lib.gui.IPresenter;
+import net.sf.anathema.lib.resources.IResources;
+
+public class EquipmentItemDataPresenter implements IPresenter {
+
+  public EquipmentItemDataPresenter(IResources resources, IEquipmentItemData model, IEquipmentItemDataView view) {
+    
+    
+  }
+  
+  public void initPresentation() {
+    
+  }
+}
---
+++ Character_Equipment/src/net/sf/anathema/character/equipment/view/IEquipmentItemDataView.java
@@ -0,0 +1,7 @@
+package net.sf.anathema.character.equipment.view;
+
+import net.sf.anathema.lib.gui.IView;
+
+public interface IEquipmentItemDataView extends IView {
+
+}
