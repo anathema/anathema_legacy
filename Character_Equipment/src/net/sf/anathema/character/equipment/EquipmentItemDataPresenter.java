3ac60f77d3d4265c5fa3e0ad2942baea68c3cdd8
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-26 14:41:54 +0000

    Aufräumen ITextualDescription

--- Character_Equipment/src/net/sf/anathema/character/equipment/EquipmentItemDataPresenter.java
+++ Character_Equipment/src/net/sf/anathema/character/equipment/EquipmentItemDataPresenter.java
@@ -4,15 +4,25 @@ import net.sf.anathema.character.equipment.model.IEquipmentItemData;
 import net.sf.anathema.character.equipment.view.IEquipmentItemDataView;
 import net.sf.anathema.lib.gui.IPresenter;
 import net.sf.anathema.lib.resources.IResources;
+import net.sf.anathema.lib.workflow.textualdescription.ITextView;
+import net.sf.anathema.lib.workflow.textualdescription.TextualPresentation;
 
 public class EquipmentItemDataPresenter implements IPresenter {
 
+  private final IResources resources;
+  private final IEquipmentItemData model;
+  private final IEquipmentItemDataView view;
+
   public EquipmentItemDataPresenter(IResources resources, IEquipmentItemData model, IEquipmentItemDataView view) {
-    
-    
+    this.resources = resources;
+    this.model = model;
+    this.view = view;
   }
-  
+
   public void initPresentation() {
-    
+    ITextView nameView = null;//view.addLineTextView("Name:", 15);
+    ITextView descriptionView = null;//view.addAreaTextView("Description:", 15, 10);
+    new TextualPresentation().initView(nameView, model.getDescription().getName());
+    new TextualPresentation().initView(descriptionView, model.getDescription().getContent());
   }
 }
--- Character_Equipment/src/net/sf/anathema/character/equipment/model/IEquipmentItemData.java
+++ Character_Equipment/src/net/sf/anathema/character/equipment/model/IEquipmentItemData.java
@@ -1,6 +1,6 @@
 package net.sf.anathema.character.equipment.model;
 
-import net.sf.anathema.framework.itemdata.IBasicItemData;
+import net.sf.anathema.framework.itemdata.model.IBasicItemData;
 
 public interface IEquipmentItemData extends IBasicItemData {
 }
