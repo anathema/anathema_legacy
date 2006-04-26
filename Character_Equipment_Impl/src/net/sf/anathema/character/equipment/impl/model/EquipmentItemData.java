8da1c68d9e97bc31d546e995e9a537e9f1a30fd2
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-26 14:37:53 +0000

    Aufräumen ITextualDescription

--- Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/model/EquipmentItemData.java
+++ Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/model/EquipmentItemData.java
@@ -1,7 +1,7 @@
 package net.sf.anathema.character.equipment.impl.model;
 
 import net.sf.anathema.character.equipment.model.IEquipmentItemData;
-import net.sf.anathema.framework.itemdata.BasicItemData;
+import net.sf.anathema.framework.itemdata.model.BasicItemData;
 
 public class EquipmentItemData extends BasicItemData implements IEquipmentItemData {
 
