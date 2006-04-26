9285e273c3b894f92a6e5eadce4df942a6ba8998
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-26 20:09:46 +0000

    Reste von Equipment

--- Lib_Workflow/src/net/sf/anathema/lib/workflow/container/factory/StandardPanelBuilder.java
+++ Lib_Workflow/src/net/sf/anathema/lib/workflow/container/factory/StandardPanelBuilder.java
@@ -1,11 +1,15 @@
 package net.sf.anathema.lib.workflow.container.factory;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import net.disy.commons.swing.border.TitledPanel;
+import net.disy.commons.swing.layout.GridDialogLayoutDataUtilities;
+import net.disy.commons.swing.layout.grid.GridDialogLayoutData;
 import net.disy.commons.swing.layout.grid.IDialogComponent;
 import net.sf.anathema.lib.gui.gridlayout.DefaultGridDialogPanel;
 import net.sf.anathema.lib.gui.gridlayout.IGridDialogPanel;
+import net.sf.anathema.lib.gui.widgets.IntegerSpinner;
 import net.sf.anathema.lib.workflow.textualdescription.ITextView;
 import net.sf.anathema.lib.workflow.textualdescription.view.AreaTextView;
 import net.sf.anathema.lib.workflow.textualdescription.view.LabelTextView;
@@ -38,5 +42,22 @@ public class StandardPanelBuilder {
 
   public JPanel getUntitledContent() {
     return dialogPanel.getContent();
+  }
+
+  public IntegerSpinner addIntegerSpinner(final String labelString, int startValue) {
+    final IntegerSpinner spinner = new IntegerSpinner(startValue);
+    dialogPanel.add(new IDialogComponent() {
+      public int getColumnCount() {
+        return 2;
+      }
+
+      public void fillInto(JPanel panel, int columnCount) {
+        panel.add(new JLabel(labelString));
+        panel.add(spinner.getComponent(), GridDialogLayoutDataUtilities.createHorizontalSpanData(
+            columnCount - 1,
+            GridDialogLayoutData.FILL_HORIZONTAL));
+      }
+    });
+    return spinner;
   }
 }
