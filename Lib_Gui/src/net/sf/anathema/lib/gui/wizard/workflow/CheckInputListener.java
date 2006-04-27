f54026650084512c877e02158fa2af3c358d5ebd
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-27 12:08:05 +0000

    CloseCombat Stats bis auf Schaden

--- Lib_Gui/src/net/sf/anathema/lib/gui/wizard/workflow/CheckInputListener.java
+++ Lib_Gui/src/net/sf/anathema/lib/gui/wizard/workflow/CheckInputListener.java
@@ -2,8 +2,9 @@ package net.sf.anathema.lib.gui.wizard.workflow;
 
 import net.disy.commons.core.util.ISimpleBlock;
 import net.sf.anathema.lib.control.change.IChangeListener;
+import net.sf.anathema.lib.control.objectvalue.IObjectValueChangedListener;
 
-public class CheckInputListener implements IChangeListener {
+public class CheckInputListener implements IChangeListener, IObjectValueChangedListener<String> {
 
   private final ISimpleBlock executionBlock;
 
@@ -12,6 +13,10 @@ public class CheckInputListener implements IChangeListener {
   }
 
   public void changeOccured() {
+    executionBlock.execute();
+  }
+
+  public void valueChanged(String newValue) {
     executionBlock.execute();
   }
 }
