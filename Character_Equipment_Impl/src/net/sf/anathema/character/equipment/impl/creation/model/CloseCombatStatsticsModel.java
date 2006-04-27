c6717e43ebc102423fa644b49608bf066798dde5
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-27 12:08:26 +0000

    CloseCombat Stats bis auf Schaden

--- Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/creation/model/CloseCombatStatsticsModel.java
+++ Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/creation/model/CloseCombatStatsticsModel.java
@@ -1,7 +1,36 @@
 package net.sf.anathema.character.equipment.impl.creation.model;
 
 import net.sf.anathema.character.equipment.creation.model.ICloseCombatStatsticsModel;
+import net.sf.anathema.lib.workflow.intvalue.IIntValueModel;
+import net.sf.anathema.lib.workflow.intvalue.SimpleIntValueModel;
+import net.sf.anathema.lib.workflow.textualdescription.ITextualDescription;
+import net.sf.anathema.lib.workflow.textualdescription.model.SimpleTextualDescription;
 
 public class CloseCombatStatsticsModel implements ICloseCombatStatsticsModel {
 
+  private final ITextualDescription name = new SimpleTextualDescription();
+  private final IIntValueModel speedModel = new SimpleIntValueModel(1);
+  private final IIntValueModel accuracyModel = new SimpleIntValueModel(0);
+  private final IIntValueModel defenseModel = new SimpleIntValueModel(0);
+  private final IIntValueModel rateModel = new SimpleIntValueModel(1);
+
+  public ITextualDescription getName() {
+    return name;
+  }
+
+  public IIntValueModel getSpeedModel() {
+    return speedModel;
+  }
+
+  public IIntValueModel getAccuracyModel() {
+    return accuracyModel;
+  }
+
+  public IIntValueModel getRateModel() {
+    return rateModel;
+  }
+
+  public IIntValueModel getDefenseModel() {
+    return defenseModel;
+  }
 }
--- Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/creation/view/CloseCombatStatisticsView.java
+++ Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/creation/view/CloseCombatStatisticsView.java
@@ -4,13 +4,29 @@ import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 import net.sf.anathema.character.equipment.creation.view.ICloseCombatStatisticsView;
+import net.sf.anathema.lib.gui.widgets.IntegerSpinner;
+import net.sf.anathema.lib.workflow.container.factory.StandardPanelBuilder;
+import net.sf.anathema.lib.workflow.textualdescription.ITextView;
 
 public class CloseCombatStatisticsView implements ICloseCombatStatisticsView {
 
-  private JPanel panel = new JPanel();
-  
+  private static final int COLUMN_COUNT = 30;
+  private final StandardPanelBuilder panelBuilder = new StandardPanelBuilder();
+  private JPanel content;
+
   public JComponent getContent() {
-    return panel;
+    if (content == null) {
+      content = panelBuilder.getUntitledContent();
+    }
+    return content;
+  }
+
+  public ITextView addLineTextView(String label) {
+    return panelBuilder.addLineTextView(label, COLUMN_COUNT);
+  }
+
+  public IntegerSpinner addIntegerSpinner(String label, int startValue) {
+    return panelBuilder.addIntegerSpinner(label, startValue);
   }
 
   public void requestFocus() {
