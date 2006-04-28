5de667581dc60d838742ab4ab47090192566653f
Urs Reupke <ursreupke@gmail.com>
2006-04-28 17:56:58 +0000

    Stringbuilder SimpleCharmSpecials

--- Character_Generic/src/net/sf/anathema/character/generic/magic/charms/type/ISimpleSpecialsModel.java
+++ Character_Generic/src/net/sf/anathema/character/generic/magic/charms/type/ISimpleSpecialsModel.java
@@ -1,6 +1,8 @@
 package net.sf.anathema.character.generic.magic.charms.type;
 
 public interface ISimpleSpecialsModel extends ITypeSpecialsModel {
+  public static final int DEFAULT_SPEED = 6;
+  public static final int DEFAULT_DEFENSE_MODIFIER = -1;
 
   public int getSpeed();
 
