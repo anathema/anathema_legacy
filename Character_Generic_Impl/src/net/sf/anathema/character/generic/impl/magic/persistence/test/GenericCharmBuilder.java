c894b57edcdc5f1834796d190c2efdc882d76cd0
Urs Reupke <ursreupke@gmail.com>
2006-04-29 09:52:35 +0000

    GenericCharmBuilderWithGenericPrerequisites

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/persistence/test/GenericCharmBuilder.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/persistence/test/GenericCharmBuilder.java
@@ -8,17 +8,14 @@ import net.sf.anathema.character.generic.impl.magic.PermanentCostList;
 import net.sf.anathema.character.generic.impl.magic.charm.type.CharmTypeModel;
 import net.sf.anathema.character.generic.impl.magic.persistence.builder.HeaderStringBuilder;
 import net.sf.anathema.character.generic.impl.magic.persistence.builder.IHeaderStringBuilder;
+import net.sf.anathema.character.generic.impl.magic.persistence.builder.prerequisite.GenericTraitPrerequisitesBuilder;
+import net.sf.anathema.character.generic.impl.magic.persistence.builder.prerequisite.PrerequisiteListBuilder;
 import net.sf.anathema.character.generic.impl.magic.persistence.prerequisite.CharmPrerequisiteList;
-import net.sf.anathema.character.generic.impl.magic.persistence.prerequisite.SelectiveCharmGroupTemplate;
 import net.sf.anathema.character.generic.magic.charms.ComboRestrictions;
 import net.sf.anathema.character.generic.magic.charms.Duration;
-import net.sf.anathema.character.generic.magic.charms.ICharmAttributeRequirement;
 import net.sf.anathema.character.generic.magic.charms.type.CharmType;
 import net.sf.anathema.character.generic.magic.general.IMagicSource;
-import net.sf.anathema.character.generic.traits.IGenericTrait;
 import net.sf.anathema.character.generic.traits.types.AbilityType;
-import net.sf.anathema.character.generic.traits.types.OtherTraitType;
-import net.sf.anathema.character.generic.traits.types.ValuedTraitType;
 import net.sf.anathema.character.generic.type.CharacterType;
 import net.sf.anathema.lib.exception.PersistenceException;
 
@@ -30,18 +27,13 @@ public class GenericCharmBuilder {
 
   public Charm buildCharm(Element charmElement, AbilityType type) throws PersistenceException {
     String id = idBuilder.build(charmElement) + type.getId();
+    PrerequisiteListBuilder builder = new PrerequisiteListBuilder(new GenericTraitPrerequisitesBuilder(type));
+    CharmPrerequisiteList list = builder.buildPrerequisiteList(charmElement.element(ICharmXMLConstants.TAG_PREREQUISITE_LIST));
     String groupdId = type.getId();
-    return getCharm(id, groupdId, type, CharmType.ExtraAction, null);
+    return getCharm(id, groupdId, CharmType.ExtraAction, list);
   }
 
-  private Charm getCharm(String id, String groupdId, AbilityType traitType, CharmType type, String parentId) {
-    final String[] parentArray = parentId == null ? new String[0] : new String[] { parentId };
-    CharmPrerequisiteList list = new CharmPrerequisiteList(
-        new IGenericTrait[] { new ValuedTraitType(traitType, 1) },
-        new ValuedTraitType(OtherTraitType.Essence, 1),
-        parentArray,
-        new SelectiveCharmGroupTemplate[0],
-        new ICharmAttributeRequirement[0]);
+  private Charm getCharm(String id, String groupdId, CharmType type, CharmPrerequisiteList list) {
     CharmTypeModel model = new CharmTypeModel();
     model.setCharmType(type);
     final Charm charm = new Charm(
--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/persistence/test/GenericCharmBuilderTest.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/persistence/test/GenericCharmBuilderTest.java
@@ -3,7 +3,6 @@ package net.sf.anathema.character.generic.impl.magic.persistence.test;
 import net.sf.anathema.character.generic.impl.magic.Charm;
 import net.sf.anathema.character.generic.traits.types.AbilityType;
 import net.sf.anathema.lib.testing.BasicTestCase;
-import net.sf.anathema.lib.xml.DocumentUtilities;
 
 import org.dom4j.Element;
 
@@ -12,51 +11,48 @@ public class GenericCharmBuilderTest extends BasicTestCase {
   private GenericCharmBuilder builder = new GenericCharmBuilder();
 
   public void testReadGenericCharmId() throws Exception {
-    String xml = "<genericCharm id=\"Solar.Generic\" />"; //$NON-NLS-1$
-    Element rootElement = DocumentUtilities.read(xml).getRootElement();
-    Charm charm = builder.buildCharm(rootElement, AbilityType.Archery);
+    Element xml = CharmXmlTestUtils.createCharmElement("Solar.Generic"); //$NON-NLS-1$
+    Charm charm = builder.buildCharm(xml, AbilityType.Archery);
     assertEquals("Solar.GenericArchery", charm.getId()); //$NON-NLS-1$
   }
 
+  private void removeAttribute(Element element, String attribute) {
+    element.remove(element.attribute(attribute));
+  }
+
+  private void removeElement(Element element, String name) {
+    element.remove(element.element(name));
+  }
+
   public void testReadGenericCharmGroup() throws Exception {
-    String xml = "<genericCharm id=\"Solar.Generic\" />"; //$NON-NLS-1$
-    Element rootElement = DocumentUtilities.read(xml).getRootElement();
-    Charm charm = builder.buildCharm(rootElement, AbilityType.Archery);
+    Element xml = CharmXmlTestUtils.createCharmElement("Solar.Generic"); //$NON-NLS-1$
+    removeElement(xml, "group"); //$NON-NLS-1$
+    Charm charm = builder.buildCharm(xml, AbilityType.Archery);
     assertEquals("Archery", charm.getGroupId()); //$NON-NLS-1$
   }
 
   public void testReadGenericPrerequisiteFirst() throws Exception {
-    String xml = "<genericCharm id=\"Solar.Generic\">" //$NON-NLS-1$
-        + "<prerequisites>" //$NON-NLS-1$
-        + "<trait value=\"1\"/>" //$NON-NLS-1$
-        + "</prerequisites>" //$NON-NLS-1$
-        + "</genericCharm>"; //$NON-NLS-1$
-    Element rootElement = DocumentUtilities.read(xml).getRootElement();
-    Charm charm = builder.buildCharm(rootElement, AbilityType.Archery);
+    Element xml = CharmXmlTestUtils.createCharmElement("Solar.Generic"); //$NON-NLS-1$
+    Element prerequisites = xml.element("prerequisite");
+    removeAttribute(prerequisites.element("trait"), "id");
+    Charm charm = builder.buildCharm(xml, AbilityType.Archery);
     assertEquals(AbilityType.Archery, charm.getPrerequisites()[0].getType());
   }
 
   public void testReadGenericPrerequisiteSecond() throws Exception {
-    String xml = "<genericCharm id=\"Solar.Generic\">" //$NON-NLS-1$
-        + "<prerequisites>" //$NON-NLS-1$
-        + "<trait value=\"1\"/>" //$NON-NLS-1$
-        + "</prerequisites>" //$NON-NLS-1$
-        + "</genericCharm>"; //$NON-NLS-1$
-    Element rootElement = DocumentUtilities.read(xml).getRootElement();
-    Charm charm = builder.buildCharm(rootElement, AbilityType.Athletics);
+    Element xml = CharmXmlTestUtils.createCharmElement("Solar.Generic"); //$NON-NLS-1$
+    Element prerequisites = xml.element("prerequisite");
+    removeAttribute(prerequisites.element("trait"), "id");
+    Charm charm = builder.buildCharm(xml, AbilityType.Athletics);
     assertEquals(AbilityType.Athletics, charm.getPrerequisites()[0].getType());
   }
 
   // Fails
   public void testReadGenericPrerequisiteHigherValue() throws Exception {
-    String xml = "<genericCharm id=\"Solar.Generic\">" //$NON-NLS-1$
-        + "<prerequisites>" //$NON-NLS-1$
-        + "<trait value=\"3\"/>" //$NON-NLS-1$
-        + "</prerequisites>" //$NON-NLS-1$
-        + "</genericCharm>"; //$NON-NLS-1$
-    Element rootElement = DocumentUtilities.read(xml).getRootElement();
-    Charm charm = builder.buildCharm(rootElement, AbilityType.Awareness);
-    assertEquals(AbilityType.Awareness, charm.getPrerequisites()[0].getType());
+    Element xml = CharmXmlTestUtils.createCharmElement("Solar.Generic"); //$NON-NLS-1$
+    Element prerequisites = xml.element("prerequisite");
+    prerequisites.element("trait").addAttribute("value", "3");
+    Charm charm = builder.buildCharm(xml, AbilityType.Awareness);
     assertEquals(3, charm.getPrerequisites()[0].getCurrentValue());
   }
 }
