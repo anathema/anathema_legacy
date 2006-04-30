1335abd0b8974840974523651b65166134b551b5
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-04-30 12:04:17 +0000

    Integration neue GISterm-Version

--- Gis_Platform/src/net/sf/anathema/gis/platform/GisModule.java
+++ Gis_Platform/src/net/sf/anathema/gis/platform/GisModule.java
@@ -3,15 +3,18 @@ package net.sf.anathema.gis.platform;
 import java.io.File;
 
 import net.disy.commons.swing.filechooser.SmartFileChooser;
+import net.sf.anathema.framework.IAnathemaModel;
 import net.sf.anathema.framework.module.AbstractAnathemaModule;
 import net.sf.anathema.framework.module.AbstractItemTypeConfiguration;
 import net.sf.anathema.framework.resources.IAnathemaResources;
 import net.sf.anathema.framework.view.IAnathemaView;
 import net.sf.anathema.gis.data.impl.GisDataDirectory;
+import net.sf.anathema.gis.main.impl.model.AnathemaLayerPopupFactory;
 import net.sf.anathema.gis.platform.menu.GisModuleMenuFactory;
 import net.sf.anathema.gis.platform.util.DefaultFileChooserProvider;
 import net.sf.anathema.lib.logging.Logger;
 import net.sf.anathema.lib.resources.IResources;
+import de.disy.gis.gisterm.pro.map.layer.LayerPanel;
 
 public class GisModule extends AbstractAnathemaModule {
 
@@ -25,9 +28,19 @@ public class GisModule extends AbstractAnathemaModule {
   }
 
   @Override
+  public void initModel(IAnathemaModel model) {
+    super.initModel(model);
+    customizeGisTermFramework();
+  }
+
+  private void customizeGisTermFramework() {
+    LayerPanel.popupFactory = new AnathemaLayerPopupFactory();
+  }
+
+  @Override
   public void initAnathemaResources(IAnathemaResources resources) {
     super.initAnathemaResources(resources);
-    //resources.addStringResourceHandler(createStringProvider("Gis", resources.getLocale())); //$NON-NLS-1$    
+    // resources.addStringResourceHandler(createStringProvider("Gis", resources.getLocale())); //$NON-NLS-1$
   }
 
   @Override
