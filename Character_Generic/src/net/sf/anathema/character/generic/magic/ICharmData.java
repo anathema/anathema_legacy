283afadd433fb0fb948a6423eaf363054eae9140
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-05-01 14:06:57 +0000

    All keywords, edition listening

--- Character_Generic/src/net/sf/anathema/character/generic/magic/ICharmData.java
+++ Character_Generic/src/net/sf/anathema/character/generic/magic/ICharmData.java
@@ -13,7 +13,7 @@ import net.sf.anathema.lib.util.Identificate;
 
 public interface ICharmData extends IMagicData {
   public static final IIdentificate FORM_ATTRIBUTE = new Identificate("Form"); //$NON-NLS-1$
-  public static final String ALLOWS_CELESTIAL_ATTRIBUTE = "AllowsCelestial"; //$NON-NLS-1$
+  public static final IIdentificate ALLOWS_CELESTIAL_ATTRIBUTE = new Identificate("AllowsCelestial"); //$NON-NLS-1$
   public static final IIdentificate UNRESTRICTED_ATTRIBUTE = new Identificate("Unrestricted"); //$NON-NLS-1$
   public static final IIdentificate NO_STYLE_ATTRIBUTE = new Identificate("NoStyle"); //$NON-NLS-1$
   public static final IIdentificate NOT_ALIEN_LEARNABLE = new Identificate("NotAlienLearnable"); //$NON-NLS-1$
