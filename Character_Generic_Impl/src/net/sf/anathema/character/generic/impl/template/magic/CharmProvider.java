8604c33dffedcc4fee211c93bfba2b78b175d6f7
Urs Reupke <ursreupke@gmail.com>
2006-05-02 19:11:35 +0000

    SpecialCharmProvider stores Charms by edition. Second Edition Solars creatable, acceptance tests fail.

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/template/magic/CharmProvider.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/template/magic/CharmProvider.java
@@ -6,30 +6,38 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import net.sf.anathema.character.generic.impl.rules.ExaltedEdition;
 import net.sf.anathema.character.generic.magic.charms.special.ISpecialCharm;
+import net.sf.anathema.character.generic.rules.IExaltedEdition;
 import net.sf.anathema.character.generic.type.CharacterType;
 
 public class CharmProvider implements ICharmProvider {
 
-  private final Map<CharacterType, ISpecialCharm[]> specialCharmsByCharacterType = new HashMap<CharacterType, ISpecialCharm[]>();
+  private final Map<IExaltedEdition, Map<CharacterType, ISpecialCharm[]>> charmsByTypeByRuleSet = new HashMap<IExaltedEdition, Map<CharacterType, ISpecialCharm[]>>();
 
-  public ISpecialCharm[] getAllSpecialCharms() {
+  public CharmProvider() {
+    for (IExaltedEdition edition : ExaltedEdition.values()) {
+      charmsByTypeByRuleSet.put(edition, new HashMap<CharacterType, ISpecialCharm[]>());
+    }
+  }
+
+  public ISpecialCharm[] getAllSpecialCharms(IExaltedEdition edition) {
     List<ISpecialCharm> list = new ArrayList<ISpecialCharm>();
     for (CharacterType type : CharacterType.values()) {
-      Collections.addAll(list, getSpecialCharms(type));
+      Collections.addAll(list, getSpecialCharms(type, edition));
     }
     return list.toArray(new ISpecialCharm[list.size()]);
   }
 
-  public ISpecialCharm[] getSpecialCharms(CharacterType characterType) {
-    ISpecialCharm[] specialCharms = specialCharmsByCharacterType.get(characterType);
+  public ISpecialCharm[] getSpecialCharms(CharacterType characterType, IExaltedEdition edition) {
+    ISpecialCharm[] specialCharms = charmsByTypeByRuleSet.get(edition).get(characterType);
     if (specialCharms == null) {
       return new ISpecialCharm[0];
     }
     return specialCharms;
   }
 
-  public void setSpecialCharms(CharacterType type, ISpecialCharm[] charms) {
-    specialCharmsByCharacterType.put(type, charms);
+  public void setSpecialCharms(CharacterType type, IExaltedEdition edition, ISpecialCharm[] charms) {
+    charmsByTypeByRuleSet.get(edition).put(type, charms);
   }
 }
--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/template/magic/ICharmProvider.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/template/magic/ICharmProvider.java
@@ -1,14 +1,14 @@
 package net.sf.anathema.character.generic.impl.template.magic;
 
 import net.sf.anathema.character.generic.magic.charms.special.ISpecialCharm;
+import net.sf.anathema.character.generic.rules.IExaltedEdition;
 import net.sf.anathema.character.generic.type.CharacterType;
 
 public interface ICharmProvider {
 
-  public ISpecialCharm[] getAllSpecialCharms();
+  public ISpecialCharm[] getSpecialCharms(CharacterType characterType, IExaltedEdition edition);
 
-  public ISpecialCharm[] getSpecialCharms(CharacterType characterType);
+  public void setSpecialCharms(CharacterType type, IExaltedEdition edition, ISpecialCharm[] charms);
 
-  public void setSpecialCharms(CharacterType type, ISpecialCharm[] charms);
-
+  public ISpecialCharm[] getAllSpecialCharms(IExaltedEdition edition);
 }
