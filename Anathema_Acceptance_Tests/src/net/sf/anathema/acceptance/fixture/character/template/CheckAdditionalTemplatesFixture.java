a23367c33647cc86431f6ee8d136e123ed24c00c
Urs Reupke <ursreupke@gmail.com>
2006-05-03 18:01:11 +0000

    Test for Global Additional Templates passes

--- Anathema_Acceptance_Tests/src/net/sf/anathema/acceptance/fixture/character/template/CheckAdditionalTemplatesFixture.java
+++ Anathema_Acceptance_Tests/src/net/sf/anathema/acceptance/fixture/character/template/CheckAdditionalTemplatesFixture.java
@@ -1,6 +1,9 @@
 package net.sf.anathema.acceptance.fixture.character.template;
 
+import java.util.Collection;
+
 import net.sf.anathema.character.generic.template.additional.IAdditionalTemplate;
+import net.sf.anathema.character.generic.template.additional.IGlobalAdditionalTemplate;
 
 public class CheckAdditionalTemplatesFixture extends AbstractTemplateColumnFixture {
 
@@ -8,6 +11,14 @@ public class CheckAdditionalTemplatesFixture extends AbstractTemplateColumnFixtu
 
   public boolean isTemplateRegistered() {
     for (IAdditionalTemplate template : getTemplate().getAdditionalTemplates()) {
+      if (template.getId().equals(id)) {
+        return true;
+      }
+    }
+    Collection<IGlobalAdditionalTemplate> globalTemplates = new CharacterTemplateSummary(summary).getCharacterGenerics()
+        .getGlobalAdditionalTemplateRegistry()
+        .getAll();
+    for (IAdditionalTemplate template : globalTemplates) {
       if (template.getId().equals(id)) {
         return true;
       }
