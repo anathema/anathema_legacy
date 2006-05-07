3d7e4b08b3f749b4c6f822f80e1e07a63858e5bd
Urs Reupke <ursreupke@gmail.com>
2006-05-07 08:08:07 +0000

    Fixed Error in reflexive special model writer.

--- Character_Generic/src/net/sf/anathema/character/generic/magic/charms/type/IReflexiveSpecialsModel.java
+++ Character_Generic/src/net/sf/anathema/character/generic/magic/charms/type/IReflexiveSpecialsModel.java
@@ -1,8 +1,10 @@
 package net.sf.anathema.character.generic.magic.charms.type;
 
-public interface IReflexiveSpecialsModel extends ITypeSpecialsModel{
+public interface IReflexiveSpecialsModel extends ITypeSpecialsModel {
 
   public int getPrimaryStep();
 
   public Integer getSecondaryStep();
+
+  public boolean isSplitEnabled();
 }
