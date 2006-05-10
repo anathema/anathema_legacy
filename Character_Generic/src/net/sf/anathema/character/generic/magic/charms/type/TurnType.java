638af20eb46a8608e4ac82287a0275fe0da93c23
Urs Reupke <ursreupke@gmail.com>
2006-05-10 15:02:37 +0000

    CharmEntryWizard, first 7 pages

--- Character_Generic/src/net/sf/anathema/character/generic/magic/charms/type/TurnType.java
+++ Character_Generic/src/net/sf/anathema/character/generic/magic/charms/type/TurnType.java
@@ -1,6 +1,12 @@
 package net.sf.anathema.character.generic.magic.charms.type;
 
-public enum TurnType {
+import net.sf.anathema.lib.util.IIdentificate;
+
+public enum TurnType implements IIdentificate {
 
   Tick, LongTick, DramaticAction;
+
+  public String getId() {
+    return name();
+  }
 }
