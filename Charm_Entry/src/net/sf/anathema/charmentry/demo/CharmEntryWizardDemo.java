b705f05b5f52ec4265951598594c1cb3db77a259
Urs Reupke <ursreupke@gmail.com>
2006-05-11 18:14:09 +0000

    DO NOT CHECKOUT - Cleanup package hierarchy and content - DO NOT CHECKOUT

--- Charm_Entry/src/net/sf/anathema/charmentry/demo/CharmEntryWizardDemo.java
+++ Charm_Entry/src/net/sf/anathema/charmentry/demo/CharmEntryWizardDemo.java
@@ -1,8 +1,11 @@
 package net.sf.anathema.charmentry.demo;
 
 import net.disy.commons.swing.dialog.wizard.WizardDialog;
-import net.sf.anathema.charmentry.demo.model.WizardCharmEntryModel;
-import net.sf.anathema.charmentry.demo.page.HeaderDataEntryPage;
+import net.sf.anathema.charmentry.model.WizardCharmEntryModel;
+import net.sf.anathema.charmentry.module.CharmEntryViewFactory;
+import net.sf.anathema.charmentry.module.ICharmEntryViewFactory;
+import net.sf.anathema.charmentry.presenter.HeaderDataEntryPage;
+import net.sf.anathema.charmentry.presenter.model.ICharmEntryModel;
 import net.sf.anathema.framework.resources.AnathemaResources;
 import net.sf.anathema.lib.gui.wizard.AnathemaWizardDialog;
 
