015fad5f021dc26f46bfc5532be3a0cd9ad7a68b
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-05-13 21:26:10 +0000

    AbilityBox noch ohne Specialties und Crafts

--- Character_Generic_Framework/src/net/sf/anathema/character/generic/framework/xml/trait/alternate/test/DummyFavorableGenericTrait.java
+++ Character_Generic_Framework/src/net/sf/anathema/character/generic/framework/xml/trait/alternate/test/DummyFavorableGenericTrait.java
@@ -6,11 +6,18 @@ import net.sf.anathema.character.generic.traits.ITraitType;
 
 public class DummyFavorableGenericTrait extends DummyGenericTrait implements IFavorableGenericTrait {
 
+  private final boolean isFavored;
+
   public DummyFavorableGenericTrait(ITraitType type, int currentValue) {
+    this(type, currentValue, false);
+  }
+
+  public DummyFavorableGenericTrait(ITraitType type, int currentValue, boolean isFavored) {
     super(type, currentValue);
+    this.isFavored = isFavored;
   }
 
   public boolean isCasteOrFavored() {
-    return false;
+    return isFavored;
   }
 }
