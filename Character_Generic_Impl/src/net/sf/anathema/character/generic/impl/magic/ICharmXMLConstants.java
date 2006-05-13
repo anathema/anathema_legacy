9c911d4d776aff675373918f3e356e8aa3a66e34
Urs Reupke <ursreupke@gmail.com>
2006-05-13 08:51:38 +0000

    Fixed NPE on Permanent Durations

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/ICharmXMLConstants.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/ICharmXMLConstants.java
@@ -60,7 +60,7 @@ public interface ICharmXMLConstants {
   public static final String TAG_HEALTH_COST = "health"; //$NON-NLS-1$
 
   public static final String VALUE_INSTANT = "Instant"; //$NON-NLS-1$
-  public static final String VALUE_POWERCOMBAT = "PowerCombat"; //$NON-NLS-1$
+  public static final String VALUE_PERMANENT = "Permanent"; //$NON-NLS-1$
   public static final String ATTRIB_COST = "cost"; //$NON-NLS-1$
   public static final String ATTRIB_TEXT = "text"; //$NON-NLS-1$
   public static final String TAG_GENERIC_CHARM_ATTRIBUTE_REQUIREMENT = "genericCharmAttributeRequirement"; //$NON-NLS-1$
--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/persistence/writer/DurationWriter.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/persistence/writer/DurationWriter.java
@@ -3,6 +3,7 @@ package net.sf.anathema.character.generic.impl.magic.persistence.writer;
 import static net.sf.anathema.character.generic.impl.magic.ICharmXMLConstants.ATTRIB_DURATION;
 import static net.sf.anathema.character.generic.impl.magic.ICharmXMLConstants.TAG_DURATION;
 import static net.sf.anathema.character.generic.impl.magic.ICharmXMLConstants.VALUE_INSTANT;
+import static net.sf.anathema.character.generic.impl.magic.ICharmXMLConstants.VALUE_PERMANENT;
 
 import net.sf.anathema.character.generic.magic.ICharmData;
 import net.sf.anathema.character.generic.magic.charms.DurationType;
@@ -15,6 +16,10 @@ public class DurationWriter {
     Element durationElement = charmElement.addElement(TAG_DURATION);
     if (charm.getDuration().getType() == DurationType.Instant) {
       durationElement.addAttribute(ATTRIB_DURATION, VALUE_INSTANT);
+      return;
+    }
+    if (charm.getDuration().getType() == DurationType.Permanent) {
+      durationElement.addAttribute(ATTRIB_DURATION, VALUE_PERMANENT);
       return;
     }
     durationElement.addAttribute(ATTRIB_DURATION, charm.getDuration().getText(null));
