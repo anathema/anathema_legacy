b27c5f96051d5428ffce0636bfb2e33301507ada
Urs Reupke <ursreupke@gmail.com>
2006-05-14 11:21:46 +0000

    XML Templates support additional essence pools Abyssals suffer

---
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/additional/MultiLearnablePool.java
@@ -0,0 +1,27 @@
+package net.sf.anathema.character.generic.impl.additional;
+
+import net.sf.anathema.character.generic.additionalrules.IAdditionalEssencePool;
+import net.sf.anathema.character.generic.character.IGenericTraitCollection;
+import net.sf.anathema.character.generic.character.IMagicCollection;
+import net.sf.anathema.character.generic.magic.charms.special.IMultiLearnableCharm;
+
+public class MultiLearnablePool implements IAdditionalEssencePool {
+
+  private final IMultiLearnableCharm charm;
+  private final int peripheralMultiplier;
+  private final int personalMultiplier;
+
+  public MultiLearnablePool(IMultiLearnableCharm charm, int personalMultiplier, int peripheralMultiplier) {
+    this.charm = charm;
+    this.personalMultiplier = personalMultiplier;
+    this.peripheralMultiplier = peripheralMultiplier;
+  }
+
+  public int getAdditionaPeripheralPool(IGenericTraitCollection traitCollection, IMagicCollection magicCollection) {
+    return magicCollection.getLearnCount(charm) * peripheralMultiplier;
+  }
+
+  public int getAdditionaPersonalPool(IGenericTraitCollection traitCollection, IMagicCollection magicCollection) {
+    return magicCollection.getLearnCount(charm) * personalMultiplier;
+  }
+}
