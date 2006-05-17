d36e7c3b1c7856c068157de88d923b13a430d5b4
Urs Reupke <ursreupke@gmail.com>
2006-05-17 10:42:33 +0000

    Experimental support for Aqua L&F

--- Platform/src/net/sf/anathema/framework/environment/LookAndFeelInitializer.java
+++ Platform/src/net/sf/anathema/framework/environment/LookAndFeelInitializer.java
@@ -8,12 +8,12 @@ import net.sf.anathema.framework.configuration.IAnathemaPreferences;
 
 import com.l2fprod.common.swing.plaf.LookAndFeelAddons;
 import com.l2fprod.common.swing.plaf.metal.MetalLookAndFeelAddons;
-import com.l2fprod.common.swing.plaf.windows.WindowsLookAndFeelAddons;
 import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
 
 public class LookAndFeelInitializer {
 
   private static final String WINDOWS_LOOK_AND_FEEL_CLASSNAME = WindowsLookAndFeel.class.getName();
+  private static final String AQUA_LOOK_AND_FEEL_CLASSNAME = "apple.laf.AquaLookAndFeel"; //$NON-NLS-1$
   private final IAnathemaPreferences anathemaPreferences;
 
   public LookAndFeelInitializer(IAnathemaPreferences anathemaPreferences) {
@@ -31,13 +31,15 @@ public class LookAndFeelInitializer {
       lookAndFeelClassName = MetalLookAndFeel.class.getName();
     }
     else {
-      LookAndFeelAddons.setAddon(WindowsLookAndFeelAddons.class);
+      LookAndFeelAddons.setAddon(LookAndFeelAddons.getSystemAddonClassName());
     }
     UIManager.setLookAndFeel(lookAndFeelClassName);
   }
 
   private boolean useMetalLookAndFeel(String lookAndFeelClassName) {
     boolean forceMetalLookAndFeel = anathemaPreferences.isMetalLookAndFeelForced();
-    return !lookAndFeelClassName.equals(WINDOWS_LOOK_AND_FEEL_CLASSNAME) || forceMetalLookAndFeel;
+    final boolean isWindowsLookAndFeel = lookAndFeelClassName.equals(WINDOWS_LOOK_AND_FEEL_CLASSNAME);
+    final boolean isAppleLookAndFeel = lookAndFeelClassName.equals(AQUA_LOOK_AND_FEEL_CLASSNAME);
+    return (!isWindowsLookAndFeel && !isAppleLookAndFeel) || forceMetalLookAndFeel;
   }
 }
