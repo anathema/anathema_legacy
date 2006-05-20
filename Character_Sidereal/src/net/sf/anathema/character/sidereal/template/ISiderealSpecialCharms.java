85cf6439d2e54a3eb4c03688fd4ba78397c926e3
Urs Reupke <ursreupke@gmail.com>
2006-05-20 12:43:36 +0000

    OBT moved to Generic_Impl

--- Character_Sidereal/src/net/sf/anathema/character/sidereal/template/ISiderealSpecialCharms.java
+++ Character_Sidereal/src/net/sf/anathema/character/sidereal/template/ISiderealSpecialCharms.java
@@ -3,9 +3,9 @@ package net.sf.anathema.character.sidereal.template;
 import java.util.LinkedHashMap;
 
 import net.sf.anathema.character.generic.health.HealthLevelType;
+import net.sf.anathema.character.generic.impl.magic.charm.special.OxBodyTechniqueCharm;
 import net.sf.anathema.character.generic.impl.magic.charm.special.StaticMultiLearnableCharm;
 import net.sf.anathema.character.generic.magic.charms.special.ISpecialCharm;
-import net.sf.anathema.character.generic.magic.charms.special.OxBodyTechniqueCharm;
 import net.sf.anathema.character.generic.traits.types.AbilityType;
 
 public interface ISiderealSpecialCharms {
