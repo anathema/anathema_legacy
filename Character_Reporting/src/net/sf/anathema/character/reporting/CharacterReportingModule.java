e863a7145bec183da5d0ab65a5b1ecb8c9890ac5
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-05-22 13:10:52 +0000

    ModuleObjects für ICharacterModules

---
+++ Character_Reporting/src/net/sf/anathema/character/reporting/CharacterReportingModule.java
@@ -0,0 +1,7 @@
+package net.sf.anathema.character.reporting;
+
+import net.sf.anathema.character.generic.framework.module.CharacterModuleAdapter;
+
+public class CharacterReportingModule extends CharacterModuleAdapter<CharacterReportingModuleObject>{
+
+}
---
+++ Character_Reporting/src/net/sf/anathema/character/reporting/CharacterReportingModuleObject.java
@@ -0,0 +1,7 @@
+package net.sf.anathema.character.reporting;
+
+import net.sf.anathema.character.generic.framework.module.object.ICharacterModuleObject;
+
+public class CharacterReportingModuleObject implements ICharacterModuleObject {
+
+}
