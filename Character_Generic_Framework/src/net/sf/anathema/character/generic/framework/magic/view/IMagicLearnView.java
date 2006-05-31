f89883c769ba21d0a991dab7ee5193059584bf29
Urs Reupke <ursreupke@gmail.com>
2006-05-31 13:31:33 +0000

    Cleaning up the SpellLearnView: Interface-Cleanup

--- Character_Generic_Framework/src/net/sf/anathema/character/generic/framework/magic/view/IMagicLearnView.java
+++ Character_Generic_Framework/src/net/sf/anathema/character/generic/framework/magic/view/IMagicLearnView.java
@@ -1,6 +1,5 @@
 package net.sf.anathema.character.generic.framework.magic.view;
 
-import javax.swing.ListModel;
 import javax.swing.event.ListSelectionListener;
 
 public interface IMagicLearnView {
@@ -9,11 +8,9 @@ public interface IMagicLearnView {
 
   public void setLearnedMagic(Object[] magics);
 
-  public void addMagicViewListener(IMagicViewListener listener);
-
-  public ListModel getLearnedListModel();
-
   public void clearSelection();
+
+  public void addMagicViewListener(IMagicViewListener listener);
 
   public void addSelectionListListener(ListSelectionListener listener);
 
