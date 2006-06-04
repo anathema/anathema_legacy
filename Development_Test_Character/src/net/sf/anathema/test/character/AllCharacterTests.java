f905279f319dbefd9395d49d5d85a7788346c176
Urs Reupke <ursreupke@gmail.com>
2006-06-04 18:13:42 +0000

    CharmBuilder Tests nach Development_Character_Test gezogen

--- Development_Test_Character/src/net/sf/anathema/test/character/AllCharacterTests.java
+++ Development_Test_Character/src/net/sf/anathema/test/character/AllCharacterTests.java
@@ -7,9 +7,10 @@ import de.jdemo.junit.Demo2TestConverter;
 public class AllCharacterTests {
 
   public static Test suite() {
-    TestSuite suite = new TestSuite("Test for net.sf.anathema.character.abyssal.suite"); //$NON-NLS-1$
+    TestSuite suite = new TestSuite("Test for net.sf.anathema.character"); //$NON-NLS-1$
     suite.addTest(net.sf.anathema.test.character.equipment.impl.creation.model.AllTests.suite());
     suite.addTest(net.sf.anathema.test.character.equipment.item.AllTests.suite());
+    suite.addTest(net.sf.anathema.test.character.generic.impl.magic.persistence.builder.AllTests.suite());
     suite.addTest(net.sf.anathema.test.character.generic.magic.charms.duration.AllTests.suite());
     suite.addTest(net.sf.anathema.test.character.generic.traits.types.AllTests.suite());
     suite.addTest(Demo2TestConverter.createTest(AllCharacterDemos.suite()));
