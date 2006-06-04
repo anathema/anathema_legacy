bebb24c7c5c9f0dfadb5b9796bd41222cd8bca9c
Urs Reupke <ursreupke@gmail.com>
2006-06-04 17:50:05 +0000

    Sources verhackstückt.

--- Character_Main_Impl/src/net/sf/anathema/character/impl/module/CharacterCoreModule.java
+++ Character_Main_Impl/src/net/sf/anathema/character/impl/module/CharacterCoreModule.java
@@ -27,23 +27,32 @@ public class CharacterCoreModule extends AbstractAnathemaModule {
   // @Override
   // public void initPresentation(IResources resources, IAnathemaModel model, IAnathemaView view) {
   // CharmCache charmCache = CharmCache.getInstance();
-  // for (CharacterType characterType : CharacterType.values()) {
+  // // for (CharacterType characterType : CharacterType.values()) {
+  // IIdentificate characterType = CharmCache.MARTIAL_ARTS_TYPE;
   // for (IExaltedRuleSet ruleSet : ExaltedRuleSet.values()) {
+  // ICharm[] charms = new ICharm[0];
   // try {
-  // ICharm[] charms = charmCache.getCharms(characterType, ruleSet);
-  // for (ICharm charm : charms) {
-  // final String durationString = charm.getDuration().getText(resources);
-  // if (durationString.contains("#")) {
-  // System.err.println(ruleSet + " - " + charm.getId());
-  // }
-  // }
+  // charms = charmCache.getCharms(characterType, ruleSet);
   // }
   // catch (Exception e) {
-  // // e.printStackTrace();
+  // System.err.println(characterType + "-" + ruleSet);
   // }
+  // System.out.println(characterType + "-" + ruleSet);
+  // for (ICharm charm : charms) {
+  // final String sourceString = charm.getSource().getSource();
+  // final String pageString = charm.getSource().getPage();
+  // final String string = sourceString + "." + charm.getId() + ".Page=" + pageString;
+  // if (ruleSet != ExaltedRuleSet.PowerCombat || sourceString.startsWith("Players")) {
+  // System.out.println(string);
   // }
+  // if (string.contains(" ") && !(charm.getId().equals("Solar.Spirit Weapons"))) {
+  // throw new RuntimeException();
   // }
-  // }
+  //        // }
+  //
+  //      }
+  //    }
+  //  }
 
   @Override
   public void initAnathemaResources(IAnathemaResources resources) {
@@ -55,18 +64,24 @@ public class CharacterCoreModule extends AbstractAnathemaModule {
     resources.addStringResourceHandler(createStringProvider("Health", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("Abyssal", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("Charms_Abyssal", resources.getLocale())); //$NON-NLS-1$
+    resources.addStringResourceHandler(createStringProvider("Charms_Abyssal_Pages", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("Dragon-Blooded", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("Charms_Dragon-Blooded", resources.getLocale())); //$NON-NLS-1$
+    resources.addStringResourceHandler(createStringProvider("Charms_Dragon-Blooded_Pages", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("Solar", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("Lunar", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("Charms_Lunar", resources.getLocale())); //$NON-NLS-1$
+    resources.addStringResourceHandler(createStringProvider("Charms_Lunar_Pages", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("Charms_Solar", resources.getLocale())); //$NON-NLS-1$
+    resources.addStringResourceHandler(createStringProvider("Charms_Solar_Pages", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("Charms_Solar_SecondEdition", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("CharacterControl", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("Sidereal", resources.getLocale())); //$NON-NLS-1$
-    resources.addStringResourceHandler(createStringProvider("Charms_Sidereal", resources.getLocale())); //$NON-NLS-1$       
+    resources.addStringResourceHandler(createStringProvider("Charms_Sidereal", resources.getLocale())); //$NON-NLS-1$
+    resources.addStringResourceHandler(createStringProvider("Charms_Sidereal_Pages", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("Charmgroups", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("CharmDuration", resources.getLocale())); //$NON-NLS-1$
+    resources.addStringResourceHandler(createStringProvider("Charms_MartialArts_Pages", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("Traits", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("Cascades", resources.getLocale())); //$NON-NLS-1$
     resources.addStringResourceHandler(createStringProvider("CharmTreeView", resources.getLocale())); //$NON-NLS-1$
--- Charm_Entry/src/net/sf/anathema/charmentry/model/ExaltedSourceBooks.java
+++ Charm_Entry/src/net/sf/anathema/charmentry/model/ExaltedSourceBooks.java
@@ -3,7 +3,9 @@ package net.sf.anathema.charmentry.model;
 import net.sf.anathema.lib.util.IIdentificate;
 
 public enum ExaltedSourceBooks implements IIdentificate {
-  SecondEdition, WondersLostAge;
+  FirstEdition, Abyssals1st, DragonBlooded1st, Lunars1st, Sidereals1st, TimeOfTumult, SavageSeas, Outcaste,
+  Illuminated, ABAir, ABEarth, ABFire, ABWater, ABWood, CBDawn, CBZenith, CBTwilight, CBNight, CBEclipse, PlayersGuide,
+  SecondEdition, WondersLostAge, Comic0;
 
   public String getId() {
     return name();
