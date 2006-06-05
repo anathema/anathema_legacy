606b6c1435baf85772a382d0bcb8eb6686d1f6d0
Urs Reupke <ursreupke@gmail.com>
2006-06-05 11:49:52 +0000

    GenericKuller auf Bogen

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/CharmUtilities.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/CharmUtilities.java
@@ -11,6 +11,12 @@ public class CharmUtilities {
   public static final String THIRD_EXCELLENCY = ".3rdExcellency"; //$NON-NLS-1$
   public static final String INFINITE_MASTERY = ".InfiniteMastery"; //$NON-NLS-1$
   public static final String ESSENCE_FLOW = ".EssenceFlow"; //$NON-NLS-1$
+  public static final String[] SOLAR_EXCELLENCIES = new String[] {
+      CharacterType.SOLAR.getId() + FIRST_EXCELLENCY,
+      CharacterType.SOLAR.getId() + SECOND_EXCELLENCY,
+      CharacterType.SOLAR.getId() + THIRD_EXCELLENCY,
+      CharacterType.SOLAR.getId() + INFINITE_MASTERY,
+      CharacterType.SOLAR.getId() + ESSENCE_FLOW };
 
   public static boolean isGenericCharmFor(ICharm charm, CharacterType type, IIdentificate groupId) {
     return isGenericCharmFor(charm, type) && charm.getId().endsWith(groupId.getId());
