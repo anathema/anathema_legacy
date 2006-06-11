474067ccd24077bc21d34e1b7e26e20b2860d3d3
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-06-11 16:57:25 +0000

    Added BackgroundIds?

---
+++ Character_Generic_Framework/src/net/sf/anathema/character/generic/framework/module/IBackgroundIds.java
@@ -0,0 +1,15 @@
+package net.sf.anathema.character.generic.framework.module;
+
+public interface IBackgroundIds {
+
+  public static final String BACKGROUND_ID_RESOURCES = "Resources"; //$NON-NLS-1$
+  public static final String BACKGROUND_ID_MENTOR = "Mentor"; //$NON-NLS-1$
+  public static final String BACKGROUND_ID_INFLUENCE = "Influence"; //$NON-NLS-1$
+  public static final String BACKGROUND_ID_FOLLOWERS = "Followers"; //$NON-NLS-1$
+  public static final String BACKGROUND_ID_FAMILIAR = "Familiar"; //$NON-NLS-1$
+  public static final String BACKGROUND_ID_CONTACTS = "Contacts"; //$NON-NLS-1$
+  public static final String BACKGROUND_ID_BACKING = "Backing"; //$NON-NLS-1$
+  public static final String BACKGROUND_ID_ALLIES = "Allies"; //$NON-NLS-1$
+  public static final String BACKGROUND_ID_CULT = "Cult"; //$NON-NLS-1$
+
+}
