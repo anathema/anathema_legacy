d8e179f201e81a5d30e32d1edaf1777f745d1f8a
Urs Reupke <ursreupke@gmail.com>
2006-06-15 17:08:15 +0000

    Plugin-Code ready to be integrated

--- Anathema/src/net/sf/anathema/initialization/ModuleCollection.java
+++ Anathema/src/net/sf/anathema/initialization/ModuleCollection.java
@@ -1,7 +1,12 @@
 package net.sf.anathema.initialization;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.Properties;
 
 import net.sf.anathema.campaign.module.CampaignModule;
 import net.sf.anathema.cascades.module.CharmCascadesModule;
@@ -12,6 +17,15 @@ import net.sf.anathema.framework.module.IAnathemaModule;
 import net.sf.anathema.initialization.modules.IModuleCollection;
 import net.sf.anathema.lib.control.IClosure;
 import net.sf.anathema.lib.logging.Logger;
+
+import org.java.plugin.JpfException;
+import org.java.plugin.ObjectFactory;
+import org.java.plugin.PluginManager;
+import org.java.plugin.PluginManager.PluginLocation;
+import org.java.plugin.boot.DefaultPluginsCollector;
+import org.java.plugin.registry.Extension;
+import org.java.plugin.registry.ExtensionPoint;
+import org.java.plugin.util.ExtendedProperties;
 
 public class ModuleCollection implements IModuleCollection {
 
@@ -32,6 +46,42 @@ public class ModuleCollection implements IModuleCollection {
   }
 
   private IAnathemaModule[] collectModules() {
+//    PluginManager manager = ObjectFactory.newInstance().createManager();
+//    try {
+//      DefaultPluginsCollector collector = new DefaultPluginsCollector();
+//      Enumeration<URL> systemResources = ClassLoader.getSystemResources("./plugin"); //$NON-NLS-1$
+//      StringBuilder builder = new StringBuilder();
+//      while (systemResources.hasMoreElements()) {
+//        if (builder.length() > 0) {
+//          builder.append(","); //$NON-NLS-1$
+//        }
+//        builder.append(systemResources.nextElement().getPath());
+//      }
+//      Properties properties = new Properties();
+//      properties.put("org.java.plugin.boot.pluginsRepositories", builder.toString()); //$NON-NLS-1$
+//      collector.configure(new ExtendedProperties(properties));
+//      Collection<PluginLocation> collection = collector.collectPluginLocations();
+//      PluginLocation[] locations = collection.toArray(new PluginLocation[collection.size()]);
+//      manager.publishPlugins(locations);
+//      ExtensionPoint point = manager.getRegistry().getExtensionPoint("net.sf.anathema.main", "MainModules"); //$NON-NLS-1$ //$NON-NLS-2$
+//      Collection<Extension> connectedExtensions = point.getConnectedExtensions();
+//      for (Extension extension : connectedExtensions) {
+//        
+//      }
+//    }
+//    catch (JpfException e) {
+//      // TODO Auto-generated catch block
+//      e.printStackTrace();
+//    }
+//    catch (MalformedURLException e) {
+//      // TODO Auto-generated catch block
+//      e.printStackTrace();
+//    }
+//    catch (Exception e) {
+//      // TODO Auto-generated catch block
+//      e.printStackTrace();
+//    }
+
     String[] classNames = new String[] {
         AnathemaCoreModule.class.getName(),
         CharacterCoreModule.class.getName(),
