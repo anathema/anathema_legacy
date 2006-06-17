623a2a70b210d78f83b52d0faf66fc5d0cc7b434
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-06-17 13:00:48 +0000

    dummy

---
+++ Development_Test_Character/src/net/sf/anathema/dummy/character/charm/DummyMagicCollection.java
@@ -0,0 +1,25 @@
+package net.sf.anathema.dummy.character.charm;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.anathema.character.generic.character.IMagicCollection;
+import net.sf.anathema.character.generic.magic.IMagic;
+import net.sf.anathema.character.generic.magic.charms.special.IMultiLearnableCharm;
+
+public class DummyMagicCollection implements IMagicCollection {
+
+  private final Map<IMultiLearnableCharm, Integer> counts = new HashMap<IMultiLearnableCharm, Integer>();
+
+  public int getLearnCount(IMultiLearnableCharm multiLearnableCharm) {
+    return counts.get(multiLearnableCharm);
+  }
+
+  public boolean isLearned(IMagic magic) {
+    return false;
+  }
+
+  public void setLearnCount(IMultiLearnableCharm multiLearnableCharm, int newValue) {
+    counts.put(multiLearnableCharm, newValue);
+  }
+}
