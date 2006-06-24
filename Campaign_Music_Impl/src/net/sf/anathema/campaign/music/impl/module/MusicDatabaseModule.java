20b173854e272bfce8aecedb3902a1a4f43aafa1
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-06-24 11:20:53 +0000

    MusicDataBaseItemTypeConfiguration über plugin

--- Campaign_Music_Impl/src/net/sf/anathema/campaign/music/impl/module/MusicDatabaseModule.java
+++ Campaign_Music_Impl/src/net/sf/anathema/campaign/music/impl/module/MusicDatabaseModule.java
@@ -4,16 +4,10 @@ import javax.swing.Action;
 
 import net.sf.anathema.framework.IAnathemaModel;
 import net.sf.anathema.framework.module.AbstractAnathemaModule;
-import net.sf.anathema.framework.module.AbstractItemTypeConfiguration;
 import net.sf.anathema.framework.view.IAnathemaView;
 import net.sf.anathema.lib.resources.IResources;
 
 public class MusicDatabaseModule extends AbstractAnathemaModule {
-  private AbstractItemTypeConfiguration musicConfiguration = new MusicDatabaseItemTypeConfiguration();
-
-  public MusicDatabaseModule() {
-    addItemTypeConfiguration(musicConfiguration);
-  }
 
   @Override
   public void initPresentation(IResources resources, IAnathemaModel model, IAnathemaView view) {
