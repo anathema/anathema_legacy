291b109430541d728b5bc6c356dd99115b39949c
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-06-24 11:20:21 +0000

    CharmCascadeItemType über plugin

--- Charm_Cascades/src/net/sf/anathema/cascades/module/CharmCascadesModule.java
+++ Charm_Cascades/src/net/sf/anathema/cascades/module/CharmCascadesModule.java
@@ -2,17 +2,10 @@ package net.sf.anathema.cascades.module;
 
 import net.sf.anathema.framework.IAnathemaModel;
 import net.sf.anathema.framework.module.AbstractAnathemaModule;
-import net.sf.anathema.framework.module.AbstractItemTypeConfiguration;
 import net.sf.anathema.framework.view.IAnathemaView;
 import net.sf.anathema.lib.resources.IResources;
 
 public class CharmCascadesModule extends AbstractAnathemaModule {
-
-  private AbstractItemTypeConfiguration charmCascadeConfiguration = new CharmCascadeItemTypeConfiguration();
-
-  public CharmCascadesModule() {
-    addItemTypeConfiguration(charmCascadeConfiguration);
-  }
 
   @Override
   public void initPresentation(IResources resources, IAnathemaModel model, IAnathemaView view) {
