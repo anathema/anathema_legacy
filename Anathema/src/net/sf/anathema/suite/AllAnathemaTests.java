ec44bd688d57c7757af30e30fb8dc28eae2026f8
Urs Reupke <ursreupke@gmail.com>
2006-06-27 04:39:52 +0000

    Removed references to campaing tests

--- Anathema/src/net/sf/anathema/suite/AllAnathemaTests.java
+++ Anathema/src/net/sf/anathema/suite/AllAnathemaTests.java
@@ -8,7 +8,6 @@ public class AllAnathemaTests {
   public static Test suite() {
     TestSuite suite = new TestSuite("Test for net.sf.anathema"); //$NON-NLS-1$
     suite.addTest(AllMainProjectTests.suite());
-    suite.addTest(net.sf.anathema.campaign.suite.AllCampaignTests.suite());
     suite.addTest(net.sf.anathema.lib.suite.AllLibTests.suite());
     // $JUnit-BEGIN$
 
