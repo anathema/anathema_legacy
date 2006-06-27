2572d849dee8b8b1bd05f1b8bec3d9102c329db8
Urs Reupke <ursreupke@gmail.com>
2006-06-27 11:30:31 +0000

    More demos from charmentry to test

---
+++ Development_Test_Character/src/net/sf/anathema/demo/charm/entry/AllDemos.java
@@ -0,0 +1,15 @@
+package net.sf.anathema.demo.charm.entry;
+
+import de.jdemo.framework.DemoSuite;
+import de.jdemo.framework.IDemo;
+
+public class AllDemos {
+
+  public static IDemo suite() {
+    DemoSuite suite = new DemoSuite("Demo for net.sf.anathema.development.character.charm.entry.demo"); //$NON-NLS-1$
+    //$JDemo-BEGIN$
+    suite.addDemo(new DemoSuite(CharmEntryWizardDemo.class));
+    //$JDemo-END$
+    return suite;
+  }
+}
