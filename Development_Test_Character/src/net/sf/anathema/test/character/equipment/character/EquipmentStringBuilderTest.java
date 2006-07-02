c14122a8c937acda8c4c8ab264bb81c5d4bbbf35
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-07-02 15:02:12 +0000

    StringBuilderTest uses JUnit4! *held der arbeit*

--- Development_Test_Character/src/net/sf/anathema/test/character/equipment/character/EquipmentStringBuilderTest.java
+++ Development_Test_Character/src/net/sf/anathema/test/character/equipment/character/EquipmentStringBuilderTest.java
@@ -1,22 +1,24 @@
 package net.sf.anathema.test.character.equipment.character;
 
+import junit.framework.Assert;
 import net.sf.anathema.character.equipment.character.EquipmentStringBuilder;
 import net.sf.anathema.character.equipment.character.IEquipmentStringBuilder;
 import net.sf.anathema.character.generic.health.HealthType;
 import net.sf.anathema.dummy.character.equipment.DemoMeleeWeapon;
 import net.sf.anathema.dummy.character.equipment.DemoRangeWeapon;
-import net.sf.anathema.lib.testing.BasicTestCase;
 import net.sf.anathema.lib.util.Identificate;
 import net.sf.anathema.test.character.generic.framework.magic.stringbuilder.DummyResources;
 
-public class EquipmentStringBuilderTest extends BasicTestCase {
+import org.junit.Before;
+import org.junit.Test;
 
-  private DummyResources resources;
+public class EquipmentStringBuilderTest {
+
   private IEquipmentStringBuilder equipmentStringBuilder;
 
-  @Override
-  protected void setUp() throws Exception {
-    resources = new DummyResources();
+  @Before
+  public void setUp() throws Exception {
+    DummyResources resources = new DummyResources();
     resources.putString("Equipment.Stats.Short.Speed", "Speed"); //$NON-NLS-1$ //$NON-NLS-2$
     resources.putString("Equipment.Stats.Short.Accuracy", "Acc"); //$NON-NLS-1$ //$NON-NLS-2$
     resources.putString("Equipment.Stats.Short.Damage", "Dam"); //$NON-NLS-1$ //$NON-NLS-2$
@@ -29,18 +31,21 @@ public class EquipmentStringBuilderTest extends BasicTestCase {
     equipmentStringBuilder = new EquipmentStringBuilder(resources);
   }
 
+  @Test
   public void testMeleeWeapon() {
     DemoMeleeWeapon weapon = new DemoMeleeWeapon(new Identificate("Sword"), 5, 2, 7, HealthType.Lethal, -1, 2); //$NON-NLS-1$
-    assertEquals("Sword: Speed:5 Acc:+2 Dam:+7L Def:-1 Rate:2", equipmentStringBuilder.createString(weapon)); //$NON-NLS-1$
+    Assert.assertEquals("Sword: Speed:5 Acc:+2 Dam:+7L Def:-1 Rate:2", equipmentStringBuilder.createString(weapon)); //$NON-NLS-1$
   }
 
+  @Test
   public void testFixedDamageRangedWeapon() {
     DemoRangeWeapon weapon = new DemoRangeWeapon(new Identificate("Bow"), 5, 2, 17, HealthType.Bashing, 200, 4, false); //$NON-NLS-1$
-    assertEquals("Bow: Speed:5 Acc:+2 Dam:17B Range:200 Rate:4", equipmentStringBuilder.createString(weapon)); //$NON-NLS-1$
+    Assert.assertEquals("Bow: Speed:5 Acc:+2 Dam:17B Range:200 Rate:4", equipmentStringBuilder.createString(weapon)); //$NON-NLS-1$
   }
 
+  @Test
   public void testNoDamageRangedWeapon() {
     DemoRangeWeapon weapon = new DemoRangeWeapon(new Identificate("Bow"), 5, 2, 17, HealthType.Bashing, 200, 4, true); //$NON-NLS-1$
-    assertEquals("Bow: Speed:5 Acc:+2 Dam:- Range:200 Rate:4", equipmentStringBuilder.createString(weapon)); //$NON-NLS-1$
+    Assert.assertEquals("Bow: Speed:5 Acc:+2 Dam:- Range:200 Rate:4", equipmentStringBuilder.createString(weapon)); //$NON-NLS-1$
   }
 }
