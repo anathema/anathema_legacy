493b4fa5c49a9d806981b188cd5848d42821b643
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-07-02 12:17:39 +0000

    EquipmentItemView der Nix kann

---
+++ Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/character/view/EquipmentItemView.java
@@ -0,0 +1,39 @@
+package net.sf.anathema.character.equipment.impl.character.view;
+
+import javax.swing.JLabel;
+
+import net.disy.commons.core.model.BooleanModel;
+import net.disy.commons.swing.action.ActionWidgetFactory;
+import net.disy.commons.swing.action.SmartToggleAction;
+import net.sf.anathema.lib.gui.GuiUtilities;
+
+import com.l2fprod.common.swing.JTaskPaneGroup;
+
+public class EquipmentItemView {
+
+  private final JTaskPaneGroup taskGroup = new JTaskPaneGroup();
+  private final JLabel descriptionLabel = new JLabel();
+
+  public EquipmentItemView() {
+    taskGroup.add(descriptionLabel);
+  }
+
+  public void setItemTitle(String title) {
+    taskGroup.setTitle(title);
+  }
+
+  public void setItemDescription(String text) {
+    descriptionLabel.setText(text);
+    GuiUtilities.revalidate(taskGroup);
+  }
+
+  public BooleanModel addStats(String description) {
+    BooleanModel isSelectedModel = new BooleanModel();
+    taskGroup.add(ActionWidgetFactory.createCheckBox(new SmartToggleAction(isSelectedModel, description)));
+    return isSelectedModel;
+  }
+
+  public JTaskPaneGroup getTaskGroup() {
+    return taskGroup;
+  }
+}
---
+++ Development_Test_Character/src/net/sf/anathema/demo/character/equipment/character/AllDemos.java
@@ -0,0 +1,15 @@
+package net.sf.anathema.demo.character.equipment.character;
+
+import de.jdemo.framework.DemoSuite;
+import de.jdemo.framework.IDemo;
+
+public class AllDemos {
+
+  public static IDemo suite() {
+    DemoSuite suite = new DemoSuite("Demo for net.sf.anathema.demo.character.equipment.character"); //$NON-NLS-1$
+    //$JDemo-BEGIN$
+    suite.addDemo(new DemoSuite(EquipmentItemViewDemo.class));
+    //$JDemo-END$
+    return suite;
+  }
+}
