5aea718460649ab0c635ae76385c1ac3abd2c2c1
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-07-16 09:49:55 +0000

    IPdfContentEncoder als abgespeckte Version eingeführt

--- Character_Reporting/src/net/sf/anathema/character/reporting/sheet/common/IPdfContentBoxEncoder.java
+++ Character_Reporting/src/net/sf/anathema/character/reporting/sheet/common/IPdfContentBoxEncoder.java
@@ -1,14 +1,6 @@
 package net.sf.anathema.character.reporting.sheet.common;
 
-import net.sf.anathema.character.generic.character.IGenericCharacter;
-import net.sf.anathema.character.reporting.util.Bounds;
-
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.pdf.PdfContentByte;
-
-public interface IPdfContentBoxEncoder {
+public interface IPdfContentBoxEncoder extends IPdfContentEncoder {
   
   public String getHeaderKey();
-
-  public void encode(PdfContentByte directContent, IGenericCharacter character, Bounds bounds) throws DocumentException;
 }
