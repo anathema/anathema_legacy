6555c0af47351699f2d5ad29fd7130351e46e039
Urs Reupke <ursreupke@gmail.com>
2006-07-19 19:43:21 +0000

    Add Menu-Action

--- Platform/src/net/sf/anathema/framework/view/AnathemaToolBar.java
+++ Platform/src/net/sf/anathema/framework/view/AnathemaToolBar.java
@@ -1,9 +1,13 @@
 package net.sf.anathema.framework.view;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.Action;
+import javax.swing.Icon;
 import javax.swing.JButton;
+import javax.swing.JPopupMenu;
 import javax.swing.JToolBar;
 
 import net.sf.anathema.framework.view.toolbar.IAnathemaToolbar;
@@ -15,6 +19,7 @@ public class AnathemaToolBar implements IAnathemaToolbar {
   public AnathemaToolBar() {
     this.toolBar = new JToolBar();
     this.toolBar.setFloatable(false);
+//    this.toolBar.setRollover(true);
   }
 
   public JToolBar getComponent() {
@@ -28,5 +33,20 @@ public class AnathemaToolBar implements IAnathemaToolbar {
       JButton button = toolBar.add(action);
       button.setPreferredSize(new Dimension(24, 24));
     }
+  }
+
+  public void addMenu(Icon buttonIcon, Action... menuActions) {
+    final JButton button = new JButton(buttonIcon);
+    final JPopupMenu menu = new JPopupMenu();
+    for (Action action : menuActions) {
+      menu.add(action);
+    }
+    button.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent ae) {
+        menu.show(button, 0, button.getHeight());
+      }
+    });
+    button.add(menu);
+    toolBar.add(button);
   }
 }
--- Platform/src/net/sf/anathema/framework/view/toolbar/IAnathemaToolbar.java
+++ Platform/src/net/sf/anathema/framework/view/toolbar/IAnathemaToolbar.java
@@ -1,8 +1,11 @@
 package net.sf.anathema.framework.view.toolbar;
 
 import javax.swing.Action;
+import javax.swing.Icon;
 
 public interface IAnathemaToolbar {
 
   public void addTools(Action... action);
+
+  public void addMenu(Icon buttonIcon, Action... menuActions);
 }
