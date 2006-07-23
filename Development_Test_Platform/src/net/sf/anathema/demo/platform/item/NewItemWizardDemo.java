3a85e956b1cf813ec10a7454e4d72268b95682fd
Urs Reupke <ursreupke@gmail.com>
2006-07-23 08:26:49 +0000

    Fixed demo

--- Development_Test_Platform/src/net/sf/anathema/demo/platform/item/NewItemWizardDemo.java
+++ Development_Test_Platform/src/net/sf/anathema/demo/platform/item/NewItemWizardDemo.java
@@ -1,17 +1,19 @@
 package net.sf.anathema.demo.platform.item;
 
-import net.disy.commons.core.message.IBasicMessage;
-import net.disy.commons.swing.dialog.core.IPageContent;
-import net.sf.anathema.lib.gui.wizard.AbstractAnathemaWizardPage;
+import net.sf.anathema.demo.platform.repository.tree.DemoResources;
 import net.sf.anathema.lib.gui.wizard.AnathemaWizardDialog;
 import net.sf.anathema.lib.gui.wizard.IAnathemaWizardPage;
-import net.sf.anathema.lib.gui.wizard.workflow.CheckInputListener;
 import de.jdemo.extensions.SwingDemoCase;
 
 public class NewItemWizardDemo extends SwingDemoCase {
 
   public void demo() {
-   
+    DemoResources resources = new DemoResources();
+    IAnathemaWizardPage page = new SelectItemTypePage(resources, new INewItemWizardModel() {
+      
+    }, new ISelectItemTypeView() {
+      
+    });
     show(new AnathemaWizardDialog(null, page).getDialog().getWindow());
   }
 }
