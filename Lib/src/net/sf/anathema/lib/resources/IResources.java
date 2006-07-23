4ca4eaee0b6f228e5e7e56ba4b0fda9f2241acb4
Urs Reupke <ursreupke@gmail.com>
2006-07-23 18:01:55 +0000

    Cleanup

--- Lib/src/net/sf/anathema/lib/resources/IResources.java
+++ Lib/src/net/sf/anathema/lib/resources/IResources.java
@@ -2,7 +2,6 @@ package net.sf.anathema.lib.resources;
 
 import net.disy.commons.swing.image.IImageProvider;
 
-
 public interface IResources extends IStringResourceHandler, IImageProvider {
   // nothing to do
 }
