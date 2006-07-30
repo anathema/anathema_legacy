cf0f65ac726163937189d89bcd662340dd033636
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-07-30 10:19:26 +0000

    Nicht mehr verwendeter Code entfernt

--- Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/character/model/EquipmentTemplate.java
+++ Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/character/model/EquipmentTemplate.java
@@ -1,14 +1,10 @@
 package net.sf.anathema.character.equipment.impl.character.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.sf.anathema.character.equipment.character.model.IEquipmentTemplate;
 import net.sf.anathema.character.generic.equipment.weapon.IEquipmentStats;
 
 public class EquipmentTemplate implements IEquipmentTemplate {
 
-  private List<IEquipmentStats> printedEquipments = new ArrayList<IEquipmentStats>();
   private final IEquipmentStats[] equipments;
   private final String description;
   private final String name;
@@ -29,14 +25,5 @@ public class EquipmentTemplate implements IEquipmentTemplate {
 
   public String getName() {
     return name;
-  }
-
-  public void setPrintEnabled(IEquipmentStats equipment, boolean enabled) {
-    if (enabled) {
-      printedEquipments.add(equipment);
-    }
-    else {
-      printedEquipments.remove(equipment);
-    }
   }
 }
