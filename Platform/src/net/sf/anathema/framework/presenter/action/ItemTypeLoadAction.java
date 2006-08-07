ffb75a39d1702a7cd1743ae10b23b8493867814e
Urs Reupke <ursreupke@gmail.com>
2006-08-07 19:35:28 +0000

    Consolidation ItemTypeLoadAction

--- Platform/src/net/sf/anathema/framework/presenter/action/ItemTypeLoadAction.java
+++ Platform/src/net/sf/anathema/framework/presenter/action/ItemTypeLoadAction.java
@@ -1,9 +1,7 @@
 package net.sf.anathema.framework.presenter.action;
 
 import java.awt.Component;
-import java.awt.Cursor;
 import java.io.File;
-import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +9,6 @@ import java.util.List;
 import javax.swing.Action;
 import javax.swing.Icon;
 
-import net.disy.commons.core.io.IOUtilities;
 import net.disy.commons.core.message.Message;
 import net.disy.commons.core.progress.IProgressMonitor;
 import net.disy.commons.core.progress.IRunnableWithProgress;
@@ -22,9 +19,7 @@ import net.sf.anathema.framework.extension.IAnathemaExtension;
 import net.sf.anathema.framework.item.IItemType;
 import net.sf.anathema.framework.message.MessageUtilities;
 import net.sf.anathema.framework.presenter.ItemManagementModelAdapter;
-import net.sf.anathema.framework.presenter.item.ItemTypeViewPropertiesExtensionPoint;
 import net.sf.anathema.framework.presenter.resources.PlatformUI;
-import net.sf.anathema.framework.presenter.view.IItemTypeViewProperties;
 import net.sf.anathema.framework.repository.IItem;
 import net.sf.anathema.framework.repository.IRepositoryFileChooser;
 import net.sf.anathema.framework.repository.access.IRepositoryReadAccess;
@@ -34,29 +29,10 @@ import net.sf.anathema.lib.exception.AnathemaException;
 import net.sf.anathema.lib.registry.IRegistry;
 import net.sf.anathema.lib.resources.IResources;
 
-public final class ItemTypeLoadAction extends SmartAction {
+public final class ItemTypeLoadAction extends AbstractItemAction {
 
-  private final IResources resources;
-  private final IAnathemaModel anathemaModel;
   private final IItemType itemType;
   private final RepositoryFileChooserPropertiesExtensionPoint propertiesExtensionPoint;
-
-  public static Action createMenuAction(IAnathemaModel anathemaModel, IItemType itemType, IResources resources) {
-    IRegistry<String, IAnathemaExtension> extensionPointRegistry = anathemaModel.getExtensionPointRegistry();
-    RepositoryFileChooserPropertiesExtensionPoint repositoryExtensionPoint = (RepositoryFileChooserPropertiesExtensionPoint) extensionPointRegistry.get(RepositoryFileChooserPropertiesExtensionPoint.ID);
-    ItemTypeViewPropertiesExtensionPoint itemExtensionPoint = (ItemTypeViewPropertiesExtensionPoint) extensionPointRegistry.get(ItemTypeViewPropertiesExtensionPoint.ID);
-    IItemTypeViewProperties properties = itemExtensionPoint.get(itemType);
-    SmartAction action = new ItemTypeLoadAction(anathemaModel, itemType, resources, repositoryExtensionPoint);
-    action.setName(properties == null ? itemType.getId() : properties.getPrintName());
-    return action;
-  }
-
-  private static SmartAction createToolAction(IAnathemaModel anathemaModel, IItemType itemType, IResources resources) {
-    IRegistry<String, IAnathemaExtension> extensionPointRegistry = anathemaModel.getExtensionPointRegistry();
-    RepositoryFileChooserPropertiesExtensionPoint repositoryExtensionPoint = (RepositoryFileChooserPropertiesExtensionPoint) extensionPointRegistry.get(RepositoryFileChooserPropertiesExtensionPoint.ID);
-    SmartAction action = new ItemTypeLoadAction(anathemaModel, itemType, resources, repositoryExtensionPoint);
-    return action;
-  }
 
   public static Action[] createToolActions(IAnathemaModel model, IResources resources) {
     List<Action> actions = new ArrayList<Action>();
@@ -66,6 +42,13 @@ public final class ItemTypeLoadAction extends SmartAction {
       actions.add(action);
     }
     return actions.toArray(new Action[actions.size()]);
+  }
+
+  private static SmartAction createToolAction(IAnathemaModel anathemaModel, IItemType itemType, IResources resources) {
+    IRegistry<String, IAnathemaExtension> extensionPointRegistry = anathemaModel.getExtensionPointRegistry();
+    RepositoryFileChooserPropertiesExtensionPoint repositoryExtensionPoint = (RepositoryFileChooserPropertiesExtensionPoint) extensionPointRegistry.get(RepositoryFileChooserPropertiesExtensionPoint.ID);
+    SmartAction action = new ItemTypeLoadAction(anathemaModel, itemType, resources, repositoryExtensionPoint);
+    return action;
   }
 
   public static Icon getButtonIcon(IResources resources) {
@@ -81,10 +64,9 @@ public final class ItemTypeLoadAction extends SmartAction {
       IItemType itemType,
       IResources resources,
       RepositoryFileChooserPropertiesExtensionPoint propertiesExtensionPoint) {
+    super(anathemaModel, resources);
     this.itemType = itemType;
     this.propertiesExtensionPoint = propertiesExtensionPoint;
-    this.resources = resources;
-    this.anathemaModel = anathemaModel;
     adjustEnabled();
     anathemaModel.getItemManagement().addListener(new ItemManagementModelAdapter() {
       @Override
@@ -100,39 +82,37 @@ public final class ItemTypeLoadAction extends SmartAction {
   }
 
   private void adjustEnabled() {
-    setEnabled(anathemaModel.getRepository().containsClosed(itemType));
+    setEnabled(getAnathemaModel().getRepository().containsClosed(itemType));
   }
 
   @Override
   protected void execute(Component parentComponent) {
-    parentComponent.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-    InputStream stream = null;
     try {
-      IRepositoryFileChooser fileChooser = new RepositoryFileChooser(anathemaModel.getRepository()
-          .getPrintNameFileAccess(), anathemaModel.getItemManagement(), propertiesExtensionPoint);
+      IRepositoryFileChooser fileChooser = new RepositoryFileChooser(getAnathemaModel().getRepository()
+          .getPrintNameFileAccess(), getAnathemaModel().getItemManagement(), propertiesExtensionPoint);
       final File file = fileChooser.getRepositoryFile(itemType);
-      final IRepositoryReadAccess readAccess = anathemaModel.getRepository().openReadAccess(
+      final IRepositoryReadAccess readAccess = getAnathemaModel().getRepository().openReadAccess(
           itemType,
           new IFileProvider() {
             public File getFile() {
               return file;
             }
           });
-      final String itemTypeDisplay = resources.getString("ItemType." + itemType.getId() + ".PrintName"); //$NON-NLS-1$ //$NON-NLS-2$
-      String title = resources.getString(
+      final String itemTypeDisplay = getResources().getString("ItemType." + itemType.getId() + ".PrintName"); //$NON-NLS-1$ //$NON-NLS-2$
+      String title = getResources().getString(
           "AnathemaPersistence.LoadAction.Progress.Title", new Object[] { itemTypeDisplay }); //$NON-NLS-1$
       new ProgressMonitorDialog(parentComponent, title).run(false, new IRunnableWithProgress() {
         public void run(IProgressMonitor monitor) throws InterruptedException, InvocationTargetException {
           try {
-            monitor.beginTask(resources.getString("AnathemaPersistence.LoadAction.Progress.Model", //$NON-NLS-1$
+            monitor.beginTask(getResources().getString("AnathemaPersistence.LoadAction.Progress.Model", //$NON-NLS-1$
                 new Object[] { itemTypeDisplay }), IProgressMonitor.UNKNOWN);
-            IItem item = anathemaModel.getPersisterRegistry().get(itemType).load(readAccess);
+            IItem item = getAnathemaModel().getPersisterRegistry().get(itemType).load(readAccess);
             if (item == null) {
               return;
             }
-            monitor.beginTask(resources.getString("AnathemaPersistence.LoadAction.Progress.View", //$NON-NLS-1$
+            monitor.beginTask(getResources().getString("AnathemaPersistence.LoadAction.Progress.View", //$NON-NLS-1$
                 new Object[] { item.getDisplayName() }), IProgressMonitor.UNKNOWN);
-            anathemaModel.getItemManagement().addItem(item);
+            getAnathemaModel().getItemManagement().addItem(item);
           }
           catch (AnathemaException e) {
             throw new InvocationTargetException(e);
@@ -141,26 +121,12 @@ public final class ItemTypeLoadAction extends SmartAction {
       });
     }
     catch (InvocationTargetException e) {
-      String message = resources.getString("AnathemaPersistence.LoadAction.Message.Error"); //$NON-NLS-1$
+      String message = getResources().getString("AnathemaPersistence.LoadAction.Message.Error"); //$NON-NLS-1$
       MessageUtilities.indicateMessage(getClass(), parentComponent, new Message(message, e.getCause()));
     }
     catch (Throwable e) {
-      String message = resources.getString("AnathemaPersistence.LoadAction.Message.Error"); //$NON-NLS-1$
+      String message = getResources().getString("AnathemaPersistence.LoadAction.Message.Error"); //$NON-NLS-1$
       MessageUtilities.indicateMessage(getClass(), parentComponent, new Message(message, e));
     }
-    finally {
-      IOUtilities.close(stream);
-      parentComponent.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-    }
-  }
-
-  private static IItemType[] collectItemTypes(IAnathemaModel model) {
-    List<IItemType> types = new ArrayList<IItemType>();
-    for (IItemType type : model.getItemTypeRegistry().getAllItemTypes()) {
-      if (type.supportsRepository()) {
-        types.add(type);
-      }
-    }
-    return types.toArray(new IItemType[types.size()]);
   }
 }
