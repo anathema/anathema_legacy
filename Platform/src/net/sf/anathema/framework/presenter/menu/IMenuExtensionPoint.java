864f5eb99bc276ddbbc74cc0080876a509bfd6d8
Urs Reupke <ursreupke@gmail.com>
2006-08-08 05:16:06 +0000

    Removed unused extensionpoint

--- Platform/src/net/sf/anathema/framework/presenter/menu/IMenuExtensionPoint.java
+++ Platform/src/net/sf/anathema/framework/presenter/menu/IMenuExtensionPoint.java
@@ -1,9 +1,8 @@
 package net.sf.anathema.framework.presenter.menu;
 
 public interface IMenuExtensionPoint {
-  public static final String NEW_MENU_EXTENSION_POINT_ID = "Menu.New"; //$NON-NLS-1$
   public static final String EXTRA_MENU_EXTENSION_POINT_ID = "Menu.Extra"; //$NON-NLS-1$
-  
+
   public IMenuItem[] getMenuItems();
 
   public void addMenuItem(IMenuItem item);
