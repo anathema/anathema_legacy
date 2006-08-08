8ece8c4f02cc47f2c8eb63bb3fd3aa83312af4f0
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-08-08 17:39:00 +0000

    Missing files

---
+++ Character_Generic_Framework/src/net/sf/anathema/character/generic/framework/additionaltemplate/model/IBasicTrait.java
@@ -0,0 +1,16 @@
+package net.sf.anathema.character.generic.framework.additionaltemplate.model;
+
+public interface IBasicTrait {
+
+  public int getCreationValue();
+
+  public int getExperiencedValue();
+
+  public int getAbsoluteMinValue();
+
+  public int getCreationCalculationValue();
+
+  public int getExperiencedCalculationValue();
+
+  public boolean isLowerable();
+}
---
+++ Character_Library/src/net/sf/anathema/character/library/trait/ITrait.java
@@ -0,0 +1,29 @@
+package net.sf.anathema.character.library.trait;
+
+import net.sf.anathema.character.generic.framework.additionaltemplate.model.IBasicTrait;
+import net.sf.anathema.character.generic.traits.IGenericTrait;
+import net.sf.anathema.character.library.trait.subtrait.ISubTraitContainer;
+import net.sf.anathema.lib.control.intvalue.IIntValueChangedListener;
+
+public interface ITrait extends IBasicTrait, IGenericTrait {
+
+  public int getInitialValue();
+
+  public int getMaximalValue();
+
+  public void addCreationPointListener(IIntValueChangedListener listener);
+
+  public void removeCreationPointListener(IIntValueChangedListener listener);
+
+  public void addCurrentValueListener(IIntValueChangedListener listener);
+
+  public void removeCurrentValueListener(IIntValueChangedListener listener);
+
+  public int getCalculationValue();
+
+  public int getZeroCalculationValue();
+
+  public boolean isCreationLearned();
+
+  public ISubTraitContainer createSpecialtiesContainer();
+}
