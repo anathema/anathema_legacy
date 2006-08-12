7e1e05861d48d982774ee974fbada5b686641af5
Urs Reupke <ursreupke@gmail.com>
2006-08-12 09:39:29 +0000

    New toolbar icons, moved toolbar icons to main folder

--- Platform/src/net/sf/anathema/framework/presenter/resources/PlatformUI.java
+++ Platform/src/net/sf/anathema/framework/presenter/resources/PlatformUI.java
@@ -12,22 +12,22 @@ public class PlatformUI extends AbstractUI {
   }
 
   public Icon getPDFTaskBarIcon() {
-    return getIcon("tools/TaskBarPDF24.png"); //$NON-NLS-1$
+    return getIcon("TaskBarPDF24.png"); //$NON-NLS-1$
   }
 
   public Icon getSaveTaskBarIcon() {
-    return getIcon("tools/TaskBarSave24.png"); //$NON-NLS-1$
+    return getIcon("TaskBarSave24.png"); //$NON-NLS-1$
   }
 
   public Icon getSaveAllTaskBarIcon() {
-    return getIcon("tools/TaskBarSaveAll24.png"); //$NON-NLS-1$
+    return getIcon("TaskBarSaveAll24.png"); //$NON-NLS-1$
   }
 
   public Icon getNewToolBarIcon() {
-    return getIcon("tools/TaskBarNewArrow24.png"); //$NON-NLS-1$
+    return getIcon("TaskBarNewArrow24.png"); //$NON-NLS-1$
   }
 
   public Icon getLoadToolBarIcon() {
-    return getIcon("tools/TaskBarOpenArrow24.png"); //$NON-NLS-1$;
+    return getIcon("TaskBarOpenArrow24.png"); //$NON-NLS-1$;
   }
 }
