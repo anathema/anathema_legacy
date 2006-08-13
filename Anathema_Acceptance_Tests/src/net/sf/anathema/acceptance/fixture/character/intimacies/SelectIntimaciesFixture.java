2c3f5c4bd3cade0952003e3306778508c9fa0d3a
Urs Reupke <ursreupke@gmail.com>
2006-08-13 09:55:07 +0000

    Tests for intimacy-BP-bug

---
+++ Anathema_Acceptance_Tests/src/net/sf/anathema/acceptance/fixture/character/intimacies/SelectIntimaciesFixture.java
@@ -0,0 +1,12 @@
+package net.sf.anathema.acceptance.fixture.character.intimacies;
+
+public class SelectIntimaciesFixture extends AbstractIntimaciesRowEntryFixture {
+  
+  public String name;
+
+  @Override
+  public void enterRow() throws Exception {
+   getModel().setCurrentName(name);
+   getModel().commitSelection();
+  }
+}
