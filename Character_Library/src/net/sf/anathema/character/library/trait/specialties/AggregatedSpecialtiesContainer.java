99dbe96e606a1b4f1bdc9b2d6d47764568d2275f
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-08-22 17:19:20 +0000

    Preparing craft specialties

---
+++ Character_Library/src/net/sf/anathema/character/library/trait/specialties/AggregatedSpecialtiesContainer.java
@@ -0,0 +1,54 @@
+package net.sf.anathema.character.library.trait.specialties;
+
+import net.sf.anathema.character.library.trait.subtrait.ISubTrait;
+import net.sf.anathema.character.library.trait.subtrait.ISubTraitContainer;
+import net.sf.anathema.character.library.trait.subtrait.ISubTraitListener;
+
+public class AggregatedSpecialtiesContainer implements ISubTraitContainer {
+
+  public ISubTrait addSubTrait(String subName) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void addSubTraitListener(ISubTraitListener listener) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public int getCreationDotTotal() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public int getCurrentDotTotal() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public int getExperienceDotTotal() {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  public ISubTrait[] getSubTraits() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public boolean isRemovable(ISubTrait subTrait) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  public void removeSubTrait(ISubTrait specialty) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public void removeSubTraitListener(ISubTraitListener listener) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
