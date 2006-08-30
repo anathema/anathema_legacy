55c74c0ab47e1083642cfa1ea4e23efcf21ce20f
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-08-30 12:49:32 +0000

    Hinzufügen von IEquipmentStats zum EditTemplate

--- Character_Equipment/src/net/sf/anathema/character/equipment/item/model/IEquipmentTemplateEditModel.java
+++ Character_Equipment/src/net/sf/anathema/character/equipment/item/model/IEquipmentTemplateEditModel.java
@@ -1,14 +1,18 @@
 package net.sf.anathema.character.equipment.item.model;
 
+import net.sf.anathema.character.generic.equipment.weapon.IEquipmentStats;
+import net.sf.anathema.character.generic.rules.IExaltedRuleSet;
 import net.sf.anathema.framework.itemdata.model.IItemDescription;
 
 public interface IEquipmentTemplateEditModel {
 
+  public void addStatistics(IExaltedRuleSet ruleSet, IEquipmentStats stats);
+
   public IItemDescription getDescription();
 
+  public IEquipmentStats[] getStats(IExaltedRuleSet ruleSet);
+
   public boolean isDirty();
-
-  public boolean isEditing(String templateId);
-
+  
   public void setEditTemplate(String templateId);
 }
--- Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/item/model/EquipmentTemplateEditModel.java
+++ Character_Equipment_Impl/src/net/sf/anathema/character/equipment/impl/item/model/EquipmentTemplateEditModel.java
@@ -1,19 +1,26 @@
 package net.sf.anathema.character.equipment.impl.item.model;
 
+import java.util.List;
+
 import net.disy.commons.core.util.Ensure;
 import net.disy.commons.core.util.ObjectUtilities;
 import net.sf.anathema.character.equipment.item.model.IEquipmentDatabase;
 import net.sf.anathema.character.equipment.item.model.IEquipmentTemplateEditModel;
 import net.sf.anathema.character.equipment.template.IEquipmentTemplate;
+import net.sf.anathema.character.generic.equipment.weapon.IEquipmentStats;
+import net.sf.anathema.character.generic.impl.rules.ExaltedRuleSet;
+import net.sf.anathema.character.generic.rules.IExaltedRuleSet;
 import net.sf.anathema.framework.itemdata.model.IItemDescription;
 import net.sf.anathema.framework.itemdata.model.ItemDescription;
 import net.sf.anathema.framework.styledtext.model.ITextPart;
+import net.sf.anathema.lib.collection.MultiEntryMap;
 
 public class EquipmentTemplateEditModel implements IEquipmentTemplateEditModel {
 
   private final IItemDescription description = new ItemDescription();
   private final IEquipmentDatabase database;
   private IEquipmentTemplate editedTemplate;
+  private final MultiEntryMap<IExaltedRuleSet, IEquipmentStats> statsByRuleSet = new MultiEntryMap<IExaltedRuleSet, IEquipmentStats>();
 
   public EquipmentTemplateEditModel(IEquipmentDatabase database) {
     this.database = database;
@@ -27,14 +34,25 @@ public class EquipmentTemplateEditModel implements IEquipmentTemplateEditModel {
     Ensure.ensureArgumentNotNull(templateId);
     editedTemplate = database.loadTemplate(templateId);
     // TODO Fehlerbehandlung bei Template nicht gefunden
-    getDescription().getName().setText(null);
-    getDescription().getContent().setText(new ITextPart[0]);
+    getDescription().getName().setText(editedTemplate.getName());
+    getDescription().getContent().setText(editedTemplate.getDescription());
+    statsByRuleSet.clear();
+    for (ExaltedRuleSet ruleSet : ExaltedRuleSet.values()) {
+      statsByRuleSet.add(ruleSet, editedTemplate.getStats(ruleSet));
+    }
+    fireStatsChangedEvent();
+  }
+
+  private void fireStatsChangedEvent() {
+    // TODO Listening für Stats
   }
 
   public void setNewTemplate() {
     editedTemplate = null;
-    getDescription().getName().setText(editedTemplate.getName());
-    getDescription().getContent().setText(editedTemplate.getDescription());
+    getDescription().getName().setText(null);
+    getDescription().getContent().setText(new ITextPart[0]);
+    statsByRuleSet.clear();
+    fireStatsChangedEvent();
   }
 
   public boolean isDirty() {
@@ -45,7 +63,13 @@ public class EquipmentTemplateEditModel implements IEquipmentTemplateEditModel {
         || !ObjectUtilities.equals(editedTemplate.getDescription(), getDescription().getContent().getText());
   }
 
-  public boolean isEditing(String templateId) {
-    return templateId == null ? editedTemplate == null : templateId.equals(editedTemplate.getName());
+  public void addStatistics(IExaltedRuleSet ruleSet, IEquipmentStats stats) {
+    statsByRuleSet.add(ruleSet, stats);
+    fireStatsChangedEvent();
+  }
+
+  public IEquipmentStats[] getStats(IExaltedRuleSet ruleSet) {
+    List<IEquipmentStats> allStats = statsByRuleSet.get(ruleSet);
+    return allStats.toArray(new IEquipmentStats[allStats.size()]);
   }
 }
