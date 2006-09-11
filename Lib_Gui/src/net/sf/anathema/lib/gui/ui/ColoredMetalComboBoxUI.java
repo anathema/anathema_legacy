e2eb2b6de2362409e67857d16b769ace16e22bf7
Urs Reupke <ursreupke@gmail.com>
2006-09-11 12:46:22 +0000

    Cleanup ColoredComboBox

--- Lib_Gui/src/net/sf/anathema/lib/gui/ui/ColoredMetalComboBoxUI.java
+++ Lib_Gui/src/net/sf/anathema/lib/gui/ui/ColoredMetalComboBoxUI.java
@@ -1,20 +1,20 @@
 package net.sf.anathema.lib.gui.ui;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
-import javax.swing.JPanel;
-import javax.swing.ListCellRenderer;
-import javax.swing.UIManager;
 import javax.swing.plaf.metal.MetalComboBoxUI;
 import javax.swing.plaf.metal.MetalLookAndFeel;
 import javax.swing.plaf.metal.OceanTheme;
 
-import sun.swing.DefaultLookup;
-
 public class ColoredMetalComboBoxUI extends MetalComboBoxUI {
+
+  private final Color disabledForegroundColor;
+
+  public ColoredMetalComboBoxUI(Color disabledForegroundColor) {
+    this.disabledForegroundColor = disabledForegroundColor;
+  }
 
   @Override
   public void paintCurrentValue(Graphics g, Rectangle bounds, boolean focused) {
@@ -23,50 +23,12 @@ public class ColoredMetalComboBoxUI extends MetalComboBoxUI {
       bounds.y += 2;
       bounds.width -= 3;
       bounds.height -= 4;
-      paintBasicComboBoxUI(g, bounds, focused);
+      new ComboBoxPainter(disabledForegroundColor, comboBox, listBox, currentValuePane).paintBasicComboBoxUI(
+          g,
+          bounds,
+          focused,
+          isPopupVisible(comboBox),
+          this);
     }
-    else if (g == null || bounds == null) {
-      throw new NullPointerException("Must supply a non-null Graphics and Rectangle"); //$NON-NLS-1$
-    }
-  }
-
-  private final Color disabledForegroundColor;
-
-  public ColoredMetalComboBoxUI(Color disabledForegroundColor) {
-    this.disabledForegroundColor = disabledForegroundColor;
-  }
-
-  protected void paintBasicComboBoxUI(Graphics g, Rectangle bounds, boolean focussed) {
-    ListCellRenderer renderer = comboBox.getRenderer();
-    Component c;
-
-    if (focussed && !isPopupVisible(comboBox)) {
-      c = renderer.getListCellRendererComponent(listBox, comboBox.getSelectedItem(), -1, true, false);
-    }
-    else {
-      c = renderer.getListCellRendererComponent(listBox, comboBox.getSelectedItem(), -1, false, false);
-      c.setBackground(UIManager.getColor("ComboBox.background")); //$NON-NLS-1$
-    }
-    c.setFont(comboBox.getFont());
-    if (focussed && !isPopupVisible(comboBox)) {
-      c.setForeground(listBox.getSelectionForeground());
-      c.setBackground(listBox.getSelectionBackground());
-    }
-    else {
-      if (comboBox.isEnabled()) {
-        c.setForeground(comboBox.getForeground());
-        c.setBackground(comboBox.getBackground());
-      }
-      else {
-        c.setForeground(disabledForegroundColor);
-        c.setBackground(DefaultLookup.getColor(comboBox, this, "ComboBox.disabledBackground", null)); //$NON-NLS-1$
-      }
-    }
-    // Fix for 4238829: should lay out the JPanel.
-    boolean shouldValidate = false;
-    if (c instanceof JPanel) {
-      shouldValidate = true;
-    }
-    currentValuePane.paintComponent(g, c, comboBox, bounds.x, bounds.y, bounds.width, bounds.height, shouldValidate);
   }
 }
--- Lib_Gui/src/net/sf/anathema/lib/gui/ui/ColoredWindowsComboBoxUI.java
+++ Lib_Gui/src/net/sf/anathema/lib/gui/ui/ColoredWindowsComboBoxUI.java
@@ -1,16 +1,11 @@
 package net.sf.anathema.lib.gui.ui;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
 
-import javax.swing.JPanel;
-import javax.swing.ListCellRenderer;
 import javax.swing.UIManager;
-
-import sun.swing.DefaultLookup;
 
 import com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel;
 import com.sun.java.swing.plaf.windows.WindowsComboBoxUI;
@@ -21,40 +16,6 @@ public class ColoredWindowsComboBoxUI extends WindowsComboBoxUI {
 
   public ColoredWindowsComboBoxUI(Color disabledForegroundColor) {
     this.disabledForegroundColor = disabledForegroundColor;
-  }
-
-  protected void paintBasicComboBoxUI(Graphics g, Rectangle bounds, boolean focussed) {
-    ListCellRenderer renderer = comboBox.getRenderer();
-    Component c;
-
-    if (focussed && !isPopupVisible(comboBox)) {
-      c = renderer.getListCellRendererComponent(listBox, comboBox.getSelectedItem(), -1, true, false);
-    }
-    else {
-      c = renderer.getListCellRendererComponent(listBox, comboBox.getSelectedItem(), -1, false, false);
-      c.setBackground(UIManager.getColor("ComboBox.background")); //$NON-NLS-1$
-    }
-    c.setFont(comboBox.getFont());
-    if (focussed && !isPopupVisible(comboBox)) {
-      c.setForeground(listBox.getSelectionForeground());
-      c.setBackground(listBox.getSelectionBackground());
-    }
-    else {
-      if (comboBox.isEnabled()) {
-        c.setForeground(comboBox.getForeground());
-        c.setBackground(comboBox.getBackground());
-      }
-      else {
-        c.setForeground(disabledForegroundColor);
-        c.setBackground(DefaultLookup.getColor(comboBox, this, "ComboBox.disabledBackground", null)); //$NON-NLS-1$
-      }
-    }
-    // Fix for 4238829: should lay out the JPanel.
-    boolean shouldValidate = false;
-    if (c instanceof JPanel) {
-      shouldValidate = true;
-    }
-    currentValuePane.paintComponent(g, c, comboBox, bounds.x, bounds.y, bounds.width, bounds.height, shouldValidate);
   }
 
   @Override
@@ -71,10 +32,15 @@ public class ColoredWindowsComboBoxUI extends WindowsComboBoxUI {
       bounds.width -= 2;
       bounds.height -= 2;
     }
-    paintBasicComboBoxUI(g, bounds, focussed);
+    new ComboBoxPainter(disabledForegroundColor, comboBox, listBox, currentValuePane).paintBasicComboBoxUI(
+        g,
+        bounds,
+        focussed,
+        isPopupVisible(comboBox),
+        this);
   }
 
-  private synchronized boolean isXP() {
+  private boolean isXP() {
     Toolkit toolkit = Toolkit.getDefaultToolkit();
     boolean themeActive = Boolean.TRUE.equals(toolkit.getDesktopProperty("win.xpstyle.themeActive")); //$NON-NLS-1$
     boolean noXP = System.getProperty("swing.noxp") != null; //$NON-NLS-1$
