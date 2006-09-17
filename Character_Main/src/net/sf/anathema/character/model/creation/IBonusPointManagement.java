52c16882ef407bccc522311489885aa7d6c20519
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-09-17 13:01:36 +0000

    Ich hoffe mein Workspace geht nun tatsächlich

--- Character_Main/src/net/sf/anathema/character/model/creation/IBonusPointManagement.java
+++ Character_Main/src/net/sf/anathema/character/model/creation/IBonusPointManagement.java
@@ -1,11 +1,27 @@
 package net.sf.anathema.character.model.creation;
 
+import net.sf.anathema.character.generic.template.points.AttributeGroupPriority;
 import net.sf.anathema.character.presenter.overview.IAdditionalSpendingModel;
 import net.sf.anathema.character.presenter.overview.IOverviewModel;
+import net.sf.anathema.character.presenter.overview.ISpendingModel;
 
 public interface IBonusPointManagement {
 
   public void recalculate();
+
+  public ISpendingModel getBackgroundModel();
+
+  public ISpendingModel getDefaultAbilityModel();
+
+  public ISpendingModel getFavoredAbilityModel();
+
+  public ISpendingModel getFavoredAbilityPickModel();
+
+  public ISpendingModel getAttributeModel(AttributeGroupPriority priority);
+
+  public ISpendingModel getFavoredCharmModel();
+
+  public IAdditionalSpendingModel getDefaultCharmModel();
 
   public IAdditionalSpendingModel getTotalModel();
 
