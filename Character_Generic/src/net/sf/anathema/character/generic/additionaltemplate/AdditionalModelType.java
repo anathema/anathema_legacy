141f8ab9837f54a5d87596f6389e2ca46bc9dcdc
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-09-17 15:00:43 +0000

    Irgendwie haben wir wohl ganz viel aufgeräumnt.    ?        ? ?

--- Character_Generic/src/net/sf/anathema/character/generic/additionaltemplate/AdditionalModelType.java
+++ Character_Generic/src/net/sf/anathema/character/generic/additionaltemplate/AdditionalModelType.java
@@ -2,5 +2,5 @@ package net.sf.anathema.character.generic.additionaltemplate;
 
 public enum AdditionalModelType {
 
-  Advantages, Miscellaneous, Magic, Concept, Abilities;
+  Abilities, Advantages, Attributes, Concept, Description, Experience, Magic, Miscellaneous;
 }
