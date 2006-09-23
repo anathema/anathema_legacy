2fa4d99f5f94e41fdbdd6b23b2e4cd57ed0c1ef8
Urs Reupke <ursreupke@gmail.com>
2006-09-23 09:08:53 +0000

    Extends IView

--- Character_Library/src/net/sf/anathema/character/library/overview/IOverviewCategory.java
+++ Character_Library/src/net/sf/anathema/character/library/overview/IOverviewCategory.java
@@ -1,11 +1,10 @@
 package net.sf.anathema.character.library.overview;
 
-import java.awt.Component;
-
+import net.sf.anathema.lib.gui.IView;
 import net.sf.anathema.lib.workflow.labelledvalue.ILabelledAlotmentView;
 import net.sf.anathema.lib.workflow.labelledvalue.IValueView;
 
-public interface IOverviewCategory {
+public interface IOverviewCategory extends IView {
 
   public ILabelledAlotmentView addAlotmentView(String labelText, int maxValueLength);
 
@@ -14,6 +13,4 @@ public interface IOverviewCategory {
   public IValueView<String> addStringValueView(String labelText);
 
   public IAdditionalAlotmentView addAdditionalAlotmentView(String labelText, int maxValueLength);
-
-  public Component getComponent();
 }
