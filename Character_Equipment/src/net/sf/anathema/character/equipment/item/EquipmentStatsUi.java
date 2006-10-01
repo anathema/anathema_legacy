020cff83f2e8c6dd466913972b1a771db21cc1ec
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-10-01 13:42:04 +0000

    Cleanup

--- Character_Equipment/src/net/sf/anathema/character/equipment/item/EquipmentStatsUi.java
+++ Character_Equipment/src/net/sf/anathema/character/equipment/item/EquipmentStatsUi.java
@@ -4,15 +4,10 @@ import javax.swing.Icon;
 
 import net.disy.commons.swing.ui.IObjectUi;
 import net.sf.anathema.character.generic.equipment.weapon.IEquipmentStats;
-import net.sf.anathema.lib.util.IIdentificate;
 
 public final class EquipmentStatsUi implements IObjectUi {
   public String getLabel(Object value) {
-    IIdentificate name = ((IEquipmentStats) value).getName();
-    if (name == null) {
-      return "Default";
-    }
-    return name.getId();
+    return ((IEquipmentStats) value).getName().getId();
   }
 
   public Icon getIcon(Object value) {
