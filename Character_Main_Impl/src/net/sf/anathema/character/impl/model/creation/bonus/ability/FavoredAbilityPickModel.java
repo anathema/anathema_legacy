0e91c9eb5eacc04a5b4f0c9cf0a1faba3d97e89c
Urs Reupke <ursreupke@gmail.com>
2006-11-08 14:37:35 +0000

    Cleanup

--- Character_Main_Impl/src/net/sf/anathema/character/impl/model/creation/bonus/ability/FavoredAbilityPickModel.java
+++ Character_Main_Impl/src/net/sf/anathema/character/impl/model/creation/bonus/ability/FavoredAbilityPickModel.java
@@ -2,13 +2,14 @@ package net.sf.anathema.character.impl.model.creation.bonus.ability;
 
 import net.sf.anathema.character.generic.template.creation.ICreationPoints;
 import net.sf.anathema.character.impl.model.advance.models.AbstractSpendingModel;
+import net.sf.anathema.character.library.trait.IFavorableTraitCostCalculator;
 
 public class FavoredAbilityPickModel extends AbstractSpendingModel {
 
-  private final IAbilityCostCalculator abilityCalculator;
+  private final IFavorableTraitCostCalculator abilityCalculator;
   private final ICreationPoints creationPoints;
 
-  public FavoredAbilityPickModel(IAbilityCostCalculator abilityCalculator, ICreationPoints creationPoints) {
+  public FavoredAbilityPickModel(IFavorableTraitCostCalculator abilityCalculator, ICreationPoints creationPoints) {
     super("Abilities", "FavoredPick"); //$NON-NLS-1$ //$NON-NLS-2$
     this.abilityCalculator = abilityCalculator;
     this.creationPoints = creationPoints;
