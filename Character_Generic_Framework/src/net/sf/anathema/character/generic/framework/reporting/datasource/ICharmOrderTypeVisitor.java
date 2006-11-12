de705763e28d4daefdbaa8a0f68e03ac7e926727
Urs Reupke <ursreupke@gmail.com>
2006-11-12 10:08:43 +0000

    Removed unused classes

--- Character_Generic_Framework/src/net/sf/anathema/character/generic/framework/reporting/datasource/ICharmOrderTypeVisitor.java
+++
@@ -1,9 +0,0 @@
-package net.sf.anathema.character.generic.framework.reporting.datasource;
-
-public interface ICharmOrderTypeVisitor {
-
-  public void visitAlphabet(CharmOrderType type);
-
-  public void visitTreeOrder(CharmOrderType type);
-
-}
