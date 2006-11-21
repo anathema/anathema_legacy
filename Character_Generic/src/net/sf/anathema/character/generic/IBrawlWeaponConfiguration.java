cf2415cc84167335f9fefbd681952f81f7c7be1c
Urs Reupke <ursreupke@gmail.com>
2006-11-21 17:20:31 +0000

    Removed BrawlWeaponConfigs

--- Character_Generic/src/net/sf/anathema/character/generic/IBrawlWeaponConfiguration.java
+++
@@ -1,8 +0,0 @@
-package net.sf.anathema.character.generic;
-
-import net.sf.anathema.character.generic.equipment.IWeaponType;
-
-public interface IBrawlWeaponConfiguration {
-
-  public IWeaponType[] getBrawlWeaponList();
-}
