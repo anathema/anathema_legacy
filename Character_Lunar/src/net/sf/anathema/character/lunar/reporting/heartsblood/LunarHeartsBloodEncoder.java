ad503ca6614cbbb6637c9d97512303932bac2360
Urs Reupke <ursreupke@gmail.com>
2006-11-21 17:16:12 +0000

    Cleanup

--- Character_Lunar/src/net/sf/anathema/character/lunar/reporting/heartsblood/LunarHeartsBloodEncoder.java
+++ Character_Lunar/src/net/sf/anathema/character/lunar/reporting/heartsblood/LunarHeartsBloodEncoder.java
@@ -8,7 +8,6 @@ import net.sf.anathema.character.lunar.heartsblood.HeartsBloodTemplate;
 import net.sf.anathema.character.lunar.heartsblood.presenter.IAnimalForm;
 import net.sf.anathema.character.lunar.heartsblood.presenter.IHeartsBloodModel;
 import net.sf.anathema.character.reporting.sheet.common.IPdfContentBoxEncoder;
-import net.sf.anathema.character.reporting.sheet.pageformat.IVoidStateFormatConstants;
 import net.sf.anathema.character.reporting.sheet.util.statstable.AbstractFixedLineStatsTableEncoder;
 import net.sf.anathema.character.reporting.sheet.util.statstable.IStatsGroup;
 import net.sf.anathema.character.reporting.util.Bounds;
@@ -23,14 +22,12 @@ import com.lowagie.text.pdf.PdfContentByte;
 public class LunarHeartsBloodEncoder extends AbstractFixedLineStatsTableEncoder<IHeartsBloodStats> implements
     IPdfContentBoxEncoder {
 
+  private final IResources resources;
+
   public LunarHeartsBloodEncoder(BaseFont baseFont, IResources resources) {
     super(baseFont);
     this.resources = resources;
   }
-
-  private final IResources resources;
-  private static final int LINE_HEIGHT = IVoidStateFormatConstants.LINE_HEIGHT - 2;
-  private final int columnCount = 1;
 
   public void encode(PdfContentByte directContent, IGenericCharacter character, Bounds bounds) throws DocumentException {
     encodeTable(directContent, character, bounds);
