8269a386608b240da54d881beae42fba565c03aa
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-12-01 11:19:40 +0000

    Specialties cleaned up

--- Character_Main/src/net/sf/anathema/character/presenter/specialty/SpecialtiesAdditionalModel.java
+++ Character_Main/src/net/sf/anathema/character/presenter/specialty/SpecialtiesAdditionalModel.java
@@ -1,18 +1,14 @@
 package net.sf.anathema.character.presenter.specialty;
 
+import net.sf.anathema.character.generic.additionaltemplate.AbstractAdditionalModelAdapter;
 import net.sf.anathema.character.generic.additionaltemplate.AdditionalModelType;
-import net.sf.anathema.character.generic.additionaltemplate.IAdditionalModelBonusPointCalculator;
-import net.sf.anathema.character.generic.additionaltemplate.IAdditionalModelExperienceCalculator;
-import net.sf.anathema.character.generic.additionaltemplate.NullAdditionalModelBonusPointCalculator;
-import net.sf.anathema.character.generic.additionaltemplate.NullAdditionalModelExperienceCalculator;
 import net.sf.anathema.character.generic.character.IGenericTraitCollection;
 import net.sf.anathema.character.generic.framework.additionaltemplate.model.ICharacterModelContext;
 import net.sf.anathema.character.generic.template.additional.IAdditionalTemplate;
 import net.sf.anathema.character.library.trait.specialties.ISpecialtiesConfiguration;
 import net.sf.anathema.character.model.traits.ICoreTraitConfiguration;
-import net.sf.anathema.lib.control.change.IChangeListener;
 
-public class SpecialtiesAdditionalModel implements ISpecialtiesAdditionalModel {
+public class SpecialtiesAdditionalModel extends AbstractAdditionalModelAdapter implements ISpecialtiesAdditionalModel {
 
   private final IAdditionalTemplate additionalTemplate;
   private final ISpecialtiesConfiguration model;
@@ -22,21 +18,11 @@ public class SpecialtiesAdditionalModel implements ISpecialtiesAdditionalModel {
     IGenericTraitCollection traitCollection = context.getTraitCollection();
     this.model = ((ICoreTraitConfiguration) traitCollection).getSpecialtyConfiguration();
   }
-
-  public void addChangeListener(IChangeListener listener) {
-    // nothing to do
-  }
+  
+  //Dirty/Changed-state handled via abilities.
 
   public AdditionalModelType getAdditionalModelType() {
     return AdditionalModelType.Abilities;
-  }
-
-  public IAdditionalModelBonusPointCalculator getBonusPointCalculator() {
-    return new NullAdditionalModelBonusPointCalculator();
-  }
-
-  public IAdditionalModelExperienceCalculator getExperienceCalculator() {
-    return new NullAdditionalModelExperienceCalculator();
   }
 
   public String getTemplateId() {
