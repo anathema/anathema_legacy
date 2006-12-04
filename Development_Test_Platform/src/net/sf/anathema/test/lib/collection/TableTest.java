add621abeb230a9404138f391fe47b1f997aa8ee
Urs Reupke <ursreupke@gmail.com>
2006-12-04 17:49:09 +0000

    Tests for Table

---
+++ Development_Test_Platform/src/net/sf/anathema/test/lib/collection/TableTest.java
@@ -0,0 +1,76 @@
+package net.sf.anathema.test.lib.collection;
+
+import net.sf.anathema.lib.collection.Table;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TableTest {
+
+  private Table<String, String, String> table;
+
+  @Before
+  public void createTable() {
+    this.table = new Table<String, String, String>(String.class);
+  }
+
+  @Test
+  public void testNewTableIsEmpty() throws Exception {
+    Assert.assertEquals(0, table.getPrimaryKeys().size());
+  }
+
+  @Test
+  public void testRetrieveFromNullCell() throws Exception {
+    Assert.assertEquals(null, table.get(null, null));
+  }
+
+  @Test
+  public void testRetrieveFromEmptyCell() throws Exception {
+    Assert.assertEquals(null, table.get("1", "2")); //$NON-NLS-1$ //$NON-NLS-2$
+  }
+
+  @Test
+  public void testStoreAndRetrieve() throws Exception {
+    table.add("1", "2", "3"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    Assert.assertEquals("3", table.get("1", "2")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+  }
+
+  @Test
+  public void testOverwrite() throws Exception {
+    String firstKey = "1"; //$NON-NLS-1$
+    String secondKey = "2"; //$NON-NLS-1$
+    String firstValue = "First"; //$NON-NLS-1$
+    String secondValue = "Second"; //$NON-NLS-1$
+    table.add(firstKey, secondKey, firstValue);
+    table.add(firstKey, secondKey, secondValue);
+    Assert.assertEquals(secondValue, table.get(firstKey, secondKey));
+  }
+
+  @Test
+  public void testStoreAndRetrieveWithDuplicateKey() throws Exception {
+    String string = "1"; //$NON-NLS-1$
+    table.add(string, string, string);
+    Assert.assertEquals(string, table.get(string, string));
+  }
+
+  @Test
+  public void testStoreAndRetrieveWithinSameRow() throws Exception {
+    String string1 = "1"; //$NON-NLS-1$
+    String string2 = "2"; //$NON-NLS-1$
+    table.add(string1, string1, string1);
+    table.add(string1, string2, string2);
+    Assert.assertEquals(string1, table.get(string1, string1));
+    Assert.assertEquals(string2, table.get(string1, string2));
+  }
+
+  @Test
+  public void testStoreAndRetrieveWithinSameColumn() throws Exception {
+    String string1 = "1"; //$NON-NLS-1$
+    String string2 = "2"; //$NON-NLS-1$
+    table.add(string1, string1, string1);
+    table.add(string2, string1, string2);
+    Assert.assertEquals(string1, table.get(string1, string1));
+    Assert.assertEquals(string2, table.get(string2, string1));
+  }
+}
