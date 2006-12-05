ad5c8048ac57dfea76469efd215ecd28a870bd4f
Urs Reupke <ursreupke@gmail.com>
2006-12-05 09:28:42 +0000

    Removed unused classes

--- Development_Test_Platform/src/net/sf/anathema/test/lib/AllLibTests.java
+++ Development_Test_Platform/src/net/sf/anathema/test/lib/AllLibTests.java
@@ -6,7 +6,6 @@ import junit.framework.TestSuite;
 public class AllLibTests {
   public static Test suite() {
     TestSuite suite = new TestSuite("Test for net.sf.anathema.lib"); //$NON-NLS-1$
-    suite.addTest(net.sf.anathema.test.lib.awt.AllTests.suite());
     suite.addTest(net.sf.anathema.test.lib.lang.AllTests.suite());
     suite.addTest(net.sf.anathema.test.lib.lang.clone.AllTests.suite());
     suite.addTest(net.sf.anathema.test.lib.AllTests.suite());
