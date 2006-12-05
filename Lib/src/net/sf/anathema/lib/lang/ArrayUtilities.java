694b61e41b162da290733022cfb5087aa4c92467
Urs Reupke <ursreupke@gmail.com>
2006-12-05 09:40:53 +0000

    Removed duplicate method

--- Lib/src/net/sf/anathema/lib/lang/ArrayUtilities.java
+++ Lib/src/net/sf/anathema/lib/lang/ArrayUtilities.java
@@ -1,6 +1,5 @@
 package net.sf.anathema.lib.lang;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,11 +9,41 @@ import net.sf.anathema.lib.collection.Predicate;
 
 public class ArrayUtilities {
 
-  public static int max(int[] array) {
-    int[] arrayCopy = new int[array.length];
-    System.arraycopy(array, 0, arrayCopy, 0, array.length);
-    Arrays.sort(arrayCopy);
-    return arrayCopy[arrayCopy.length - 1];
+  public static void copyAll(Object[] sourceArray, Object[] targetArray) {
+    System.arraycopy(sourceArray, 0, targetArray, 0, sourceArray.length);
+  }
+
+  public static int[] createIndexArray(int length) {
+    int[] indexArray = new int[length];
+    for (int index = 0; index < length; index++) {
+      indexArray[index] = index;
+    }
+    return indexArray;
+  }
+
+  public static Integer[] createIntegerArray(int maximalValue) {
+    Integer[] ranks = new Integer[Math.abs(maximalValue) + 1];
+    for (int index = 0; index < ranks.length; index++) {
+      ranks[index] = Integer.signum(maximalValue) * index;
+    }
+    return ranks;
+  }
+
+  public static Integer[] createIntegerArray(int minimalValue, int maximalValue) {
+    Ensure.ensureArgumentTrue("MinimalValue must be lower than mximalValue", minimalValue < maximalValue); //$NON-NLS-1$
+    Integer[] ranks = new Integer[maximalValue - minimalValue + 1];
+    for (int index = 0; index < ranks.length; index++) {
+      ranks[index] = minimalValue + index;
+    }
+    return ranks;
+  }
+
+  private static int[] createInvertedIndexArray(int length) {
+    int[] indexArray = new int[length];
+    for (int index = 0; index < length; index++) {
+      indexArray[index] = length - 1 - index;
+    }
+    return indexArray;
   }
 
   public static boolean equals(Object[] first, Object[] second) {
@@ -27,19 +56,6 @@ public class ArrayUtilities {
       }
     }
     return true;
-  }
-
-  public static void copyAll(Object[] sourceArray, Object[] targetArray) {
-    System.arraycopy(sourceArray, 0, targetArray, 0, sourceArray.length);
-  }
-
-  @SuppressWarnings("unchecked")
-  public static <R, I> R[] transform(ITransformer<I, R> transformer, I[] inputArray, Class<R> resultClass) {
-    R[] resultArray = (R[]) Array.newInstance(resultClass, inputArray.length);
-    for (int index = 0; index < inputArray.length; index++) {
-      resultArray[index] = transformer.transform(inputArray[index]);
-    }
-    return resultArray;
   }
 
   public static <T> T find(Predicate<T> predicate, T[] inputArray) {
@@ -60,52 +76,17 @@ public class ArrayUtilities {
     throw new IllegalArgumentException("Value not contained in array: " + value); //$NON-NLS-1$
   }
 
-  public static <T> void reorder(T[] objects, int[] originalIndices, int[] newIndices) {
-    if (originalIndices.length <= 1) {
-      return;
-    }
-    Map<Integer, T> nodesByOriginalIndex = new HashMap<Integer, T>();
-    for (int element : originalIndices) {
-      nodesByOriginalIndex.put(element, objects[element]);
-    }
-    for (int indexIndex = 0; indexIndex < originalIndices.length; indexIndex++) {
-      int originalIndex = originalIndices[indexIndex];
-      int newIndex = newIndices[indexIndex];
-      objects[newIndex] = nodesByOriginalIndex.get(originalIndex);
-    }
+  public static void invert(Object[] array) {
+    int[] indices = createIndexArray(array.length);
+    int[] invertedIndices = createInvertedIndexArray(array.length);
+    reorder(array, indices, invertedIndices);
   }
 
-  public static Integer[] createIntegerArray(int maximalValue) {
-    Integer[] ranks = new Integer[Math.abs(maximalValue) + 1];
-    for (int index = 0; index < ranks.length; index++) {
-      ranks[index] = Integer.signum(maximalValue) * index;
-    }
-    return ranks;
-  }
-
-  public static Integer[] createIntegerArray(int minimalValue, int maximalValue) {
-    Ensure.ensureArgumentTrue("MinimalValue must be lower than mximalValue", minimalValue < maximalValue); //$NON-NLS-1$
-    Integer[] ranks = new Integer[maximalValue - minimalValue + 1];
-    for (int index = 0; index < ranks.length; index++) {
-      ranks[index] = minimalValue + index;
-    }
-    return ranks;
-  }
-
-  public static int[] createIndexArray(int length) {
-    int[] indexArray = new int[length];
-    for (int index = 0; index < length; index++) {
-      indexArray[index] = index;
-    }
-    return indexArray;
-  }
-
-  private static int[] createInvertedIndexArray(int length) {
-    int[] indexArray = new int[length];
-    for (int index = 0; index < length; index++) {
-      indexArray[index] = length - 1 - index;
-    }
-    return indexArray;
+  public static int max(int[] array) {
+    int[] arrayCopy = new int[array.length];
+    System.arraycopy(array, 0, arrayCopy, 0, array.length);
+    Arrays.sort(arrayCopy);
+    return arrayCopy[arrayCopy.length - 1];
   }
 
   public static void moveObject(Object[] array, int originalIndex, int targetIndex) {
@@ -120,10 +101,19 @@ public class ArrayUtilities {
     }
   }
 
-  public static void invert(Object[] array) {
-    int[] indices = createIndexArray(array.length);
-    int[] invertedIndices = createInvertedIndexArray(array.length);
-    reorder(array, indices, invertedIndices);
+  public static <T> void reorder(T[] objects, int[] originalIndices, int[] newIndices) {
+    if (originalIndices.length <= 1) {
+      return;
+    }
+    Map<Integer, T> nodesByOriginalIndex = new HashMap<Integer, T>();
+    for (int element : originalIndices) {
+      nodesByOriginalIndex.put(element, objects[element]);
+    }
+    for (int indexIndex = 0; indexIndex < originalIndices.length; indexIndex++) {
+      int originalIndex = originalIndices[indexIndex];
+      int newIndex = newIndices[indexIndex];
+      objects[newIndex] = nodesByOriginalIndex.get(originalIndex);
+    }
   }
 
   public static float[] toPrimitive(Float[] objectArray) {
