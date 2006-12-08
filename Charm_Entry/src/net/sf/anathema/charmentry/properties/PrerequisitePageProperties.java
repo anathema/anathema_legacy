2bc50200970bebf578a4b90ef92a762247196923
Urs Reupke <ursreupke@gmail.com>
2006-12-08 10:21:48 +0000

    Prerequiste Traits done

--- Charm_Entry/src/net/sf/anathema/charmentry/properties/PrerequisitePageProperties.java
+++ Charm_Entry/src/net/sf/anathema/charmentry/properties/PrerequisitePageProperties.java
@@ -7,11 +7,14 @@ import net.sf.anathema.lib.resources.IResources;
 public class PrerequisitePageProperties {
 
   private final IResources resources;
-  private final IBasicMessage defaultMessage = new BasicMessage("Please enter the Charm's trait minima.");
-  private final IBasicMessage primaryMissingMessage = new BasicMessage("Please select the primary trait.");
+  private final IBasicMessage defaultMessage;
+  private final IBasicMessage primaryMissingMessage;
 
   public PrerequisitePageProperties(IResources resources) {
     this.resources = resources;
+    defaultMessage = new BasicMessage(resources.getString("CharmEntry.PrerequisiteTraits.Message.Default")); //$NON-NLS-1$
+    primaryMissingMessage = new BasicMessage(
+        resources.getString("CharmEntry.PrerequisiteTraits.Message.PrimaryMissing")); //$NON-NLS-1$
   }
 
   public IBasicMessage getPrerequisitesMessage() {
@@ -23,15 +26,15 @@ public class PrerequisitePageProperties {
   }
 
   public String getPageTitle() {
-    return "Trait Minima";
+    return resources.getString("CharmEntry.PrerequisiteTraits.Title"); //$NON-NLS-1$
   }
 
   public String getEssenceLabel() {
-    return "Essence prerequisite";
+    return resources.getString("CharmEntry.PrerequisiteTraits.Essence"); //$NON-NLS-1$
   }
 
   public String getPrimaryPrerequisiteLabel() {
-    return "Primary prerequisite";
+    return resources.getString("CharmEntry.PrerequisiteTraits.Primary"); //$NON-NLS-1$
   }
 
   public String getEssenceMinimumLabel() {
