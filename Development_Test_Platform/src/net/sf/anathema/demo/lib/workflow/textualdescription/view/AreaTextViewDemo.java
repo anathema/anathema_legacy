1a3c400ffffc22d1ac892ef5a36aa1108f1dbd33
Urs Reupke <ursreupke@gmail.com>
2006-12-08 11:56:55 +0000

    Added demo for Labelled AreaTextView

--- Development_Test_Platform/src/net/sf/anathema/demo/lib/workflow/textualdescription/view/AreaTextViewDemo.java
+++ Development_Test_Platform/src/net/sf/anathema/demo/lib/workflow/textualdescription/view/AreaTextViewDemo.java
@@ -1,7 +1,10 @@
 package net.sf.anathema.demo.lib.workflow.textualdescription.view;
 
-import net.sf.anathema.lib.workflow.textualdescription.view.AreaTextView;
+import javax.swing.JPanel;
 
+import net.disy.commons.swing.layout.grid.GridDialogLayout;
+import net.sf.anathema.lib.workflow.textualdescription.view.AreaTextView;
+import net.sf.anathema.lib.workflow.textualdescription.view.LabelTextView;
 import de.jdemo.extensions.SwingDemoCase;
 
 public class AreaTextViewDemo extends SwingDemoCase {
@@ -11,5 +14,14 @@ public class AreaTextViewDemo extends SwingDemoCase {
     textView.setText("Ich bin ein Testtext."); //$NON-NLS-1$
     textView.setEnabled(false);
     show(textView.getComponent());
+  }
+
+  public void demoLabelledAreaTextView() {
+    AreaTextView areaTextView = new AreaTextView(3, 18);
+    areaTextView.setText("Content"); //$NON-NLS-1$
+    LabelTextView labelTextView = new LabelTextView("Label:", areaTextView); //$NON-NLS-1$
+    JPanel panel = new JPanel(new GridDialogLayout(2, false));
+    labelTextView.addToStandardPanel(panel);
+    show(panel);
   }
 }
