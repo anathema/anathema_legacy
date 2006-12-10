80acde1f3a14423ce9957a1195c4c43aee863e02
Urs Reupke <ursreupke@gmail.com>
2006-12-10 10:23:00 +0000

    Removed GridDialogPanel-addTo's

--- Character_Library/src/net/sf/anathema/character/library/trait/view/FrontToggleButtonTraitViewWrapper.java
+++ Character_Library/src/net/sf/anathema/character/library/trait/view/FrontToggleButtonTraitViewWrapper.java
@@ -3,14 +3,11 @@ package net.sf.anathema.character.library.trait.view;
 import javax.swing.JPanel;
 
 import net.disy.commons.swing.layout.grid.GridDialogLayoutData;
-import net.disy.commons.swing.layout.grid.IDialogComponent;
 import net.sf.anathema.character.library.intvalue.IIconToggleButtonProperties;
 import net.sf.anathema.character.library.intvalue.IToggleButtonTraitView;
-import net.sf.anathema.lib.gui.dialogcomponent.grouped.IGridDialogPanelContent;
-import net.sf.anathema.lib.gui.gridlayout.IGridDialogPanel;
 
 public class FrontToggleButtonTraitViewWrapper<K extends ITraitView< ? >> extends
-    AbstractToggleButtonTraitViewWrapper<K> implements IToggleButtonTraitView<K>, IGridDialogPanelContent {
+    AbstractToggleButtonTraitViewWrapper<K> implements IToggleButtonTraitView<K> {
 
   public FrontToggleButtonTraitViewWrapper(K view, IIconToggleButtonProperties properties, boolean selected) {
     super(view, properties, selected);
@@ -23,17 +20,5 @@ public class FrontToggleButtonTraitViewWrapper<K extends ITraitView< ? >> extend
     gridLayoutData.setHorizontalIndent(5);
     panel.add(getButton().getComponent(), gridLayoutData);
     addInnerView(panel);
-  }
-
-  public void addComponents(IGridDialogPanel abilityPanel) {
-    abilityPanel.add(new IDialogComponent() {
-      public int getColumnCount() {
-        return 2;
-      }
-
-      public void fillInto(JPanel panel, int columnCount) {
-        addComponents(panel);
-      }
-    });
   }
 }
--- Character_Library/src/net/sf/anathema/character/library/trait/view/SimpleTraitView.java
+++ Character_Library/src/net/sf/anathema/character/library/trait/view/SimpleTraitView.java
@@ -7,12 +7,9 @@ import javax.swing.JPanel;
 
 import net.disy.commons.swing.layout.grid.GridAlignment;
 import net.disy.commons.swing.layout.grid.GridDialogLayoutData;
-import net.disy.commons.swing.layout.grid.IDialogComponent;
 import net.sf.anathema.character.library.intvalue.IIntValueDisplayFactory;
-import net.sf.anathema.lib.gui.dialogcomponent.grouped.IGridDialogPanelContent;
-import net.sf.anathema.lib.gui.gridlayout.IGridDialogPanel;
 
-public class SimpleTraitView extends AbstractTraitView implements IGridDialogPanelContent, ITraitView<SimpleTraitView> {
+public class SimpleTraitView extends AbstractTraitView implements ITraitView<SimpleTraitView> {
 
   private final JLabel label;
   private final Component displayComponent;
@@ -33,18 +30,6 @@ public class SimpleTraitView extends AbstractTraitView implements IGridDialogPan
     this.label = new JLabel(getLabelText());
     this.displayComponent = getValueDisplay().getComponent();
     this.dotAlignment = dotAlignment;
-  }
-
-  public void addComponents(IGridDialogPanel dialogPanel) {
-    dialogPanel.add(new IDialogComponent() {
-      public int getColumnCount() {
-        return 2;
-      }
-
-      public void fillInto(JPanel panel, int columnCount) {
-        addComponents(panel);
-      }
-    });
   }
 
   public void addComponents(JPanel panel) {
