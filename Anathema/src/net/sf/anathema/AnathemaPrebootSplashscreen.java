f1a5f5b7535374641792e748994251c6bf825679
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2006-12-17 12:54:00 +0000

    1.5 compatible

--- Anathema/src/net/sf/anathema/AnathemaPrebootSplashscreen.java
+++ Anathema/src/net/sf/anathema/AnathemaPrebootSplashscreen.java
@@ -1,27 +1,22 @@
 package net.sf.anathema;
 
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.SplashScreen;
-import java.awt.font.FontRenderContext;
-import java.awt.font.TextLayout;
 
 public class AnathemaPrebootSplashscreen {
 
   public void displayStatusMessage(String message) {
-    if (System.getProperty("java.version").startsWith("1.5")) { //$NON-NLS-1$ //$NON-NLS-2$
-      return;
-    }
-    SplashScreen splashScreen = SplashScreen.getSplashScreen();
-    if (splashScreen == null || !splashScreen.isVisible()) {
-      return;
-    }
-    Graphics2D graphics = SplashScreen.getSplashScreen().createGraphics();
-    TextLayout layout = new TextLayout(message, graphics.getFont().deriveFont(Font.BOLD), new FontRenderContext(
-        null,
-        true,
-        false));
-    layout.draw(graphics, 105, 333);
-    splashScreen.update();
+//    if (System.getProperty("java.version").startsWith("1.5")) { //$NON-NLS-1$ //$NON-NLS-2$
+//      return;
+//    }
+//    SplashScreen splashScreen = SplashScreen.getSplashScreen();
+//    if (splashScreen == null || !splashScreen.isVisible()) {
+//      return;
+//    }
+//    Graphics2D graphics = SplashScreen.getSplashScreen().createGraphics();
+//    TextLayout layout = new TextLayout(message, graphics.getFont().deriveFont(Font.BOLD), new FontRenderContext(
+//        null,
+//        true,
+//        false));
+//    layout.draw(graphics, 105, 333);
+//    splashScreen.update();
   }
 }
--- Platform/src/net/sf/anathema/AnathemaSplashscreen.java
+++ Platform/src/net/sf/anathema/AnathemaSplashscreen.java
@@ -1,39 +1,30 @@
 package net.sf.anathema;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GradientPaint;
-import java.awt.Graphics2D;
-import java.awt.Paint;
-import java.awt.SplashScreen;
-import java.awt.font.FontRenderContext;
-import java.awt.font.TextLayout;
-import java.awt.geom.Rectangle2D;
 
 public class AnathemaSplashscreen {
-  private final static Rectangle2D.Double textAreaRectangle = new Rectangle2D.Double(93, 318, 454, 19);
+//  private final static Rectangle2D.Double textAreaRectangle = new Rectangle2D.Double(93, 318, 454, 19);
   private final static AnathemaSplashscreen instance = new AnathemaSplashscreen();
-  private final FontRenderContext renderContext = new FontRenderContext(null, true, false);
-  private Graphics2D graphics;
-  private Paint textAreaGradient;
-  private Font font;
+//  private final FontRenderContext renderContext = new FontRenderContext(null, true, false);
+//  private Graphics2D graphics;
+//  private Paint textAreaGradient;
+//  private Font font;
 
   private AnathemaSplashscreen() {
-    if (!isSplashScreenSupported()) {
-      return;
-    }
-    Color startColor = new Color(12, 28, 59);
-    Color endColor = new Color(10, 21, 46);
-    Rectangle2D bounds = textAreaRectangle.getBounds2D();
-    this.textAreaGradient = new GradientPaint(
-        (int) bounds.getMinX(),
-        (int) bounds.getMinY(),
-        startColor,
-        (int) bounds.getMaxX(),
-        (int) bounds.getMaxY(),
-        endColor);
-    this.graphics = SplashScreen.getSplashScreen().createGraphics();
-    this.font = graphics.getFont().deriveFont(Font.BOLD);
+//    if (!isSplashScreenSupported()) {
+//      return;
+//    }
+//    Color startColor = new Color(12, 28, 59);
+//    Color endColor = new Color(10, 21, 46);
+//    Rectangle2D bounds = textAreaRectangle.getBounds2D();
+//    this.textAreaGradient = new GradientPaint(
+//        (int) bounds.getMinX(),
+//        (int) bounds.getMinY(),
+//        startColor,
+//        (int) bounds.getMaxX(),
+//        (int) bounds.getMaxY(),
+//        endColor);
+//    this.graphics = SplashScreen.getSplashScreen().createGraphics();
+//    this.font = graphics.getFont().deriveFont(Font.BOLD);
   }
 
   public static AnathemaSplashscreen getInstance() {
@@ -41,40 +32,40 @@ public class AnathemaSplashscreen {
   }
 
   public void displayStatusMessage(String message) {
-    if (!isSplashScreenSupported()) {
-      return;
-    }
-    SplashScreen splashScreen = SplashScreen.getSplashScreen();
-    if (splashScreen == null || !splashScreen.isVisible()) {
-      return;
-    }
-    resetTextArea();
-    TextLayout layout = new TextLayout(message, font, renderContext);
-    layout.draw(graphics, 105, 333);
-    splashScreen.update();
+//    if (!isSplashScreenSupported()) {
+//      return;
+//    }
+//    SplashScreen splashScreen = SplashScreen.getSplashScreen();
+//    if (splashScreen == null || !splashScreen.isVisible()) {
+//      return;
+//    }
+//    resetTextArea();
+//    TextLayout layout = new TextLayout(message, font, renderContext);
+//    layout.draw(graphics, 105, 333);
+//    splashScreen.update();
   }
 
   private void resetTextArea() {
-    Paint oldPaint = graphics.getPaint();
-    graphics.setPaint(textAreaGradient);
-    graphics.fill(textAreaRectangle);
-    graphics.setPaint(oldPaint);
+//    Paint oldPaint = graphics.getPaint();
+//    graphics.setPaint(textAreaGradient);
+//    graphics.fill(textAreaRectangle);
+//    graphics.setPaint(oldPaint);
   }
 
   public void displayVersion(String string) {
-    if (!isSplashScreenSupported()) {
-      return;
-    }
-    SplashScreen splashScreen = SplashScreen.getSplashScreen();
-    if (splashScreen == null || !splashScreen.isVisible()) {
-      return;
-    }
-    TextLayout layout = new TextLayout(string, font.deriveFont(font.getSize2D() + 2), renderContext);
-    layout.draw(graphics, 445, 91);
-    splashScreen.update();
+//    if (!isSplashScreenSupported()) {
+//      return;
+//    }
+//    SplashScreen splashScreen = SplashScreen.getSplashScreen();
+//    if (splashScreen == null || !splashScreen.isVisible()) {
+//      return;
+//    }
+//    TextLayout layout = new TextLayout(string, font.deriveFont(font.getSize2D() + 2), renderContext);
+//    layout.draw(graphics, 445, 91);
+//    splashScreen.update();
   }
 
-  private boolean isSplashScreenSupported() {
-    return !System.getProperty("java.version").startsWith("1.5"); //$NON-NLS-1$ //$NON-NLS-2$
-  }
+//  private boolean isSplashScreenSupported() {
+//   return !System.getProperty("java.version").startsWith("1.5"); //$NON-NLS-1$ //$NON-NLS-2$
+//  }
 }
