9c9df70dd0d482b31f39aec141920993dc7637df
Urs Reupke <ursreupke@gmail.com>
2006-12-17 08:04:58 +0000

    Cleanup in CharacterScanner

--- Character_Main_Impl/src/net/sf/anathema/character/impl/module/CharacterPrintNameFileScanner.java
+++ Character_Main_Impl/src/net/sf/anathema/character/impl/module/CharacterPrintNameFileScanner.java
@@ -20,58 +20,52 @@ public class CharacterPrintNameFileScanner {
 
   private static final Pattern typePattern = Pattern.compile("<CharacterType.*>(.*?)</CharacterType>"); //$NON-NLS-1$
   private static final Pattern castePattern = Pattern.compile("<Caste type=\"(.*?)\"/>"); //$NON-NLS-1$
-  private final Map<String, CharacterType> typesByFile = new HashMap<String, CharacterType>();
-  private final Map<String, IIdentificate> castesByFile = new HashMap<String, IIdentificate>();
+  private final Map<PrintNameFile, CharacterType> typesByFile = new HashMap<PrintNameFile, CharacterType>();
+  private final Map<PrintNameFile, IIdentificate> castesByFile = new HashMap<PrintNameFile, IIdentificate>();
   private final IRegistry<CharacterType, ICasteCollection> registry;
 
   public CharacterPrintNameFileScanner(IRegistry<CharacterType, ICasteCollection> registry) {
     this.registry = registry;
   }
 
-  private void scan(String content, String repositoryId) {
+  private void scan(PrintNameFile file) throws IOException {
+    String content = FileUtils.readFileToString(new File(file.getFile(), "head.ecg"), PrintNameFileAccess.ENCODING);
     Matcher typeMatcher = typePattern.matcher(content);
     CharacterType characterType;
     typeMatcher.find();
     characterType = CharacterType.getById(typeMatcher.group(1));
-    typesByFile.put(repositoryId, characterType);
+    typesByFile.put(file, characterType);
     Matcher casteMatcher = castePattern.matcher(content);
     if (!casteMatcher.find()) {
-      castesByFile.put(repositoryId, null);
+      castesByFile.put(file, null);
       return;
     }
     IIdentificate casteType = registry.get(characterType).getById(casteMatcher.group(1));
-    castesByFile.put(repositoryId, casteType);
+    castesByFile.put(file, casteType);
   }
 
   public CharacterType getCharacterType(PrintNameFile file) {
-    String id = file.getRepositoryId();
-    CharacterType characterType = typesByFile.get(id);
+    CharacterType characterType = typesByFile.get(file);
     if (characterType != null) {
       return characterType;
     }
     try {
-      scanFile(file, id);
-      return typesByFile.get(id);
+      scan(file);
+      return typesByFile.get(file);
     }
     catch (IOException e) {
       return null;
     }
   }
 
-  private void scanFile(PrintNameFile file, String id) throws IOException {
-    String string = FileUtils.readFileToString(new File(file.getFile(), "head.ecg"), PrintNameFileAccess.ENCODING);
-    scan(string, id);
-  }
-
   public IIdentificate getCasteType(PrintNameFile file) {
-    String id = file.getRepositoryId();
-    IIdentificate caste = castesByFile.get(id);
+    IIdentificate caste = castesByFile.get(file);
     if (caste != null) {
       return caste;
     }
     try {
-      scanFile(file, id);
-      return castesByFile.get(id);
+      scan(file);
+      return castesByFile.get(file);
     }
     catch (IOException e) {
       return null;
