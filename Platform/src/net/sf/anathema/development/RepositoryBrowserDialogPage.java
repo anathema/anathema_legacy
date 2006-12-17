bef3e290122b3a7a9eef25ed82047d81a8cd80c5
Urs Reupke <ursreupke@gmail.com>
2006-12-17 19:29:01 +0000

    RepositoryView uses common Dialog framework

---
+++ Platform/src/net/sf/anathema/development/RepositoryBrowserDialogPage.java
@@ -0,0 +1,46 @@
+package net.sf.anathema.development;
+
+import javax.swing.JComponent;
+
+import net.disy.commons.core.message.BasicMessage;
+import net.disy.commons.core.message.IBasicMessage;
+import net.disy.commons.swing.dialog.userdialog.AbstractDialogPage;
+import net.sf.anathema.framework.IAnathemaModel;
+import net.sf.anathema.framework.presenter.item.ItemTypeCreationViewPropertiesExtensionPoint;
+import net.sf.anathema.framework.repository.tree.RepositoryTreeModel;
+import net.sf.anathema.framework.repository.tree.RepositoryTreePresenter;
+import net.sf.anathema.framework.repository.tree.RepositoryTreeView;
+import net.sf.anathema.lib.resources.IResources;
+
+public class RepositoryBrowserDialogPage extends AbstractDialogPage {
+
+  private final IResources resources;
+  private final IAnathemaModel model;
+
+  public RepositoryBrowserDialogPage(IResources resources, IAnathemaModel model) {
+    super(new BasicMessage(resources.getString("AnathemaCore.Tools.RepositoryView.DialogMessage"))); //$NON-NLS-1$
+    this.resources = resources;
+    this.model = model;
+
+  }
+
+  public JComponent createContent() {
+    RepositoryTreeView treeView = new RepositoryTreeView();
+    ItemTypeCreationViewPropertiesExtensionPoint extension = (ItemTypeCreationViewPropertiesExtensionPoint) model.getExtensionPointRegistry()
+        .get(ItemTypeCreationViewPropertiesExtensionPoint.ID);
+    ObjectUiTreeCellRenderer renderer = new ObjectUiTreeCellRenderer(new ItemTypeTreeUi(resources, extension));
+    new RepositoryTreePresenter(resources, new RepositoryTreeModel(
+        model.getRepository(),
+        model.getItemManagement(),
+        model.getItemTypeRegistry()), treeView, renderer, "AnathemaCore.Tools.RepositoryView.TreeRoot").initPresentation(); //$NON-NLS-1$
+    return treeView.getComponent();
+  }
+
+  public IBasicMessage createCurrentMessage() {
+    return new BasicMessage(resources.getString("AnathemaCore.Tools.RepositoryView.DialogMessage")); //$NON-NLS-1$
+  }
+
+  public String getTitle() {
+    return resources.getString("AnathemaCore.Tools.RepositoryView.DialogTitle"); //$NON-NLS-1$
+  }
+}
--- Platform/src/net/sf/anathema/development/RepositoryViewAction.java
+++ Platform/src/net/sf/anathema/development/RepositoryViewAction.java
@@ -1,20 +1,12 @@
 package net.sf.anathema.development;
 
 import java.awt.Component;
-import java.awt.Dimension;
 
 import javax.swing.Action;
-import javax.swing.JComponent;
-import javax.swing.JDialog;
-import javax.swing.JScrollPane;
 
 import net.disy.commons.swing.action.SmartAction;
+import net.disy.commons.swing.dialog.userdialog.UserDialog;
 import net.sf.anathema.framework.IAnathemaModel;
-import net.sf.anathema.framework.presenter.item.ItemTypeCreationViewPropertiesExtensionPoint;
-import net.sf.anathema.framework.repository.tree.RepositoryTreeModel;
-import net.sf.anathema.framework.repository.tree.RepositoryTreePresenter;
-import net.sf.anathema.framework.repository.tree.RepositoryTreeView;
-import net.sf.anathema.lib.gui.GuiUtilities;
 import net.sf.anathema.lib.resources.IResources;
 
 public final class RepositoryViewAction extends SmartAction {
@@ -34,24 +26,9 @@ public final class RepositoryViewAction extends SmartAction {
 
   @Override
   protected void execute(Component parentComponent) {
-    JDialog dialog = GuiUtilities.createDialog(
-        parentComponent,
-        resources.getString("AnathemaCore.Tools.RepositoryView.DialogTitle"), true); //$NON-NLS-1$
-    JScrollPane scrollPane = new JScrollPane(createRepositoryTreeComponent());
-    scrollPane.setPreferredSize(new Dimension(500, 700));
-    dialog.getContentPane().add(scrollPane);
-    GuiUtilities.show(dialog);
-  }
-
-  private JComponent createRepositoryTreeComponent() {
-    RepositoryTreeView treeView = new RepositoryTreeView();
-    ItemTypeCreationViewPropertiesExtensionPoint extension = (ItemTypeCreationViewPropertiesExtensionPoint) model.getExtensionPointRegistry()
-        .get(ItemTypeCreationViewPropertiesExtensionPoint.ID);
-    ObjectUiTreeCellRenderer renderer = new ObjectUiTreeCellRenderer(new ItemTypeTreeUi(resources, extension));
-    new RepositoryTreePresenter(resources, new RepositoryTreeModel(
-        model.getRepository(),
-        model.getItemManagement(),
-        model.getItemTypeRegistry()), treeView, renderer, "AnathemaCore.Tools.RepositoryView.TreeRoot").initPresentation(); //$NON-NLS-1$
-    return treeView.getComponent();
+    RepositoryBrowserDialogPage page = new RepositoryBrowserDialogPage(resources, model);
+    UserDialog userDialog = new UserDialog(parentComponent, page);
+    userDialog.getDialog().setModal(true);
+    userDialog.show();
   }
 }
