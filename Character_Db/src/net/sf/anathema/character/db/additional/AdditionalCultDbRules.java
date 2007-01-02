8ed4ea55f7cd2a996e259d08f85f1f38abe0afb7
Urs Reupke <ursreupke@gmail.com>
2007-01-02 17:29:38 +0000

    Removed unused constructor

--- Character_Db/src/net/sf/anathema/character/db/additional/AdditionalCultDbRules.java
+++ Character_Db/src/net/sf/anathema/character/db/additional/AdditionalCultDbRules.java
@@ -1,16 +1,11 @@
 package net.sf.anathema.character.db.additional;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import net.sf.anathema.character.generic.backgrounds.IBackgroundTemplate;
 import net.sf.anathema.character.generic.impl.additional.DefaultAdditionalRules;
 
 public class AdditionalCultDbRules extends DefaultAdditionalRules {
-
-  public AdditionalCultDbRules(List<String> rejectedBackgrounds) {
-    super(rejectedBackgrounds);
-  }
 
   public AdditionalCultDbRules() {
     super(new ArrayList<String>());
