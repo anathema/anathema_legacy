509448d82ec8119baaabb14fdf34d6d6165b1f11
Urs Reupke <ursreupke@gmail.com>
2007-02-05 20:44:46 +0000

    Abyssals use generic pool instead of necromancy pool

--- Character_Abyssal/src/net/sf/anathema/character/abyssal/additional/AdditionalAbyssalRules.java
+++ Character_Abyssal/src/net/sf/anathema/character/abyssal/additional/AdditionalAbyssalRules.java
@@ -1,10 +1,14 @@
 package net.sf.anathema.character.abyssal.additional;
 
+import net.sf.anathema.character.generic.additionalrules.IAdditionalMagicLearnPool;
 import net.sf.anathema.character.generic.backgrounds.IBackgroundTemplate;
 import net.sf.anathema.character.generic.impl.additional.AdditionalEssencePool;
 import net.sf.anathema.character.generic.impl.additional.DefaultAdditionalRules;
+import net.sf.anathema.character.generic.impl.additional.GenericMagicLearnPool;
 import net.sf.anathema.character.generic.impl.additional.MultiLearnableCharmPool;
 import net.sf.anathema.character.generic.magic.charms.special.IMultiLearnableCharm;
+import net.sf.anathema.character.generic.magic.spells.CircleType;
+import net.sf.anathema.character.generic.util.IPointModification;
 
 public class AdditionalAbyssalRules extends DefaultAdditionalRules {
 
@@ -13,10 +17,22 @@ public class AdditionalAbyssalRules extends DefaultAdditionalRules {
   }
 
   public void addNecromancyRules(IBackgroundTemplate necromancyTemplate) {
-    addMagicLearnPool(new NecromancyLearnPool(necromancyTemplate));
+    addMagicLearnPool(getPool(necromancyTemplate));
   }
 
   public void addEssenceEngorgementTechniqueRules(IMultiLearnableCharm technique) {
     addEssencePool(new MultiLearnableCharmPool(technique, new AdditionalEssencePool(0), new AdditionalEssencePool(10)));
+  }
+
+  private IAdditionalMagicLearnPool getPool(IBackgroundTemplate necromancyTemplate) {
+    GenericMagicLearnPool pool = new GenericMagicLearnPool(necromancyTemplate, true);
+    pool.setMaximumCircle(CircleType.Labyrinth);
+    pool.attachCondition(CircleType.Labyrinth, 4);
+    pool.setCostModification(new IPointModification() {
+      public int getAdditionalPoints(int value) {
+        return Math.max(0, value - 3);
+      }
+    });
+    return pool;
   }
 }
