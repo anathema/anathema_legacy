575633c55e17bca32fc46e1c3d3d463f2cdf0b4b
Urs Reupke <ursreupke@gmail.com>
2007-02-27 18:25:41 +0000

    Replaced 200 refs to CharacterType in a move to get rid of the Enum

--- Character_Craft/src/net/sf/anathema/character/craft/CraftViewFactory.java
+++ Character_Craft/src/net/sf/anathema/character/craft/CraftViewFactory.java
@@ -5,14 +5,14 @@ import net.sf.anathema.character.craft.presenter.ICraftModel;
 import net.sf.anathema.character.craft.view.CraftView;
 import net.sf.anathema.character.generic.additionaltemplate.IAdditionalModel;
 import net.sf.anathema.character.generic.framework.additionaltemplate.IAdditionalViewFactory;
-import net.sf.anathema.character.generic.type.CharacterType;
+import net.sf.anathema.character.generic.type.ICharacterType;
 import net.sf.anathema.character.library.intvalue.MarkerIntValueDisplayFactory;
 import net.sf.anathema.lib.gui.IView;
 import net.sf.anathema.lib.resources.IResources;
 
 public class CraftViewFactory implements IAdditionalViewFactory {
 
-  public IView createView(IAdditionalModel model, final IResources resources, CharacterType type) {
+  public IView createView(IAdditionalModel model, final IResources resources, ICharacterType type) {
     ICraftModel craftModel = ((ICraftAdditionalModel) model).getCraftModel();
     int maximum = craftModel.getAbsoluteMaximum();
     CraftView view = new CraftView(new MarkerIntValueDisplayFactory(resources, type), maximum);
