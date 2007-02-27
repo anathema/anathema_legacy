38594488e0e1851b303c0ab2f52495029e9c4051
Urs Reupke <ursreupke@gmail.com>
2007-02-27 18:27:52 +0000

    Replaced 200 refs to CharacterType in a move to get rid of the Enum

--- Character_Db/src/net/sf/anathema/character/db/virtueflaw/DbVirtueFlawViewFactory.java
+++ Character_Db/src/net/sf/anathema/character/db/virtueflaw/DbVirtueFlawViewFactory.java
@@ -2,7 +2,7 @@ package net.sf.anathema.character.db.virtueflaw;
 
 import net.sf.anathema.character.generic.additionaltemplate.IAdditionalModel;
 import net.sf.anathema.character.generic.framework.additionaltemplate.IAdditionalViewFactory;
-import net.sf.anathema.character.generic.type.CharacterType;
+import net.sf.anathema.character.generic.type.ICharacterType;
 import net.sf.anathema.character.library.virtueflaw.presenter.IVirtueFlawModel;
 import net.sf.anathema.character.library.virtueflaw.presenter.IVirtueFlawView;
 import net.sf.anathema.character.library.virtueflaw.presenter.VirtueFlawPresenter;
@@ -12,7 +12,7 @@ import net.sf.anathema.lib.resources.IResources;
 
 public class DbVirtueFlawViewFactory implements IAdditionalViewFactory {
 
-  public IView createView(IAdditionalModel model, IResources resources, CharacterType type) {
+  public IView createView(IAdditionalModel model, IResources resources, ICharacterType type) {
     IVirtueFlawView virtueFlawView = new VirtueFlawView();
     new VirtueFlawPresenter(resources, virtueFlawView, (IVirtueFlawModel) model).initPresentation();
     return virtueFlawView;
