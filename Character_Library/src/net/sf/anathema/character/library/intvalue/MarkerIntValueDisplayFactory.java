036915245263edb61151df030ded285f2bb4c59b
Urs Reupke <ursreupke@gmail.com>
2007-02-27 18:25:48 +0000

    Replaced 200 refs to CharacterType in a move to get rid of the Enum

--- Character_Library/src/net/sf/anathema/character/library/intvalue/MarkerIntValueDisplayFactory.java
+++ Character_Library/src/net/sf/anathema/character/library/intvalue/MarkerIntValueDisplayFactory.java
@@ -3,7 +3,7 @@ package net.sf.anathema.character.library.intvalue;
 import javax.swing.Icon;
 
 import net.sf.anathema.character.generic.framework.resources.CharacterUI;
-import net.sf.anathema.character.generic.type.CharacterType;
+import net.sf.anathema.character.generic.type.ICharacterType;
 import net.sf.anathema.framework.value.IIntValueDisplay;
 import net.sf.anathema.lib.resources.IResources;
 
@@ -12,7 +12,7 @@ public class MarkerIntValueDisplayFactory implements IIntValueDisplayFactory {
   private final Icon activeBallIcon;
   private final Icon passiveBallIcon;
 
-  public MarkerIntValueDisplayFactory(IResources resources, CharacterType type) {
+  public MarkerIntValueDisplayFactory(IResources resources, ICharacterType type) {
     CharacterUI characterUI = new CharacterUI(resources);
     this.activeBallIcon = characterUI.getMediumBallResource(type);
     this.passiveBallIcon = characterUI.getUnselectedBallResource();
