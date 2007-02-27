c57a44aa31182a39b5668b44a63a4f3e093eb4c3
Urs Reupke <ursreupke@gmail.com>
2007-02-27 18:25:55 +0000

    Replaced 200 refs to CharacterType in a move to get rid of the Enum

--- Character_Linguistics/src/net/sf/anathema/character/linguistics/LinguisticsViewFactory.java
+++ Character_Linguistics/src/net/sf/anathema/character/linguistics/LinguisticsViewFactory.java
@@ -2,7 +2,7 @@ package net.sf.anathema.character.linguistics;
 
 import net.sf.anathema.character.generic.additionaltemplate.IAdditionalModel;
 import net.sf.anathema.character.generic.framework.additionaltemplate.IAdditionalViewFactory;
-import net.sf.anathema.character.generic.type.CharacterType;
+import net.sf.anathema.character.generic.type.ICharacterType;
 import net.sf.anathema.character.linguistics.presenter.ILinguisticsModel;
 import net.sf.anathema.character.linguistics.presenter.LinguisticsPresenter;
 import net.sf.anathema.character.linguistics.view.LinguisticsView;
@@ -11,7 +11,7 @@ import net.sf.anathema.lib.resources.IResources;
 
 public class LinguisticsViewFactory implements IAdditionalViewFactory {
 
-  public IView createView(IAdditionalModel model, final IResources resources, CharacterType type) {
+  public IView createView(IAdditionalModel model, final IResources resources, ICharacterType type) {
     ILinguisticsModel linguisticsModel = ((ILinguisticsAdditionalModel) model).getLinguisticsModel();
     LinguisticsView view = new LinguisticsView();
     new LinguisticsPresenter(linguisticsModel, view, resources).initPresentation();
