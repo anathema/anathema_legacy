1ac54056b19bee4d12637373943cc83f9c51b121
Urs Reupke <ursreupke@gmail.com>
2007-03-25 16:13:57 +0000

    Fixing generics, solar way

---
+++ Character_Solar/src/net/sf/anathema/character/solar/generic/EssenceFlow.java
@@ -0,0 +1,35 @@
+package net.sf.anathema.character.solar.magic;
+
+import net.sf.anathema.character.generic.framework.magic.AbstractGenericCharm;
+import net.sf.anathema.character.generic.framework.magic.stringbuilder.type.ShortCharmTypeStringBuilder;
+import net.sf.anathema.character.generic.impl.magic.charm.type.CharmTypeModel;
+import net.sf.anathema.character.generic.magic.charms.duration.SimpleDuration;
+import net.sf.anathema.character.generic.magic.charms.type.CharmType;
+import net.sf.anathema.lib.resources.IResources;
+
+public class EssenceFlow extends AbstractGenericCharm {
+
+  @Override
+  protected String getId() {
+    return "Solar.EssenceFlow"; //$NON-NLS-1$
+  }
+
+  @Override
+  protected boolean isComboOk() {
+    return false;
+  }
+
+  public String getCostString(IResources resources) {
+    return "-"; //$NON-NLS-1$
+  }
+
+  public String getDurationString(IResources resources) {
+    return SimpleDuration.PERMANENT_DURATION.getText(resources);
+  }
+
+  public String getType(IResources resources) {
+    CharmTypeModel model = new CharmTypeModel();
+    model.setCharmType(CharmType.Permanent);
+    return new ShortCharmTypeStringBuilder(resources).createTypeString(model);
+  }
+}
