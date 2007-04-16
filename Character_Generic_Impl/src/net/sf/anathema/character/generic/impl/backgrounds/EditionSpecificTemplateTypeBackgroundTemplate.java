dbed64763758b141472c36dc123ddc33cbd21d3c
Urs Reupke <ursreupke@gmail.com>
2007-04-16 20:47:54 +0000

    Forgot to commit code to remove sorcery

---
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/backgrounds/EditionSpecificTemplateTypeBackgroundTemplate.java
@@ -0,0 +1,24 @@
+package net.sf.anathema.character.generic.impl.backgrounds;
+
+import net.sf.anathema.character.generic.rules.IExaltedEdition;
+import net.sf.anathema.character.generic.template.ITemplateType;
+import net.sf.anathema.character.generic.traits.LowerableState;
+
+public class EditionSpecificTemplateTypeBackgroundTemplate extends TemplateTypeBackgroundTemplate {
+
+  private final IExaltedEdition edition;
+
+  public EditionSpecificTemplateTypeBackgroundTemplate(
+      String id,
+      ITemplateType[] types,
+      IExaltedEdition edition,
+      LowerableState state) {
+    super(id, types, state);
+    this.edition = edition;
+  }
+
+  @Override
+  public boolean acceptsTemplate(ITemplateType templateType, IExaltedEdition requestEdition) {
+    return super.acceptsTemplate(templateType, edition) && this.edition == requestEdition;
+  }
+}
