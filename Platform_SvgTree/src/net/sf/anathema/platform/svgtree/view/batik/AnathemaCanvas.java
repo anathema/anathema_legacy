1ea54e85b21239d316fe4f3b745af080d190411b
Urs Reupke <ursreupke@gmail.com>
2007-04-24 18:20:41 +0000

    Removed obsolete implementation

--- Platform_SvgTree/src/net/sf/anathema/platform/svgtree/view/batik/AnathemaCanvas.java
+++ Platform_SvgTree/src/net/sf/anathema/platform/svgtree/view/batik/AnathemaCanvas.java
@@ -2,13 +2,11 @@ package net.sf.anathema.platform.svgtree.view.batik;
 
 import java.awt.Color;
 import java.awt.Cursor;
-import java.util.List;
 
 import net.sf.anathema.platform.svgtree.presenter.view.IAnathemaCanvas;
 
 import org.apache.batik.dom.svg.SVGOMDocument;
 import org.apache.batik.swing.JSVGCanvas;
-import org.apache.batik.swing.gvt.Interactor;
 import org.apache.batik.swing.svg.AbstractJSVGComponent;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -47,11 +45,5 @@ public class AnathemaCanvas extends JSVGCanvas implements IAnathemaCanvas {
 
   public Element getElementById(String id) {
     return getSVGDocument().getRootElement().getElementById(id);
-  }
-
-  @SuppressWarnings("unchecked")
-  @Override
-  public List<Interactor> getInteractors() {
-    return super.getInteractors();
   }
 }
