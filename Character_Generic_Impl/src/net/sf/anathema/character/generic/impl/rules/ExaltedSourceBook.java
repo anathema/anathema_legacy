6a2014fca7bfb1abb30744a64b6cf6deac66d31f
Urs Reupke <ursreupke@gmail.com>
2007-05-28 18:55:22 +0000

    Imperfect Lotus

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/rules/ExaltedSourceBook.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/rules/ExaltedSourceBook.java
@@ -19,8 +19,8 @@ public enum ExaltedSourceBook implements IExaltedSourceBook {
   CBEclipse(ExaltedEdition.FirstEdition), PlayersGuide(ExaltedEdition.FirstEdition), SecondEdition(
       ExaltedEdition.SecondEdition), DragonBlooded2nd(ExaltedEdition.SecondEdition), Lunars2nd(
       ExaltedEdition.SecondEdition), WondersLostAge(ExaltedEdition.SecondEdition), ScrollMonk(
-      ExaltedEdition.SecondEdition), WhiteTreatise(ExaltedEdition.SecondEdition), BlackTreatise(
-      ExaltedEdition.SecondEdition), Comic0(ExaltedEdition.FirstEdition);
+      ExaltedEdition.SecondEdition), ImperfectLotus(ExaltedEdition.SecondEdition), WhiteTreatise(
+      ExaltedEdition.SecondEdition), BlackTreatise(ExaltedEdition.SecondEdition), Comic0(ExaltedEdition.FirstEdition);
 
   private final IExaltedEdition edition;
 
