df4f4ea6a722b37a08e96618a6a934e87431e3c5
Urs Reupke <ursreupke@gmail.com>
2007-06-06 17:48:27 +0000

    Cleanup TwoCommonParentsOptimizer Test for Path of the Arbiter setup

--- Development_Test_Character/src/net/sf/anathema/test/character/generic/framework/treelayout/layering/LongestPathLayererTest.java
+++ Development_Test_Character/src/net/sf/anathema/test/character/generic/framework/treelayout/layering/LongestPathLayererTest.java
@@ -3,6 +3,7 @@ package net.sf.anathema.test.character.generic.framework.treelayout.layering;
 import net.sf.anathema.lib.testing.BasicTestCase;
 import net.sf.anathema.platform.svgtree.graph.layering.LongestPathLayerer;
 import net.sf.anathema.platform.svgtree.graph.nodes.IRegularNode;
+import net.sf.anathema.platform.svgtree.graph.nodes.ISimpleNode;
 import net.sf.anathema.platform.svgtree.graph.nodes.IdentifiedRegularNode;
 import net.sf.anathema.platform.svgtree.graph.nodes.NodeFactory;
 
@@ -44,6 +45,7 @@ public class LongestPathLayererTest extends BasicTestCase {
     IRegularNode root1 = new IdentifiedRegularNode(new IRegularNode[] { leaf1, leaf2 }, "root1"); //$NON-NLS-1$
     IRegularNode root2 = new IdentifiedRegularNode(new IRegularNode[] { leaf1, leaf2 }, "root2"); //$NON-NLS-1$
     IRegularNode[] acyclicGraph = new IRegularNode[] { leaf1, leaf2, root1, root2 };
+    connect(acyclicGraph);
     int deepestLayer = layerer.layerGraph(acyclicGraph);
     assertEquals(3, deepestLayer);
     assertEquals(root1.getLayer(), 1);
@@ -51,5 +53,31 @@ public class LongestPathLayererTest extends BasicTestCase {
     assertTrue(leaf1.getLayer() == 2 || leaf1.getLayer() == 3);
     assertTrue(leaf2.getLayer() == 2 || leaf2.getLayer() == 3);
     assertTrue(leaf1.getLayer() != leaf2.getLayer());
+  }
+
+  private void connect(IRegularNode[] acyclicGraph) {
+    for (IRegularNode node : acyclicGraph) {
+      ISimpleNode[] children = node.getChildren();
+      for (ISimpleNode child : children) {
+        ((IRegularNode) child).addParent(node);
+      }
+    }
+  }
+
+  public void testLayerDualRootTripleChild() throws Exception {
+    IRegularNode leaf1 = NodeFactory.createChildlessNode("leaf1"); //$NON-NLS-1$
+    IRegularNode leaf2 = NodeFactory.createChildlessNode("leaf2"); //$NON-NLS-1$
+    IRegularNode leaf3 = NodeFactory.createChildlessNode("leaf3"); //$NON-NLS-1$
+    IRegularNode root1 = new IdentifiedRegularNode(new IRegularNode[] { leaf1, leaf2, leaf3 }, "root1"); //$NON-NLS-1$
+    IRegularNode root2 = new IdentifiedRegularNode(new IRegularNode[] { leaf1, leaf2, leaf3 }, "root2"); //$NON-NLS-1$
+    IRegularNode[] acyclicGraph = new IRegularNode[] { leaf1, leaf2, leaf3, root1, root2 };
+    connect(acyclicGraph);
+    int deepestLayer = layerer.layerGraph(acyclicGraph);
+    assertEquals(3, deepestLayer);
+    assertEquals(root1.getLayer(), 1);
+    assertEquals(root2.getLayer(), 1);
+    assertTrue(leaf1.getLayer() == 2);
+    assertTrue(leaf2.getLayer() == 2);
+    assertTrue(leaf3.getLayer() == 2);
   }
 }
--- Platform_SvgTree/src/net/sf/anathema/platform/svgtree/graph/layering/TwoCommonParentsOptimizer.java
+++ Platform_SvgTree/src/net/sf/anathema/platform/svgtree/graph/layering/TwoCommonParentsOptimizer.java
@@ -1,68 +1,65 @@
 package net.sf.anathema.platform.svgtree.graph.layering;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import net.disy.commons.core.util.ArrayUtilities;
 import net.sf.anathema.lib.collection.MultiEntryMap;
 import net.sf.anathema.platform.svgtree.graph.nodes.IRegularNode;
+import net.sf.anathema.platform.svgtree.graph.nodes.ISimpleNode;
 
 public class TwoCommonParentsOptimizer {
 
   public static boolean moveDownOvercrossingTwoTupels(
       MultiEntryMap<Integer, IRegularNode> nodesByLayer,
-      int layerIndex,
+      int layerToOptimize,
       int deepestLayer) {
-    Map<IRegularNode, List<IRegularNode>> parentsByNode = new HashMap<IRegularNode, List<IRegularNode>>();
-    for (IRegularNode layerNode : nodesByLayer.get(layerIndex)) {
-      List<IRegularNode> parentNodes = getParents(layerNode, nodesByLayer);
-      if (parentNodes.size() == 2) {
-        parentsByNode.put(layerNode, parentNodes);
-      }
-    }
-    if (parentsByNode.size() < 2) {
+    List<IRegularNode> dualParentNodes = getDualParentNodes(nodesByLayer, layerToOptimize);
+    if (dualParentNodes.size() < 2) {
       return false;
     }
     boolean increaseDeepestLayer = false;
-    Set<IRegularNode> doubleParentNodeSet = new HashSet<IRegularNode>(parentsByNode.keySet());
-    for (IRegularNode doubleParentNode : parentsByNode.keySet()) {
-      if (!doubleParentNodeSet.contains(doubleParentNode)) {
+    Set<IRegularNode> doubleParentNodeSet = new HashSet<IRegularNode>(dualParentNodes);
+    for (IRegularNode node : dualParentNodes) {
+      if (!doubleParentNodeSet.contains(node)) {
         continue;
       }
-      IRegularNode nodeToBeMovedDown = null;
-      doubleParentNodeSet.remove(doubleParentNode);
-      Set<IRegularNode> doubleParentNodeSetCopy = new HashSet<IRegularNode>(doubleParentNodeSet);
-      List<IRegularNode> doubleParentNodeParents = parentsByNode.get(doubleParentNode);
-      for (IRegularNode remainingNode : doubleParentNodeSetCopy) {
-        List<IRegularNode> remainingNodeParents = parentsByNode.get(remainingNode);
-        if (doubleParentNodeParents.containsAll(remainingNodeParents)) {
-          // todo vom (26.10.2005) (sieroux): Kindreicher Knoten hinab
-          nodeToBeMovedDown = doubleParentNode;
-          doubleParentNodeSet.remove(remainingNode);
+      doubleParentNodeSet.remove(node);
+      ISimpleNode[] nodeParents = node.getParents();
+      for (IRegularNode otherNode : new HashSet<IRegularNode>(doubleParentNodeSet)) {
+        if (containsAll(nodeParents, otherNode.getParents())) {
+          // TODO vom 26.10.2005: Kindreicher Knoten hinab
+          doubleParentNodeSet.remove(otherNode);
+          int targetLayer = node.getLayer() + 1;
+          node.setLayer(targetLayer);
+          increaseDeepestLayer |= targetLayer > deepestLayer;
+          break;
         }
-      }
-      if (nodeToBeMovedDown != null) {
-        int targetLayer = nodeToBeMovedDown.getLayer() + 1;
-        nodeToBeMovedDown.setLayer(targetLayer);
-        increaseDeepestLayer = increaseDeepestLayer || targetLayer > deepestLayer;
       }
     }
     return increaseDeepestLayer;
   }
 
-  private static List<IRegularNode> getParents(IRegularNode node, MultiEntryMap<Integer, IRegularNode> nodesByLayer) {
-    List<IRegularNode> parentNodes = new ArrayList<IRegularNode>();
-    for (int index = 1; index < node.getLayer(); index++) {
-      for (IRegularNode potentialParent : nodesByLayer.get(index)) {
-        if (ArrayUtilities.contains(potentialParent.getChildren(), node)) {
-          parentNodes.add(potentialParent);
-        }
+  private static boolean containsAll(ISimpleNode[] nodeParents, ISimpleNode[] otherNodeParents) {
+    for (ISimpleNode node : otherNodeParents) {
+      if (!ArrayUtilities.contains(nodeParents, node)) {
+        return false;
       }
     }
-    return parentNodes;
+    return true;
+  }
+
+  private static List<IRegularNode> getDualParentNodes(
+      MultiEntryMap<Integer, IRegularNode> nodesByLayer,
+      int layerToOptimize) {
+    List<IRegularNode> dualParentNodes = new ArrayList<IRegularNode>();
+    for (IRegularNode node : nodesByLayer.get(layerToOptimize)) {
+      if (node.getParents().length == 2) {
+        dualParentNodes.add(node);
+      }
+    }
+    return dualParentNodes;
   }
 }
