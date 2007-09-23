e1ec864c53e4c944d57abef21956e40c102988a6
Urs Reupke <ursreupke@gmail.com>
2007-09-23 07:40:50 +0000

    Cleanup

--- Development_Test_Platform/src/net/sf/anathema/test/lib/lang/ArrayUtiltiesTest.java
+++ Development_Test_Platform/src/net/sf/anathema/test/lib/lang/ArrayUtiltiesTest.java
@@ -26,30 +26,30 @@ public class ArrayUtiltiesTest {
   @Test
   public void testCreateIntegerArray() throws Exception {
     Integer[] integers = ArrayUtilities.createIntegerArray(3);
-    Assert.assertEquals(new Integer[] { 0, 1, 2, 3 }, integers);
+    Assert.assertArrayEquals(new Integer[] { 0, 1, 2, 3 }, integers);
   }
 
   @Test
   public void testCreateNegativeIntegerArray() throws Exception {
     Integer[] integers = ArrayUtilities.createIntegerArray(-3);
-    Assert.assertEquals(new Integer[] { -0, -1, -2, -3 }, integers);
+    Assert.assertArrayEquals(new Integer[] { -0, -1, -2, -3 }, integers);
   }
 
   @Test
   public void testCreateLowBoundedIntegerArray() throws Exception {
     Integer[] integers = ArrayUtilities.createIntegerArray(1, 5);
-    Assert.assertEquals(new Integer[] { 1, 2, 3, 4, 5 }, integers);
+    Assert.assertArrayEquals(new Integer[] { 1, 2, 3, 4, 5 }, integers);
   }
 
   @Test
   public void testCreateNegativeLowBoundedIntegerArray() throws Exception {
     Integer[] integers = ArrayUtilities.createIntegerArray(-5, -1);
-    Assert.assertEquals(new Integer[] { -5, -4, -3, -2, -1 }, integers);
+    Assert.assertArrayEquals(new Integer[] { -5, -4, -3, -2, -1 }, integers);
   }
 
   @Test
   public void testCreateCrossingLowBoundedIntegerArray() throws Exception {
     Integer[] integers = ArrayUtilities.createIntegerArray(-5, 1);
-    Assert.assertEquals(new Integer[] { -5, -4, -3, -2, -1, 0, 1 }, integers);
+    Assert.assertArrayEquals(new Integer[] { -5, -4, -3, -2, -1, 0, 1 }, integers);
   }
 }
