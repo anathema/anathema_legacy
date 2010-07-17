1bce69b634e06c771232ba285606315414c0e73f
Rob Rutherford <rlrutherford@gmail.com>
2010-07-17 07:56:22 +0000

    removed from build directory

--- Character_Craft/src/net/sf/anathema/character/craft/ICraftAdditionalModel.java
+++
@@ -1,9 +0,0 @@
-package net.sf.anathema.character.craft;
-
-import net.sf.anathema.character.craft.presenter.ICraftModel;
-import net.sf.anathema.character.generic.additionaltemplate.IAdditionalModel;
-
-public interface ICraftAdditionalModel extends IAdditionalModel {
-
-  public ICraftModel getCraftModel();
-}
