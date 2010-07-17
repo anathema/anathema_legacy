a97c46c0a3a9e302065389fa6ea8e4051a5df766
Rob Rutherford <rlrutherford@gmail.com>
2010-07-17 08:30:28 +0000

    Partial merge with actual outcaste branch, with fixed compilation issues; still need to add additional changed to fully integrate what was being done.

--- Lib_Gui/src/net/sf/anathema/lib/gui/selection/IListObjectSelectionView.java
+++ Lib_Gui/src/net/sf/anathema/lib/gui/selection/IListObjectSelectionView.java
@@ -5,9 +5,14 @@ import javax.swing.ListCellRenderer;
 import net.sf.anathema.lib.gui.IView;
 import net.sf.anathema.lib.gui.list.ListSelectionMode;
 
-public interface IListObjectSelectionView<V> extends IVetoableObjectSelectionView<V>, IView {
+public interface IListObjectSelectionView<V> extends
+		IVetoableObjectSelectionView<V>, IView {
 
-  public void setCellRenderer(ListCellRenderer renderer);
+	public void setCellRenderer(ListCellRenderer renderer);
 
-  public void setSelectionType(ListSelectionMode type);
+	public V[] getSelectedObjects();
+
+	public void setSelectedObjects(V[] objects);
+
+	public void setSelectionType(ListSelectionMode type);
 }
--- Lib_Gui/src/net/sf/anathema/lib/gui/selection/ListObjectSelectionView.java
+++ Lib_Gui/src/net/sf/anathema/lib/gui/selection/ListObjectSelectionView.java
@@ -13,64 +13,80 @@ import net.sf.anathema.lib.gui.list.veto.VetoableListSelectionModel;
 
 public class ListObjectSelectionView<V> implements IListObjectSelectionView<V> {
 
-  private final SmartJList<V> smartList;
-  private final VetoableListSelectionModel selectionModel;
+	private final SmartJList<V> smartList;
+	private final VetoableListSelectionModel selectionModel;
 
-  public ListObjectSelectionView(Class< ? extends V> contentClazz) {
-    this.smartList = new SmartJList<V>(contentClazz);
-    this.selectionModel = new VetoableListSelectionModel();
-    this.smartList.setSelectionModel(selectionModel);
-  }
+	public ListObjectSelectionView(Class<? extends V> contentClazz) {
+		this(contentClazz, ListSelectionMode.SingleSelection);
+	}
 
-  public void setEnabled(boolean enabled) {
-    smartList.setEnabled(enabled);
-  }
+	public ListObjectSelectionView(Class<? extends V> contentClazz,
+			ListSelectionMode mode) {
+		this.smartList = new SmartJList<V>(contentClazz);
+		this.selectionModel = new VetoableListSelectionModel(mode);
+		this.smartList.setSelectionModel(selectionModel);
+	}
 
-  public void setCellRenderer(ListCellRenderer renderer) {
-    smartList.setCellRenderer(renderer);
+	public void setEnabled(boolean enabled) {
+		smartList.setEnabled(enabled);
+	}
 
-  }
+	public void setCellRenderer(ListCellRenderer renderer) {
+		smartList.setCellRenderer(renderer);
 
-  public void addObjectSelectionChangedListener(final IObjectValueChangedListener<V> listener) {
-    smartList.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+	}
 
-      public void valueChanged(ListSelectionEvent e) {
-        listener.valueChanged(smartList.getSelectedValue());
-      }
-    });
-  }
+	public void addObjectSelectionChangedListener(
+			final IObjectValueChangedListener<V> listener) {
+		smartList.getSelectionModel().addListSelectionListener(
+				new ListSelectionListener() {
 
-  public void setObjects(V[] objects) {
-    smartList.setObjects(objects);
-  }
+					public void valueChanged(ListSelectionEvent e) {
+						listener.valueChanged(smartList.getSelectedValue());
+					}
+				});
+	}
 
-  @SuppressWarnings("unchecked")
-  public void setSelectedObject(V object) {
-    // URS: Ich habe die Array-Konversion an dieser Stelle entfernt, weil ich einen Fehler vermutete.
-    smartList.setSelectedObjects(object);
-  }
+	public void setObjects(V[] objects) {
+		smartList.setObjects(objects);
+	}
 
-  public JComponent getComponent() {
-    return smartList;
-  }
+	@SuppressWarnings("unchecked")
+	public void setSelectedObject(V object) {
+		// URS: Ich habe die Array-Konversion an dieser Stelle entfernt, weil
+		// ich einen Fehler vermutete.
+		smartList.setSelectedObjects(object);
+	}
 
-  public V getSelectedObject() {
-    return smartList.getSelectedValue();
-  }
+	public void setSelectedObjects(V[] objects) {
+		smartList.setSelectedObjects(objects);
+	}
 
-  public boolean isObjectSelected() {
-    return getSelectedObject() != null;
-  }
+	public JComponent getComponent() {
+		return smartList;
+	}
 
-  public void setSelectionType(ListSelectionMode mode) {
-    smartList.setSelectionMode(mode);
-  }
+	public V getSelectedObject() {
+		return smartList.getSelectedValue();
+	}
 
-  public void addSelectionVetor(IVetor vetor) {
-    selectionModel.addVetor(vetor);
-  }
+	public V[] getSelectedObjects() {
+		return smartList.getSelectedValues();
+	}
 
-  public void removeSelectionVetor(IVetor vetor) {
-    selectionModel.removeVetor(vetor);
-  }
+	public boolean isObjectSelected() {
+		return getSelectedObject() != null;
+	}
+
+	public void setSelectionType(ListSelectionMode mode) {
+		smartList.setSelectionMode(mode);
+	}
+
+	public void addSelectionVetor(IVetor vetor) {
+		selectionModel.addVetor(vetor);
+	}
+
+	public void removeSelectionVetor(IVetor vetor) {
+		selectionModel.removeVetor(vetor);
+	}
 }
