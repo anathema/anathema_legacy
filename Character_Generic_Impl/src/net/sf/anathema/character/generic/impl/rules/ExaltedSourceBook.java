9533cbe032eaf7038b64fe99fad8264f118e5e83
Paul Adams <jontu.kontar@gmail.com>
2010-11-01 02:04:37 +0000

    Added Ink Monkeys 5 (prereqs for 37). Added a bunch of stuff for Ink Monkeys 37. Added BIM_5 and BIM_37 to booklist. Pending adding charms to plugin-fragment.xml and language files. Waiting on Infernals charms (non-MA) until I get an idea for the directory layout.

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/rules/ExaltedSourceBook.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/rules/ExaltedSourceBook.java
@@ -24,7 +24,7 @@ public enum ExaltedSourceBook implements IExaltedSourceBook {
  GloriesMaidens(ExaltedEdition.SecondEdition),GloriesUCS(ExaltedEdition.SecondEdition), DebrisFallenRaces(ExaltedEdition.SecondEdition),
  ScrollExalts(ExaltedEdition.SecondEdition), InkMonkeys(ExaltedEdition.SecondEdition),ReturnEmpress(ExaltedEdition.SecondEdition),
  UnderRose(ExaltedEdition.SecondEdition), ContagionLaw(ExaltedEdition.SecondEdition), ScrollErrata(ExaltedEdition.SecondEdition),
- Alchemicals2nd(ExaltedEdition.SecondEdition);
+ Alchemicals2nd(ExaltedEdition.SecondEdition), BIM_5(ExaltedEdition.SecondEdition), BIM_37(ExaltedEdition.SecondEdition);
 
   private final IExaltedEdition edition;
 
