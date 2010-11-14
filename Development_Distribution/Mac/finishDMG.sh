832dba4433f60b3f5f0227f068e4187dcb7a8300
Eric Astor <eric.astor@gmail.com>
2010-11-14 09:43:22 +0000

    A few more changes to the automatic Mac build system... still needs significant tweaking.

--- Development_Distribution/Mac/finishDMG.sh
+++ Development_Distribution/Mac/finishDMG.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]
+if [ $# -ne 4 ]
 then
-	echo "Usage: $0 directory dmgfile mountdir"
+	echo "Usage: $0 directory dmgfile volname mountdir"
 	exit 1
 fi
 			
@@ -14,24 +14,24 @@ image_size=`expr $appdir_size + $background_size + $dsstore_size + $volicon_size
 echo "$1: ${appdir_size}K"
 
 dd if=/dev/zero of=$2 bs=1024 count=$image_size status=noxfer
-mkfs.hfsplus -v Anathema $2
+mkfs.hfsplus -v '$3' $2
 
-echo "Creating mount location $3"
-mkdir $3
-echo "Mounting the image $2 in $3"
-mount -o loop -t hfsplus $2 $3
+echo "Creating mount location $4"
+mkdir $4
+echo "Mounting the image $2 in $4"
+mount -o loop -t hfsplus $2 $4
 
 echo "Adding Applications link, along with assorted image and layout data"
-ln -s /Applications $3/Applications
-cp Mac/background.tiff $3/.background.tiff
-cp Mac/VolumeIcon.icns $3/.VolumeIcon.icns
-cp Mac/DS_Store $3/.DS_Store
-cp $1/Contents/Resources/Java/license.txt $3/License.txt
+ln -s /Applications $4/Applications
+cp Mac/background.tiff $4/.background.tiff
+cp Mac/VolumeIcon.icns $4/.VolumeIcon.icns
+mv Mac/DS_Store.tmp $4/.DS_Store
+cp $1/Contents/Resources/Java/license.txt $4/License.txt
 
-echo "Copying the application $1 into the image $2, mounted at $3"
-cp -a $1 $3
+echo "Copying the application $1 into the image $2, mounted at $4"
+cp -a $1 $4
 
-echo "Umounting the image from $3"
-umount $3
-echo "Removing mount location $3"
-rmdir $3
+echo "Umounting the image from $4"
+umount $4
+echo "Removing mount location $4"
+rmdir $4
