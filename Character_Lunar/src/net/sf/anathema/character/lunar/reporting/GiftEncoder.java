c946c7b28b57ca6a6b7d9aad0084aa7bec8df355
Sean Mattox <sean.mattox@gmail.com>
2011-05-07 00:02:11 +0000

    Fixing Lunar 2E printing after mutation revision.

--- Character_Lunar/src/net/sf/anathema/character/lunar/reporting/GiftEncoder.java
+++ Character_Lunar/src/net/sf/anathema/character/lunar/reporting/GiftEncoder.java
@@ -3,8 +3,10 @@ package net.sf.anathema.character.lunar.reporting;
 import net.sf.anathema.character.generic.character.IGenericCharacter;
 import net.sf.anathema.character.library.quality.presenter.IQualitySelection;
 import net.sf.anathema.character.lunar.beastform.BeastformTemplate;
+import net.sf.anathema.character.lunar.beastform.model.FirstEditionBeastformModel;
 import net.sf.anathema.character.lunar.beastform.model.gift.IGift;
 import net.sf.anathema.character.lunar.beastform.presenter.IBeastformModel;
+import net.sf.anathema.character.mutations.model.IMutation;
 import net.sf.anathema.character.reporting.sheet.util.AbstractLineTextEncoder;
 import net.sf.anathema.lib.resources.IResources;
 import net.sf.anathema.lib.util.IIdentificate;
@@ -30,13 +32,26 @@ public class GiftEncoder extends AbstractLineTextEncoder {
   @Override
   protected void addToPhrase(IGenericCharacter character, Font font, Phrase phrase) {
     IBeastformModel model = (IBeastformModel) character.getAdditionalModel(BeastformTemplate.TEMPLATE_ID);
-    IQualitySelection<IGift>[] gifts = model.getGiftModel().getSelectedQualities();
-    for (int index = 0; index < gifts.length; index++) {
-      IIdentificate gift = gifts[index].getQuality();
-      String text = resources.getString("DeadlyBeastmanTransformation.Gift." + gift.getId()); //$NON-NLS-1$
-      text = text.replaceFirst("\\(\\p{Alnum}+\\) ", "");
-      text += index + 1 < gifts.length ? ", " : ""; //$NON-NLS-1$ //$NON-NLS-2$
-      phrase.add(new Chunk(text, font));
+    if (model instanceof FirstEditionBeastformModel)
+    {
+	    IQualitySelection<IGift>[] gifts = model.getGiftModel().getSelectedQualities();
+	    for (int index = 0; index < gifts.length; index++) {
+	      IIdentificate gift = gifts[index].getQuality();
+	      String text = resources.getString("DeadlyBeastmanTransformation.Gift." + gift.getId()); //$NON-NLS-1$
+	      text = text.replaceFirst("\\(\\p{Alnum}+\\) ", "");
+	      text += index + 1 < gifts.length ? ", " : ""; //$NON-NLS-1$ //$NON-NLS-2$
+	      phrase.add(new Chunk(text, font));
+	    }
+    }
+    else
+    {
+    	IQualitySelection<IMutation>[] mutations = model.getMutationModel().getSelectedQualities();
+	    for (int index = 0; index < mutations.length; index++) {
+	      IIdentificate mutation = mutations[index].getQuality();
+	      String text = resources.getString("Mutations.Mutation." + mutation.getId()); //$NON-NLS-1$
+	      text += index + 1 < mutations.length ? ", " : ""; //$NON-NLS-1$ //$NON-NLS-2$
+	      phrase.add(new Chunk(text, font));
+	    }
     }
   }
 }
