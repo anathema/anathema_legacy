1993ad0caa2c81eea740913909d646f300b6b9f1
Scott Olson <scottolson275@gmail.com>
2011-06-14 00:11:14 +0000

    Added procedures for Elemental Summoning, Enchantment, Geomancy, & Husbandry.

--- Character_Thaumaturgy/src/net/sf/anathema/character/thaumaturgy/model/ThaumaturgyProvider.java
+++ Character_Thaumaturgy/src/net/sf/anathema/character/thaumaturgy/model/ThaumaturgyProvider.java
@@ -40,6 +40,30 @@ public class ThaumaturgyProvider
 				                  "Dishonest (Creature)'s Rebuke", "Expulsion", "Banish (Creature)",
 				                  "Demonsight", "Summon (Species)", "Banish the Faithless Servant", 
 				                  "Beckon (Unique Demon)", "Five Days Foresight", "The Year and a Day"};
+		if (art.equals("Elemental Summoning"))
+			return new String[] { "Alarm War Against (Creature)", "Lesser Ward Against (Creature)",
+				                  "Ward Against (Creature)", "Greater Ward Against (Creature)", "Alarm Ward Maintenance",
+				                  "Lesser Ward Maintenance", "Ward Maintenance", "Keyed Ward",
+				                  "Dishonest (Creature)'s Rebuke",  "Banish (Creature)", "Jade Extraction Method",
+				                  "Elemental Sight", "Summon (Species)", 
+				                  "Beckon (Species)", "Invoke the Elemental Benediction"};
+		if (art.equals("Enchantment"))
+			return new String[] { "Least Wonder", "Lesser Wonder", "Talisman", "Analyze Talisman",
+				                  "Fading Color Technique", "Strengthen Ironwood", "Enchant Lucky Rock",
+				                  "Process Steelsilk", "Aegis-Inset Amulats", "Warding Talisman"};
+		if (art.equals("Geomancy"))
+			return new String[] { "Essence Sense", "Blessing", "Curse", "Dragon Line Compass",
+				                  "Alloyed Essence Indicator", "Magical Attunement", "Bathing in the River Meditation",
+				                  "Dragon Nest Compass", "Pearl Collecting Rite", "Ritual of Dedicated Purification",
+				                  "Pulse of the Demense's Heart", "Rattle the Sanctum's Gate", "Essence Enlightening Sutra",
+				                  "Yoga of Celestial Refinement", "House of Good/Ill Fortune",
+				                  "Geomantic Countermagic", "Scrying"};
+		if (art.equals("Husbandry"))
+			return new String[] { "Alarm Ward Against (Creature)", "Lesser Ward Against (Creature)",
+				                  "Ward Against (Creature)", "Greater Ward Against (Creature)", "Alarm Ward Maintenance",
+				                  "Lesser Ward Maintenance", "Ward Maintenance", "Keyed Ward",
+				                  "Judge the Pure Beast", "Warding the Crops", "Control (Species)",
+				                  "Summon Human", "Summon (Species)", "Improved (Species) Breeding"};
 
 		return new String[0];
 	}
