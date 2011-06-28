9e813c2d92714355473990cf2ecb1db6e6c283e9
Sean Mattox <sean.mattox@gmail.com>
2011-06-28 18:42:44 +0000

    Re-enabling Ghosts for now; fixing a few bugs in their data.

--- Character_Generic_Framework/src/net/sf/anathema/character/generic/framework/CharacterModuleContainerInitializer.java
+++ Character_Generic_Framework/src/net/sf/anathema/character/generic/framework/CharacterModuleContainerInitializer.java
@@ -34,7 +34,7 @@ public class CharacterModuleContainerInitializer {
       add("net.sf.anathema.character.lunar.LunarCharacterModule"); //$NON-NLS-1$
       add("net.sf.anathema.character.solar.SolarCharacterModule"); //$NON-NLS-1$
       add("net.sf.anathema.character.spirit.SpiritCharacterModule"); //$NON-NLS-1$
-      //add("net.sf.anathema.character.ghost.GhostCharacterModule"); //$NON-NLS-1$
+      add("net.sf.anathema.character.ghost.GhostCharacterModule"); //$NON-NLS-1$
       add("net.sf.anathema.character.sidereal.SiderealCharacterModule"); //$NON-NLS-1$
       add("net.sf.anathema.character.infernal.InfernalCharacterModule"); //$NON-NLS-1$
       add("net.sf.anathema.character.godblooded.GodBloodedCharacterModule"); //$NON-NLS-1$
