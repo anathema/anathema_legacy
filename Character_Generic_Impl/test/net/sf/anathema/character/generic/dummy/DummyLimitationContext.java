3d3bceff92512a9f632af1df316f680f7cd62486
Urs Reupke <ursreupke@gmail.com>
2011-12-11 15:57:16 +0100

    Activated Dummys and another Test

---
+++ Character_Generic_Impl/test/net/sf/anathema/character/generic/dummy/DummyLimitationContext.java
@@ -0,0 +1,61 @@
+package net.sf.anathema.character.generic.dummy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.anathema.character.generic.caste.ICasteType;
+import net.sf.anathema.character.generic.character.IGenericTraitCollection;
+import net.sf.anathema.character.generic.character.ILimitationContext;
+import net.sf.anathema.character.generic.impl.traits.limitation.StaticTraitLimitation;
+import net.sf.anathema.character.generic.template.ITraitLimitation;
+import net.sf.anathema.character.generic.traits.IFavorableGenericTrait;
+import net.sf.anathema.character.generic.traits.IGenericTrait;
+import net.sf.anathema.character.generic.traits.ITraitType;
+import net.sf.anathema.lib.exception.NotYetImplementedException;
+
+public class DummyLimitationContext implements ILimitationContext {
+  private final Map<ITraitType, IGenericTrait> traits = new HashMap<ITraitType, IGenericTrait>();
+
+  public ITraitLimitation getEssenceLimitation() {
+    return new StaticTraitLimitation(7);
+  }
+
+  public ICasteType getCasteType() {
+    return null;
+  }
+
+  public IGenericTraitCollection getTraitCollection() {
+    return new IGenericTraitCollection() {
+
+      public IFavorableGenericTrait getFavorableTrait(ITraitType type) {
+        return (IFavorableGenericTrait) getTrait(type);
+      }
+
+      public IGenericTrait getTrait(ITraitType type) {
+        return traits.get(type);
+      }
+
+      public IGenericTrait[] getTraits(ITraitType[] traitTypes) {
+        throw new NotYetImplementedException();
+      }
+
+      public boolean isFavoredOrCasteTrait(ITraitType type) {
+        return getFavorableTrait(type).isCasteOrFavored();
+      }
+
+    };
+  }
+  
+  public int getEssenceCap(boolean modified)
+  {
+	  return 0;
+  }
+  
+  public int getAge() {
+    return 0;
+  }
+
+  public void addTrait(IGenericTrait trait) {
+    traits.put(trait.getType(), trait);
+  }
+}
---
+++ Character_Main_Impl/test/net/sf/anathema/character/generic/dummy/magic/DummyMagicCollection.java
@@ -0,0 +1,36 @@
+package net.sf.anathema.character.generic.dummy.magic;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.anathema.character.generic.character.IMagicCollection;
+import net.sf.anathema.character.generic.magic.IMagic;
+import net.sf.anathema.character.generic.magic.charms.special.IMultiLearnableCharm;
+
+public class DummyMagicCollection implements IMagicCollection {
+
+  private final Map<IMultiLearnableCharm, Integer> counts = new HashMap<IMultiLearnableCharm, Integer>();
+
+  public int getLearnCount(IMultiLearnableCharm multiLearnableCharm) {
+    return counts.get(multiLearnableCharm);
+  }
+
+  public boolean isLearned(IMagic magic) {
+    return false;
+  }
+
+  public void setLearnCount(IMultiLearnableCharm multiLearnableCharm, int newValue) {
+    counts.put(multiLearnableCharm, newValue);
+  }
+
+	@Override
+	public int getLearnCount(String charmName) {
+		return 0;
+	}
+
+	@Override
+	public void setLearnCount(String charmName, int newValue) {
+		// TODO Auto-generated method stub
+		
+	}
+}
