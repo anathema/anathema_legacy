bf206a14bef5f27d35a0895b00ca4698a30194b8
Urs Reupke <ursreupke@gmail.com>
2011-12-11 21:26:16 +0100

    Reactivated initialization-tests

---
+++ Anathema/test/net/sf/anathema/initialization/PluginRepositoryCollectorTest.java
@@ -0,0 +1,18 @@
+package net.sf.anathema.initialization;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+import net.sf.anathema.PluginRepositoryCollector;
+
+import org.junit.Test;
+
+public class PluginRepositoryCollectorTest {
+	private final PluginRepositoryCollector collector = new PluginRepositoryCollector();
+
+	@Test
+	public void doesNotCollectRootFolderAsPluginLocation() throws Exception {
+		String repositories = collector.getPluginRepositories();
+		assertThat(repositories, not(containsString(",/,")));
+	}
+}
