6aa830363868d3e301119a99ad7ca36bc5dffa3a
Urs Reupke <ursreupke@gmail.com>
2011-12-11 21:30:38 +0100

    Replaced some German umlauts

--- Lib/test/net/sf/anathema/lib/lang/StringUtilitiesTest.java
+++ Lib/test/net/sf/anathema/lib/lang/StringUtilitiesTest.java
@@ -13,9 +13,10 @@ public class StringUtilitiesTest {
     Assert.assertTrue(Arrays.equals(new int[] { 6 }, AnathemaStringUtilities.findBreakPoints("Hallo-Du!", 2))); //$NON-NLS-1$
     Assert.assertTrue(Arrays.equals(new int[] { 6, 9 }, AnathemaStringUtilities.findBreakPoints("Hallo-Du da!", 3))); //$NON-NLS-1$
     Assert.assertTrue(Arrays.equals(new int[] { 6, 13 }, AnathemaStringUtilities.findBreakPoints(
-        "Oh Du Lebest drauﬂen!", 3))); //$NON-NLS-1$
-    Assert.assertTrue(Arrays.equals(new int[] { 6, 12 }, AnathemaStringUtilities.findBreakPoints(
-        "Hallo Du da drauﬂen!", 3))); //$NON-NLS-1$
+        "Oh Du Lebest draussen!", 3))); //$NON-NLS-1$
+    int[] breakPoints = AnathemaStringUtilities.findBreakPoints(
+      "Hallo Du da drueben!", 3);
+    Assert.assertTrue(Arrays.equals(new int[] { 6, 12 }, breakPoints)); //$NON-NLS-1$
     Assert.assertTrue(Arrays.equals(new int[] { 0, 3 }, AnathemaStringUtilities.findBreakPoints("Zu kurz.", 3))); //$NON-NLS-1$
   }
 
