66224fd62da7f39c0d5a62f11653dd086c8002aa
Urs Reupke <ursreupke@gmail.com>
2011-12-11 14:35:56 +0100

    Activated more test

---
+++ Character_Equipment_Impl/test/net/sf/anathema/character/equipment/view/EquipmentAdditionalPresenterTest.java
@@ -0,0 +1,29 @@
+package net.sf.anathema.character.equipment.view;
+
+import net.sf.anathema.character.equipment.character.EquipmentAdditionalPresenter;
+import net.sf.anathema.character.equipment.character.EquipmentObjectCellRenderer;
+import net.sf.anathema.character.equipment.character.model.IEquipmentAdditionalModel;
+import net.sf.anathema.character.equipment.character.view.IEquipmentAdditionalView;
+import net.sf.anathema.lib.dummy.DummyResources;
+import net.sf.anathema.lib.gui.selection.IListObjectSelectionView;
+import net.sf.anathema.lib.testing.BasicTestCase;
+import org.easymock.EasyMock;
+
+public class EquipmentAdditionalPresenterTest extends BasicTestCase {
+
+  @SuppressWarnings("unchecked")
+  public void testInitPresentation() throws Exception {
+    DummyResources resources = new DummyResources();
+    String[] equipmentTemplateIds = new String[0];
+    IEquipmentAdditionalModel model = EasyMock.createNiceMock(IEquipmentAdditionalModel.class);
+    IEquipmentAdditionalView view = EasyMock.createNiceMock(IEquipmentAdditionalView.class);
+    EasyMock.expect(model.getAvailableTemplateIds()).andReturn(equipmentTemplateIds).once();
+    IListObjectSelectionView<String> equipmentPickList = EasyMock.createMock(IListObjectSelectionView.class);
+    equipmentPickList.setObjects(equipmentTemplateIds);
+    equipmentPickList.setCellRenderer(new EquipmentObjectCellRenderer());
+    EasyMock.expect(view.getEquipmentTemplatePickList()).andReturn(equipmentPickList);
+    EasyMock.replay(model, view, equipmentPickList);
+    new EquipmentAdditionalPresenter(resources, model, view).initPresentation();
+    EasyMock.verify(model, view, equipmentPickList);
+  }
+}
