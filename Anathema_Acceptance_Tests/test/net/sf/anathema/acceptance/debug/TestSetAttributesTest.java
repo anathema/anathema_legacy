3ed3d02dd654be0da2b9d4459b505f48ce19bfb1
Urs Reupke <ursreupke@gmail.com>
2011-12-14 00:38:54 +0100

    Some last fixes for #13. Please rerun the commands from the readme to pick up Anathema_Acceptance_Tests, Development_Code and Development_Test_Character again.

--- Anathema_Acceptance_Tests/test/net/sf/anathema/acceptance/debug/TestSetAttributesTest.java
+++ Anathema_Acceptance_Tests/test/net/sf/anathema/acceptance/debug/TestSetAttributesTest.java
@@ -5,24 +5,24 @@ import net.sf.anathema.acceptance.fixture.character.SetupCharacterPlatformFixtur
 import net.sf.anathema.acceptance.fixture.character.costs.CheckBonusPointsFixture;
 import net.sf.anathema.acceptance.fixture.character.miscellaneous.SetConceptFixture;
 import net.sf.anathema.acceptance.fixture.character.traits.SetAttributesFixture;
-import net.sf.anathema.lib.testing.BasicTestCase;
+import org.junit.Test;
 
-public class TestSetAttributesTest extends BasicTestCase {
+import static org.junit.Assert.assertEquals;
 
+public class TestSetAttributesTest {
+
+  @Test
   public void testIt() throws Exception {
     SetupCharacterPlatformFixture platformFixture = new SetupCharacterPlatformFixture();
     platformFixture.doTable(null);
-
     CreateCharacterFixture createCharacterFixture = new CreateCharacterFixture();
     createCharacterFixture.summary = platformFixture.summary;
     createCharacterFixture.characterType = "Lunar"; //$NON-NLS-1$
     createCharacterFixture.enterRow();
-
     SetConceptFixture setConceptFixture = new SetConceptFixture();
     setConceptFixture.summary = createCharacterFixture.summary;
     setConceptFixture.caste = "NoMoon"; //$NON-NLS-1$
     setConceptFixture.enterRow();
-
     SetAttributesFixture attributeFixtures = new SetAttributesFixture();
     attributeFixtures.summary = setConceptFixture.summary;
     attributeFixtures.traitType = "Strength"; //$NON-NLS-1$
@@ -34,14 +34,12 @@ public class TestSetAttributesTest extends BasicTestCase {
     attributeFixtures.traitType = "Stamina"; //$NON-NLS-1$
     attributeFixtures.value = 3;
     attributeFixtures.enterRow();
-
     attributeFixtures.traitType = "Wits"; //$NON-NLS-1$
     attributeFixtures.value = 5;
     attributeFixtures.enterRow();
     attributeFixtures.traitType = "Intelligence"; //$NON-NLS-1$
     attributeFixtures.value = 5;
     attributeFixtures.enterRow();
-
     CheckBonusPointsFixture checkBonusPointsFixture = new CheckBonusPointsFixture();
     checkBonusPointsFixture.summary = setConceptFixture.summary;
     assertEquals(7, checkBonusPointsFixture.regularSpent());
