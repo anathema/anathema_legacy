cd5190d86f9fdaa2d403a376465fbd1a02424857
vestrial <sandra.sieroux@googlemail.com>
2011-12-19 18:10:31 +0100

    Clean Coding.

--- Character_Reporting/src/net/sf/anathema/character/reporting/pdf/rendering/boxes/essence/SimpleEssenceBoxContentEncoder.java
+++ Character_Reporting/src/net/sf/anathema/character/reporting/pdf/rendering/boxes/essence/SimpleEssenceBoxContentEncoder.java
@@ -2,7 +2,6 @@ package net.sf.anathema.character.reporting.pdf.rendering.boxes.essence;
 
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.pdf.PdfContentByte;
-import net.sf.anathema.character.generic.traits.types.OtherTraitType;
 import net.sf.anathema.character.reporting.pdf.content.ReportContent;
 import net.sf.anathema.character.reporting.pdf.content.essence.SimpleEssenceContent;
 import net.sf.anathema.character.reporting.pdf.rendering.elements.Bounds;
@@ -15,44 +14,45 @@ import static com.lowagie.text.pdf.PdfContentByte.ALIGN_RIGHT;
 
 public class SimpleEssenceBoxContentEncoder implements IBoxContentEncoder {
 
-  private PdfTraitEncoder largeTraitEncoder;
-
-  public SimpleEssenceBoxContentEncoder() {
-    this.largeTraitEncoder = PdfTraitEncoder.createTraitEncoder(PdfTraitEncoder.LARGE_DOT_SIZE);
-  }
-
   public void encode(SheetGraphics graphics, ReportContent reportContent, Bounds bounds) throws DocumentException {
     SimpleEssenceContent content = createContent(reportContent);
-    String personalPool = content.getPersonalPool();
-    String peripheralPool = content.getPeripheralPool();
-    float traitHeight = largeTraitEncoder.getTraitHeight();
-    int numberOfLines = (personalPool == null ? 0 : 1) + (peripheralPool == null ? 0 : 1);
-    SimpleEssenceBoxLayout layout = new SimpleEssenceBoxLayout(graphics, bounds, traitHeight, numberOfLines);
-    int value = reportContent.getCharacter().getTraitCollection().getTrait(OtherTraitType.Essence).getCurrentValue();
-    Position essencePosition = layout.getEssencePosition();
-    largeTraitEncoder.encodeDotsCenteredAndUngrouped(graphics, essencePosition, bounds.width, value, content.getEssenceMax());
+    SimpleEssenceBoxLayout layout = new SimpleEssenceBoxLayout(graphics, bounds, content.getNumberOfPoolLines());
+    encodeEssenceTrait(graphics, content, layout);
+    encodePersonalPool(graphics, content, layout);
+    encodePeripheralPool(graphics, content, layout);
+  }
 
-    if (personalPool != null) {
+  private void encodeEssenceTrait(SheetGraphics graphics, SimpleEssenceContent content, SimpleEssenceBoxLayout layout) {
+    Position essencePosition = layout.getEssencePosition();
+    int essenceValue = content.getEssenceValue();
+    int essenceMax = content.getEssenceMax();
+    PdfTraitEncoder largeTraitEncoder = PdfTraitEncoder.createTraitEncoder(layout.getEssenceDotSize());
+    largeTraitEncoder.encodeDotsCenteredAndUngrouped(graphics, essencePosition, layout.geWidth(), essenceValue, essenceMax);
+  }
+
+  private void encodePersonalPool(SheetGraphics graphics, SimpleEssenceContent content, SimpleEssenceBoxLayout layout) {
+    if (content.hasPersonalPool()) {
       Position personalPosition = layout.getFirstPoolPosition();
       String personalLabel = content.getPersonalPoolLabel();
-      encodePool(graphics, content, personalLabel, personalPool, personalPosition, layout);
-    }
-
-    if (peripheralPool != null) {
-      Position peripheralPosition = numberOfLines == 1 ? layout.getFirstPoolPosition() : layout.getSecondPoolPosition();
-      String peripheralLabel = content.getPeripheralPoolLabel();
-      encodePool(graphics, content, peripheralLabel, peripheralPool, peripheralPosition, layout);
+      encodePool(graphics, content, layout, personalLabel, content.getPersonalPool(), personalPosition);
     }
   }
 
-  private void encodePool(SheetGraphics graphics,SimpleEssenceContent content, String label, String poolValue, Position poolPosition,
-    SimpleEssenceBoxLayout layout) {
+  private void encodePeripheralPool(SheetGraphics graphics, SimpleEssenceContent content, SimpleEssenceBoxLayout layout) {
+    if (content.hasPeripheralPool()) {
+      Position peripheralPosition = content.getNumberOfPoolLines() == 1 ? layout.getFirstPoolPosition() : layout.getSecondPoolPosition();
+      String peripheralLabel = content.getPeripheralPoolLabel();
+      encodePool(graphics, content, layout, peripheralLabel, content.getPeripheralPool(), peripheralPosition);
+    }
+  }
+
+  private void encodePool(SheetGraphics graphics, SimpleEssenceContent content, SimpleEssenceBoxLayout layout, String label, String poolValue, Position poolPosition) {
     graphics.drawText(label, poolPosition, PdfContentByte.ALIGN_LEFT);
     String availableString = content.getAvailableString();
     Position availablePosition = layout.getAvailabilityPositionRightAligned(poolPosition);
     graphics.drawText(availableString, availablePosition, ALIGN_RIGHT);
     Position lineStartPoint = layout.getAvailabilityLineStartPosition(poolPosition, availableString);
-    graphics.drawMissingTextLine(lineStartPoint, layout.getAvailabilityLineLength());
+    graphics.drawMissingTextLine(lineStartPoint, layout.getMissingValueLineLength());
     String totalString = content.getTotalString(poolValue);
     graphics.drawText(totalString, lineStartPoint, ALIGN_RIGHT);
   }
