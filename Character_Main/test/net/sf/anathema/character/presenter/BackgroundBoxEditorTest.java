8b396502211031c4be4c3a63b877f35e144526f7
Urs Reupke <ursreupke@gmail.com>
2011-12-31 14:12:27 +0100

    Cleaned up yesterday's test

--- Character_Main/test/net/sf/anathema/character/presenter/BackgroundBoxEditorTest.java
+++ Character_Main/test/net/sf/anathema/character/presenter/BackgroundBoxEditorTest.java
@@ -1,5 +1,6 @@
 package net.sf.anathema.character.presenter;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -8,16 +9,20 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
 public class BackgroundBoxEditorTest {
+  private static final Object id = "MyBackgroundId";
+  private static final Object displayedText = "My Background Name";
+  private Displayer displayer = Mockito.mock(Displayer.class);
+  private BackgroundBoxEditor editor = new BackgroundBoxEditor(displayer);
+
+  @Before
+  public void linkIdToDisplayedText() {
+    when(displayer.getDisplayObject(id)).thenReturn(displayedText);
+  }
 
   @Test
   public void internationalizesBackgroundsInEditor() throws Exception {
-    Object input = "MyBackgroundId";
-    Object expectation = "My Background Name";
-    Displayer displayer = Mockito.mock(Displayer.class);
-    when(displayer.getDisplayObject(input)).thenReturn(expectation);
-    BackgroundBoxEditor editor = new BackgroundBoxEditor(displayer);
-    editor.setItem(input);
+    editor.setItem(id);
     Object result = editor.getItem();
-    assertThat(result, is(expectation));
+    assertThat(result, is(displayedText));
   }
 }
