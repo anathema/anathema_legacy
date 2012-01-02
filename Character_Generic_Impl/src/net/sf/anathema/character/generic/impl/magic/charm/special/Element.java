e50a0a445a44f7025dd0a43e040487275cfd325d
Urs Reupke <ursreupke@gmail.com>
2012-01-02 14:24:56 +0100

    Elements match any identificate (#28)

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/charm/special/Element.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/charm/special/Element.java
@@ -1,11 +1,12 @@
 package net.sf.anathema.character.generic.impl.magic.charm.special;
 
-import net.sf.anathema.character.generic.caste.ICasteType;
+import net.sf.anathema.lib.util.IIdentificate;
+import net.sf.anathema.lib.util.Identificate;
 
 enum Element {
   Air, Earth, Fire, Water, Wood;
 
-  public boolean matches(ICasteType caste) {
+  public boolean matches(IIdentificate caste) {
     return caste.getId().equals(name());
   }
 }
--- Character_Generic_Impl/test/net/sf/anathema/character/generic/impl/magic/charm/special/ElementTest.java
+++ Character_Generic_Impl/test/net/sf/anathema/character/generic/impl/magic/charm/special/ElementTest.java
@@ -1,7 +1,8 @@
 package net.sf.anathema.character.generic.impl.magic.charm.special;
 
-import net.sf.anathema.character.generic.caste.ICasteType;
 import net.sf.anathema.character.generic.dummy.DummyCasteType;
+import net.sf.anathema.lib.util.IIdentificate;
+import net.sf.anathema.lib.util.Identificate;
 import org.junit.Test;
 
 import static net.sf.anathema.character.generic.impl.magic.charm.special.Element.Fire;
@@ -11,14 +12,14 @@ import static org.junit.Assert.assertThat;
 public class ElementTest {
 
   @Test
-  public void matchesAspectWithSameName() throws Exception {
-    ICasteType castetype = new DummyCasteType(Fire.name());
-    assertThat(Fire.matches(castetype), is(true));
+  public void matchesIdentificateWithSameName() throws Exception {
+    IIdentificate identificate = new Identificate(Fire.name());
+    assertThat(Fire.matches(identificate), is(true));
   }
 
   @Test
-  public void doesNotMatchDifferentCaste() throws Exception {
-    ICasteType castetype = new DummyCasteType("Dawn");
-    assertThat(Fire.matches(castetype), is(false));
+  public void doesNotMatchDifferentIdentificate() throws Exception {
+    IIdentificate identificate = new Identificate("Dawn");
+    assertThat(Fire.matches(identificate), is(false));
   }
 }
