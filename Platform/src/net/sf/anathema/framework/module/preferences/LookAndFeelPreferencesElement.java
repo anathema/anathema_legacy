c9a457f467e617a44267f98822dc7c5994ef74e8
Urs Reupke <ursreupke@gmail.com>
2012-01-03 13:38:32 +0100

    Gave names to anonymous classes

--- Platform/src/net/sf/anathema/framework/module/preferences/LookAndFeelPreferencesElement.java
+++ Platform/src/net/sf/anathema/framework/module/preferences/LookAndFeelPreferencesElement.java
@@ -53,7 +53,7 @@ public class LookAndFeelPreferencesElement implements IPreferencesElement {
     int selectIndex = -1;
     String resolvedClassName = resolveLookAndFeelClassName(className);
     for (int i = 0; i < itemCount; i++) {
-      LookAndFeelItem item = (LookAndFeelItem)currentCombo.getItemAt(i);
+      LookAndFeelItem item = (LookAndFeelItem) currentCombo.getItemAt(i);
       if (compareClassNames(resolvedClassName, item.getClassName())) {
         selected = item;
         selectIndex = i;
@@ -63,7 +63,7 @@ public class LookAndFeelPreferencesElement implements IPreferencesElement {
 
     if (selectIndex < 0) {
       for (int i = 0; i < itemCount; i++) {
-        LookAndFeelItem item = (LookAndFeelItem)currentCombo.getItemAt(i);
+        LookAndFeelItem item = (LookAndFeelItem) currentCombo.getItemAt(i);
         if (item.getClassName() == null) {
           selectIndex = i;
         }
@@ -83,8 +83,7 @@ public class LookAndFeelPreferencesElement implements IPreferencesElement {
     final String className = getLookAndFeelClassName();
     if (SwingUtilities.isEventDispatchThread()) {
       selectClass(className);
-    }
-    else {
+    } else {
       SwingUtilities.invokeLater(new Runnable() {
         @Override
         public void run() {
@@ -111,76 +110,26 @@ public class LookAndFeelPreferencesElement implements IPreferencesElement {
     // I simply mimicked the previous implementation.
 
     combo = new JComboBox();
-    combo.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        selected = (LookAndFeelItem)combo.getSelectedItem();
-        if (selected != null && selected.getClassName() == null) {
-          selected = new LookAndFeelItem(resources, null, getLookAndFeelClassName());
-        }
-        updateSelectedClassName();
-      }
-    });
+    combo.addActionListener(new LookAndFeelChangeListener(resources));
 
     if (allowCustom) {
       customLaf = new JTextField(25);
 
-      customLaf.getDocument().addDocumentListener(new DocumentListener() {
-        private void onChange() {
-          LookAndFeelItem currentSelection = (LookAndFeelItem)combo.getSelectedItem();
-          if (currentSelection != null) {
-            if (currentSelection.getClassName() == null) {
-              selected = new LookAndFeelItem(resources, null, customLaf.getText().trim());
-            }
-          }
-        }
-  
-        @Override
-        public void removeUpdate(DocumentEvent arg0) {
-          onChange();
-        }
-  
-        @Override
-        public void insertUpdate(DocumentEvent arg0) {
-          onChange();
-        }
-  
-        @Override
-        public void changedUpdate(DocumentEvent arg0) {
-          onChange();
-        }
-      });
+      customLaf.getDocument().addDocumentListener(new CustomLookAndFeelListener(resources));
     }
 
     List<LookAndFeelItem> items = new LinkedList<LookAndFeelItem>();
     if (allowCustom) {
       items.add(new LookAndFeelItem(resources));
     }
-    for (UIManager.LookAndFeelInfo info: UIManager.getInstalledLookAndFeels()) {
+    for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
       items.add(new LookAndFeelItem(resources, info.getName(), info.getClassName()));
     }
 
     combo.setModel(new DefaultComboBoxModel(items.toArray()));
     selectCurrentSettings();
 
-    return new IDialogComponent() {
-
-      public int getColumnCount() {
-        return 2;
-      }
-
-      public void fillInto(JPanel panel, int columnCount) {
-        panel.add(label);
-        if (allowCustom) {
-          panel.add(combo, GridDialogLayoutDataUtilities.createHorizontalSpanData(columnCount - 1));
-          panel.add(customLaf, GridDialogLayoutDataUtilities.createHorizontalSpanData(
-              columnCount,
-              GridDialogLayoutData.FILL_HORIZONTAL));
-        }
-        else {
-          panel.add(combo);
-        }
-      }
-    };
+    return new LookAndFeelDialogComponent(label);
   }
 
   @Override
@@ -191,16 +140,15 @@ public class LookAndFeelPreferencesElement implements IPreferencesElement {
   private String getSelectedClassName() {
     LookAndFeelItem currentSelected = selected;
     return currentSelected != null
-        ? currentSelected.getClassName()
-        : getLookAndFeelClassName();
+            ? currentSelected.getClassName()
+            : getLookAndFeelClassName();
   }
 
   public void savePreferences() {
     String selectedClass = getSelectedClassName();
     if (selectedClass != null) {
       SYSTEM_PREFERENCES.put(USER_LOOK_AND_FEEL_CLASSNAME, selectedClass);
-    }
-    else {
+    } else {
       SYSTEM_PREFERENCES.remove(USER_LOOK_AND_FEEL_CLASSNAME);
     }
   }
@@ -208,9 +156,9 @@ public class LookAndFeelPreferencesElement implements IPreferencesElement {
   private static String getLookAndFeelClassName() {
     return AnathemaPreferences.getDefaultPreferences().getUserLookAndFeel();
   }
-  
+
   private static String resolveLookAndFeelClassName(String className) {
-    return className != null ? className : UIManager.getSystemLookAndFeelClassName(); 
+    return className != null ? className : UIManager.getSystemLookAndFeelClassName();
   }
 
   public IIdentificate getCategory() {
@@ -220,18 +168,7 @@ public class LookAndFeelPreferencesElement implements IPreferencesElement {
   @Override
   public boolean isValid() {
     String className = getSelectedClassName();
-    if (className == null) {
-      return true;
-    }
-
-    Class<?> classOfLaf;
-    try {
-      classOfLaf = Class.forName(className);
-    } catch (ClassNotFoundException e) {
-      return false;
-    }
-
-    return LookAndFeel.class.isAssignableFrom(classOfLaf);
+    return className == null || isLookAndFeelClass(className);
   }
 
   @Override
@@ -241,8 +178,8 @@ public class LookAndFeelPreferencesElement implements IPreferencesElement {
       return false;
     }
     return !compareClassNames(
-        resolveLookAndFeelClassName(currentSelected.getClassName()),
-        resolveLookAndFeelClassName(getLookAndFeelClassName()));
+            resolveLookAndFeelClassName(currentSelected.getClassName()),
+            resolveLookAndFeelClassName(getLookAndFeelClassName()));
   }
 
   @Override
@@ -250,37 +187,85 @@ public class LookAndFeelPreferencesElement implements IPreferencesElement {
     selectCurrentSettings();
   }
 
-  private static class LookAndFeelItem {
-    private final boolean custom;
-    private final String name;
-    private final String className;
+  private boolean isLookAndFeelClass(String className) {
+    try {
+      Class<?> classOfLaf = Class.forName(className);
+      return LookAndFeel.class.isAssignableFrom(classOfLaf);
+    } catch (ClassNotFoundException e) {
+      return false;
+    }
+  }
 
-    public LookAndFeelItem(IResources resources) {
-      this(resources, null, null);
+  private class LookAndFeelDialogComponent implements IDialogComponent {
+
+    private final JLabel label;
+
+    public LookAndFeelDialogComponent(JLabel label) {
+      this.label = label;
     }
 
-    public LookAndFeelItem(IResources resources, String name, String className) {
-      this.custom = name == null;
-      this.name = name != null
-          ? name
-          : resources.getString("AnathemaCore.Tools.Preferences.CustomLookAndFeel"); //$NON-NLS-1$
-      this.className = !"".equals(className) ? className : null;
+    public int getColumnCount() {
+      return 2;
     }
 
-    public boolean isCustom() {
-      return custom;
+    public void fillInto(JPanel panel, int columnCount) {
+      panel.add(label);
+      if (allowCustom) {
+        panel.add(combo, GridDialogLayoutDataUtilities.createHorizontalSpanData(columnCount - 1));
+        panel.add(customLaf, GridDialogLayoutDataUtilities.createHorizontalSpanData(
+                columnCount,
+                GridDialogLayoutData.FILL_HORIZONTAL));
+      } else {
+        panel.add(combo);
+      }
+    }
+  }
+
+  private class CustomLookAndFeelListener implements DocumentListener {
+    private final IResources resources;
+
+    public CustomLookAndFeelListener(IResources resources) {
+      this.resources = resources;
     }
 
-    public String getName() {
-      return name;
+    private void onChange() {
+      LookAndFeelItem currentSelection = (LookAndFeelItem) combo.getSelectedItem();
+      if (currentSelection != null) {
+        if (currentSelection.getClassName() == null) {
+          selected = new LookAndFeelItem(resources, null, customLaf.getText().trim());
+        }
+      }
     }
 
-    public String getClassName() {
-      return className;
+    @Override
+    public void removeUpdate(DocumentEvent arg0) {
+      onChange();
     }
 
-    public String toString() {
-      return getName();
+    @Override
+    public void insertUpdate(DocumentEvent arg0) {
+      onChange();
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent arg0) {
+      onChange();
+    }
+  }
+
+  private class LookAndFeelChangeListener implements ActionListener {
+    private final IResources resources;
+
+    public LookAndFeelChangeListener(IResources resources) {
+      this.resources = resources;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+      selected = (LookAndFeelItem) combo.getSelectedItem();
+      if (selected != null && selected.getClassName() == null) {
+        selected = new LookAndFeelItem(resources, null, getLookAndFeelClassName());
+      }
+      updateSelectedClassName();
     }
   }
 }
