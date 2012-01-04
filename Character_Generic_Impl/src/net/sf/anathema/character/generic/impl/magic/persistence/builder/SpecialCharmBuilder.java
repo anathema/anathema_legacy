9b6cb9cf4a77345c625d3f864d1b0a175b366a97
Urs Reupke <ursreupke@gmail.com>
2012-01-04 12:40:48 +0100

    Traittypes are looked up in a separate class

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/persistence/builder/SpecialCharmBuilder.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/persistence/builder/SpecialCharmBuilder.java
@@ -1,35 +1,15 @@
 package net.sf.anathema.character.generic.impl.magic.persistence.builder;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
 import net.sf.anathema.character.generic.health.HealthLevelType;
-import net.sf.anathema.character.generic.impl.magic.charm.special.CharmTier;
-import net.sf.anathema.character.generic.impl.magic.charm.special.ComplexMultipleEffectCharm;
-import net.sf.anathema.character.generic.impl.magic.charm.special.ElementalMultipleEffectCharm;
-import net.sf.anathema.character.generic.impl.magic.charm.special.EssenceFixedMultiLearnableCharm;
-import net.sf.anathema.character.generic.impl.magic.charm.special.OxBodyTechniqueCharm;
-import net.sf.anathema.character.generic.impl.magic.charm.special.PrerequisiteModifyingCharm;
-import net.sf.anathema.character.generic.impl.magic.charm.special.StaticMultiLearnableCharm;
-import net.sf.anathema.character.generic.impl.magic.charm.special.StaticPainToleranceCharm;
-import net.sf.anathema.character.generic.impl.magic.charm.special.SubeffectCharm;
-import net.sf.anathema.character.generic.impl.magic.charm.special.TieredMultiLearnableCharm;
-import net.sf.anathema.character.generic.impl.magic.charm.special.TraitCapModifyingCharm;
-import net.sf.anathema.character.generic.impl.magic.charm.special.TraitDependentMultiLearnableCharm;
-import net.sf.anathema.character.generic.impl.magic.charm.special.UpgradableCharm;
+import net.sf.anathema.character.generic.impl.magic.charm.special.*;
 import net.sf.anathema.character.generic.impl.traits.EssenceTemplate;
 import net.sf.anathema.character.generic.magic.charms.special.ISpecialCharm;
 import net.sf.anathema.character.generic.traits.ITraitType;
-import net.sf.anathema.character.generic.traits.types.AbilityType;
-import net.sf.anathema.character.generic.traits.types.AttributeType;
 import net.sf.anathema.character.generic.traits.types.OtherTraitType;
-import net.sf.anathema.character.generic.traits.types.VirtueType;
 import net.sf.anathema.lib.xml.ElementUtilities;
-
 import org.dom4j.Element;
+
+import java.util.*;
 
 public class SpecialCharmBuilder
 {
@@ -38,7 +18,7 @@ public class SpecialCharmBuilder
 	private static final String ATTRIB_TRAIT = "trait";
 	private static final String ATTRIB_VALUE = "value";
 	private static final String ATTRIB_ESSENCE = "essence";
-	
+
 	private static final String TAG_OXBODY_CHARM = "oxbody";
 	private static final String TAG_OXBODY_PICK = "pick";
 	private static final String TAG_ZERO_HEALTH = "zeroHealthLevel";
@@ -47,43 +27,43 @@ public class SpecialCharmBuilder
 	private static final String TAG_FOUR_HEALTH = "fourHealthLevel";
 	private static final String TAG_INCAP_HEALTH = "incapHealthLevel";
 	private static final String TAG_DYING_HEALTH = "dyingHealthLevel";
-	
+
 	private static final String TAG_PAIN_TOLERANCE = "painTolerance";
 	private static final String TAG_LEVEL = "level";
-	
+
 	private static final String TAG_TRAIT_CAP_MODIFIER = "traitCapModifier";
-	
+
 	private static final String TAG_TRANSCENDENCE = "transcendence";
-	
+
 	private static final String TAG_REPURCHASES = "repurchases";
 	private static final String TAG_REPURCHASE = "repurchase";
 	private static final String ATTRIB_ABSOLUTE_MAX = "absoluteMax";
 	private static final String ATTRIB_LIMITING_TRAIT = "limitingTrait";
 	private static final String ATTRIB_LIMIT = "limit";
-	
+
 	private static final String TAG_ESSENCE_FIXED_REPURCHASES = "essenceFixedRepurchases";
-	
+
 	private static final String TAG_MULTI_EFFECT = "multiEffects";
 	private static final String TAG_EFFECT = "effect";
 	private static final String ATTRIB_PREREQ_EFFECT = "prereqEffect";
-	
+
 	private static final String TAG_UPGRADABLE = "upgradeable";
 	private static final String TAG_UPGRADE = "upgrade";
 	private static final String ATTRIB_BP_COST = "bpCost";
 	private static final String ATTRIB_XP_COST = "xpCost";
 	private static final String ATTRIB_REQUIRES_BASE = "requiresBase";
 	private static final String ATTRIB_TRAIT_VALUE = "traitValue";
-	
+
 	private static final String TAG_ELEMENTAL = "elemental";
-	
+
 	private static final String TAG_SUBEFFECTS = "subeffects";
 	private static final String TAG_SUBEFFECT = "subeffect";
 	private static final String TAG_BP_COST = "bpCost";
-	
-	public ISpecialCharm readSpecialCharm(Element charmElement, String id)
+  private final TraitTypeFinder traitTypeFinder = new TraitTypeFinder();
+
+  public ISpecialCharm readSpecialCharm(Element charmElement, String id)
 	{
-		ISpecialCharm specialCharm = null;
-		specialCharm = specialCharm == null ? readOxBodyCharm(charmElement, id) : specialCharm;
+		ISpecialCharm specialCharm = readOxBodyCharm(charmElement, id);
 		specialCharm = specialCharm == null ? readPainToleranceCharm(charmElement, id) : specialCharm;
 		specialCharm = specialCharm == null ? readTraitCapModifierCharm(charmElement, id) : specialCharm;
 		specialCharm = specialCharm == null ? readTranscendenceCharm(charmElement, id) : specialCharm;
@@ -95,7 +75,7 @@ public class SpecialCharmBuilder
 		specialCharm = specialCharm == null ? readSubEffectCharm(charmElement, id) : specialCharm;
 		return specialCharm;
 	}
-	
+
 	private ISpecialCharm readOxBodyCharm(Element charmElement, String id)
 	{
 		Element oxbodyElement = charmElement.element(TAG_OXBODY_CHARM);
@@ -104,7 +84,7 @@ public class SpecialCharmBuilder
 		String[] traitNameList = oxbodyElement.attributeValue(ATTRIB_TRAIT).split(",");
 		ITraitType[] traitList = new ITraitType[traitNameList.length];
 		for (int i = 0; i != traitList.length; i++)
-			traitList[i] = getTrait(traitNameList[i]);
+			traitList[i] = traitTypeFinder.getTrait(traitNameList[i]);
 		LinkedHashMap<String, HealthLevelType[]> healthPicks = new LinkedHashMap<String, HealthLevelType[]>();
 		for (Object pickObj : oxbodyElement.elements(TAG_OXBODY_PICK))
 		{
@@ -131,10 +111,9 @@ public class SpecialCharmBuilder
 			healthLevels.toArray(levels);
 			healthPicks.put(name, levels);
 		}
-		
 		return new OxBodyTechniqueCharm(id, traitList, healthPicks);
 	}
-	
+
 	private ISpecialCharm readPainToleranceCharm(Element charmElement, String id)
 	{
 		Element painToleranceElement = charmElement.element(TAG_PAIN_TOLERANCE);
@@ -145,11 +124,11 @@ public class SpecialCharmBuilder
 		int[] levelArray = new int[elements.size()];
 		for (int i = 0; i != elements.size(); i++)
 			levelArray[i] = Integer.parseInt(elements.get(i).attributeValue(ATTRIB_VALUE));
-				
+
 		return new StaticPainToleranceCharm(id, levelArray.length, levelArray);
-		
+
 	}
-	
+
 	private ISpecialCharm readTraitCapModifierCharm(Element charmElement, String id)
 	{
 		Element traitCapModifierElement = charmElement.element(TAG_TRAIT_CAP_MODIFIER);
@@ -158,11 +137,11 @@ public class SpecialCharmBuilder
 		String traitString = traitCapModifierElement.attributeValue(ATTRIB_TRAIT);
 		if (traitString == null)
 			traitString = id.split("\\.")[2];
-		ITraitType trait = getTrait(traitString);
+		ITraitType trait = traitTypeFinder.getTrait(traitString);
 		int modifier = Integer.parseInt(traitCapModifierElement.attributeValue(ATTRIB_MODIFIER));
 		return new TraitCapModifyingCharm(id, trait, modifier);
 	}
-	
+
 	private ISpecialCharm readTranscendenceCharm(Element charmElement, String id)
 	{
 		Element transcendenceElement = charmElement.element(TAG_TRANSCENDENCE);
@@ -172,47 +151,47 @@ public class SpecialCharmBuilder
 		int modifier = Integer.parseInt(transcendenceElement.attributeValue(ATTRIB_MODIFIER));
 		return new PrerequisiteModifyingCharm(id, trait, modifier);
 	}
-	
+
 	private ISpecialCharm readRepurchaseCharm(Element charmElement, String id)
 	{
 		Element repurchaseElement = charmElement.element(TAG_REPURCHASES);
 		if (repurchaseElement == null)
 			return null;
-		
+
 		String limitingTraitString = repurchaseElement.attributeValue(ATTRIB_LIMITING_TRAIT);
 		if (limitingTraitString != null)
 		{
-			ITraitType trait = getTrait(limitingTraitString);
-			
+			ITraitType trait = traitTypeFinder.getTrait(limitingTraitString);
+
 			int modifier = 0;
 			String modifierString = repurchaseElement.attributeValue(ATTRIB_MODIFIER);
 			try
 			{
 				modifier = Integer.parseInt(modifierString);
 			} catch (Exception e) { }
-			
+
 			int absoluteMax = EssenceTemplate.SYSTEM_ESSENCE_MAX + modifier;
 			String maxString = repurchaseElement.attributeValue(ATTRIB_ABSOLUTE_MAX);
 			try
 			{
 				absoluteMax = Integer.parseInt(maxString);
 			} catch (Exception e) { }
-			
+
 			return new TraitDependentMultiLearnableCharm(id, absoluteMax, trait, modifier);
 		}
-		
+
 		String limitString = repurchaseElement.attributeValue(ATTRIB_LIMIT);
 		if (limitString != null)
 		{
 			int limit = Integer.parseInt(limitString);
-			
+
 			return new StaticMultiLearnableCharm(id, limit);
 		}
-		
+
 		String traitString = repurchaseElement.attributeValue(ATTRIB_TRAIT);
-		ITraitType trait = getTrait(traitString);
+		ITraitType trait = traitTypeFinder.getTrait(traitString);
 		List<CharmTier> tiers = new ArrayList<CharmTier>();
-		
+
 		for (Object repurchaseObj : repurchaseElement.elements(TAG_REPURCHASE))
 		{
 			Element repurchase = (Element) repurchaseObj;
@@ -225,10 +204,10 @@ public class SpecialCharmBuilder
 				int traitValue = Integer.parseInt(repurchase.attributeValue(ATTRIB_TRAIT));
 				tier = new CharmTier(essence, traitValue);
 			}
-			
+
 			tiers.add(tier);
 		}
-		
+
 		CharmTier[] tierArray = new CharmTier[tiers.size()];
 		tiers.toArray(tierArray);
         return createTieredCharm(id, trait, tierArray);
@@ -248,7 +227,7 @@ public class SpecialCharmBuilder
 			return null;
 		return new EssenceFixedMultiLearnableCharm(id, EssenceTemplate.SYSTEM_ESSENCE_MAX, OtherTraitType.Essence);
 	}
-	
+
 	private ISpecialCharm readMultiEffectCharm(Element charmElement, String id)
 	{
 		Element multiEffectElement = charmElement.element(TAG_MULTI_EFFECT);
@@ -261,15 +240,15 @@ public class SpecialCharmBuilder
 			Element effect = (Element)effectObj;
 			String name = effect.attributeValue(ATTRIB_NAME);
 			effects.add(name);
-			
+
 			String prereqEffect = effect.attributeValue(ATTRIB_PREREQ_EFFECT);
-			prereqEffectMap.put(name, prereqEffect);			
+			prereqEffectMap.put(name, prereqEffect);
 		}
 		String[] effectArray = new String[effects.size()];
 		effects.toArray(effectArray);
 		return new ComplexMultipleEffectCharm(id, effectArray, prereqEffectMap);
 	}
-	
+
 	private ISpecialCharm readUpgradableCharm(Element charmElement, String id)
 	{
 		Element upgradableElement = charmElement.element(TAG_UPGRADABLE);
@@ -283,30 +262,30 @@ public class SpecialCharmBuilder
 		Map<String, Integer> essenceMins = new HashMap<String, Integer>();
 		Map<String, Integer> traitMins = new HashMap<String, Integer>();
 		Map<String, ITraitType> traits = new HashMap<String, ITraitType>();
-		
+
 		for (Object upgradeObj : upgradableElement.elements(TAG_UPGRADE))
 		{
 			Element upgrade = (Element)upgradeObj;
 			String name = upgrade.attributeValue(ATTRIB_NAME);
 			upgrades.add(name);
-			
+
 			try
 			{
 				Integer bpCost = ElementUtilities.getIntAttrib(upgrade, ATTRIB_BP_COST, -1);
 				if (bpCost != -1) bpCosts.put(name, bpCost);
-			
+
 				Integer xpCost = ElementUtilities.getIntAttrib(upgrade, ATTRIB_XP_COST, 1);
 				xpCosts.put(name, xpCost);
-				
+
 				Integer essenceMin = ElementUtilities.getIntAttrib(upgrade, ATTRIB_ESSENCE, -1);
 				if (essenceMin != -1) essenceMins.put(name, essenceMin);
-				
+
 				Integer traitMin = ElementUtilities.getIntAttrib(upgrade, ATTRIB_TRAIT_VALUE, -1);
 				if (traitMin != -1) traitMins.put(name, essenceMin);
-				
+
 				String trait = upgrade.attributeValue(ATTRIB_TRAIT);
-				if (trait != null) traits.put(name, getTrait(trait));
-				
+				if (trait != null) traits.put(name, traitTypeFinder.getTrait(trait));
+
 			} catch (Exception e) { }
 		}
 		String[] upgradeArray = new String[upgrades.size()];
@@ -314,7 +293,7 @@ public class SpecialCharmBuilder
 		return new UpgradableCharm(id, upgradeArray, requiresBase,
 				bpCosts, xpCosts, essenceMins, traitMins, traits);
 	}
-	
+
 	private ISpecialCharm readElementalCharm(Element charmElement, String id)
 	{
 		Element elementalElement = charmElement.element(TAG_ELEMENTAL);
@@ -322,7 +301,7 @@ public class SpecialCharmBuilder
 			return null;
 		return new ElementalMultipleEffectCharm(id);
 	}
-	
+
 	private ISpecialCharm readSubEffectCharm(Element charmElement, String id)
 	{
 		Element subeffectElement = charmElement.element(TAG_SUBEFFECTS);
@@ -340,71 +319,11 @@ public class SpecialCharmBuilder
 		subeffects.toArray(effects);
 		return new SubeffectCharm(id, effects, cost);
 	}
-	
+
 	private ITraitType getGenericTraitType(String value)
 	{
 		String[] split = value.split("\\.");
 		String traitString = split[split.length - 1];
-		return getTrait(traitString);
+		return traitTypeFinder.getTrait(traitString);
 	}
-	
-	private ITraitType getTrait(String value)
-	{
-		ITraitType trait = null;
-		trait = trait == null ? getAbilityType(value) : trait;
-		trait = trait == null ? getAttributeType(value) : trait;
-		trait = trait == null ? getVirtueType(value) : trait;
-		trait = trait == null ? getOtherType(value) : trait;
-		return trait;
-	}
-	
-	private AbilityType getAbilityType(String value)
-	{
-		try
-		{
-			return AbilityType.valueOf(value);
-		}
-		catch (Exception e)
-		{
-			return null;
-		}
-	}
-	
-	private AttributeType getAttributeType(String value)
-	{
-		try
-		{
-			return AttributeType.valueOf(value);
-		}
-		catch (Exception e)
-		{
-			return null;
-		}
-	}
-	
-	private VirtueType getVirtueType(String value)
-	{
-		try
-		{
-			return VirtueType.valueOf(value);
-		}
-		catch (Exception e)
-		{
-			return null;
-		}
-	}
-	
-	private OtherTraitType getOtherType(String value)
-	{
-		try
-		{
-			return OtherTraitType.valueOf(value);
-		}
-		catch (Exception e)
-		{
-			return null;
-		}
-	}
-	
-	
 }
