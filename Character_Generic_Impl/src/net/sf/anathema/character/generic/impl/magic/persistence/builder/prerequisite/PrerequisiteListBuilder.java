cdf3b93b3df329eb96ca07bbc74ad188eb29d483
Urs Reupke <ursreupke@gmail.com>
2012-01-18 18:31:13 +0100

    Inlined return value

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/persistence/builder/prerequisite/PrerequisiteListBuilder.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/magic/persistence/builder/prerequisite/PrerequisiteListBuilder.java
@@ -4,6 +4,7 @@ import static net.sf.anathema.character.generic.impl.magic.ICharmXMLConstants.AT
 import static net.sf.anathema.character.generic.impl.magic.ICharmXMLConstants.ATTRIB_VALUE;
 import static net.sf.anathema.character.generic.impl.magic.ICharmXMLConstants.TAG_ESSENCE;
 import static net.sf.anathema.character.generic.impl.magic.ICharmXMLConstants.TAG_SELECTIVE_CHARM_GROUP;
+import static net.sf.anathema.character.generic.traits.types.OtherTraitType.Essence;
 
 import java.util.List;
 
@@ -59,8 +60,7 @@ public class PrerequisiteListBuilder {
     catch (NumberFormatException e) {
       throw new CharmException("Bad value on essence prerequisite."); //$NON-NLS-1$
     }
-    IGenericTrait essence = new ValuedTraitType(OtherTraitType.Essence, minValue);
-    return essence;
+    return new ValuedTraitType(Essence, minValue);
   }
 
   private SelectiveCharmGroupTemplate[] buildSelectiveCharmGroups(Element prerequisiteListElement)
