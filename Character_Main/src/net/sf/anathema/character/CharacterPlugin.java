a222f6ba3ffe9c401ea04649041209b856b3d105
Urs Reupke <ursreupke@gmail.com>
2012-01-20 21:24:00 +0100

    CharacterPlugin uses Reflections to look up charms Alternative to existing system

--- Character_Main/src/net/sf/anathema/character/CharacterPlugin.java
+++ Character_Main/src/net/sf/anathema/character/CharacterPlugin.java
@@ -3,8 +3,12 @@ package net.sf.anathema.character;
 import static java.text.MessageFormat.format;
 
 import java.net.URL;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.sf.anathema.ProxySplashscreen;
+import net.sf.anathema.character.generic.framework.reflections.DefaultCharacterReflections;
 import net.sf.anathema.character.generic.impl.magic.persistence.CharmCompiler;
 import net.sf.anathema.initialization.plugin.AnathemaPluginManager;
 
@@ -20,6 +24,9 @@ public class CharacterPlugin extends Plugin {
   private static final String PARAM_LIST = "list"; //$NON-NLS-1$
   private static final String EXTENSION_POINT_CHARM_LIST = "CharmList"; //$NON-NLS-1$
   private static final String PLUGIN_ID = "net.sf.anathema.character"; //$NON-NLS-1$
+  private static final String Charm_File_Recognition_Pattern = "Charms.*\\.xml";
+  //matches stuff like data/charms/solar/Charms_Solar_SecondEdition_Occult.xml
+  private static final String Charm_Data_Extraction_Pattern = ".*/Charms_(.*?)_(.*?)(?:_.*)?\\.xml";
 
   @Override
   protected void doStart() throws Exception {
@@ -34,14 +41,33 @@ public class CharacterPlugin extends Plugin {
         String typeString = typeParameter.valueAsString();
         String ruleString = rules.valueAsString();
         String pathString = path.valueAsString();
-        URL resource = getClass().getClassLoader().getResource(pathString);
-        if (resource == null) {
-          throw new Exception(format("No resource found at {0} for {1}, {2}.", pathString, typeString, ruleString));
-        }
-        charmCompiler.registerCharmFile(typeString, ruleString, resource);
+        registerCharmFile(charmCompiler, typeString, ruleString, pathString);
       }
     }
+    DefaultCharacterReflections reflections = new DefaultCharacterReflections();
+    getCharmFilesFromReflection(reflections, charmCompiler);
     charmCompiler.buildCharms();
+  }
+
+  private void getCharmFilesFromReflection(DefaultCharacterReflections reflections, CharmCompiler charmCompiler) throws Exception {
+    Set<String> charmFiles = reflections.getResourcesMatching(Charm_File_Recognition_Pattern);
+    System.out.println(charmFiles);
+    Pattern pattern = Pattern.compile(Charm_Data_Extraction_Pattern);
+    for (String charmFile : charmFiles) {
+      Matcher matcher = pattern.matcher(charmFile);
+      matcher.matches();
+      String typeString = matcher.group(1);
+      String ruleString = matcher.group(2);
+      registerCharmFile(charmCompiler, typeString, ruleString, charmFile);
+    }
+  }
+
+  private void registerCharmFile(CharmCompiler charmCompiler, String typeString, String ruleString, String pathString) throws Exception {
+    URL resource = getClass().getClassLoader().getResource(pathString);
+    if (resource == null) {
+      throw new Exception(format("No resource found at {0} for {1}, {2}.", pathString, typeString, ruleString));
+    }
+    charmCompiler.registerCharmFile(typeString, ruleString, resource);
   }
 
   @Override
