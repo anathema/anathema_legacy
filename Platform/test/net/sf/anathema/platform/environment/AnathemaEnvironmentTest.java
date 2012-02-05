a6a47bad7e961989d2f5322b201b9bfb84173dac
Urs Reupke <ursreupke@gmail.com>
2012-02-05 11:12:52 +0100

    Removed system property "development"

--- Platform/test/net/sf/anathema/platform/environment/AnathemaEnvironmentTest.java
+++ Platform/test/net/sf/anathema/platform/environment/AnathemaEnvironmentTest.java
@@ -1,38 +1,17 @@
 package net.sf.anathema.platform.environment;
 
-import java.util.Locale;
+import net.sf.anathema.framework.environment.AnathemaEnvironment;
+import org.junit.Assert;
+import org.junit.Test;
 
 import javax.swing.ToolTipManager;
 import javax.swing.UIManager;
 import javax.swing.plaf.metal.MetalLookAndFeel;
-
-import net.sf.anathema.framework.environment.AnathemaEnvironment;
-
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import java.util.Locale;
 
 public class AnathemaEnvironmentTest {
 
-  private DummyAnathemaPreferences preferences;
-
-  @Before
-  public void setUp() throws Exception {
-    System.setProperty("development", "true"); //$NON-NLS-1$//$NON-NLS-2$
-    preferences = new DummyAnathemaPreferences();
-  }
-
-  @Test
-  public void testNotDevelopment() throws Exception {
-    System.clearProperty("development"); //$NON-NLS-1$
-    Assert.assertFalse(AnathemaEnvironment.isDevelopment());
-  }
-
-  @Test
-  public void testDevelopment() throws Exception {
-    Assert.assertTrue(AnathemaEnvironment.isDevelopment());
-  }
+  private DummyAnathemaPreferences preferences = new DummyAnathemaPreferences();
 
   @Test
   public void testSetDefaultLocale() throws Exception {
@@ -64,10 +43,5 @@ public class AnathemaEnvironmentTest {
     preferences.setUserLookAndFeel(MetalLookAndFeel.class.getName());
     AnathemaEnvironment.initLookAndFeel(preferences);
     Assert.assertEquals(MetalLookAndFeel.class, UIManager.getLookAndFeel().getClass());
-  }
-
-  @After
-  public void tearDown() throws Exception {
-    System.clearProperty("development"); //$NON-NLS-1$    
   }
 }
