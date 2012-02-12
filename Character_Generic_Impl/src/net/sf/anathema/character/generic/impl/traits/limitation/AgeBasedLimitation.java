afd2108c9369ef62e4218222a38b1ccce6a0bf03
Urs Reupke <ursreupke@gmail.com>
2012-02-12 19:27:47 +0100

    Age based limitations are as written, thanks to Kotonaru

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/traits/limitation/AgeBasedLimitation.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/traits/limitation/AgeBasedLimitation.java
@@ -3,36 +3,32 @@ package net.sf.anathema.character.generic.impl.traits.limitation;
 import net.sf.anathema.character.generic.character.ILimitationContext;
 import net.sf.anathema.character.generic.template.ITraitLimitation;
 
-public class AgeBasedLimitation implements ITraitLimitation
-{
-	private final int absoluteLimit;
-	private final int[] ageArray = { 100, 200, 300, 400, 500 };
-	
-	public AgeBasedLimitation(int maxValue)
-	{
-		this.absoluteLimit = maxValue;
-	}
-	
-	@Override
-	public int getAbsoluteLimit(ILimitationContext limitationContext)
-	{
-		return absoluteLimit;
-	}
+public class AgeBasedLimitation implements ITraitLimitation {
+  private final int absoluteLimit;
+  private final int[] ageArray = {100, 250, 500, 1000};
 
-	@Override
-	public int getCurrentMaximum(ILimitationContext limitationContext, boolean modified)
-	{
-		int age = limitationContext.getAge();
-		int categories = 0;
-		for (; categories != ageArray.length; categories++)
-			if (age < ageArray[categories])
-				break;
-		return 5 + categories;
-	}
-	
-	@Override
-	public AgeBasedLimitation clone()
-	{
-	  return this;
-	}
+  public AgeBasedLimitation(int maxValue) {
+    this.absoluteLimit = maxValue;
+  }
+
+  @Override
+  public int getAbsoluteLimit(ILimitationContext limitationContext) {
+    return absoluteLimit;
+  }
+
+  @Override
+  public int getCurrentMaximum(ILimitationContext limitationContext, boolean modified) {
+    int age = limitationContext.getAge();
+    for (int categories = 0; categories != ageArray.length; categories++) {
+      if (age < ageArray[categories]) {
+        return 5 + categories;
+      }
+    }
+    return absoluteLimit;
+  }
+
+  @Override
+  public AgeBasedLimitation clone() {
+    return this;
+  }
 }
