64a506cce55f8dd4c6feb5f4edb3b2717780e202
Attila Kelemen <attila.kelemen85@gmail.com>
2012-02-17 00:08:54 +0100

    Anathema can now be started from the IDE even if it the path to the repository contains a space character.

--- Platform/src/net/sf/anathema/initialization/reflections/EncodingClasspathHelper.java
+++ Platform/src/net/sf/anathema/initialization/reflections/EncodingClasspathHelper.java
@@ -5,38 +5,37 @@ import net.sf.anathema.lib.logging.Logger;
 import sun.net.www.ParseUtil;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.Set;
 
 public class EncodingClasspathHelper {
-
   private static final Logger logger = Logger.getLogger(EncodingClasspathHelper.class);
-
 
   public static Set<URL> forPackage(String name, ClassLoader... classLoaders) {
     Set<URL> result = Sets.newHashSet();
     String resourceName = name.replace(".", "/");
-    String encodedResourceName = createUrlCompareString(resourceName);
     for (ClassLoader classLoader : classLoaders) {
       try {
         Enumeration<URL> urls = classLoader.getResources(resourceName);
         while (urls.hasMoreElements()) {
           URL url = urls.nextElement();
-          String urlCompareString = createUrlCompareString(url.toExternalForm());
-          int index = urlCompareString.lastIndexOf(encodedResourceName);
+          URI uri = url.toURI();
+          String uriPath = uri.getPath();
+
+          int index = uriPath.lastIndexOf(resourceName);
           if (index != -1) {
-            String partialUrl = urlCompareString.substring(0, index);
-            if (partialUrl.contains("!")) {
-              String jarUrl = deconstructUrlCompareString(partialUrl);
-              result.add(new URL(jarUrl));
-            } else {
-              result.add(new URL(partialUrl));
-            }
+            String partialPath = uriPath.substring(0, index);
+            URI trimmedUri = new URI(uri.getScheme(), uri.getAuthority(), partialPath, uri.getQuery(), uri.getFragment());
+            result.add(trimmedUri.toURL());
           }
         }
       } catch (IOException e) {
         logger.error("Could not resolve URL.", e);
+      } catch (URISyntaxException e) {
+        logger.error("URL could not be converted to URI.", e);
       }
     }
     return result;
