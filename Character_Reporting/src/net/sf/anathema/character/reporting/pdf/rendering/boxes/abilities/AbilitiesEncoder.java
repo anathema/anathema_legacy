64a22a9130c58cfdecb1ac1ea8f925d88be67c59
dirk <dirkwalter@users.sourceforge.net>
2012-02-17 12:02:46 -0500

    Minor cleanup to remove some warnings.

--- Character_Reporting/src/net/sf/anathema/character/reporting/pdf/rendering/boxes/abilities/AbilitiesEncoder.java
+++ Character_Reporting/src/net/sf/anathema/character/reporting/pdf/rendering/boxes/abilities/AbilitiesEncoder.java
@@ -7,23 +7,23 @@ import net.sf.anathema.lib.resources.IResources;
 
 public class AbilitiesEncoder {
 
-  public static FavorableTraitBoxContentEncoder createWithCraftsOnly(IResources resources, int craftCount) {
-    FavorableTraitBoxContentEncoder encoder = new FavorableTraitBoxContentEncoder(AbilitiesContent.class);
+  public static FavorableTraitBoxContentEncoder<AbilitiesContent> createWithCraftsOnly(IResources resources, int craftCount) {
+    FavorableTraitBoxContentEncoder<AbilitiesContent> encoder = new FavorableTraitBoxContentEncoder<AbilitiesContent>(AbilitiesContent.class);
     PdfTraitEncoder traitEncoder = encoder.getTraitEncoder();
     CraftEncoder craftEncoder = new CraftEncoder(resources, traitEncoder, craftCount);
     encoder.addNamedTraitEncoder(craftEncoder);
     return encoder;
   }
 
-  public static FavorableTraitBoxContentEncoder createWithSpecialtiesOnly(IResources resources, int specialtyCount) {
-    FavorableTraitBoxContentEncoder encoder = new FavorableTraitBoxContentEncoder(AbilitiesContent.class);
+  public static FavorableTraitBoxContentEncoder<AbilitiesContent> createWithSpecialtiesOnly(IResources resources, int specialtyCount) {
+    FavorableTraitBoxContentEncoder<AbilitiesContent> encoder = new FavorableTraitBoxContentEncoder<AbilitiesContent>(AbilitiesContent.class);
     PdfTraitEncoder traitEncoder = encoder.getTraitEncoder();
     encoder.addNamedTraitEncoder(new SpecialtiesEncoder(resources, traitEncoder, specialtyCount));
     return encoder;
   }
 
-  public static FavorableTraitBoxContentEncoder createWithCraftsAndSpecialties(IResources resources, int craftCount, int specialtyCount) {
-    FavorableTraitBoxContentEncoder encoder = new FavorableTraitBoxContentEncoder(AbilitiesContent.class);
+  public static FavorableTraitBoxContentEncoder<AbilitiesContent> createWithCraftsAndSpecialties(IResources resources, int craftCount, int specialtyCount) {
+    FavorableTraitBoxContentEncoder<AbilitiesContent> encoder = new FavorableTraitBoxContentEncoder<AbilitiesContent>(AbilitiesContent.class);
     PdfTraitEncoder traitEncoder = encoder.getTraitEncoder();
     encoder.addNamedTraitEncoder(new CraftEncoder(resources, traitEncoder, craftCount));
     encoder.addNamedTraitEncoder(new SpecialtiesEncoder(resources, traitEncoder, specialtyCount));
