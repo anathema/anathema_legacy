7d89e55133fbf398e757d21ac27272065689aa8a
Attila Kelemen <attila.kelemen85@gmail.com>
2012-02-21 00:33:00 +0100

    Fixed path bug introduced in commit 64a506cce55f8dd4c6feb5f4edb3b2717780e202 which prevents to use jars for class paths.

--- Platform/src/net/sf/anathema/initialization/reflections/EncodingClasspathHelper.java
+++ Platform/src/net/sf/anathema/initialization/reflections/EncodingClasspathHelper.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Sets;
 import net.sf.anathema.lib.logging.Logger;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -13,6 +14,21 @@ import java.util.Set;
 public class EncodingClasspathHelper {
   private static final Logger logger = Logger.getLogger(EncodingClasspathHelper.class);
 
+  private static URL removeTrailingPath(URL url, String trailingPath) throws URISyntaxException, MalformedURLException {
+    URI uri = url.toURI();
+    String specificPart = uri.getSchemeSpecificPart();
+    if (specificPart == null) {
+      return null;
+    }
+    int index = specificPart.lastIndexOf(trailingPath);
+    if (index < 0) {
+      return null;
+    }
+    String partialPart = specificPart.substring(0, index);
+    URI trimmedUri = new URI(uri.getScheme(), partialPart, uri.getFragment());
+    return trimmedUri.toURL();
+  }
+
   public static Set<URL> forPackage(String name, ClassLoader... classLoaders) {
     Set<URL> result = Sets.newHashSet();
     String resourceName = name.replace(".", "/");
@@ -21,14 +37,9 @@ public class EncodingClasspathHelper {
         Enumeration<URL> urls = classLoader.getResources(resourceName);
         while (urls.hasMoreElements()) {
           URL url = urls.nextElement();
-          URI uri = url.toURI();
-          String uriPath = uri.getPath();
-
-          int index = uriPath.lastIndexOf(resourceName);
-          if (index != -1) {
-            String partialPath = uriPath.substring(0, index);
-            URI trimmedUri = new URI(uri.getScheme(), uri.getAuthority(), partialPath, uri.getQuery(), uri.getFragment());
-            result.add(trimmedUri.toURL());
+          URL trimmedUrl = removeTrailingPath(url, resourceName);
+          if (trimmedUrl != null) {
+            result.add(trimmedUrl);
           }
         }
       } catch (IOException e) {
