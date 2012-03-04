954cb6164ebf072c4ae403ca9892d590ed9c9ecd
Urs Reupke <ursreupke@gmail.com>
2012-03-04 22:32:11 +0100

    Cleanup

--- Character_Library/src/net/sf/anathema/character/library/overview/OverviewCategory.java
+++ Character_Library/src/net/sf/anathema/character/library/overview/OverviewCategory.java
@@ -1,11 +1,5 @@
 package net.sf.anathema.character.library.overview;
 
-import java.awt.Font;
-
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-import javax.swing.border.TitledBorder;
-
 import net.disy.commons.swing.layout.grid.GridDialogLayout;
 import net.sf.anathema.lib.gui.IView;
 import net.sf.anathema.lib.workflow.labelledvalue.ILabelledAlotmentView;
@@ -13,6 +7,11 @@ import net.sf.anathema.lib.workflow.labelledvalue.IValueView;
 import net.sf.anathema.lib.workflow.labelledvalue.view.AbstractLabelledValueView;
 import net.sf.anathema.lib.workflow.labelledvalue.view.LabelledAlotmentView;
 import net.sf.anathema.lib.workflow.labelledvalue.view.LabelledIntegerValueView;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+import java.awt.Font;
 
 public class OverviewCategory implements IOverviewCategory, IView {
 
@@ -33,24 +32,28 @@ public class OverviewCategory implements IOverviewCategory, IView {
     return panel;
   }
 
+  @Override
   public ILabelledAlotmentView addAlotmentView(String labelText, int maxValueLength) {
     LabelledAlotmentView view = new LabelledAlotmentView(labelText, 0, 0, maxValueLength);
     view.addTo(panel);
     return view;
   }
 
+  @Override
   public IAdditionalAlotmentView addAdditionalAlotmentView(String labelText, int maxValueLength) {
     LabelledAdditionalAlotmentView view = new LabelledAdditionalAlotmentView(labelText, maxValueLength);
     view.addTo(panel);
     return view;
   }
 
+  @Override
   public IValueView<Integer> addIntegerValueView(String labelText, int maxValueLength) {
     LabelledIntegerValueView view = new LabelledIntegerValueView(labelText, 0, true, maxValueLength);
     view.addComponents(panel, 4);
     return view;
   }
 
+  @Override
   public IValueView<String> addStringValueView(String labelText) {
     LabelledOverviewStringValueView view = new LabelledOverviewStringValueView(labelText, ""); //$NON-NLS-1$
     view.addComponents(panel, 4);
