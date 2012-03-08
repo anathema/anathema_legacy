9899058e5e83e69c89e1d6199aea5de1072d2702
Urs Reupke <ursreupke@gmail.com>
2012-03-08 19:18:09 +0100

    Deleted Acceptance-Tests, since they were inactive for ages.

--- Anathema_Acceptance_Tests/test/net/sf/anathema/acceptance/fixture/character/template/CheckEditionFixture.java
+++
@@ -1,8 +0,0 @@
-package net.sf.anathema.acceptance.fixture.character.template;
-
-public class CheckEditionFixture extends AbstractTemplateColumnFixture {
-
-  public String getEdition() {
-    return getTemplate().getEdition().getId();
-  }
-}
