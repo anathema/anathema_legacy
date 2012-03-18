267076e0d4df11c096e058ee9d08dc41dd6a1631
Urs Reupke <ursreupke@gmail.com>
2012-03-18 19:13:00 +0100

    Removed 1st Edition Abyssals

--- Character_Abyssal/src/net/sf/anathema/character/abyssal/resonance/AbyssalResonanceParser.java
+++ Character_Abyssal/src/net/sf/anathema/character/abyssal/resonance/AbyssalResonanceParser.java
@@ -1,13 +1,12 @@
 package net.sf.anathema.character.abyssal.resonance;
 
-import net.sf.anathema.character.abyssal.resonance.AbyssalResonanceTemplate;
 import net.sf.anathema.character.generic.framework.xml.additional.IAdditionalTemplateParser;
 import net.sf.anathema.character.generic.template.additional.IAdditionalTemplate;
-
 import org.dom4j.Element;
 
 public class AbyssalResonanceParser implements IAdditionalTemplateParser {
 
+  @Override
   public IAdditionalTemplate parse(Element element) {
     return new AbyssalResonanceTemplate();
   }
