c8ee633afd8f8e049c31784672b437c2cef91a58
Urs Reupke <ursreupke@gmail.com>
2012-03-24 14:59:02 +0100

    Deletion does nothing if no characters exist

--- Character_Main_Impl/src/net/sf/anathema/character/generic/impl/bootjob/FirstEditionDeleter.java
+++ Character_Main_Impl/src/net/sf/anathema/character/generic/impl/bootjob/FirstEditionDeleter.java
@@ -7,6 +7,7 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 import static net.sf.anathema.character.impl.module.ExaltedCharacterItemTypeConfiguration.CHARACTER_ITEM_TYPE_ID;
@@ -37,6 +38,9 @@ public class FirstEditionDeleter {
   private List<File> getCharacter(IAnathemaModel model) {
     IItemType character = model.getItemTypeRegistry().getById(CHARACTER_ITEM_TYPE_ID);
     File itemTypeFolder = model.getRepository().getRepositoryFileResolver().getItemTypeFolder(character);
+    if (!itemTypeFolder.exists()){
+      return Collections.emptyList();
+    }
     return Lists.newArrayList(itemTypeFolder.listFiles());
   }
 }
