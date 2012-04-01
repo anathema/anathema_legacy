5fdc277893f756ec690ecff9d58da77461ccc075
Urs Reupke <ursreupke@gmail.com>
2012-04-01 21:39:37 +0200

    Extended sheet is beta content

--- Character_Main_Impl/src/net/sf/anathema/character/impl/module/reporting/SheetReportFactory.java
+++ Character_Main_Impl/src/net/sf/anathema/character/impl/module/reporting/SheetReportFactory.java
@@ -1,5 +1,6 @@
 package net.sf.anathema.character.impl.module.reporting;
 
+import com.google.common.collect.Lists;
 import net.sf.anathema.character.generic.framework.CharacterGenericsExtractor;
 import net.sf.anathema.character.generic.framework.ICharacterGenerics;
 import net.sf.anathema.character.impl.reporting.ExtendedSheetReport;
@@ -10,18 +11,28 @@ import net.sf.anathema.framework.IAnathemaModel;
 import net.sf.anathema.framework.initialization.IReportFactory;
 import net.sf.anathema.framework.module.preferences.PageSizePreference;
 import net.sf.anathema.framework.reporting.Report;
+import net.sf.anathema.framework.reporting.pdf.AbstractPdfReport;
 import net.sf.anathema.initialization.ReportFactory;
 import net.sf.anathema.lib.resources.IResources;
+
+import java.util.List;
+
+import static net.sf.anathema.framework.module.preferences.EnableBetaContentPreferencesElement.enableBetaContent;
 
 @ReportFactory
 public class SheetReportFactory implements IReportFactory {
 
+  @Override
   public Report[] createReport(IResources resources, IAnathemaModel model) {
     ICharacterGenerics characterGenerics = CharacterGenericsExtractor.getGenerics(model);
     PageSizePreference pageSizePreference = new PageSizePreference();
-    return new Report[]{new PortraitSimpleExaltSheetReport(resources, characterGenerics, pageSizePreference),
+    List<AbstractPdfReport> reports = Lists.newArrayList(
+            new PortraitSimpleExaltSheetReport(resources, characterGenerics, pageSizePreference),
             new PortraitSimpleMortalSheetReport(resources, characterGenerics, pageSizePreference),
-            new ExtendedSheetReport(resources, characterGenerics, pageSizePreference),
-            new LandscapeExaltSheetReport(resources, characterGenerics, pageSizePreference)};
+            new LandscapeExaltSheetReport(resources, characterGenerics, pageSizePreference));
+    if (enableBetaContent()) {
+      reports.add(new ExtendedSheetReport(resources, characterGenerics, pageSizePreference));
+    }
+    return reports.toArray(new Report[reports.size()]);
   }
 }
