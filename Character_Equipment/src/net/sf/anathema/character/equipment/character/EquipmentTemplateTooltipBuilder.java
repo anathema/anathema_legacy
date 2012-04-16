a639e4dbe09604c03eca4a26c65ac35d18dfe563
Sean Mattox <sean.mattox@gmail.com>
2012-04-16 12:59:55 -0700

    Beautifying the character equipment tooltips a bit.

--- Character_Equipment/src/net/sf/anathema/character/equipment/character/EquipmentTemplateTooltipBuilder.java
+++ Character_Equipment/src/net/sf/anathema/character/equipment/character/EquipmentTemplateTooltipBuilder.java
@@ -2,24 +2,30 @@ package net.sf.anathema.character.equipment.character;
 
 import net.sf.anathema.character.equipment.template.IEquipmentTemplate;
 import net.sf.anathema.character.generic.equipment.weapon.IEquipmentStats;
+import net.sf.anathema.lib.lang.AnathemaStringUtilities;
 
 public class EquipmentTemplateTooltipBuilder {
 	private final String UNICODE_DOT = "\u25CF";
+	private final String BREAKPOINT = "<br>";
 	
 	public String getTooltipDescription(IEquipmentTemplate template) {
 		  StringBuilder builder = new StringBuilder();
 		  builder.append("<html>");
-		  builder.append(template.getName());
+		  builder.append("<b>" + template.getName() + "</b>");
 		  if (template.getCost() != null) {
 			builder.append(" (" + template.getCost().toString().replace("*", UNICODE_DOT) + ")");
 		  }
-		  builder.append("<br>");
-		  if (!template.getDescription().isEmpty()) {
-			builder.append(template.getDescription() + "<br>");
-		  }
+		  builder.append(BREAKPOINT);
 		  IEquipmentStats[] statsSet = template.getStats();
 		  for (IEquipmentStats stats : statsSet) {
 			builder.append((stats != statsSet[0] ? ", " : "") + stats.getId());
+		  }
+		  builder.append(BREAKPOINT);
+		  if (!template.getDescription().isEmpty()) {
+			builder.append("<i>" +
+					AnathemaStringUtilities.createFixedWidthParagraph(template.getDescription(),
+							BREAKPOINT, 80) +
+						   "</i>");
 		  }
 		  builder.append("</html>");
 		  return builder.toString();
