f083c036e7e1ed79b2c01d13a98b5d6922bf84d6
Urs Reupke <ursreupke@gmail.com>
2012-04-21 13:17:22 +0200

    Disabled cache, it hinders changed descriptions to show up and is not the main problem. #152.

--- Character_Main/src/net/sf/anathema/character/presenter/magic/combo/LegalityCharmRenderer.java
+++ Character_Main/src/net/sf/anathema/character/presenter/magic/combo/LegalityCharmRenderer.java
@@ -1,6 +1,5 @@
 package net.sf.anathema.character.presenter.magic.combo;
 
-import net.sf.anathema.character.generic.framework.magic.stringbuilder.CharmInfoStringCache;
 import net.sf.anathema.character.generic.framework.magic.stringbuilder.ICharmInfoStringBuilder;
 import net.sf.anathema.character.generic.magic.ICharm;
 import net.sf.anathema.lib.gui.list.LegalityCheckListCellRenderer;
@@ -16,7 +15,7 @@ public abstract class LegalityCharmRenderer extends LegalityCheckListCellRendere
 
   public LegalityCharmRenderer(IResources resources, ICharmInfoStringBuilder charmInfoStringProvider) {
     super(resources);
-    this.charmInfoStringProvider = new CharmInfoStringCache(charmInfoStringProvider);
+    this.charmInfoStringProvider = charmInfoStringProvider;
   }
 
   @Override
