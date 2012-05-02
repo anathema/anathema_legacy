b279c2dccaec874a00e805d306858f1615b765e2
Sean Mattox <sean.mattox@gmail.com>
2012-05-02 10:02:31 -0700

    Fixing generic charm display in Combos.

--- Character_Main/src/net/sf/anathema/character/presenter/magic/combo/LegalityCharmRenderer.java
+++ Character_Main/src/net/sf/anathema/character/presenter/magic/combo/LegalityCharmRenderer.java
@@ -1,7 +1,9 @@
 package net.sf.anathema.character.presenter.magic.combo;
 
+import net.sf.anathema.character.generic.framework.magic.MagicDisplayLabeler;
 import net.sf.anathema.character.generic.framework.magic.stringbuilder.ICharmInfoStringBuilder;
 import net.sf.anathema.character.generic.magic.ICharm;
+import net.sf.anathema.character.generic.magic.IMagic;
 import net.sf.anathema.lib.gui.list.LegalityCheckListCellRenderer;
 import net.sf.anathema.lib.resources.IResources;
 
@@ -12,19 +14,26 @@ import java.awt.Component;
 public abstract class LegalityCharmRenderer extends LegalityCheckListCellRenderer {
 
   private final ICharmInfoStringBuilder charmInfoStringProvider;
+  private final MagicDisplayLabeler labeler;
 
   public LegalityCharmRenderer(IResources resources, ICharmInfoStringBuilder charmInfoStringProvider) {
     super(resources);
     this.charmInfoStringProvider = charmInfoStringProvider;
+    this.labeler = new MagicDisplayLabeler(resources);
   }
 
   @Override
   public Component getListCellRendererComponent(JList list, final Object value, int index, boolean isSelected,
                                                 boolean cellHasFocus) {
     final JComponent renderComponent = (JComponent) super.getListCellRendererComponent(list, value, index, isSelected,
-            cellHasFocus);
+    		cellHasFocus);
     String tooltipString = charmInfoStringProvider.getInfoString((ICharm) value, null);
     renderComponent.setToolTipText(tooltipString);
     return renderComponent;
+  }
+  
+  @Override
+  protected String getPrintName(IResources resources, Object value) {
+	  return labeler.getLabelForMagic((IMagic) value);
   }
 }
