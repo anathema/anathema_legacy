6b6e16e656524f701b3bacc70a0d45ac4e3d7ad0
Urs Reupke <ursreupke@gmail.com>
2012-05-06 16:04:51 +0200

    Swing nodes are visible. No arrows, no text, no interaction, no zoom to max at start, no special nodes, and the defined polygon isn't honored. But they *are* visible.

--- Platform_SvgTree/src/net/sf/anathema/platform/svgtree/document/components/SvgNodeElementAdder.java
+++ Platform_SvgTree/src/net/sf/anathema/platform/svgtree/document/components/SvgNodeElementAdder.java
@@ -1,6 +1,7 @@
 package net.sf.anathema.platform.svgtree.document.components;
 
 import net.sf.anathema.platform.svgtree.document.util.SVGCreationUtils;
+import net.sf.anathema.platform.svgtree.document.visualizer.NodeAdder;
 import org.apache.batik.util.SVGConstants;
 import org.dom4j.Element;
 import org.dom4j.QName;
@@ -20,21 +21,22 @@ import static org.apache.batik.util.SVGConstants.SVG_USE_TAG;
 import static org.apache.batik.util.SVGConstants.SVG_X_ATTRIBUTE;
 import static org.apache.batik.util.SVGConstants.SVG_Y_ATTRIBUTE;
 
-public class SvgNodeElementAdder {
+public class SvgNodeElementAdder implements NodeAdder<Element> {
 
-  private final ILayer layer;
   private final String id;
   private final Integer xPosition;
   private final Dimension nodeDimension;
+  private final int yPosition;
 
-  public SvgNodeElementAdder(ILayer layer, String id, Integer xPosition, Dimension nodeDimension) {
-    this.layer = layer;
+  public SvgNodeElementAdder(String id, Dimension nodeDimension, Integer xPosition, int yPosition) {
     this.id = id;
     this.xPosition = xPosition;
     this.nodeDimension = nodeDimension;
+    this.yPosition = yPosition;
   }
 
-  public void toXML(Element element) {
+  @Override
+  public void addTo(Element element) {
     QName group = SVGCreationUtils.createSVGQName(SVGConstants.SVG_G_TAG);
     Element g = element.addElement(group);
     g.addAttribute(SVGConstants.SVG_ID_ATTRIBUTE, id);
@@ -47,7 +49,7 @@ public class SvgNodeElementAdder {
     QName textName = SVGCreationUtils.createSVGQName(SVGConstants.SVG_TEXT_TAG);
     Element text = g.addElement(textName);
     text.addAttribute(SVG_X_ATTRIBUTE, String.valueOf(xPosition));
-    text.addAttribute(SVG_Y_ATTRIBUTE, String.valueOf(layer.getYPosition() + nodeDimension.getHeight() / 2));
+    text.addAttribute(SVG_Y_ATTRIBUTE, String.valueOf(yPosition + nodeDimension.getHeight() / 2));
     text.addAttribute(SVG_TEXT_ANCHOR_ATTRIBUTE, SVG_MIDDLE_VALUE);
     text.addAttribute(SVG_FONT_SIZE_ATTRIBUTE, ISVGCascadeXMLConstants.VALUE_15);
     text.addAttribute(ATTRIB_POINTER_EVENTS, SVG_NONE_VALUE);
@@ -59,7 +61,7 @@ public class SvgNodeElementAdder {
     QName useName = SVGCreationUtils.createSVGQName(SVG_USE_TAG);
     Element use = g.addElement(useName);
     use.addAttribute(SVG_X_ATTRIBUTE, String.valueOf(xPosition - nodeDimension.width / 2));
-    use.addAttribute(SVG_Y_ATTRIBUTE, String.valueOf(layer.getYPosition()));
+    use.addAttribute(SVG_Y_ATTRIBUTE, String.valueOf(yPosition));
     use.addAttribute(ATTRIB_POINTER_EVENTS, VALUE_VISIBLE);
     use.addAttribute(SVG_FILL_ATTRIBUTE, SVG_NONE_VALUE);
     use.addAttribute(SVGCreationUtils.createXLinkQName(), VALUE_FRAME_REFERENCE);
---
+++ Platform_SvgTree/src/net/sf/anathema/platform/svgtree/document/visualizer/NodeAdder.java
@@ -0,0 +1,6 @@
+package net.sf.anathema.platform.svgtree.document.visualizer;
+
+public interface NodeAdder<PARENT> {
+
+  void addTo(PARENT parent);
+}
