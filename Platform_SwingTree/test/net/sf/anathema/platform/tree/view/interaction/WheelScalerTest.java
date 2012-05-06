28af939d116b3bfc38ba3def727d02ca413d26b7
Urs Reupke <ursreupke@gmail.com>
2012-05-06 20:39:33 +0200

    Polygonpanel can translate and scale Doesn't repaint afterwards for better performance.

--- Platform_SwingTree/test/net/sf/anathema/platform/tree/view/interaction/WheelScalerTest.java
+++ Platform_SwingTree/test/net/sf/anathema/platform/tree/view/interaction/WheelScalerTest.java
@@ -1,24 +1,50 @@
 package net.sf.anathema.platform.tree.view.interaction;
 
 import net.sf.anathema.platform.tree.view.PolygonPanel;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
 
 import java.awt.event.MouseWheelEvent;
 
+import static org.mockito.Matchers.anyDouble;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class WheelScalerTest {
 
-  private static final int AnyNumberOfUnits = 5;
   PolygonPanel panel = mock(PolygonPanel.class);
   MouseWheelEvent event = mock(MouseWheelEvent.class);
 
+  @Before
+  public void setUp() throws Exception {
+    when(event.getX()).thenReturn(100);
+    when(event.getY()).thenReturn(120);
+    when(event.getUnitsToScroll()).thenReturn(5);
+  }
+
   @Test
-  public void triggersScaleOnPanel() throws Exception {
-    when(event.getUnitsToScroll()).thenReturn(AnyNumberOfUnits);
+  public void triggersScaleOnPanelCenteredOnEvent() throws Exception {
     new WheelScaler(panel).mouseWheelMoved(event);
-    verify(panel).scale(AnyNumberOfUnits);
+    InOrder inOrder = inOrder(panel);
+    inOrder.verify(panel).translate(100, 120);
+    inOrder.verify(panel).scale(anyDouble());
+    inOrder.verify(panel).translate(-100, -120);
+  }
+
+  @Test
+  public void calculatesScaleFromNumberOfUnits() throws Exception {
+    new WheelScaler(panel).mouseWheelMoved(event);
+    verify(panel).scale(0.5);
+  }
+
+  @Test
+  public void repaintsAfterTranslation() throws Exception {
+    new WheelScaler(panel).mouseWheelMoved(event);
+    InOrder inOrder = inOrder(panel);
+    inOrder.verify(panel).translate(-100, -120);
+    inOrder.verify(panel).repaint();
   }
 }
