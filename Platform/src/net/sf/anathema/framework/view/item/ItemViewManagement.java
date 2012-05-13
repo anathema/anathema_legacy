92f97d37cf2bba1488420f2bbbefe4d639aafdd9
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2012-05-13 21:51:06 +0200

    DefaultTabbedPane

--- Platform/src/net/sf/anathema/framework/view/item/ItemViewManagement.java
+++ Platform/src/net/sf/anathema/framework/view/item/ItemViewManagement.java
@@ -1,102 +1,43 @@
 package net.sf.anathema.framework.view.item;
 
 import net.infonode.gui.colorprovider.FixedColorProvider;
-import net.infonode.gui.hover.HoverEvent;
-import net.infonode.gui.hover.HoverListener;
-import net.infonode.tabbedpanel.Tab;
-import net.infonode.tabbedpanel.TabAdapter;
-import net.infonode.tabbedpanel.TabDropDownListVisiblePolicy;
-import net.infonode.tabbedpanel.TabStateChangedEvent;
-import net.infonode.tabbedpanel.TabbedPanel;
-import net.infonode.tabbedpanel.TabbedPanelProperties;
 import net.infonode.tabbedpanel.theme.ShapedGradientTheme;
-import net.infonode.tabbedpanel.titledtab.TitledTab;
-import net.infonode.tabbedpanel.titledtab.TitledTabProperties;
-import net.infonode.util.Direction;
 import net.sf.anathema.framework.view.IItemView;
 import net.sf.anathema.framework.view.IViewSelectionListener;
 import net.sf.anathema.lib.control.IObjectValueChangedListener;
+import org.jdesktop.swingx.JXLabel;
+import org.jdesktop.swingx.painter.*;
 import org.jmock.example.announcer.Announcer;
 
-import javax.swing.AbstractButton;
-import javax.swing.Action;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Insets;
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ItemViewManagement implements IComponentItemViewManagement {
 
-  private final TabbedPanel tabbedPane = new TabbedPanel();
+  private final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.BOTTOM);
   private final Map<Component, IItemView> itemViewsByComponent = new HashMap<Component, IItemView>();
   private final Map<IItemView, IObjectValueChangedListener<String>> nameListenersByView = new HashMap<IItemView, IObjectValueChangedListener<String>>();
   private final Announcer<IViewSelectionListener> control = Announcer.to(IViewSelectionListener.class);
-  private final TitledTabProperties titledTabProperties = new TitledTabProperties();
-  private final ShapedGradientTheme theme = new ShapedGradientTheme(0f, 0.5f, new FixedColorProvider(new Color(
-      150,
-      150,
-      150)), null);
-
-  public ItemViewManagement() {
-    initPaneProperties();
-    titledTabProperties.addSuperObject(theme.getTitledTabProperties());
-    titledTabProperties.getNormalProperties().setTitleComponentVisible(false);
-    titledTabProperties.getHighlightedProperties().setTitleComponentVisible(true);
-	titledTabProperties.setFocusable(false);
-	titledTabProperties.setHighlightedRaised(3);
-    titledTabProperties.setHoverListener(new HoverListener() {
-      @Override
-      public void mouseEntered(HoverEvent event) {
-        TitledTab tab = (TitledTab) event.getSource();
-        tab.getProperties().getNormalProperties().setTitleComponentVisible(true);
-      }
-
-      @Override
-      public void mouseExited(HoverEvent event) {
-        TitledTab tab = (TitledTab) event.getSource();
-        tab.getProperties().getNormalProperties().setTitleComponentVisible(false);
-      }
-    });
-  }
-
-  private void initPaneProperties() {
-    TabbedPanelProperties paneProperties = tabbedPane.getProperties();
-    paneProperties.removeSuperObject(tabbedPane.getProperties());
-    paneProperties.addSuperObject(theme.getTabbedPanelProperties());
-    paneProperties.setTabAreaOrientation(Direction.DOWN);
-    paneProperties.setTabReorderEnabled(true);
-    paneProperties.setTabDeselectable(false);
-    paneProperties.setEnsureSelectedTabVisible(true);
-    paneProperties.setHighlightPressedTab(true);
-    paneProperties.setTabDropDownListVisiblePolicy(TabDropDownListVisiblePolicy.TABS_NOT_VISIBLE);
-  }
 
   @Override
   public void addItemView(final IItemView view, Action closeAction) {
     JComponent component = view.getComponent();
     itemViewsByComponent.put(component, view);
-    TitledTab tab = new TitledTab(view.getName(), view.getIcon(), component, createButton(closeAction));
-    tab.getProperties().addSuperObject(titledTabProperties);
-    setTabFontToSupportUnicode(tab);
-    tabbedPane.addTab(tab);
-    tab.addTabListener(new TabAdapter() {
+    tabbedPane.addTab(view.getName(), view.getIcon(), component);
+    int currentIndex = tabbedPane.getTabCount() - 1;
+    tabbedPane.setTabComponentAt(currentIndex, createTabComponent(view, closeAction));
+    tabbedPane.addChangeListener(new ChangeListener() {
       @Override
-      public void tabSelected(TabStateChangedEvent event) {
+      public void stateChanged(ChangeEvent e) {
         fireItemViewChanged(getSelectedItemView());
       }
     });
     initNameListening(view);
-    tabbedPane.setSelectedTab(tab);
-  }
-
-  private void setTabFontToSupportUnicode(TitledTab tab) {
-    tab.getProperties().getNormalProperties().getComponentProperties().setFont(FontHolder.TAB_CAPTION_FONT);
-    tab.getProperties().getDisabledProperties().getComponentProperties().setFont(FontHolder.TAB_CAPTION_FONT);
-    tab.getProperties().getHighlightedProperties().getComponentProperties().setFont(FontHolder.TAB_CAPTION_FONT);
+    tabbedPane.setSelectedIndex(currentIndex);
   }
 
   private AbstractButton createButton(Action closeAction) {
@@ -130,7 +71,7 @@ public class ItemViewManagement implements IComponentItemViewManagement {
   }
 
   private IItemView getSelectedItemView() {
-    return itemViewsByComponent.get(tabbedPane.getSelectedTab().getContentComponent());
+    return itemViewsByComponent.get(tabbedPane.getSelectedComponent());
   }
 
   @Override
@@ -144,30 +85,31 @@ public class ItemViewManagement implements IComponentItemViewManagement {
 
   @Override
   public void setSelectedItemView(IItemView view) {
-    tabbedPane.setSelectedTab(getTab(view));
-  }
-
-  private TitledTab getTab(IItemView view) {
-    JComponent viewComponent = view.getComponent();
-    for (int index = 0; index < tabbedPane.getTabCount(); index++) {
-      Tab tab = tabbedPane.getTabAt(index);
-      if (tab.getContentComponent() == viewComponent) {
-        return (TitledTab) tab;
-      }
-    }
-    return null;
+    tabbedPane.setSelectedComponent(view.getComponent());
   }
 
   @Override
   public void removeItemView(IItemView view) {
     Component component = view.getComponent();
     itemViewsByComponent.remove(component);
-    tabbedPane.removeTab(getTab(view));
+    tabbedPane.remove(component);
     disposeNameListening(view);
     view.dispose();
   }
 
+  private JComponent createTabComponent(IItemView view, Action closeAction) {
+    JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+    JXLabel label = new JXLabel(view.getName(), view.getIcon(), SwingConstants.LEFT);
+    panel.add(label);
+    panel.add(createButton(closeAction));
+    return panel;
+  }
+
   private void setItemViewName(IItemView view, String name) {
-    getTab(view).setText(name);
+    int index = tabbedPane.indexOfComponent(view.getComponent());
+    JPanel tabComponent = (JPanel) tabbedPane.getTabComponentAt(index);
+    JXLabel label = (JXLabel) tabComponent.getComponent(0);
+    label.setText(name);
+    tabComponent.revalidate();
   }
 }
