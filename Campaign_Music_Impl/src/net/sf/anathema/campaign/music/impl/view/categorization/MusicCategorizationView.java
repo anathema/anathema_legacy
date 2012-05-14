973982914d8edfa9d7bb6472c07fc70c5ffe61cc
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2012-05-14 13:49:00 +0200

    MigLayout

--- Campaign_Music_Impl/src/net/sf/anathema/campaign/music/impl/view/categorization/MusicCategorizationView.java
+++ Campaign_Music_Impl/src/net/sf/anathema/campaign/music/impl/view/categorization/MusicCategorizationView.java
@@ -1,13 +1,6 @@
 package net.sf.anathema.campaign.music.impl.view.categorization;
 
-import javax.swing.JComponent;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.border.TitledBorder;
-
-import net.disy.commons.swing.layout.grid.GridDialogLayout;
-import net.disy.commons.swing.layout.grid.GridDialogLayoutData;
+import net.miginfocom.swing.MigLayout;
 import net.sf.anathema.campaign.music.presenter.IMusicEvent;
 import net.sf.anathema.campaign.music.presenter.IMusicMood;
 import net.sf.anathema.campaign.music.presenter.IMusicTheme;
@@ -16,14 +9,17 @@ import net.sf.anathema.campaign.music.view.categorization.IMusicCategorizationVi
 import net.sf.anathema.lib.workflow.container.ISelectionContainerView;
 import net.sf.anathema.lib.workflow.container.view.SelectionContainerListView;
 
+import javax.swing.JComponent;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.TitledBorder;
+
 public class MusicCategorizationView implements IMusicCategorizationView {
 
-  private final SelectionContainerListView<IMusicEvent> eventsView = new SelectionContainerListView<IMusicEvent>(
-      IMusicEvent.class);
-  private final SelectionContainerListView<IMusicTheme> themesView = new SelectionContainerListView<IMusicTheme>(
-      IMusicTheme.class);
-  private final SelectionContainerListView<IMusicMood> moodsView = new SelectionContainerListView<IMusicMood>(
-      IMusicMood.class);
+  private final SelectionContainerListView<IMusicEvent> eventsView = new SelectionContainerListView<IMusicEvent>(IMusicEvent.class);
+  private final SelectionContainerListView<IMusicTheme> themesView = new SelectionContainerListView<IMusicTheme>(IMusicTheme.class);
+  private final SelectionContainerListView<IMusicMood> moodsView = new SelectionContainerListView<IMusicMood>(IMusicMood.class);
   private JPanel content;
 
   public JComponent getContent(IMusicCategorizationProperties properties) {
@@ -34,23 +30,18 @@ public class MusicCategorizationView implements IMusicCategorizationView {
   }
 
   private JPanel createContent(IMusicCategorizationProperties properties) {
-    JPanel musicCatagorizePanel = new JPanel(new GridDialogLayout(3, true));
-    musicCatagorizePanel.add(
-        createListPanel(properties.getThemesString(), themesView.getComponent()),
-        GridDialogLayoutData.FILL_BOTH);
-    musicCatagorizePanel.add(
-        createListPanel(properties.getEventsString(), eventsView.getComponent()),
-        GridDialogLayoutData.FILL_BOTH);
-    musicCatagorizePanel.add(
-        createListPanel(properties.getMoodsString(), moodsView.getComponent()),
-        GridDialogLayoutData.FILL_BOTH);
+    MigLayout layout = new MigLayout("", "[grow, fill][grow, fill][grow, fill]", "");
+    JPanel musicCatagorizePanel = new JPanel(layout);
+    musicCatagorizePanel.add(createListPanel(properties.getThemesString(), themesView.getComponent()));
+    musicCatagorizePanel.add(createListPanel(properties.getEventsString(), eventsView.getComponent()));
+    musicCatagorizePanel.add(createListPanel(properties.getMoodsString(), moodsView.getComponent()));
     return musicCatagorizePanel;
   }
 
   private JPanel createListPanel(String string, JList list) {
-    JPanel panel = new JPanel(new GridDialogLayout(1, false));
+    JPanel panel = new JPanel(new MigLayout("", "0[grow, fill]0", "0[]0"));
     panel.setBorder(new TitledBorder(string));
-    panel.add(new JScrollPane(list), GridDialogLayoutData.FILL_BOTH);
+    panel.add(new JScrollPane(list));
     return panel;
   }
 
