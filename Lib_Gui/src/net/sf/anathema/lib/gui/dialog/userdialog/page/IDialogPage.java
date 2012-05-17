e148dc16e03e53065909766d5786f6ce25960899
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2012-05-17 13:31:57 +0200

    hierarchy of dialog page reduced

--- Lib_Gui/src/net/sf/anathema/lib/gui/dialog/userdialog/page/IDialogPage.java
+++ Lib_Gui/src/net/sf/anathema/lib/gui/dialog/userdialog/page/IDialogPage.java
@@ -1,8 +1,23 @@
 package net.sf.anathema.lib.gui.dialog.userdialog.page;
 
+import net.sf.anathema.lib.gui.dialog.core.IPage;
+import net.sf.anathema.lib.gui.dialog.events.ICheckInputValidListener;
+import net.sf.anathema.lib.gui.swing.IDisposable;
 import net.sf.anathema.lib.message.IBasicMessage;
 
-public interface IDialogPage extends IBasicDialogPage {
+import javax.swing.JComponent;
+
+public interface IDialogPage extends IPage, IDisposable {
+
+  IBasicMessage createCurrentMessage();
+
+  void requestFocus();
+
+  JComponent createContent();
+
+  void setInputValidListener(ICheckInputValidListener inputValidListener);
+
+  void updateInputValid();
 
   IBasicMessage getDefaultMessage();
 }
