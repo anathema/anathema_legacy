f703111d7f1e08b6950ca89c591fc3417c8f97cf
Urs Reupke <ursreupke@gmail.com>
2012-06-15 18:15:23 +0200

    Cleans up

--- Platform/src/net/sf/anathema/framework/message/MessageUtilities.java
+++ Platform/src/net/sf/anathema/framework/message/MessageUtilities.java
@@ -1,6 +1,5 @@
 package net.sf.anathema.framework.message;
 
-import net.sf.anathema.lib.exception.UnreachableCodeReachedException;
 import net.sf.anathema.lib.gui.dialog.message.MessageDialogFactory;
 import net.sf.anathema.lib.logging.Logger;
 import net.sf.anathema.lib.message.Message;
@@ -8,10 +7,6 @@ import net.sf.anathema.lib.message.Message;
 import java.awt.Component;
 
 public class MessageUtilities {
-
-  private MessageUtilities() {
-    throw new UnreachableCodeReachedException();
-  }
 
   public static void indicateMessage(Class< ? > clazz, Component parentComponent, Message message) {
     Throwable throwable = message.getThrowable();
