dcea92d07c0f2c004763d33a57f8354997e2a7ea
Urs Reupke <ursreupke@gmail.com>
2012-08-19 17:43:29 +0200

    Cleanup

--- Platform_SvgTree/src/net/sf/anathema/platform/svgtree/document/components/VisualizableNodeLeftSideComparator.java
+++ Platform_SvgTree/src/net/sf/anathema/platform/svgtree/document/components/VisualizableNodeLeftSideComparator.java
@@ -3,7 +3,7 @@ package net.sf.anathema.platform.svgtree.document.components;
 import java.util.Comparator;
 
 
-public final class VisualizableNodeLeftSideComparator implements Comparator<IVisualizableNode> {
+public class VisualizableNodeLeftSideComparator implements Comparator<IVisualizableNode> {
   @Override
   public int compare(IVisualizableNode node1, IVisualizableNode node2) {
     return node1.getLeftSide() - node2.getLeftSide();
