2082fedad6a372014606953ed8f0a2bedae6a45f
Urs Reupke <ursreupke@gmail.com>
2012-09-04 21:05:23 +0200

    Cleaning up while looking for an easy win on #286.

--- Charm_Tree/src/net/sf/anathema/character/generic/framework/magic/CharmGraphNodeBuilder.java
+++ Charm_Tree/src/net/sf/anathema/character/generic/framework/magic/CharmGraphNodeBuilder.java
@@ -1,17 +1,18 @@
 package net.sf.anathema.character.generic.framework.magic;
 
+import net.sf.anathema.character.generic.magic.ICharm;
+import net.sf.anathema.graph.nodes.IIdentifiedRegularNode;
+
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
-import net.sf.anathema.character.generic.magic.ICharm;
-import net.sf.anathema.graph.nodes.IIdentifiedRegularNode;
 
 public class CharmGraphNodeBuilder {
   public static IIdentifiedRegularNode[] createNodesFromCharms(Collection<ICharm> groupCharms) {
     Map<String, IIdentifiedRegularNode> charmNodesById = new LinkedHashMap<String, IIdentifiedRegularNode>();
     CharmNodeBuilder.buildNodes(groupCharms, charmNodesById);
     CharmNodeConnector.connectNodes(groupCharms, charmNodesById);
-    return charmNodesById.values().toArray(new IIdentifiedRegularNode[0]);
+    Collection<IIdentifiedRegularNode> nodes = charmNodesById.values();
+    return nodes.toArray(new IIdentifiedRegularNode[nodes.size()]);
   }
 }
