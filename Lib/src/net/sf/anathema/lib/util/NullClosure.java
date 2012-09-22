b9abd87b8fa2aaf449b3565c7425d44f9ccd4d81
Urs Reupke <ursreupke@gmail.com>
2012-09-22 16:43:49 +0200

    Removes useless constructors * Makes NullClosure redundant

--- Lib/src/net/sf/anathema/lib/util/NullClosure.java
+++
@@ -1,9 +0,0 @@
-package net.sf.anathema.lib.util;
-
-public class NullClosure<T> implements Closure<T> {
-
-  @Override
-  public void execute(T each) {
-    // nothing to do
-  }
-}
--- Lib_Gui/src/net/sf/anathema/lib/gui/container/TitledPanel.java
+++ Lib_Gui/src/net/sf/anathema/lib/gui/container/TitledPanel.java
@@ -2,12 +2,9 @@ package net.sf.anathema.lib.gui.container;
 
 import com.google.common.base.Preconditions;
 import net.disy.commons.swing.layout.grid.GridDialogLayoutData;
-import net.disy.commons.swing.layout.grid.IGridDialogLayoutData;
 import net.disy.commons.swing.layout.util.LayoutUtilities;
 import net.sf.anathema.lib.gui.swing.BorderUtilities;
 import net.sf.anathema.lib.gui.swing.GuiUtilities;
-import net.sf.anathema.lib.util.Closure;
-import net.sf.anathema.lib.util.NullClosure;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
@@ -20,44 +17,28 @@ import java.beans.PropertyChangeListener;
 
 public class TitledPanel extends JPanel {
 
-  public TitledPanel(String title, JComponent content) {
-    this(title, content, new GridDialogLayoutData());
+  public TitledPanel(String title, final JComponent content) {
+      super(new GridLayout(1, 0));
+      Preconditions.checkNotNull(title);
+      Preconditions.checkNotNull(content);
+      TitledBorder titledBorder = new TitledBorder(title);
+      BorderUtilities.attachDisableableTitledBorder(this, titledBorder);
+
+      setBorder(new CompoundBorder(getBorder(), new EmptyBorder(
+              LayoutUtilities.getDpiAdjusted(2),
+              LayoutUtilities.getDpiAdjusted(4),
+              LayoutUtilities.getDpiAdjusted(4),
+              LayoutUtilities.getDpiAdjusted(4))));
+      add(content, new GridDialogLayoutData());
+
+      content.addPropertyChangeListener(
+              GuiUtilities.ENABLED_PROPERTY_NAME,
+              new PropertyChangeListener() {
+                @Override
+                public void propertyChange(PropertyChangeEvent evt) {
+                  setEnabled(content.isEnabled());
+                }
+              });
+      setEnabled(content.isEnabled());
+    }
   }
-
-  public TitledPanel(
-      String title,
-      JComponent content,
-      IGridDialogLayoutData layoutData) {
-    this(title, content, layoutData, new NullClosure<TitledBorder>());
-  }
-
-  public TitledPanel(
-      String title,
-      final JComponent content,
-      IGridDialogLayoutData layoutData,
-      Closure<TitledBorder> decorator) {
-    super(new GridLayout(1, 0));
-    Preconditions.checkNotNull(title);
-    Preconditions.checkNotNull(content);
-    TitledBorder titledBorder = new TitledBorder(title);
-    BorderUtilities.attachDisableableTitledBorder(this, titledBorder);
-    decorator.execute(titledBorder);
-
-    setBorder(new CompoundBorder(getBorder(), new EmptyBorder(
-        LayoutUtilities.getDpiAdjusted(2),
-        LayoutUtilities.getDpiAdjusted(4),
-        LayoutUtilities.getDpiAdjusted(4),
-        LayoutUtilities.getDpiAdjusted(4))));
-    add(content, layoutData);
-
-    content.addPropertyChangeListener(
-        GuiUtilities.ENABLED_PROPERTY_NAME,
-        new PropertyChangeListener() {
-          @Override
-          public void propertyChange(PropertyChangeEvent evt) {
-            setEnabled(content.isEnabled());
-          }
-        });
-    setEnabled(content.isEnabled());
-  }
-}
