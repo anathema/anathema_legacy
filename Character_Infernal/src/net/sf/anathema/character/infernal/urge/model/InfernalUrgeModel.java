e464a7b9471e40551252da51459b5edbdc4bf93d
Urs Reupke <ursreupke@gmail.com>
2012-09-29 13:59:58 +0200

    Moves "Urge" to spiritual traits (#281)

--- Character_Infernal/src/net/sf/anathema/character/infernal/urge/model/InfernalUrgeModel.java
+++ Character_Infernal/src/net/sf/anathema/character/infernal/urge/model/InfernalUrgeModel.java
@@ -1,8 +1,5 @@
 package net.sf.anathema.character.infernal.urge.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.sf.anathema.character.generic.additionaltemplate.AdditionalModelType;
 import net.sf.anathema.character.generic.framework.additionaltemplate.model.ICharacterModelContext;
 import net.sf.anathema.character.generic.traits.ITraitType;
@@ -13,60 +10,56 @@ import net.sf.anathema.character.library.virtueflaw.model.VirtueFlawModel;
 import net.sf.anathema.lib.control.IChangeListener;
 import net.sf.anathema.lib.workflow.textualdescription.ITextualDescription;
 
-public class InfernalUrgeModel extends VirtueFlawModel implements IInfernalUrgeModel
-{
+import java.util.ArrayList;
+import java.util.List;
+
+public class InfernalUrgeModel extends VirtueFlawModel implements IInfernalUrgeModel {
   private InfernalUrge virtueFlaw;
   private InfernalUrgeTemplate template;
 
-  public InfernalUrgeModel(InfernalUrgeTemplate template, ICharacterModelContext context)
-  {
-	  super(context, template);
-	  this.template = template;
-	  virtueFlaw = new InfernalUrge(context);
+  public InfernalUrgeModel(InfernalUrgeTemplate template, ICharacterModelContext context) {
+    super(context, template);
+    this.template = template;
+    virtueFlaw = new InfernalUrge(context);
   }
-  
+
   @Override
-  public IVirtueFlaw getVirtueFlaw()
-  {
-	  return virtueFlaw;
+  public IVirtueFlaw getVirtueFlaw() {
+    return virtueFlaw;
   }
-  
+
   @Override
   public ITraitType[] getFlawVirtueTypes() {
-	    List<ITraitType> flawVirtues = new ArrayList<ITraitType>();
-	    for (VirtueType virtueType : VirtueType.values()) {
-	      if (getContext().getTraitCollection().getTrait(virtueType).getCurrentValue() > 2) {
-	        flawVirtues.add(virtueType);
-	      }
-	    }
-	    return flawVirtues.toArray(new ITraitType[0]);
-	  }
-  
+    List<ITraitType> flawVirtues = new ArrayList<ITraitType>();
+    for (VirtueType virtueType : VirtueType.values()) {
+      if (getContext().getTraitCollection().getTrait(virtueType).getCurrentValue() > 2) {
+        flawVirtues.add(virtueType);
+      }
+    }
+    return flawVirtues.toArray(new ITraitType[0]);
+  }
+
   @Override
   public AdditionalModelType getAdditionalModelType() {
-  	return AdditionalModelType.Concept;
+    return AdditionalModelType.Advantages;
   }
 
   @Override
-  public String getTemplateId()
-  {
-  	return template.getId();
-  }
-  
-  @Override
-  public void addChangeListener(IChangeListener listener)
-  {
+  public String getTemplateId() {
+    return template.getId();
   }
 
   @Override
-  public ITextualDescription getDescription()
-  {
+  public void addChangeListener(IChangeListener listener) {
+  }
+
+  @Override
+  public ITextualDescription getDescription() {
     return virtueFlaw.getDescription();
   }
-  
+
   @Override
-  public void setDescription(String urge)
-  {
-	virtueFlaw.getDescription().setText(urge);
+  public void setDescription(String urge) {
+    virtueFlaw.getDescription().setText(urge);
   }
 }
