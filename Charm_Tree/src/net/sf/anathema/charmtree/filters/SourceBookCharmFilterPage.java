6dfc7520d5cddc4f15a50b37e2bdde8f59acadc4
Urs Reupke <ursreupke@gmail.com>
2012-10-03 21:56:52 +0200

    Filter page doesn't use disy layout * Less duplication, less components

--- Charm_Tree/src/net/sf/anathema/charmtree/filters/SourceBookCharmFilterPage.java
+++ Charm_Tree/src/net/sf/anathema/charmtree/filters/SourceBookCharmFilterPage.java
@@ -1,6 +1,8 @@
 package net.sf.anathema.charmtree.filters;
 
-import net.disy.commons.swing.layout.grid.GridDialogLayout;
+import net.miginfocom.layout.CC;
+import net.miginfocom.layout.LC;
+import net.miginfocom.swing.MigLayout;
 import net.sf.anathema.character.generic.rules.IExaltedSourceBook;
 import net.sf.anathema.lib.gui.action.SmartAction;
 import net.sf.anathema.lib.resources.IResources;
@@ -28,9 +30,7 @@ public class SourceBookCharmFilterPage implements ICharmFilterPage {
   private List<IExaltedSourceBook> excludedBooks;
   private boolean[] includePrereqs;
   private IResources resources;
-
   private Map<String, IExaltedSourceBook> namesToBooksMap = new HashMap<String, IExaltedSourceBook>();
-
   private JList allowedList;
   private JList excludedList;
 
@@ -44,84 +44,75 @@ public class SourceBookCharmFilterPage implements ICharmFilterPage {
 
   @Override
   public JPanel getContent() {
-    JPanel panel = new JPanel(new GridDialogLayout(1, false));
-    JPanel swapPanel = new JPanel();
-
-    JPanel allowedPanel = new JPanel(new GridDialogLayout(1, false));
-    allowedPanel.add(new JLabel(resources.getString("CharmFilters.SourceBook.Included")));
-    allowedList = new JList(convertBooksToNames(allowedBooks));
-    allowedList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-    allowedPanel.setSize(300, 100);
-    allowedPanel.add(new JScrollPane(allowedList));
-    swapPanel.add(allowedPanel);
-
-    JPanel buttonGrid = new JPanel();
-    buttonGrid.setLayout(new GridLayout(2, 1));
-    JButton addSourceButton = new JButton();
-    JButton removeSourceButton = new JButton();
-    buttonGrid.add(addSourceButton);
-    buttonGrid.add(removeSourceButton);
-    swapPanel.add(buttonGrid);
-
-    removeSourceButton.setAction(new SmartAction() {
-
-      @Override
-      protected void execute(Component parentComponent) {
-        removeBooks(allowedList.getSelectedValues());
-      }
-    });
-    removeSourceButton.setText(">>");
-
-    addSourceButton.setAction(new SmartAction() {
-
-      @Override
-      protected void execute(Component parentComponent) {
-        addBooks(excludedList.getSelectedValues());
-      }
-    });
-    addSourceButton.setText("<<");
-
-    JPanel deniedPanel = new JPanel(new GridDialogLayout(1, false));
-    deniedPanel.add(new JLabel(resources.getString("CharmFilters.SourceBook.Excluded")));
-    excludedList = new JList(convertBooksToNames(excludedBooks));
-    excludedList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-    deniedPanel.setSize(300, 100);
-    deniedPanel.add(new JScrollPane(excludedList));
-    swapPanel.add(deniedPanel);
-
-    panel.add(swapPanel);
-
+    JPanel panel = new JPanel(new MigLayout(new LC().wrapAfter(3).fill()));
+    this.allowedList = addTitledJList(panel, allowedBooks, "CharmFilters.SourceBook.Included");
+    createButtonPanel(panel);
+    this.excludedList = addTitledJList(panel, excludedBooks, "CharmFilters.SourceBook.Excluded");
     JRadioButton prereqShownButton = new JRadioButton();
     JRadioButton prereqHiddenButton = new JRadioButton();
-    panel.add(prereqShownButton);
-    panel.add(prereqHiddenButton);
+    panel.add(prereqShownButton, new CC().spanX());
+    panel.add(prereqHiddenButton, new CC().spanX());
     ButtonGroup prereqButtonGroup = new ButtonGroup();
     prereqButtonGroup.add(prereqShownButton);
     prereqButtonGroup.add(prereqHiddenButton);
     prereqShownButton.setAction(new SmartAction() {
-
       @Override
       protected void execute(Component parentComponent) {
         includePrereqs[0] = true;
       }
     });
     prereqHiddenButton.setAction(new SmartAction() {
-
       @Override
       protected void execute(Component parentComponent) {
         includePrereqs[0] = false;
       }
     });
-    if (includePrereqs[0]) prereqShownButton.setSelected(true);
-    else prereqHiddenButton.setSelected(true);
+    if (includePrereqs[0]) {
+      prereqShownButton.setSelected(true);
+    } else {
+      prereqHiddenButton.setSelected(true);
+    }
     prereqShownButton.setText(resources.getString("CharmFilters.SourceBook.ShowPrereqs"));
     prereqHiddenButton.setText(resources.getString("CharmFilters.SourceBook.HidePrereqs"));
-
-    TitledBorder title;
-    title = BorderFactory.createTitledBorder(resources.getString("CharmFilters.SourceBook.SourceFilters"));
-    panel.setBorder(title);
-
+    String title = resources.getString("CharmFilters.SourceBook.SourceFilters");
+    TitledBorder border = BorderFactory.createTitledBorder(title);
+    panel.setBorder(border);
     return panel;
+  }
+
+  private JList addTitledJList(JPanel panel, List<IExaltedSourceBook> books, String title) {
+    panel.add(new JLabel(resources.getString(title)), new CC().split(2).flowY());
+    JList list = new JList(convertBooksToNames(books));
+    list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+    list.setSize(300, 100);
+    JScrollPane allowedPane = new JScrollPane(list);
+    allowedPane.setSize(300, 100);
+    panel.add(allowedPane, new CC().grow().push());
+    return list;
+  }
+
+  private void createButtonPanel(JPanel panel) {
+    JPanel buttonGrid = new JPanel();
+    buttonGrid.setLayout(new GridLayout(2, 1));
+    JButton addSourceButton = new JButton();
+    JButton removeSourceButton = new JButton();
+    buttonGrid.add(addSourceButton);
+    buttonGrid.add(removeSourceButton);
+    panel.add(buttonGrid);
+    removeSourceButton.setAction(new SmartAction() {
+      @Override
+      protected void execute(Component parentComponent) {
+        removeBooks(allowedList.getSelectedValues());
+      }
+    });
+    removeSourceButton.setText(">>");
+    addSourceButton.setAction(new SmartAction() {
+      @Override
+      protected void execute(Component parentComponent) {
+        addBooks(excludedList.getSelectedValues());
+      }
+    });
+    addSourceButton.setText("<<");
   }
 
   private Object[] convertBooksToNames(List<IExaltedSourceBook> bookList) {
