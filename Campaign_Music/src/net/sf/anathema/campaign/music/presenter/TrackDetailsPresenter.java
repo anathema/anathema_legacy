fcbda3fe2eefd228acb76ce21db5751f2b6a1fc2
Urs Reupke <ursreupke@gmail.com>
2012-10-08 19:27:38 +0200

    Cleanup

--- Campaign_Music/src/net/sf/anathema/campaign/music/presenter/TrackDetailsPresenter.java
+++ Campaign_Music/src/net/sf/anathema/campaign/music/presenter/TrackDetailsPresenter.java
@@ -1,102 +1,1 @@
-package net.sf.anathema.campaign.music.presenter;
-
-import net.sf.anathema.campaign.music.model.selection.ITrackDetailModel;
-import net.sf.anathema.campaign.music.model.track.IMp3Track;
-import net.sf.anathema.campaign.music.view.selection.ITrackDetailsView;
-import net.sf.anathema.lib.control.IChangeListener;
-import net.sf.anathema.lib.control.ObjectValueListener;
-import net.sf.anathema.lib.gui.Presenter;
-import net.sf.anathema.lib.lang.StringUtilities;
-import net.sf.anathema.lib.resources.IResources;
-import net.sf.anathema.lib.util.Closure;
-
-public class TrackDetailsPresenter implements Presenter {
-
-  private final ITrackDetailModel trackDetailModel;
-  private final ITrackDetailsView trackDetailsView;
-  private final String unknownString;
-
-  public TrackDetailsPresenter(
-      IResources resources,
-      ITrackDetailsView trackDetailsView,
-      ITrackDetailModel trackDetailModel) {
-    this.trackDetailModel = trackDetailModel;
-    this.trackDetailsView = trackDetailsView;
-    this.unknownString = resources.getString("Music.TrackDetails.Unknown"); //$NON-NLS-1$
-  }
-
-  @Override
-  public void initPresentation() {
-    trackDetailModel.addTrackChangeListener(new IChangeListener() {
-      @Override
-      public void changeOccurred() {
-        updateTrackInfo(trackDetailModel.getSelectedTrack());
-      }
-    });
-    trackDetailsView.getGivenNameView().addTextChangedListener(new ObjectValueListener<String>() {
-      @Override
-      public void valueChanged(String newValue) {
-        trackDetailModel.updateGivenName(newValue);
-      }
-    });
-    new SelectionContainerPresenter<IMusicTheme>(
-        trackDetailModel.getThemesModel(),
-        trackDetailsView.getMusicCategorizationView().getThemesView(),
-        IMusicTheme.class).initPresentation();
-    new SelectionContainerPresenter<IMusicMood>(
-        trackDetailModel.getMoodsModel(),
-        trackDetailsView.getMusicCategorizationView().getMoodsView(),
-        IMusicMood.class).initPresentation();
-    new SelectionContainerPresenter<IMusicEvent>(
-        trackDetailModel.getEventsModel(),
-        trackDetailsView.getMusicCategorizationView().getEventsView(),
-        IMusicEvent.class).initPresentation();
-  }
-
-  private void updateTrackInfo(IMp3Track mp3Track) {
-    if (mp3Track == null) {
-      trackDetailsView.showTrackInfo(false);
-      return;
-    }
-    trackDetailsView.showTrackInfo(true);
-    setStringValue(mp3Track.getTitle(), new Closure<String>() {
-      @Override
-      public void execute(String input) {
-        trackDetailsView.setOriginalTitle(input);
-      }
-    });
-    setStringValue(mp3Track.getAlbum(), new Closure<String>() {
-      @Override
-      public void execute(String input) {
-        trackDetailsView.setAlbumTitle(input);
-      }
-    });
-    setStringValue(mp3Track.getTrack(), new Closure<String>() {
-      @Override
-      public void execute(String input) {
-        trackDetailsView.setTrackNumber(input);
-      }
-    });
-    setStringValue(mp3Track.getArtist(), new Closure<String>() {
-      @Override
-      public void execute(String input) {
-        trackDetailsView.setArtistName(input);
-      }
-    });
-    String givenName = mp3Track.getGivenName();
-    if (StringUtilities.isNullOrEmpty(givenName)) {
-      givenName = mp3Track.getTitle();
-    }
-    setStringValue(givenName, new Closure<String>() {
-      @Override
-      public void execute(String input) {
-        trackDetailsView.getGivenNameView().setText(input);
-      }
-    });
-  }
-
-  private void setStringValue(String value, Closure<String> setter) {
-    String setValue = StringUtilities.isNullOrEmpty(value) ? unknownString : value;
-    setter.execute(setValue);
-  }
-}
+package net.sf.anathema.campaign.music.presenter;import net.sf.anathema.campaign.music.model.selection.ITrackDetailModel;import net.sf.anathema.campaign.music.model.track.IMp3Track;import net.sf.anathema.campaign.music.view.selection.ITrackDetailsView;import net.sf.anathema.lib.control.IChangeListener;import net.sf.anathema.lib.control.ObjectValueListener;import net.sf.anathema.lib.gui.Presenter;import net.sf.anathema.lib.lang.StringUtilities;import net.sf.anathema.lib.resources.IResources;import net.sf.anathema.lib.util.Closure;public class TrackDetailsPresenter implements Presenter {  private final ITrackDetailModel trackDetailModel;  private final ITrackDetailsView trackDetailsView;  private final String unknownString;  public TrackDetailsPresenter(IResources resources, ITrackDetailsView trackDetailsView,                               ITrackDetailModel trackDetailModel) {    this.trackDetailModel = trackDetailModel;    this.trackDetailsView = trackDetailsView;    this.unknownString = resources.getString("Music.TrackDetails.Unknown"); //$NON-NLS-1$  }  @Override  public void initPresentation() {    trackDetailModel.addTrackChangeListener(new IChangeListener() {      @Override      public void changeOccurred() {        updateTrackInfo(trackDetailModel.getSelectedTrack());      }    });    trackDetailsView.getGivenNameView().addTextChangedListener(new ObjectValueListener<String>() {      @Override      public void valueChanged(String newValue) {        trackDetailModel.updateGivenName(newValue);      }    });    new SelectionContainerPresenter<IMusicTheme>(trackDetailModel.getThemesModel(),            trackDetailsView.getMusicCategorizationView().getThemesView(), IMusicTheme.class).initPresentation();    new SelectionContainerPresenter<IMusicMood>(trackDetailModel.getMoodsModel(),            trackDetailsView.getMusicCategorizationView().getMoodsView(), IMusicMood.class).initPresentation();    new SelectionContainerPresenter<IMusicEvent>(trackDetailModel.getEventsModel(),            trackDetailsView.getMusicCategorizationView().getEventsView(), IMusicEvent.class).initPresentation();  }  private void updateTrackInfo(IMp3Track mp3Track) {    if (mp3Track == null) {      trackDetailsView.showTrackInfo(false);      return;    }    trackDetailsView.showTrackInfo(true);    setStringValue(mp3Track.getTitle(), new Closure<String>() {      @Override      public void execute(String input) {        trackDetailsView.setOriginalTitle(input);      }    });    setStringValue(mp3Track.getAlbum(), new Closure<String>() {      @Override      public void execute(String input) {        trackDetailsView.setAlbumTitle(input);      }    });    setStringValue(mp3Track.getTrack(), new Closure<String>() {      @Override      public void execute(String input) {        trackDetailsView.setTrackNumber(input);      }    });    setStringValue(mp3Track.getArtist(), new Closure<String>() {      @Override      public void execute(String input) {        trackDetailsView.setArtistName(input);      }    });    String givenName = mp3Track.getGivenName();    if (StringUtilities.isNullOrEmpty(givenName)) {      givenName = mp3Track.getTitle();    }    setStringValue(givenName, new Closure<String>() {      @Override      public void execute(String input) {        trackDetailsView.getGivenNameView().setText(input);      }    });  }  private void setStringValue(String value, Closure<String> setter) {    String setValue = StringUtilities.isNullOrEmpty(value) ? unknownString : value;    setter.execute(setValue);  }}
--- Campaign_Music/src/net/sf/anathema/campaign/music/view/categorization/IMusicCategorizationView.java
+++ Campaign_Music/src/net/sf/anathema/campaign/music/view/categorization/IMusicCategorizationView.java
@@ -1,15 +1,1 @@
-package net.sf.anathema.campaign.music.view.categorization;
-
-import net.sf.anathema.campaign.music.presenter.IMusicEvent;
-import net.sf.anathema.campaign.music.presenter.IMusicMood;
-import net.sf.anathema.campaign.music.presenter.IMusicTheme;
-import net.sf.anathema.campaign.music.presenter.ISelectionContainerView;
-
-public interface IMusicCategorizationView {
-
-  ISelectionContainerView<IMusicMood> getMoodsView();
-
-  ISelectionContainerView<IMusicEvent> getEventsView();
-
-  ISelectionContainerView<IMusicTheme> getThemesView();
-}
+package net.sf.anathema.campaign.music.view.categorization;import net.sf.anathema.campaign.music.presenter.IMusicEvent;import net.sf.anathema.campaign.music.presenter.IMusicMood;import net.sf.anathema.campaign.music.presenter.IMusicTheme;import net.sf.anathema.campaign.music.presenter.ISelectionContainerView;public interface IMusicCategorizationView {  ISelectionContainerView<IMusicMood> getMoodsView();  ISelectionContainerView<IMusicEvent> getEventsView();  ISelectionContainerView<IMusicTheme> getThemesView();}
