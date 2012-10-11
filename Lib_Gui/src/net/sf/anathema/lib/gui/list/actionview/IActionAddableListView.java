93aafa706d9b8b8ebecd30602633bb0e7069b776
Urs Reupke <ur@idos.de>
2012-10-11 10:06:34 +0200

    ActionAddableListViews don't expose "getComponent"

--- Lib_Gui/src/net/sf/anathema/lib/gui/list/actionview/IActionAddableListView.java
+++ Lib_Gui/src/net/sf/anathema/lib/gui/list/actionview/IActionAddableListView.java
@@ -1,11 +1,9 @@
 package net.sf.anathema.lib.gui.list.actionview;
 
-import net.sf.anathema.lib.gui.IView;
-
 import javax.swing.Action;
 import javax.swing.event.ListSelectionListener;
 
-public interface IActionAddableListView<T> extends IView {
+public interface IActionAddableListView<T> {
 
   void setObjects(T[] items);
 
