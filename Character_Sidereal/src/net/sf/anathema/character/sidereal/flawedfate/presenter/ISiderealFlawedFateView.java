45baec447fcce62174dec6ba664affb252313030
Urs Reupke <ur@idos.de>
2012-10-11 10:38:32 +0200

    Removes unused classes for Flawed Fate

--- Character_Sidereal/src/net/sf/anathema/character/sidereal/flawedfate/presenter/ISiderealFlawedFateView.java
+++
@@ -1,9 +0,0 @@
-package net.sf.anathema.character.sidereal.flawedfate.presenter;
-
-import net.sf.anathema.character.library.virtueflaw.presenter.IVirtueFlawView;
-import net.sf.anathema.lib.workflow.textualdescription.ITextView;
-
-public interface ISiderealFlawedFateView extends IVirtueFlawView {
-
-  ITextView addTextView(String label, int columnCount, int rows);
-}
