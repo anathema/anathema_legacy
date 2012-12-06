cdccb1840113da47a6e8bf3e95ed0ddab2da3c44
Urs Reupke <ur@idos.de>
2012-12-06 18:02:02 +0100

    Integration test reproduces issue #311

--- Character_Generic/src/net/sf/anathema/character/generic/caste/ICasteType.java
+++ Character_Generic/src/net/sf/anathema/character/generic/caste/ICasteType.java
@@ -2,8 +2,6 @@ package net.sf.anathema.character.generic.caste;
 
 public interface ICasteType extends ITypedDescriptionType {
 
-	String noFirst = "NOFIRST";
-	
   ICasteType NULL_CASTE_TYPE = new ICasteType() {
     @Override
     public String getId() {
--- Integrationtest/test/net/sf/anathema/Integrationtest.java
+++ Integrationtest/test/net/sf/anathema/Integrationtest.java
@@ -1,9 +1,12 @@
 package net.sf.anathema;
 
+import net.sf.anathema.character.generic.caste.ICasteCollection;
+import net.sf.anathema.character.generic.caste.ICasteType;
 import net.sf.anathema.character.generic.framework.CharacterGenericsExtractor;
 import net.sf.anathema.character.generic.framework.ICharacterGenerics;
 import net.sf.anathema.character.generic.template.ICharacterTemplate;
 import net.sf.anathema.character.impl.model.CharacterStatisticsConfiguration;
+import net.sf.anathema.character.impl.model.creation.bonus.BonusPointManagement;
 import net.sf.anathema.character.model.ICharacter;
 import net.sf.anathema.framework.IAnathemaModel;
 import net.sf.anathema.framework.item.IItemType;
@@ -11,32 +14,57 @@ import net.sf.anathema.framework.persistence.IRepositoryItemPersister;
 import net.sf.anathema.framework.repository.IItem;
 import net.sf.anathema.lib.registry.IRegistry;
 
-import java.util.Arrays;
-
 import static net.sf.anathema.character.generic.type.CharacterType.SIDEREAL;
 import static net.sf.anathema.character.impl.module.ExaltedCharacterItemTypeConfiguration.CHARACTER_ITEM_TYPE_ID;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class Integrationtest {
 
   @org.junit.Test
   public void testName() throws Exception {
-    TestInitializer initializer = new TestInitializer();
-    IAnathemaModel model = initializer.initialize();
-    CharacterStatisticsConfiguration template = createCreationInstructionsForDefaultSidereal(model);
+    IAnathemaModel model = initializeAnathema();
+    ICharacterTemplate defaultSiderealTemplate = loadDefaultSiderealTemplate(model);
+    ICharacter character = createCharacterFromTemplate(model, defaultSiderealTemplate);
+    setCasteToSerenity(defaultSiderealTemplate, character);
+    assertThatSevenDotsAreSpentOnFavoredAbilities(character);
+  }
+
+  private ICharacter createCharacterFromTemplate(IAnathemaModel model, ICharacterTemplate defaultSiderealTemplate) {
+    CharacterStatisticsConfiguration template = createCreationInstructionsForDefaultSidereal(defaultSiderealTemplate);
     IRegistry<IItemType, IRepositoryItemPersister> persisterRegistry = model.getPersisterRegistry();
     IItemType characterItemType = model.getItemTypeRegistry().getById(CHARACTER_ITEM_TYPE_ID);
     IRepositoryItemPersister itemPersister = persisterRegistry.get(characterItemType);
     IItem item = itemPersister.createNew(template);
-    ICharacter character = (ICharacter) item.getItemData();
-    character.
-    System.out.println(Arrays.deepToString(character.getTraitConfiguration().getAllAbilities()));
+    return (ICharacter) item.getItemData();
   }
 
-  private CharacterStatisticsConfiguration createCreationInstructionsForDefaultSidereal(IAnathemaModel model) {
-    CharacterStatisticsConfiguration template = new CharacterStatisticsConfiguration();
+  private IAnathemaModel initializeAnathema() {
+    TestInitializer initializer = new TestInitializer();
+    return initializer.initialize();
+  }
+
+  private void assertThatSevenDotsAreSpentOnFavoredAbilities(ICharacter character) {
+    BonusPointManagement bonusPointManagement = new BonusPointManagement(character);
+    bonusPointManagement.recalculate();
+    Integer dotsSpent = bonusPointManagement.getFavoredAbilityModel().getValue();
+    assertThat(dotsSpent, is(7));
+  }
+
+  private void setCasteToSerenity(ICharacterTemplate template, ICharacter character) {
+    ICasteCollection casteCollection = template.getCasteCollection();
+    ICasteType serenityCaste = casteCollection.getById("Serenity");
+    character.getCharacterConcept().getCaste().setType(serenityCaste);
+  }
+
+  private ICharacterTemplate loadDefaultSiderealTemplate(IAnathemaModel model) {
     ICharacterGenerics generics = CharacterGenericsExtractor.getGenerics(model);
-    ICharacterTemplate siderealDefaultTemplate = generics.getTemplateRegistry().getDefaultTemplate(SIDEREAL);
-    template.setTemplate(siderealDefaultTemplate);
+    return generics.getTemplateRegistry().getDefaultTemplate(SIDEREAL);
+  }
+
+  private CharacterStatisticsConfiguration createCreationInstructionsForDefaultSidereal(ICharacterTemplate innerTemplate) {
+    CharacterStatisticsConfiguration template = new CharacterStatisticsConfiguration();
+    template.setTemplate(innerTemplate);
     return template;
   }
 }
