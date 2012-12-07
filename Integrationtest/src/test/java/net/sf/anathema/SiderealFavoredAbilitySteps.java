ff859c7498627a0b64c189f9b681dc930ac048ea
Urs Reupke <ursreupke@gmail.com>
2012-12-07 22:22:57 +0100

    Refactored test to allow any of Character/Caste It launches Anathema, loads the default template and just assumes there is a caste of the given name. Right now, it can't do much - checking favored dots is the only option - but it's a start. It certainly shows that the system is terribly slow, even with an SSD.

--- Integrationtest/src/test/java/net/sf/anathema/SiderealFavoredAbilitySteps.java
+++ Integrationtest/src/test/java/net/sf/anathema/SiderealFavoredAbilitySteps.java
@@ -1,7 +1,6 @@
 package net.sf.anathema;
 
 import cucumber.api.java.Before;
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.sf.anathema.character.generic.caste.ICasteCollection;
@@ -9,6 +8,7 @@ import net.sf.anathema.character.generic.caste.ICasteType;
 import net.sf.anathema.character.generic.framework.CharacterGenericsExtractor;
 import net.sf.anathema.character.generic.framework.ICharacterGenerics;
 import net.sf.anathema.character.generic.template.ICharacterTemplate;
+import net.sf.anathema.character.generic.type.CharacterType;
 import net.sf.anathema.character.impl.model.CharacterStatisticsConfiguration;
 import net.sf.anathema.character.impl.model.creation.bonus.BonusPointManagement;
 import net.sf.anathema.character.model.ICharacter;
@@ -18,15 +18,13 @@ import net.sf.anathema.framework.persistence.IRepositoryItemPersister;
 import net.sf.anathema.framework.repository.IItem;
 import net.sf.anathema.lib.registry.IRegistry;
 
-import static net.sf.anathema.character.generic.type.CharacterType.SIDEREAL;
 import static net.sf.anathema.character.impl.module.ExaltedCharacterItemTypeConfiguration.CHARACTER_ITEM_TYPE_ID;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class Integrationtest {
+public class SiderealFavoredAbilitySteps {
   private IAnathemaModel model;
   private ICharacter character;
-  private ICharacterTemplate defaultSiderealTemplate;
 
   @Before
   public void startAnathema() {
@@ -34,45 +32,39 @@ public class Integrationtest {
     this.model = initializer.initialize();
   }
 
-  @When("^I create a new Sidereal$")
-  public void I_create_a_new_Sidereal() throws Throwable {
-    this.defaultSiderealTemplate = loadDefaultSiderealTemplate(model);
-    this.character = createCharacterFromTemplate(model, defaultSiderealTemplate);
+  @When("^I create a new (.*)$")
+  public void I_create_a_new_character(String type) throws Throwable {
+    ICharacterTemplate characterTemplate = loadDefaultTemplateForType(type);
+    this.character = createCharacter(characterTemplate);
   }
 
-  @And("^I set her Caste to Serenity$")
-  public void I_set_her_Caste_to_Serenity() throws Throwable {
-    ICasteCollection casteCollection = defaultSiderealTemplate.getCasteCollection();
-    ICasteType serenityCaste = casteCollection.getById("Serenity");
-    character.getCharacterConcept().getCaste().setType(serenityCaste);
+  @When("^I set her Caste to (.*)$")
+  public void I_set_her_Caste(String casteName) throws Throwable {
+    ICasteCollection casteCollection = character.getCharacterTemplate().getCasteCollection();
+    ICasteType caste = casteCollection.getById(casteName);
+    character.getCharacterConcept().getCaste().setType(caste);
   }
 
   @Then("^she has (\\d+) favored dots spent.$")
-  public void she_has_favored_dots_spent(int arg1) throws Throwable {
+  public void she_has_favored_dots_spent(int amount) throws Throwable {
     BonusPointManagement bonusPointManagement = new BonusPointManagement(character);
     bonusPointManagement.recalculate();
     Integer dotsSpent = bonusPointManagement.getFavoredAbilityModel().getValue();
-    assertThat(dotsSpent, is(7));
+    assertThat(dotsSpent, is(amount));
   }
 
-  private ICharacter createCharacterFromTemplate(IAnathemaModel model, ICharacterTemplate defaultSiderealTemplate) {
-    CharacterStatisticsConfiguration template = createCreationInstructionsForDefaultSidereal(defaultSiderealTemplate);
+  private ICharacterTemplate loadDefaultTemplateForType(String type) {
+    ICharacterGenerics generics = CharacterGenericsExtractor.getGenerics(model);
+    return generics.getTemplateRegistry().getDefaultTemplate(CharacterType.getById(type));
+  }
+
+  private ICharacter createCharacter(ICharacterTemplate template) {
+    CharacterStatisticsConfiguration creationRules = new CharacterStatisticsConfiguration();
+    creationRules.setTemplate(template);
     IRegistry<IItemType, IRepositoryItemPersister> persisterRegistry = model.getPersisterRegistry();
     IItemType characterItemType = model.getItemTypeRegistry().getById(CHARACTER_ITEM_TYPE_ID);
     IRepositoryItemPersister itemPersister = persisterRegistry.get(characterItemType);
-    IItem item = itemPersister.createNew(template);
+    IItem item = itemPersister.createNew(creationRules);
     return (ICharacter) item.getItemData();
-  }
-
-  private ICharacterTemplate loadDefaultSiderealTemplate(IAnathemaModel model) {
-    ICharacterGenerics generics = CharacterGenericsExtractor.getGenerics(model);
-    return generics.getTemplateRegistry().getDefaultTemplate(SIDEREAL);
-  }
-
-  private CharacterStatisticsConfiguration createCreationInstructionsForDefaultSidereal(
-          ICharacterTemplate innerTemplate) {
-    CharacterStatisticsConfiguration template = new CharacterStatisticsConfiguration();
-    template.setTemplate(innerTemplate);
-    return template;
   }
 }
--- Integrationtest/src/test/resources/net/sf/anathema/SiderealFavoredAbility.feature
+++ Integrationtest/src/test/resources/net/sf/anathema/SiderealFavoredAbility.feature
@@ -1,6 +1,6 @@
 Feature: Sidereals have minimum Abilities depending on their Caste
 
-  Scenario: Serenity Caste counts preset dots in Craft
+  Scenario: Serenity Caste counts preset dots in Craft (Issue #311)
     When I create a new Sidereal
     And I set her Caste to Serenity
     Then she has 7 favored dots spent.
