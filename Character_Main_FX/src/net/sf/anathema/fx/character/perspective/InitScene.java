3995366af6cec8ec469e14a2a2ee458a74b30716
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2013-02-05 09:11:58 +0100

    MigPane is direct parent of scene.

--- Character_Main_FX/src/net/sf/anathema/fx/character/perspective/InitScene.java
+++ Character_Main_FX/src/net/sf/anathema/fx/character/perspective/InitScene.java
@@ -1,18 +1,16 @@
 package net.sf.anathema.fx.character.perspective;
 
 import javafx.embed.swing.JFXPanel;
-import javafx.scene.Group;
-import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 
 public class InitScene implements Runnable {
 
   private final JFXPanel panel;
-  private Node content;
+  private Parent content;
 
-  public InitScene(JFXPanel panel, Node content) {
+  public InitScene(JFXPanel panel, Parent content) {
     this.panel = panel;
     this.content = content;
   }
@@ -24,12 +22,8 @@ public class InitScene implements Runnable {
   }
 
   private Scene createScene() {
-    Group root = new Group();
-    Scene scene = new Scene(root, Color.LAVENDER);
+    Scene scene = new Scene(content, Color.LAVENDER);
     scene.getStylesheets().add("skin/sandra/sandra.css");
-    BorderPane borderPane = new BorderPane();
-    root.getChildren().add(borderPane);
-    borderPane.setCenter(content);
     return scene;
   }
 }
