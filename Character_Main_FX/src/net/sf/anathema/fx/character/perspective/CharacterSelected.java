253dea266b7a0237f39bdcd01ec199c169e71550
Urs Reupke <ursreupke@gmail.com>
2013-02-11 18:58:20 +0100

    Loads character on EventDispatchThread This solves the diagnosed threading issue and fixes #316.

--- Character_Main_FX/src/net/sf/anathema/fx/character/perspective/CharacterSelected.java
+++ Character_Main_FX/src/net/sf/anathema/fx/character/perspective/CharacterSelected.java
@@ -2,8 +2,10 @@ package net.sf.anathema.fx.character.perspective;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import net.sf.anathema.character.perspective.model.model.CharacterIdentifier;
 import net.sf.anathema.character.perspective.Selector;
+import net.sf.anathema.character.perspective.model.model.CharacterIdentifier;
+
+import javax.swing.SwingUtilities;
 
 public class CharacterSelected implements EventHandler<ActionEvent> {
   private final Selector<CharacterIdentifier> characterSelector;
@@ -16,6 +18,11 @@ public class CharacterSelected implements EventHandler<ActionEvent> {
 
   @Override
   public void handle(ActionEvent actionEvent) {
-    characterSelector.selected(identifier);
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        characterSelector.selected(identifier);
+      }
+    });
   }
 }
