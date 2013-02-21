8e1b7f6344314ce513c9f2287489e8558568e75d
Urs Reupke <ursreupke@gmail.com>
2013-02-21 21:21:22 +0100

    Adds tests for Alien Charms. * Suprisingly, they run out of the box.

--- Integrationtest/src/test/java/net/sf/anathema/CharmSteps.java
+++ Integrationtest/src/test/java/net/sf/anathema/CharmSteps.java
@@ -1,6 +1,7 @@
 package net.sf.anathema;
 
 import com.google.inject.Inject;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import net.sf.anathema.character.generic.magic.ICharm;
 
@@ -9,24 +10,36 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CharmSteps {
 
-  private final CharacterHolder holder;
+  private final CharacterHolder character;
 
   @Inject
-  public CharmSteps(CharacterHolder holder) {
-    this.holder = holder;
+  public CharmSteps(CharacterHolder character) {
+    this.character = character;
+  }
+
+  @And("^she learns the Charm (.*)$")
+  public void she_learns_the_Charm(String id) throws Throwable {
+    ICharm charm = character.getCharms().getCharmById(id);
+    character.getCharms().getGroup(charm).toggleLearned(charm);
   }
 
   @Then("^she can learn the Charm (.*)$")
   public void she_can_learn_the_Charm(String id) throws Throwable {
-    ICharm charm = holder.getCharms().getCharmById(id);
-    boolean learnable = holder.getCharms().isLearnable(charm);
+    ICharm charm = character.getCharms().getCharmById(id);
+    boolean learnable = character.getCharms().isLearnable(charm);
     assertThat(learnable, is(true));
   }
 
   @Then("^she can not learn the Charm (.*)$")
   public void she_can_not_learn_the_Charm(String id) throws Throwable {
-    ICharm charm = holder.getCharms().getCharmById(id);
-    boolean learnable = holder.getCharms().isLearnable(charm);
+    ICharm charm = character.getCharms().getCharmById(id);
+    boolean learnable = character.getCharms().isLearnable(charm);
     assertThat(learnable, is(false));
+  }
+
+  @Then("^she does not know the Charm (.*)$")
+  public void she_does_not_know_the_Charm(String id) throws Throwable {
+    boolean learned = character.getCharms().isLearned(id);
+    assertThat(learned, is(false));
   }
 }
