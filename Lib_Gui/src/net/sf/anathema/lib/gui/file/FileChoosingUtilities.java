9b53c74a720931651af96c4853e8f29bfca6d0e3
Urs Reupke <ursreupke@gmail.com>
2013-03-01 22:00:13 +0100

    Suggests a name for exports on OS X.

--- Lib_Gui/src/net/sf/anathema/lib/gui/file/FileChoosingUtilities.java
+++ Lib_Gui/src/net/sf/anathema/lib/gui/file/FileChoosingUtilities.java
@@ -3,8 +3,9 @@ package net.sf.anathema.lib.gui.file;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.plaf.FileChooserUI;
-import javax.swing.plaf.basic.BasicFileChooserUI;
 import java.awt.Component;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -14,11 +15,7 @@ public class FileChoosingUtilities {
 
   public static Path selectSaveFile(Component parentComponent, String suggestedFileName) {
     JFileChooser chooser = new JFileChooser(workingDirectory.toFile());
-    FileChooserUI chooserUi = chooser.getUI();
-    if (chooserUi instanceof BasicFileChooserUI) {
-      BasicFileChooserUI basicUi = (BasicFileChooserUI) chooserUi;
-      basicUi.setFileName(suggestedFileName);
-    }
+    setSuggestedFileName(suggestedFileName, chooser);
     int approveOption = chooser.showSaveDialog(parentComponent);
     if (approveOption != JFileChooser.APPROVE_OPTION) {
       return null;
@@ -26,6 +23,16 @@ public class FileChoosingUtilities {
     Path selectedFile = chooser.getSelectedFile().toPath();
     workingDirectory = selectedFile.getParent();
     return selectedFile;
+  }
+
+  private static void setSuggestedFileName(String suggestedFileName, JFileChooser chooser) {
+    FileChooserUI chooserUi = chooser.getUI();
+    try {
+      Method method = chooserUi.getClass().getMethod("setFileName", String.class);
+      method.invoke(chooserUi, suggestedFileName);
+    } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+      //Don't set the name, we can live with that.
+    }
   }
 
   public static Path chooseFile(String confirm, Component parentComponent, FileFilter filter) {
