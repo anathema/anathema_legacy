dca27ff44917d5ec0ba2c47a10e34c7398ff4312
Urs Reupke <ursreupke@gmail.com>
2013-03-07 19:40:09 +0100

    Less reliance on Arrays

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/traits/TraitTypeUtils.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/traits/TraitTypeUtils.java
@@ -9,37 +9,29 @@ import net.sf.anathema.character.generic.traits.types.YoziType;
 
 import java.util.ArrayList;
 
+import static java.util.Collections.addAll;
+
 public class TraitTypeUtils {
 
   private final ArrayList<ITraitType> allPrerequisiteTypeList = new ArrayList<>();
 
-  private ITraitType[] getAllCoreTraitTypes() {
-    if (allPrerequisiteTypeList.isEmpty()) {
-      for (ITraitType type : AbilityType.values()) {
-        allPrerequisiteTypeList.add(type);
-      }
-      for (ITraitType type : AttributeType.values()) {
-        allPrerequisiteTypeList.add(type);
-      }
-      for (ITraitType type : VirtueType.values()) {
-        allPrerequisiteTypeList.add(type);
-      }
-      for (ITraitType type : YoziType.values())
-      	allPrerequisiteTypeList.add(type);
-      for (ITraitType type : OtherTraitType.values()) {
-        allPrerequisiteTypeList.add(type);
-      }
-    }
-    return allPrerequisiteTypeList.toArray(new ITraitType[allPrerequisiteTypeList.size()]);
-  }
-
   public ITraitType getTraitTypeById(String id) {
-    ITraitType[] allTraits = getAllCoreTraitTypes();
-    for (ITraitType type : allTraits) {
+    for (ITraitType type : getAllCoreTraitTypes()) {
       if (id.equals(type.getId())) {
         return type;
       }
     }
     throw new IllegalArgumentException("No trait type with id: " + id); //$NON-NLS-1$
+  }
+
+  private Iterable<ITraitType> getAllCoreTraitTypes() {
+    if (allPrerequisiteTypeList.isEmpty()) {
+      addAll(allPrerequisiteTypeList, AbilityType.values());
+      addAll(allPrerequisiteTypeList, AttributeType.values());
+      addAll(allPrerequisiteTypeList, VirtueType.values());
+      addAll(allPrerequisiteTypeList, YoziType.values());
+      addAll(allPrerequisiteTypeList, OtherTraitType.values());
+    }
+    return allPrerequisiteTypeList;
   }
 }
