e1584cf99c400d5b1e1ab984bf8aaa3a597bfb18
Urs Reupke <ursreupke@gmail.com>
2013-03-20 19:47:56 +0100

    Removes unused class & method

--- Character_Generic/src/net/sf/anathema/character/generic/template/abilities/AbilityGroupType.java
+++ Character_Generic/src/net/sf/anathema/character/generic/template/abilities/AbilityGroupType.java
@@ -4,34 +4,12 @@ import net.sf.anathema.lib.util.Identified;
 
 public enum AbilityGroupType implements Identified {
 
-  Life {
-    @Override
-    public void accept(IAbilityGroupTypeVisitor visitor) {
-      visitor.visitLife(this);
-    }
-  },
-  Wisdom {
-    @Override
-    public void accept(IAbilityGroupTypeVisitor visitor) {
-      visitor.visitWisdom(this);
-    }
-  },
-  War {
-    @Override
-    public void accept(IAbilityGroupTypeVisitor visitor) {
-      visitor.visitWar(this);
-    }
-  };
-
-  public abstract void accept(IAbilityGroupTypeVisitor visitor);
+  Life ,
+  Wisdom,
+  War;
 
   @Override
   public String getId() {
     return name();
-  }
-
-  @Override
-  public String toString() {
-    return getId();
   }
 }
