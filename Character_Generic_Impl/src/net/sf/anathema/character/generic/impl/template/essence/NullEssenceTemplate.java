2a32873baa4bc303cb497026e59a3a4b72f2fdb4
Urs Reupke <ursreupke@gmail.com>
2013-03-27 19:25:02 +0100

    Cleanup

--- Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/template/essence/NullEssenceTemplate.java
+++ Character_Generic_Impl/src/net/sf/anathema/character/generic/impl/template/essence/NullEssenceTemplate.java
@@ -3,7 +3,6 @@ package net.sf.anathema.character.generic.impl.template.essence;
 import net.sf.anathema.character.generic.template.essence.FactorizedTrait;
 import net.sf.anathema.character.generic.template.essence.IEssenceTemplate;
 import net.sf.anathema.character.generic.traits.IGenericTrait;
-import net.sf.anathema.lib.data.Range;
 
 public class NullEssenceTemplate implements IEssenceTemplate {
 
@@ -15,10 +14,6 @@ public class NullEssenceTemplate implements IEssenceTemplate {
   @Override
   public FactorizedTrait[] getPeripheralTraits(IGenericTrait willpower, IGenericTrait[] virtues, IGenericTrait essence) {
     return new FactorizedTrait[0];
-  }
-
-  public Range getEssenceRange() {
-    return new Range(1, 1);
   }
 
   @Override
