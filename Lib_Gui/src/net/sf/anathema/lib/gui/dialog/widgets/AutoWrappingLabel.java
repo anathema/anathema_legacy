785f16b01b1f8d80851c8d42cc57fb33cea14d84
Urs Reupke <ursreupke@gmail.com>
2013-03-28 11:12:43 +0100

    Cleanup

--- Lib_Gui/src/net/sf/anathema/lib/gui/dialog/widgets/AutoWrappingLabel.java
+++ Lib_Gui/src/net/sf/anathema/lib/gui/dialog/widgets/AutoWrappingLabel.java
@@ -11,20 +11,10 @@ import java.awt.Font;
  */
 public class AutoWrappingLabel {
 
-  private static final int INITIAL_WIDTH = 330;
-
   //10.11.2007 (Markus Gebhard): I have tried to implement this using a JTextArea, but this failed.
   // Problem is the size: there is no good way to find out the required height in order not to crop
   // the text. So after trying really hard, I decided to write my own text swing
   private final AutoWrappingTextComponent component;
-
-  public AutoWrappingLabel() {
-    this(""); //$NON-NLS-1$
-  }
-
-  public AutoWrappingLabel(String text) {
-    this(text, INITIAL_WIDTH);
-  }
 
   public AutoWrappingLabel(String text, int width) {
     component = new AutoWrappingTextComponent(text, width);
