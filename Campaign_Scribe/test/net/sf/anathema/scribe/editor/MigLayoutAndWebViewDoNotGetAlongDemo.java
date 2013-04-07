f2caa85ae3b4d74035324c5a8d254ae55c500a72
Urs Reupke <ursreupke@gmail.com>
2013-04-07 21:22:41 +0200

    Dumbs down demo to the very bottom * Something between miglayout and webview is broken. * BorderPane works like a charm

--- Campaign_Scribe/test/net/sf/anathema/scribe/editor/MigLayoutAndWebViewDoNotGetAlongDemo.java
+++ Campaign_Scribe/test/net/sf/anathema/scribe/editor/MigLayoutAndWebViewDoNotGetAlongDemo.java
@@ -5,14 +5,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
-import net.miginfocom.layout.AC;
 import net.miginfocom.layout.CC;
-import net.miginfocom.layout.LC;
 import org.tbee.javafx.scene.layout.MigPane;
 
-public class ScrollPreviewDemo extends Application {
-
-  private static final int DEBUG = 0;
+public class MigLayoutAndWebViewDoNotGetAlongDemo extends Application {
 
   public static void main(String[] arguments) {
     launch();
@@ -20,14 +16,12 @@ public class ScrollPreviewDemo extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
-    TextField titleDisplay = new TextField("");
     WebView content = new WebView();
-    MigPane pane = new MigPane(new LC().insets("0").gridGap("0", "2").wrapAfter(1).debug(DEBUG), new AC().grow().fill(),
-            new AC().fill());
-    pane.add(titleDisplay, new CC().width("100%").grow());
-    pane.add(content, new CC().push());
+    MigPane pane = new MigPane();
+    TextField node = new TextField("London!");
+    pane.add(node, new CC().wrap());
+    pane.add(content);
     Scene scene = new Scene(pane);
-    titleDisplay.setText("London!");
     content.getEngine().loadContent("Hullo!");
     stage.setScene(scene);
     stage.setWidth(800);
