14d31e2bca5a3559848e43ce9388d0535992357a
Urs Reupke <ursreupke@gmail.com>
2013-05-22 19:05:40 +0200

    Makes Interaction extend Tool

--- Platform_Swing/src/main/java/net/sf/anathema/interaction/Interaction.java
+++ Platform_Swing/src/main/java/net/sf/anathema/interaction/Interaction.java
@@ -1,21 +1,8 @@
 package net.sf.anathema.interaction;
 
 import net.sf.anathema.framework.perspective.ToolBar;
-import net.sf.anathema.lib.file.RelativePath;
 
-public interface Interaction {
-
-  void setIcon(RelativePath relativePath);
-
-  void setTooltip(String key);
-
-  void setText(String key);
-
-  void enable();
-
-  void disable();
-
-  void setCommand(Command command);
+public interface Interaction extends Tool {
 
   void addTo(ToolBar toolbar);
 }
