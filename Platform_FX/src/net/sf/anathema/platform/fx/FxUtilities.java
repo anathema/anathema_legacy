c1e526b6c4c1c94ef29ec309aea93ec27c0844dd
Urs Reupke <ursreupke@gmail.com>
2013-06-12 17:51:43 +0200

    Renames method to reflect extended intent (#332)

--- Platform_FX/src/net/sf/anathema/platform/fx/FxUtilities.java
+++ Platform_FX/src/net/sf/anathema/platform/fx/FxUtilities.java
@@ -3,7 +3,7 @@ package net.sf.anathema.platform.fx;
 import org.apache.commons.lang3.SystemUtils;
 
 public class FxUtilities {
-  public static boolean systemSupportsEmbeddedTooltips() {
+  public static boolean systemSupportsPopUpsWhileEmbeddingFxIntoSwing() {
     return !SystemUtils.IS_OS_MAC;
   }
 }
