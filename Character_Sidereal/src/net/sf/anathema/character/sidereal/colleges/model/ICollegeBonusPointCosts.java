45be117d5605fe8db33f6697c6103c157b1ad2ce
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2013-06-13 10:57:59 +0200

    Eliminated Character_Sidereal

--- Character_Sidereal/src/net/sf/anathema/character/sidereal/colleges/model/ICollegeBonusPointCosts.java
+++
@@ -1,6 +0,0 @@
-package net.sf.anathema.character.sidereal.colleges.model;
-
-public interface ICollegeBonusPointCosts {
-
-  int getCollegeCosts(boolean favored);
-}
--- Character_Sidereal/src/net/sf/anathema/character/sidereal/template/ICollegeExperienceCosts.java
+++
@@ -1,8 +0,0 @@
-package net.sf.anathema.character.sidereal.template;
-
-import net.sf.anathema.character.generic.template.experience.CurrentRatingCosts;
-
-public interface ICollegeExperienceCosts {
-
-  CurrentRatingCosts getCollegeExperienceCost(boolean favored);
-}
