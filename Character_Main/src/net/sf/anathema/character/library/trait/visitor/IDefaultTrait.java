fadd82cd73961a83572b913cde5a927826d7ac5a
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2013-06-15 22:51:08 +0200

    Deleted IModifiableGenericTrait

--- Character_Main/src/net/sf/anathema/character/library/trait/visitor/IDefaultTrait.java
+++ Character_Main/src/net/sf/anathema/character/library/trait/visitor/IDefaultTrait.java
@@ -1,12 +1,14 @@
 package net.sf.anathema.character.library.trait.visitor;
 
 import net.sf.anathema.character.generic.framework.additionaltemplate.model.IModifiableBasicTrait;
+import net.sf.anathema.character.generic.traits.IGenericTrait;
 import net.sf.anathema.character.library.trait.IModifiableCapTrait;
-import net.sf.anathema.character.library.trait.IModifiableGenericTrait;
 import net.sf.anathema.character.library.trait.ITrait;
 import net.sf.anathema.lib.control.IChangeListener;
 
-public interface IDefaultTrait extends ITrait, IModifiableBasicTrait, IModifiableGenericTrait, IModifiableCapTrait {
+public interface IDefaultTrait extends ITrait, IModifiableBasicTrait, IGenericTrait, IModifiableCapTrait {
+
+  void setCurrentValue(int value);
 
   int getMinimalValue();
 
