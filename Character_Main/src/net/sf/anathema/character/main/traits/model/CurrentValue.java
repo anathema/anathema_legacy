82c775904b469d6ccc0908faa2ede9334e174cbf
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2013-06-15 19:36:21 +0200

    Deleted unused code

--- Character_Main/src/net/sf/anathema/character/main/traits/model/CurrentValue.java
+++
@@ -1,8 +0,0 @@
-package net.sf.anathema.character.main.traits.model;
-
-public interface CurrentValue {
-
-  int getValue();
-
-  void setValue(int value);
-}
--- Character_Main/src/net/sf/anathema/character/main/traits/model/DecisiveCurrentValue.java
+++
@@ -1,4 +0,0 @@
-package net.sf.anathema.character.main.traits.model;
-
-public class DecisiveCurrentValue {
-}
