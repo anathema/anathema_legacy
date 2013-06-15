0d2a5cf26ec09b75d57be7696a62f4a5dd0955a0
Urs Reupke <ursreupke@gmail.com>
2013-06-15 10:53:56 +0200

    Cleanup

--- Character_Main/src/net/sf/anathema/character/view/CharacterView.java
+++ Character_Main/src/net/sf/anathema/character/view/CharacterView.java
@@ -1,6 +1,5 @@
 package net.sf.anathema.character.view;
 
-import net.sf.anathema.framework.presenter.view.MultipleContentView;
 import net.sf.anathema.framework.swing.IView;
 
 public interface CharacterView extends IView, OverviewContainer {
