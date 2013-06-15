dd22481b9335a308a88aa6f5c13cd74aea42d741
Urs Reupke <ursreupke@gmail.com>
2013-06-15 10:56:12 +0200

    Cleanup

--- Character_Generic/src/net/sf/anathema/character/generic/framework/additionaltemplate/IAdditionalInitializer.java
+++ Character_Generic/src/net/sf/anathema/character/generic/framework/additionaltemplate/IAdditionalInitializer.java
@@ -2,7 +2,6 @@ package net.sf.anathema.character.generic.framework.additionaltemplate;
 
 import net.sf.anathema.character.generic.additionaltemplate.IAdditionalModel;
 import net.sf.anathema.character.generic.type.ICharacterType;
-import net.sf.anathema.framework.swing.IView;
 import net.sf.anathema.lib.resources.Resources;
 
 public interface IAdditionalInitializer<T> {
