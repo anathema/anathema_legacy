2a10381918eaae9a78fd37299f90fad7d4cf404e
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2013-06-16 16:40:42 +0200

    cleanup

--- Character_Main/src/net/sf/anathema/character/impl/model/traits/DefaultTraitModel.java
+++ Character_Main/src/net/sf/anathema/character/impl/model/traits/DefaultTraitModel.java
@@ -1,6 +1,5 @@
 package net.sf.anathema.character.impl.model.traits;
 
-import net.sf.anathema.character.generic.traits.TraitType;
 import net.sf.anathema.character.library.trait.Trait;
 import net.sf.anathema.character.main.traits.model.DefaultTraitMap;
 import net.sf.anathema.character.main.traits.model.TraitMap;
