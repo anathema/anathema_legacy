68643f86cd1be9b0626a53a31b4ea6fb684d77e4
Urs Reupke <ursreupke@gmail.com>
2013-06-16 15:38:54 +0200

    Removes Swing from Virtue Flaw View

--- Character_Main/src/net/sf/anathema/character/library/virtueflaw/presenter/IVirtueFlawView.java
+++ Character_Main/src/net/sf/anathema/character/library/virtueflaw/presenter/IVirtueFlawView.java
@@ -1,20 +1,19 @@
 package net.sf.anathema.character.library.virtueflaw.presenter;
 
 import net.sf.anathema.character.generic.traits.TraitType;
-import net.sf.anathema.character.library.trait.view.SimpleTraitView;
-import net.sf.anathema.framework.swing.IView;
+import net.sf.anathema.framework.value.IIntValueView;
 import net.sf.anathema.lib.gui.selection.IObjectSelectionView;
 import net.sf.anathema.lib.workflow.textualdescription.ITextView;
 
 import javax.swing.ListCellRenderer;
 
-public interface IVirtueFlawView extends IView {
+public interface IVirtueFlawView {
 
   ITextView addTextView(String label, int columnCount);
 
   IObjectSelectionView<TraitType> addVirtueFlawRootSelectionView(String string, ListCellRenderer renderer);
 
-  SimpleTraitView addLimitValueView(String label, int value, int maxValue);
+  IIntValueView addLimitValueView(String label, int value, int maxValue);
 
   void setEnabled(boolean enabled);
 }
