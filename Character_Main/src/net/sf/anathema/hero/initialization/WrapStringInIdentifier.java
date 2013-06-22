3a1d38305e14368449e173e4ced37d330e6c10ba
Urs Reupke <ursreupke@gmail.com>
2013-06-22 15:21:19 +0200

    Removes Nullable where null cannot occur.

--- Character_Main/src/net/sf/anathema/hero/initialization/WrapStringInIdentifier.java
+++ Character_Main/src/net/sf/anathema/hero/initialization/WrapStringInIdentifier.java
@@ -4,11 +4,9 @@ import com.google.common.base.Function;
 import net.sf.anathema.lib.util.Identifier;
 import net.sf.anathema.lib.util.SimpleIdentifier;
 
-import javax.annotation.Nullable;
-
 public class WrapStringInIdentifier implements Function<String, Identifier> {
   @Override
-  public Identifier apply(@Nullable String input) {
+  public Identifier apply(String input) {
     return new SimpleIdentifier(input);
   }
 }
