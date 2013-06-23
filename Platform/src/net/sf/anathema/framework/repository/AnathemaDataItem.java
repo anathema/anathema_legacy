e060051fff1f7ceeea1e5411bb6c81d95eebd08b
Urs Reupke <ursreupke@gmail.com>
2013-06-23 10:46:19 +0200

    Merges AbstractAnathemaItem into its sole child

--- Platform/src/net/sf/anathema/framework/repository/AnathemaDataItem.java
+++ Platform/src/net/sf/anathema/framework/repository/AnathemaDataItem.java
@@ -1,19 +1,35 @@
 package net.sf.anathema.framework.repository;
 
+import com.google.common.base.Objects;
 import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import net.sf.anathema.framework.item.IItemType;
 import net.sf.anathema.framework.itemdata.model.IItemData;
 import net.sf.anathema.framework.presenter.itemmanagement.PrintNameAdjuster;
 import net.sf.anathema.lib.control.IChangeListener;
+import net.sf.anathema.lib.util.Identifier;
+import org.jmock.example.announcer.Announcer;
 
-public class AnathemaDataItem extends AbstractAnathemaItem {
+public class AnathemaDataItem implements IItem {
 
   private final IItemData itemData;
+  private final IItemType itemType;
+  private final RepositoryLocation repositoryLocation;
+  private final Identifier identificate;
+  private final Announcer<IItemListener> repositoryItemListeners = Announcer.to(IItemListener.class);
+  private String printName;
 
   public AnathemaDataItem(IItemType type, IItemData itemData) {
-    super(type);
     Preconditions.checkArgument(type.supportsRepository());
     Preconditions.checkNotNull(itemData);
+    this.itemType = type;
+    this.repositoryLocation = new RepositoryLocation(this);
+    this.identificate = new Identifier() {
+      @Override
+      public String getId() {
+        return repositoryLocation.getId();
+      }
+    };
     this.itemData = itemData;
     itemData.setPrintNameAdjuster(new PrintNameAdjuster(this));
   }
@@ -41,5 +57,56 @@ public class AnathemaDataItem extends AbstractAnathemaItem {
   @Override
   public void removeDirtyListener(IChangeListener changeListener) {
     itemData.removeDirtyListener(changeListener);
+  }
+
+  @Override
+  public void addItemListener(IItemListener listener) {
+    repositoryItemListeners.addListener(listener);
+  }
+
+  @Override
+  public void removeItemListener(IItemListener listener) {
+    repositoryItemListeners.removeListener(listener);
+  }
+
+  private void firePrintNameChanged(String name) {
+    repositoryItemListeners.announce().printNameChanged(name);
+  }
+
+  @Override
+  public final IItemType getItemType() {
+    return itemType;
+  }
+
+  @Override
+  public final synchronized String getId() {
+    return identificate.getId();
+  }
+
+  @Override
+  public String getDisplayName() {
+    return printName == null ? DEFAULT_PRINT_NAME : printName;
+  }
+
+  @Override
+  public void setPrintName(String printName) {
+    if (Strings.isNullOrEmpty(printName)) {
+      printName = null;
+    }
+    if (Objects.equal(this.printName, printName)) {
+      return;
+    }
+    this.printName = printName;
+    firePrintNameChanged(getDisplayName());
+  }
+
+  @Override
+  public IItemRepositoryLocation getRepositoryLocation() {
+    return repositoryLocation;
+  }
+
+  @Override
+  public String toString() {
+    return getItemType() + ": " + getDisplayName();
   }
 }
