724e7c9921d16b6533ba44145a90010fc6ab5afd
Urs Reupke <ursreupke@gmail.com>
2013-06-23 12:50:27 +0200

    Simplifies hierarchy of itemtype configurations

--- Character_Equipment/src/net/sf/anathema/character/equipment/impl/module/EquipmentDatabaseItemTypeConfiguration.java
+++ Character_Equipment/src/net/sf/anathema/character/equipment/impl/module/EquipmentDatabaseItemTypeConfiguration.java
@@ -1,17 +1,23 @@
 package net.sf.anathema.character.equipment.impl.module;
 
-import net.sf.anathema.framework.module.AbstractNonPersistableItemTypeConfiguration;
+import net.sf.anathema.framework.item.IItemType;
 import net.sf.anathema.framework.repository.ItemType;
 import net.sf.anathema.framework.repository.RepositoryConfiguration;
 import net.sf.anathema.initialization.ItemTypeConfiguration;
 
 @ItemTypeConfiguration
-public class EquipmentDatabaseItemTypeConfiguration extends AbstractNonPersistableItemTypeConfiguration {
+public class EquipmentDatabaseItemTypeConfiguration implements net.sf.anathema.framework.module.ItemTypeConfiguration {
 
   public static final String EQUIPMENT_DATABASE_ITEM_TYPE_ID = "EquipmentDatabase";
   private static final RepositoryConfiguration REPOSITORY_CONFIGURATION = new RepositoryConfiguration(".item", "equipment/");
+  private final IItemType type;
 
   public EquipmentDatabaseItemTypeConfiguration() {
-    super(new ItemType(EQUIPMENT_DATABASE_ITEM_TYPE_ID, REPOSITORY_CONFIGURATION, false));
+    this.type = new ItemType(EQUIPMENT_DATABASE_ITEM_TYPE_ID, REPOSITORY_CONFIGURATION, false);
+  }
+
+  @Override
+  public IItemType getItemType() {
+    return type;
   }
 }
--- Magic_Description/src/net/sf/anathema/magic/description/module/MagicDescriptionItemTypeConfiguration.java
+++ Magic_Description/src/net/sf/anathema/magic/description/module/MagicDescriptionItemTypeConfiguration.java
@@ -1,17 +1,23 @@
 package net.sf.anathema.magic.description.module;
 
-import net.sf.anathema.framework.module.AbstractNonPersistableItemTypeConfiguration;
+import net.sf.anathema.framework.item.IItemType;
 import net.sf.anathema.framework.repository.ItemType;
 import net.sf.anathema.framework.repository.RepositoryConfiguration;
 import net.sf.anathema.initialization.ItemTypeConfiguration;
 
 @ItemTypeConfiguration
-public class MagicDescriptionItemTypeConfiguration extends AbstractNonPersistableItemTypeConfiguration {
+public class MagicDescriptionItemTypeConfiguration implements net.sf.anathema.framework.module.ItemTypeConfiguration {
 
   public static final String MAGIC_DESCRIPTION_ITEM_TYPE_ID = "MagicDescription";
   private static final RepositoryConfiguration REPOSITORY_CONFIGURATION = new RepositoryConfiguration(".mdsc", "MagicDescription/");
+  private IItemType type;
 
   public MagicDescriptionItemTypeConfiguration() {
-    super(new ItemType(MAGIC_DESCRIPTION_ITEM_TYPE_ID, REPOSITORY_CONFIGURATION, false));
+    this.type = new ItemType(MAGIC_DESCRIPTION_ITEM_TYPE_ID, REPOSITORY_CONFIGURATION, false);
+  }
+
+  @Override
+  public IItemType getItemType() {
+    return type;
   }
 }
