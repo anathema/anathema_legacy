d883758ceb9342613d203ad686e1de1ca316326d
Urs Reupke <ursreupke@gmail.com>
2013-06-24 15:25:32 +0200

    cleanup

--- Platform/src/net/sf/anathema/framework/configuration/IInitializationPreferences.java
+++ Platform/src/net/sf/anathema/framework/configuration/IInitializationPreferences.java
@@ -6,13 +6,6 @@ public interface IInitializationPreferences {
 
   boolean initMaximized();
 
-  /**
-   * Returns the class name of the user defined Look and Feel.
-   * Don't assume that the returned class name is valid.
-   * 
-   *  @return the class name of the user defined Look and Feel or {@code null}
-   *    if it was defined by the user.
-   */
   String getUserLookAndFeel();
 
   Locale getPreferredLocale();
