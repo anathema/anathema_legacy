93a18af0ccc0ce8c6463fbef14e93611f6d771e8
Urs Reupke <ursreupke@gmail.com>
2013-06-27 19:05:18 +0200

    Uses more abstract interface (#343)

--- Character_Main/src/net/sf/anathema/character/presenter/magic/VisibilityPredicate.java
+++ Character_Main/src/net/sf/anathema/character/presenter/magic/VisibilityPredicate.java
@@ -3,22 +3,22 @@ package net.sf.anathema.character.presenter.magic;
 import com.google.common.base.Predicate;
 import net.sf.anathema.character.generic.magic.ICharm;
 import net.sf.anathema.character.generic.magic.charms.ICharmGroup;
-import net.sf.anathema.character.model.charm.ICharmConfiguration;
+import net.sf.anathema.character.generic.magic.charms.ICharmIdMap;
 import net.sf.anathema.charmtree.presenter.view.CharmGroupInformer;
 
 public class VisibilityPredicate implements Predicate<String> {
 
-  private final ICharmConfiguration charmConfiguration;
+  private final ICharmIdMap charmMap;
   private final CharmGroupInformer charmGroupInformer;
 
-  public VisibilityPredicate(ICharmConfiguration charmConfiguration, CharmGroupInformer informer) {
-    this.charmConfiguration = charmConfiguration;
+  public VisibilityPredicate(ICharmIdMap charmMap, CharmGroupInformer informer) {
+    this.charmMap = charmMap;
     this.charmGroupInformer = informer;
   }
 
   @Override
   public boolean apply(String charmId) {
-    ICharm charm = charmConfiguration.getCharmById(charmId);
+    ICharm charm = charmMap.getCharmById(charmId);
     return isVisible(charm);
   }
 
