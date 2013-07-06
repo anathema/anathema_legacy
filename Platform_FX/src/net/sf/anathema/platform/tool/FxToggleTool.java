8d655576d33d2ba735862415e3c87d342eca582d
Urs Reupke <ursreupke@gmail.com>
2013-07-06 18:57:37 +0200

    Improves visibility of caste/favored icons when XP

--- Platform_FX/src/net/sf/anathema/platform/tool/FxToggleTool.java
+++ Platform_FX/src/net/sf/anathema/platform/tool/FxToggleTool.java
@@ -38,6 +38,10 @@ public class FxToggleTool extends FxBaseTool implements ToggleTool {
     button.setSelected(false);
   }
 
+  public void setStyle(String style) {
+    button.setStyle(style);
+  }
+
   private class DeselectToggleButtonAndProcess implements Command {
     private Command command;
 
