eb80f63504e55068bfb7f53800fb0843dedc6c59
Urs Reupke <ursreupke@gmail.com>
2013-07-06 18:05:34 +0200

    Simplifies class name

--- Hero_Languages/src/net/sf/anathema/hero/languages/display/LanguagesViewFactory.java
+++ Hero_Languages/src/net/sf/anathema/hero/languages/display/LanguagesViewFactory.java
@@ -5,7 +5,8 @@ import net.sf.anathema.character.platform.RegisteredCharacterView;
 import net.sf.anathema.character.view.SubViewFactory;
 
 @RegisteredCharacterView(LanguagesView.class)
-public class LanguagesSubViewFactory implements SubViewFactory {
+public class LanguagesViewFactory implements SubViewFactory {
+  @SuppressWarnings("unchecked")
   @Override
   public <T> T create(ICharacterType type) {
     return (T) new LanguagesViewImpl();
