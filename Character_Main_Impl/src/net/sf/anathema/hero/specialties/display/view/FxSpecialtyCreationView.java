f95f443eb4433ca3eab92892ce3a452fee30f3a2
Urs Reupke <ursreupke@gmail.com>
2013-07-06 15:29:43 +0200

    i18ns Specialty selection

--- Character_Main_Impl/src/net/sf/anathema/hero/specialties/display/view/FxSpecialtyCreationView.java
+++ Character_Main_Impl/src/net/sf/anathema/hero/specialties/display/view/FxSpecialtyCreationView.java
@@ -1,10 +1,8 @@
 package net.sf.anathema.hero.specialties.display.view;
 
-import com.sun.javafx.collections.ObservableListWrapper;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.Node;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import net.sf.anathema.character.generic.traits.TraitType;
 import net.sf.anathema.hero.specialties.display.presenter.SpecialtyCreationView;
@@ -13,20 +11,20 @@ import net.sf.anathema.lib.control.ObjectValueListener;
 import net.sf.anathema.lib.file.RelativePath;
 import net.sf.anathema.lib.gui.AgnosticUIConfiguration;
 import net.sf.anathema.platform.fx.FxThreading;
+import net.sf.anathema.platform.fx.selection.ComboBoxSelectionView;
 import net.sf.anathema.platform.tool.FxButtonTool;
 import org.tbee.javafx.scene.layout.MigPane;
 
-import java.util.Arrays;
-
 public class FxSpecialtyCreationView implements SpecialtyCreationView {
 
-  private final ComboBox<TraitType> box = new ComboBox<>();
+  private final ComboBoxSelectionView<TraitType> box;
   private final TextField field = new TextField();
   private final FxButtonTool tool = FxButtonTool.ForToolbar();
   private final MigPane pane = new MigPane();
 
   public FxSpecialtyCreationView(AgnosticUIConfiguration<TraitType> configuration, RelativePath addIcon) {
-    pane.add(box);
+    this.box = new ComboBoxSelectionView<>("", configuration);
+    pane.add(box.getNode());
     pane.add(field);
     pane.add(tool.getNode());
     tool.setIcon(addIcon);
@@ -34,12 +32,7 @@ public class FxSpecialtyCreationView implements SpecialtyCreationView {
 
   @Override
   public void addSelectionChangedListener(final ObjectValueListener<TraitType> listener) {
-    box.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TraitType>() {
-      @Override
-      public void changed(ObservableValue observableValue, TraitType o, TraitType newValue) {
-        listener.valueChanged(newValue);
-      }
-    });
+    box.addObjectSelectionChangedListener(listener);
   }
 
   @Override
@@ -59,7 +52,7 @@ public class FxSpecialtyCreationView implements SpecialtyCreationView {
 
   @Override
   public void clear() {
-    box.getSelectionModel().clearSelection();
+    box.clearSelection();
     field.textProperty().setValue(null);
   }
 
@@ -74,22 +67,12 @@ public class FxSpecialtyCreationView implements SpecialtyCreationView {
 
   @Override
   public void setObjects(final TraitType[] references) {
-    FxThreading.runOnCorrectThread(new Runnable() {
-      @Override
-      public void run() {
-        box.setItems(new ObservableListWrapper<>(Arrays.asList(references)));
-      }
-    });
+    box.setObjects(references);
   }
 
   @Override
   public void selectTrait(final TraitType currentTrait) {
-    FxThreading.runOnCorrectThread(new Runnable() {
-      @Override
-      public void run() {
-        box.getSelectionModel().select(currentTrait);
-      }
-    });
+    box.setSelectedObject(currentTrait);
   }
 
   @Override
