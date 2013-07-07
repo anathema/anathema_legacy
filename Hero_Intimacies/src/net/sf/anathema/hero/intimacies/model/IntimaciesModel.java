560c7ef3a856cb74897257692360c825905a6f0a
Urs Reupke <ursreupke@gmail.com>
2013-07-07 17:15:13 +0200

    Reduces hierarchy of Intimacies

--- Hero_Intimacies/src/net/sf/anathema/hero/intimacies/model/IntimaciesModel.java
+++ Hero_Intimacies/src/net/sf/anathema/hero/intimacies/model/IntimaciesModel.java
@@ -1,14 +1,19 @@
 package net.sf.anathema.hero.intimacies.model;
 
-import net.sf.anathema.character.main.library.selection.IStringEntryTraitModel;
+import net.sf.anathema.character.main.library.removableentry.IRemovableEntryModel;
+import net.sf.anathema.hero.change.FlavoredChangeListener;
 import net.sf.anathema.hero.model.HeroModel;
 import net.sf.anathema.lib.control.ChangeListener;
 import net.sf.anathema.lib.util.Identifier;
 import net.sf.anathema.lib.util.SimpleIdentifier;
 
-public interface IntimaciesModel extends IStringEntryTraitModel<Intimacy>, HeroModel {
+public interface IntimaciesModel extends IRemovableEntryModel<Intimacy>, HeroModel {
 
   Identifier ID = new SimpleIdentifier("Intimacies");
+
+  void setCurrentName(String newValue);
+
+  void addChangeListener(FlavoredChangeListener listener);
 
   int getFreeIntimacies();
 
