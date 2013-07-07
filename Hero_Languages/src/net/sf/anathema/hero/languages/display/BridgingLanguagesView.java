0b224caeb4c6d9f78aedbeb258cdf1e8a4ccba3d
Urs Reupke <ursreupke@gmail.com>
2013-07-07 20:02:06 +0200

    Migrates Languages to FX

---
+++ Hero_Languages/src/net/sf/anathema/hero/languages/display/BridgingLanguagesView.java
@@ -0,0 +1,44 @@
+package net.sf.anathema.hero.languages.display;
+
+import net.sf.anathema.character.main.library.overview.OverviewCategory;
+import net.sf.anathema.framework.swing.IView;
+import net.sf.anathema.lib.file.RelativePath;
+import net.sf.anathema.lib.gui.AgnosticUIConfiguration;
+import net.sf.anathema.platform.fx.BridgingPanel;
+
+import javax.swing.JComponent;
+
+public class BridgingLanguagesView implements LanguagesView, IView {
+  private final FxLanguagesView fxView;
+  private final BridgingPanel panel = new BridgingPanel();
+
+  public BridgingLanguagesView(FxLanguagesView fxView) {
+    this.fxView = fxView;
+    panel.init(fxView);
+  }
+
+  @Override
+  public JComponent getComponent() {
+    return panel.getComponent();
+  }
+
+  @Override
+  public ObjectSelectionViewWithTool<Object> addSelectionView(String labelText, AgnosticUIConfiguration<Object> renderer) {
+    return fxView.addSelectionView(labelText, renderer);
+  }
+
+  @Override
+  public OverviewCategory addOverview(String border) {
+    return fxView.addOverview(border);
+  }
+
+  @Override
+  public RemovableEntryView addEntryView(RelativePath removeIcon, String label) {
+    return fxView.addEntryView(removeIcon, label);
+  }
+
+  @Override
+  public void removeEntryView(RemovableEntryView removableView) {
+    fxView.removeEntryView(removableView);
+  }
+}
--- Hero_Languages/src/net/sf/anathema/hero/languages/display/LanguagesViewFactory.java
+++ Hero_Languages/src/net/sf/anathema/hero/languages/display/LanguagesViewFactory.java
@@ -1,7 +1,7 @@
 package net.sf.anathema.hero.languages.display;
 
-import net.sf.anathema.character.main.type.ICharacterType;
 import net.sf.anathema.character.main.framework.RegisteredCharacterView;
+import net.sf.anathema.character.main.type.ICharacterType;
 import net.sf.anathema.character.main.view.SubViewFactory;
 
 @RegisteredCharacterView(LanguagesView.class)
@@ -9,6 +9,7 @@ public class LanguagesViewFactory implements SubViewFactory {
   @SuppressWarnings("unchecked")
   @Override
   public <T> T create(ICharacterType type) {
-    return (T) new LanguagesViewImpl();
+    FxLanguagesView view = new FxLanguagesView();
+    return (T) new BridgingLanguagesView(view);
   }
 }
