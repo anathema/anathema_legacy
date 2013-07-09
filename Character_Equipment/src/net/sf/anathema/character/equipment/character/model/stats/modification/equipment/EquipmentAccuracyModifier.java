6e46d9c54cbdc78583f1073b6c0cd26ce4950cff
Urs Reupke <ursreupke@gmail.com>
2013-07-09 14:35:30 +0200

    Adds missing classes from previous commit

---
+++ Character_Equipment/src/net/sf/anathema/character/equipment/character/model/stats/modification/equipment/EquipmentAccuracyModifier.java
@@ -0,0 +1,23 @@
+package net.sf.anathema.character.equipment.character.model.stats.modification.equipment;
+
+import net.sf.anathema.character.equipment.IWeaponModifiers;
+import net.sf.anathema.character.equipment.character.model.stats.modification.StatModifier;
+import net.sf.anathema.character.equipment.character.model.stats.modification.WeaponStatsType;
+
+public class EquipmentAccuracyModifier implements StatModifier {
+  private final IWeaponModifiers modifiers;
+  private final WeaponStatsType type;
+
+  public EquipmentAccuracyModifier(IWeaponModifiers modifiers, WeaponStatsType type) {
+    this.modifiers = modifiers;
+    this.type = type;
+  }
+
+  @Override
+  public int calculate() {
+    if (type.isRanged()) {
+      return modifiers.getRangedAccuracyMod();
+    }
+    return modifiers.getMeleeAccuracyMod();
+  }
+}
---
+++ Character_Equipment/src/net/sf/anathema/character/equipment/character/model/stats/modification/equipment/EquipmentDamageModifier.java
@@ -0,0 +1,23 @@
+package net.sf.anathema.character.equipment.character.model.stats.modification.equipment;
+
+import net.sf.anathema.character.equipment.IWeaponModifiers;
+import net.sf.anathema.character.equipment.character.model.stats.modification.StatModifier;
+import net.sf.anathema.character.equipment.character.model.stats.modification.WeaponStatsType;
+
+public class EquipmentDamageModifier implements StatModifier {
+  private final IWeaponModifiers modifiers;
+  private final WeaponStatsType type;
+
+  public EquipmentDamageModifier(IWeaponModifiers modifiers, WeaponStatsType type) {
+    this.modifiers = modifiers;
+    this.type = type;
+  }
+
+  @Override
+  public int calculate() {
+    if (type.isRanged()) {
+      return modifiers.getRangedDamageMod();
+    }
+    return modifiers.getMeleeDamageMod();
+  }
+}
