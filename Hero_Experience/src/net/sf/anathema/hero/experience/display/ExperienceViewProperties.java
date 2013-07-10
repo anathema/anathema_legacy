bd996c61d60068ffe1c54233bff52b4f528cc0a2
Urs Reupke <ursreupke@gmail.com>
2013-07-10 20:29:58 +0200

    Cleans up ExperienceViewProperties

--- Hero_Experience/src/net/sf/anathema/hero/experience/display/ExperienceViewProperties.java
+++ Hero_Experience/src/net/sf/anathema/hero/experience/display/ExperienceViewProperties.java
@@ -1,27 +1,13 @@
 package net.sf.anathema.hero.experience.display;
 
-import net.sf.anathema.framework.presenter.resources.BasicUi;
-import net.sf.anathema.lib.file.RelativePath;
 import net.sf.anathema.lib.resources.Resources;
 
 public class ExperienceViewProperties implements IExperienceViewProperties {
 
-  private final BasicUi basicUi;
   private Resources resources;
 
   public ExperienceViewProperties(Resources resources) {
-    this.basicUi = new BasicUi();
     this.resources = resources;
-  }
-
-  @Override
-  public RelativePath getDeleteIcon() {
-    return basicUi.getRemoveIconPath();
-  }
-
-  @Override
-  public RelativePath getAddIcon() {
-    return basicUi.getAddIconPath();
   }
 
   @Override
--- Hero_Experience/src/net/sf/anathema/hero/experience/display/IExperienceViewProperties.java
+++ Hero_Experience/src/net/sf/anathema/hero/experience/display/IExperienceViewProperties.java
@@ -1,12 +1,6 @@
 package net.sf.anathema.hero.experience.display;
 
-import net.sf.anathema.lib.file.RelativePath;
-
 public interface IExperienceViewProperties {
-
-  RelativePath getDeleteIcon();
-
-  RelativePath getAddIcon();
 
   String getTotalString();
 
