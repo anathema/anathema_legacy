dc3ee80bb436955faf22ba7378d232ee5dcf819c
Urs Reupke <ursreupke@gmail.com>
2013-07-12 21:34:44 +0200

    Adds dummy implementation of FXpersonalization edit view

---
+++ Character_Equipment/src/net/sf/anathema/hero/equipment/display/view/personalization/FxPersonalizationEditView.java
@@ -0,0 +1,36 @@
+package net.sf.anathema.hero.equipment.display.view.personalization;
+
+import net.sf.anathema.hero.equipment.display.presenter.PersonalizationEditView;
+import net.sf.anathema.lib.util.Closure;
+
+public class FxPersonalizationEditView implements PersonalizationEditView{
+  @Override
+  public void show() {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+  @Override
+  public void whenChangeIsConfirmed(Runnable runnable) {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+  @Override
+  public void setTitle(String title) {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+  @Override
+  public void setDescription(String description) {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+  @Override
+  public void whenTitleChanges(Closure<String> closure) {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+
+  @Override
+  public void whenDescriptionChanges(Closure<String> closure) {
+    //To change body of implemented methods use File | Settings | File Templates.
+  }
+}
