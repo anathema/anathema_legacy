61fda5ff819064f198f1c33ce3e0c2f1e3e57525
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2013-07-14 21:15:22 +0200

    MagicAdvanceTto predesigned

---
+++ Hero_Magic/src/main/java/net/sf/anathema/hero/magic/template/CostTto.java
@@ -0,0 +1,7 @@
+package net.sf.anathema.hero.magic.template;
+
+public class CostTto {
+
+  public int general = 0;
+  public int favored = 0;
+}
---
+++ Hero_Magic/src/main/java/net/sf/anathema/hero/magic/template/MagicAdvanceTto.java
@@ -0,0 +1,9 @@
+package net.sf.anathema.hero.magic.template;
+
+public class MagicAdvanceTto {
+
+  public CostTto freePicks = new CostTto();
+  public CostTto bonusCosts = new CostTto();
+  public CostTto experienceCosts = new CostTto();
+  public CostTto highMartialArtsExperience = new CostTto();
+}
