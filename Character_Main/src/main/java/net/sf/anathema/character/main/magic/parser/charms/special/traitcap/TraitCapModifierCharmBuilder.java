86c762a3447f2b0a0a86693545e70e5ff2205ff7
Sandra Reupke-Sieroux <sandra.sieroux@googlemail.com>
2013-07-16 15:29:37 +0200

    Separated parser und builder

--- Character_Main/src/main/java/net/sf/anathema/character/main/magic/parser/charms/special/traitcap/TraitCapModifierCharmBuilder.java
+++ Character_Main/src/main/java/net/sf/anathema/character/main/magic/parser/charms/special/traitcap/TraitCapModifierCharmBuilder.java
@@ -5,6 +5,7 @@ import net.sf.anathema.character.main.magic.charm.special.TraitCapModifyingCharm
 import net.sf.anathema.character.main.magic.parser.charms.TraitTypeFinder;
 import net.sf.anathema.character.main.magic.parser.charms.special.RegisteredSpecialCharmBuilder;
 import net.sf.anathema.character.main.magic.parser.charms.special.SpecialCharmBuilder;
+import net.sf.anathema.character.main.magic.parser.dto.special.SpecialCharmDto;
 import net.sf.anathema.character.main.magic.parser.dto.special.TraitCapModifierDto;
 import org.dom4j.Element;
 
@@ -15,24 +16,11 @@ public class TraitCapModifierCharmBuilder implements SpecialCharmBuilder {
 
   @Override
   public ISpecialCharm readCharm(Element charmElement, String id) {
-    TraitCapModifierDto dto = createDto(charmElement, id);
+    SpecialCharmDto overallDto = new SpecialCharmDto();
+    overallDto.charmId = id;
+    new TraitCapModifierParser().parse(charmElement, overallDto);
+    TraitCapModifierDto dto = overallDto.traitCapModifier;
     return new TraitCapModifyingCharm(id, traitTypeFinder.getTrait(dto.trait), dto.modifier);
-  }
-
-  private TraitCapModifierDto createDto(Element charmElement, String id) {
-    TraitCapModifierDto dto = new TraitCapModifierDto();
-    Element traitCapModifierElement = charmElement.element(TAG_TRAIT_CAP_MODIFIER);
-    dto.trait = readTraitString(id, traitCapModifierElement);
-    dto.modifier =  Integer.parseInt(traitCapModifierElement.attributeValue(ATTRIB_MODIFIER));
-    return dto;
-  }
-
-  private String readTraitString(String id, Element traitCapModifierElement) {
-    String traitString = traitCapModifierElement.attributeValue(ATTRIB_TRAIT);
-    if (traitString == null) {
-      traitString = id.split("\\.")[2];
-    }
-    return traitString;
   }
 
   @Override
---
+++ Character_Main/src/main/java/net/sf/anathema/character/main/magic/parser/charms/special/traitcap/TraitCapModifierParser.java
@@ -0,0 +1,38 @@
+package net.sf.anathema.character.main.magic.parser.charms.special.traitcap;
+
+import net.sf.anathema.character.main.magic.parser.charms.special.RegisteredSpecialCharmParser;
+import net.sf.anathema.character.main.magic.parser.charms.special.SpecialCharmParser;
+import net.sf.anathema.character.main.magic.parser.dto.special.SpecialCharmDto;
+import net.sf.anathema.character.main.magic.parser.dto.special.TraitCapModifierDto;
+import org.dom4j.Element;
+
+@RegisteredSpecialCharmParser
+public class TraitCapModifierParser implements SpecialCharmParser {
+
+  private static final String TAG_TRAIT_CAP_MODIFIER = "traitCapModifier";
+  private static final String TAG_PAIN_TOLERANCE = "painTolerance";
+  private static final String ATTRIB_MODIFIER = "modifier";
+  private static final String ATTRIB_TRAIT = "trait";
+
+  @Override
+  public void parse(Element charmElement, SpecialCharmDto overallDto) {
+    TraitCapModifierDto dto = new TraitCapModifierDto();
+    Element traitCapModifierElement = charmElement.element(TAG_TRAIT_CAP_MODIFIER);
+    dto.trait = readTraitString(overallDto.charmId, traitCapModifierElement);
+    dto.modifier = Integer.parseInt(traitCapModifierElement.attributeValue(ATTRIB_MODIFIER));
+    overallDto.traitCapModifier = dto;
+  }
+
+  private String readTraitString(String id, Element traitCapModifierElement) {
+    String traitString = traitCapModifierElement.attributeValue(ATTRIB_TRAIT);
+    if (traitString == null) {
+      traitString = id.split("\\.")[2];
+    }
+    return traitString;
+  }
+
+  @Override
+  public boolean supports(Element charmElement) {
+    return charmElement.element(TAG_TRAIT_CAP_MODIFIER) != null;
+  }
+}
