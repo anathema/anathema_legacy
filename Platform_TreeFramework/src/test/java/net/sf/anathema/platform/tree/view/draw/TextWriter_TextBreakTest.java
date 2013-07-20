f1b50b15be4fc9c0fff72fba759dec78a934f0af
Urs Reupke <ursreupke@gmail.com>
2013-07-20 21:20:29 +0200

    Exposes more unexpected behaviour in TextWriter

--- Platform_TreeFramework/src/test/java/net/sf/anathema/platform/tree/view/draw/TextWriter_TextBreakTest.java
+++ Platform_TreeFramework/src/test/java/net/sf/anathema/platform/tree/view/draw/TextWriter_TextBreakTest.java
@@ -2,11 +2,15 @@ package net.sf.anathema.platform.tree.view.draw;
 
 import net.sf.anathema.framework.ui.Area;
 import net.sf.anathema.framework.ui.Coordinate;
+import net.sf.anathema.platform.tree.display.shape.AgnosticShape;
 import net.sf.anathema.platform.tree.display.shape.Polygon;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.mockito.Matchers.isA;
+import static net.sf.anathema.platform.tree.view.draw.FilledPolygon_GraphicsTest.ANY_AREA_WITH_CONTENT;
+import static net.sf.anathema.platform.tree.view.draw.FilledPolygon_GraphicsTest.ANY_RECTANGLE;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -26,16 +30,23 @@ public class TextWriter_TextBreakTest {
     writer.setText("Forceful Arrow");
   }
 
+  @Before
+  public void configureCanvas() throws Exception {
+    when(canvas.calculateBounds(any(AgnosticShape.class))).thenReturn(ANY_RECTANGLE);
+    when(canvas.measureText(anyString())).thenReturn(ANY_AREA_WITH_CONTENT);
+  }
+
+
   @Test
   public void breaksText() throws Exception {
-    when(lineSuggestion.suggestNumberOfLines(isA(Area.class))).thenReturn(2);
+    when(lineSuggestion.suggestNumberOfLines(any(Area.class))).thenReturn(2);
     writer.write(canvas);
     verify(canvas).drawString("Forceful", new Coordinate(50, 38));
   }
 
   @Test
   public void keepsBreaksOnceEstablished() throws Exception {
-    when(lineSuggestion.suggestNumberOfLines(isA(Area.class))).thenReturn(2, 1);
+    when(lineSuggestion.suggestNumberOfLines(any(Area.class))).thenReturn(2, 1);
     writer.write(canvas);
     writer.write(canvas);
     verify(canvas, times(2)).drawString("Forceful", new Coordinate(50, 38));
