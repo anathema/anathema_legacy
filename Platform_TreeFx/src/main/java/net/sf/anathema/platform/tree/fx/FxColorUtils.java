c713ec432107e69c816ded3d20efc9cc80db6cc7
Urs Reupke <ursreupke@gmail.com>
2013-08-03 11:38:19 +0200

    Fixes color calculation for FX

--- Platform_TreeFx/src/main/java/net/sf/anathema/platform/tree/fx/FxColorUtils.java
+++ Platform_TreeFx/src/main/java/net/sf/anathema/platform/tree/fx/FxColorUtils.java
@@ -5,7 +5,14 @@ import javafx.scene.paint.Paint;
 import net.sf.anathema.framework.ui.RGBColor;
 
 public class FxColorUtils {
+
+  public static final double Normalizer = 255.0;
+
   public static Paint toFxColor(RGBColor color) {
-    return new Color(color.red, color.green, color.blue, color.alpha / 255.0);
+    double fxRed = color.red / Normalizer;
+    double fxGreen = color.green / Normalizer;
+    double fxBlue = color.blue / Normalizer;
+    double fxAlpha = color.alpha / Normalizer;
+    return new Color(fxRed, fxGreen, fxBlue, fxAlpha);
   }
 }
