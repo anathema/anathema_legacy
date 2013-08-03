bdfaf95a8710e6c54d9b9d0bce6bda1951e4a4f4
Urs Reupke <ursreupke@gmail.com>
2013-08-03 13:38:43 +0200

    Converts Cascades to FX

--- Charm_Cascades/src/main/java/net/sf/anathema/cascades/module/CascadeViewFactory.java
+++
@@ -1,8 +0,0 @@
-package net.sf.anathema.cascades.module;
-
-import net.sf.anathema.hero.charms.display.view.CharmView;
-
-public interface CascadeViewFactory {
-
-  CharmView createCascadeView();
-}
--- Charm_Cascades/src/main/java/net/sf/anathema/cascades/module/CharmCascadePerspective.java
+++ Charm_Cascades/src/main/java/net/sf/anathema/cascades/module/CharmCascadePerspective.java
@@ -3,17 +3,19 @@ package net.sf.anathema.cascades.module;
 import net.sf.anathema.cascades.presenter.CharmCascadesPresenterImpl;
 import net.sf.anathema.cascades.presenter.CharmTreeIdentifierMap;
 import net.sf.anathema.character.main.magic.description.MagicDescriptionProvider;
-import net.sf.anathema.hero.charms.display.presenter.CharmDescriptionProviderExtractor;
 import net.sf.anathema.framework.IApplicationModel;
 import net.sf.anathema.framework.view.perspective.Container;
 import net.sf.anathema.framework.view.perspective.Perspective;
 import net.sf.anathema.framework.view.perspective.PerspectiveAutoCollector;
 import net.sf.anathema.framework.view.perspective.PerspectiveToggle;
+import net.sf.anathema.hero.charms.display.presenter.CharmDescriptionProviderExtractor;
+import net.sf.anathema.hero.charms.display.view.FxCharmView;
 import net.sf.anathema.hero.framework.HeroEnvironment;
 import net.sf.anathema.hero.framework.HeroEnvironmentExtractor;
 import net.sf.anathema.initialization.reflections.Weight;
 import net.sf.anathema.lib.file.RelativePath;
 import net.sf.anathema.lib.resources.Resources;
+import net.sf.anathema.platform.fx.BridgingPanel;
 
 @PerspectiveAutoCollector
 @Weight(weight = 6000)
@@ -26,12 +28,14 @@ public class CharmCascadePerspective implements Perspective {
 
   @Override
   public void initContent(Container container, IApplicationModel applicationModel, Resources resources) {
-    CharmCascadeModuleView view = new CharmCascadeModuleView();
     HeroEnvironment characterGenerics = HeroEnvironmentExtractor.getGenerics(applicationModel);
     MagicDescriptionProvider magicDescriptionProvider = getCharmDescriptionProvider(applicationModel, resources);
-    new CharmCascadesPresenterImpl(resources, characterGenerics, view.createCascadeView(), magicDescriptionProvider,
+    FxCharmView cascadeView = new FxCharmView();
+    new CharmCascadesPresenterImpl(resources, characterGenerics, cascadeView, magicDescriptionProvider,
             new CharmTreeIdentifierMap()).initPresentation();
-    container.setSwingContent(view.getComponent());
+    BridgingPanel bridgingPanel = new BridgingPanel();
+    bridgingPanel.init(cascadeView);
+    container.setSwingContent(bridgingPanel.getComponent());
   }
 
   private MagicDescriptionProvider getCharmDescriptionProvider(IApplicationModel model, Resources resources) {
