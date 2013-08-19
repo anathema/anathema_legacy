8e8f89e18aa9c03a6ebba92b9094bea1089005d0
Urs Reupke <ursreupke@gmail.com>
2013-08-19 22:50:54 +0200

    Removes swing-glue between fx components for character

--- Character_Main_Impl/src/main/java/net/sf/anathema/fx/hero/perspective/TaskedCharacterPane.java
+++ Character_Main_Impl/src/main/java/net/sf/anathema/fx/hero/perspective/TaskedCharacterPane.java
@@ -1,11 +1,12 @@
 package net.sf.anathema.fx.hero.perspective;
 
 import javafx.scene.Node;
+import javafx.scene.layout.BorderPane;
 import net.miginfocom.layout.LC;
 import net.sf.anathema.character.main.view.CharacterPane;
-import net.sf.anathema.hero.display.MultipleContentView;
 import net.sf.anathema.framework.view.util.OptionalView;
 import net.sf.anathema.hero.advance.overview.view.OverviewDisplay;
+import net.sf.anathema.hero.display.MultipleContentView;
 import net.sf.anathema.lib.gui.layout.LayoutUtils;
 import net.sf.anathema.platform.fx.BridgingPanel;
 import net.sf.anathema.platform.fx.FxThreading;
@@ -14,25 +15,25 @@ import net.sf.anathema.swing.hero.perspective.FxOptionalOverview;
 import org.tbee.javafx.scene.layout.MigPane;
 
 import javax.swing.JComponent;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
-import java.awt.Dimension;
 
 public class TaskedCharacterPane implements CharacterPane, OverviewDisplay {
 
   private final MigPane paneContainer = new MigPane(new LC().wrapAfter(1));
   private final MigPane viewPanel = new MigPane();
-  private final JPanel content = new JPanel(new BorderLayout());
+  private final BorderPane content = new BorderPane();
   private final MigPane overviewPane = new MigPane(LayoutUtils.fillWithoutInsets().wrapAfter(1));
+  private final BridgingPanel panel = new BridgingPanel();
 
   public TaskedCharacterPane() {
-    JComponent navigationComponent = createNavigationComponent();
-    JComponent overviewComponent = createOverviewComponent();
-    JComponent characterComponent = createCharacterComponent();
-    content.add(navigationComponent, BorderLayout.WEST);
-    content.add(characterComponent, BorderLayout.CENTER);
-    content.add(overviewComponent, BorderLayout.EAST);
+    content.setLeft(paneContainer);
+    content.setCenter(viewPanel);
+    content.setRight(overviewPane);
+    panel.init(new NodeHolder() {
+      @Override
+      public Node getNode() {
+        return content;
+      }
+    });
   }
 
   public OptionalView getOverview() {
@@ -57,45 +58,6 @@ public class TaskedCharacterPane implements CharacterPane, OverviewDisplay {
 
   @Override
   public JComponent getComponent() {
-    return content;
-  }
-
-  private JComponent createNavigationComponent() {
-    BridgingPanel bridgingPanel = new BridgingPanel();
-    bridgingPanel.init(new NodeHolder() {
-      @Override
-      public Node getNode() {
-        return paneContainer;
-      }
-    });
-    JComponent navigationComponent = bridgingPanel.getComponent();
-    //TODO (Swing->FX) Layout properly when everything is in FX
-    navigationComponent.setPreferredSize(new Dimension(200, content.getHeight()));
-    return navigationComponent;
-  }
-
-  private JComponent createCharacterComponent() {
-    BridgingPanel bridgingPanel = new BridgingPanel();
-    bridgingPanel.init(new NodeHolder() {
-      @Override
-      public Node getNode() {
-        return viewPanel;
-      }
-    });
-    return bridgingPanel.getComponent();
-  }
-
-  private JComponent createOverviewComponent() {
-    BridgingPanel bridgingPanel = new BridgingPanel();
-    bridgingPanel.init(new NodeHolder() {
-      @Override
-      public Node getNode() {
-        return overviewPane;
-      }
-    });
-    JComponent overviewComponent = bridgingPanel.getComponent();
-    //TODO (Swing->FX) Layout properly when everything is in FX
-    overviewComponent.setPreferredSize(new Dimension(200, content.getHeight()));
-    return overviewComponent;
+    return panel.getComponent();
   }
 }
