eb8868518833adb42ae72066c8a91d9bcdd89010
Urs Reupke <ursreupke@gmail.com>
2013-08-25 10:33:43 +0200

    Perspective-Pane honors the navigation's size requirements

--- Platform_FX/src/main/java/net/sf/anathema/platform/fx/PerspectivePane.java
+++ Platform_FX/src/main/java/net/sf/anathema/platform/fx/PerspectivePane.java
@@ -1,34 +1,35 @@
 package net.sf.anathema.platform.fx;
 
 import javafx.scene.Node;
+import net.miginfocom.layout.AC;
 import net.miginfocom.layout.CC;
 import net.miginfocom.layout.LC;
 import org.tbee.javafx.scene.layout.MigPane;
 
 public class PerspectivePane {
 
-  private MigPane outerPane = new MigPane(new LC().fill());
+  private final MigPane content = new MigPane(new LC().fill(), new AC().index(1).shrink().shrinkPrio(200));
 
   public PerspectivePane(final String... styleSheetPaths) {
     for (String sheetPath : styleSheetPaths) {
-      new Stylesheet(sheetPath).applyToParent(outerPane);
+      new Stylesheet(sheetPath).applyToParent(content);
     }
   }
 
   public void setNavigationComponent(final Node component) {
-    outerPane.add(component, new CC().grow().minWidth("200").width("200").maxWidth("200"));
+    content.add(component, new CC().grow().minWidth("200").width("200").maxWidth("200"));
   }
 
   public void setContentComponent(final Node component) {
-    outerPane.add(component, new CC().grow().push());
+    content.add(component, new CC().grow().push());
   }
 
 
   public void addStyleSheetClass(String styleClass) {
-    outerPane.getStyleClass().add(styleClass);
+    content.getStyleClass().add(styleClass);
   }
 
   public Node getNode() {
-    return outerPane;
+    return content;
   }
 }
