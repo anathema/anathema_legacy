940d669867bda8e11f5d3152b2756437c02c9ede
Urs Reupke <ursreupke@gmail.com>
2013-08-25 10:30:58 +0200

    Removes nested panes from perspectivepane

--- Platform_FX/src/main/java/net/sf/anathema/platform/fx/PerspectivePane.java
+++ Platform_FX/src/main/java/net/sf/anathema/platform/fx/PerspectivePane.java
@@ -1,56 +1,26 @@
 package net.sf.anathema.platform.fx;
 
-import javafx.geometry.Insets;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.layout.BorderPane;
 import net.miginfocom.layout.CC;
 import net.miginfocom.layout.LC;
-import net.sf.anathema.lib.gui.layout.LayoutUtils;
 import org.tbee.javafx.scene.layout.MigPane;
-
-import static net.sf.anathema.platform.fx.FxThreading.runOnCorrectThread;
 
 public class PerspectivePane {
 
   private MigPane outerPane = new MigPane(new LC().fill());
-  private MigPane navigationPanel = new MigPane(LayoutUtils.fillWithoutInsets());
-  private MigPane contentPanel = new MigPane(LayoutUtils.fillWithoutInsets());
 
   public PerspectivePane(final String... styleSheetPaths) {
-    runOnCorrectThread(new InitNavigationPane());
-    runOnCorrectThread(new InitContentPane());
-    runOnCorrectThread(new InitOuterPane());
-    runOnCorrectThread(new Runnable() {
-      @Override
-      public void run() {
-        for (String sheetPath : styleSheetPaths) {
-          new Stylesheet(sheetPath).applyToParent(outerPane);
-        }
-      }
-    });
-  }
-
-  private void initBorderedPane(Parent pane) {
-    BorderPane.setMargin(pane, new Insets(3));
+    for (String sheetPath : styleSheetPaths) {
+      new Stylesheet(sheetPath).applyToParent(outerPane);
+    }
   }
 
   public void setNavigationComponent(final Node component) {
-    runOnCorrectThread(new Runnable() {
-      @Override
-      public void run() {
-        navigationPanel.add(component, new CC().grow().push());
-      }
-    });
+    outerPane.add(component, new CC().grow().minWidth("200").width("200").maxWidth("200"));
   }
 
   public void setContentComponent(final Node component) {
-    runOnCorrectThread(new Runnable() {
-      @Override
-      public void run() {
-        contentPanel.add(component, new CC().grow().push());
-      }
-    });
+    outerPane.add(component, new CC().grow().push());
   }
 
 
@@ -60,27 +30,5 @@ public class PerspectivePane {
 
   public Node getNode() {
     return outerPane;
-  }
-
-  private class InitOuterPane implements Runnable {
-    @Override
-    public void run() {
-      outerPane.add(navigationPanel, new CC().grow().minWidth("200").width("200").maxWidth("200"));
-      outerPane.add(contentPanel, new CC().grow().push());
-    }
-  }
-
-  private class InitNavigationPane implements Runnable {
-    @Override
-    public void run() {
-      initBorderedPane(navigationPanel);
-    }
-  }
-
-  private class InitContentPane implements Runnable {
-    @Override
-    public void run() {
-      initBorderedPane(contentPanel);
-    }
   }
 }
