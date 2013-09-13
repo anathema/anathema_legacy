7b0504d27c19f99a542ac55b1c6921c83f6f86f4
Urs Reupke <ursreupke@gmail.com>
2013-09-13 22:13:14 +0200

    Fixes test

--- Platform/src/test/java/net/sf/anathema/initialization/RepositoryLocationResolverTest.java
+++ Platform/src/test/java/net/sf/anathema/initialization/RepositoryLocationResolverTest.java
@@ -2,14 +2,11 @@ package net.sf.anathema.initialization;
 
 import net.sf.anathema.initialization.repository.RepositoryLocationResolver;
 import net.sf.anathema.platform.environment.DummyInitializationPreferences;
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class RepositoryLocationResolverTest {
-  private static final String SYSTEM_PROPERTY_REPOSITORY = "repository";
-  private static final String SYSTEM_PROPERTY_DEFAULT_REPOSITORY = "defaultrepository";
 
   private DummyInitializationPreferences anathemaPreferences;
 
@@ -33,32 +30,8 @@ public class RepositoryLocationResolverTest {
   }
 
   @Test
-  public void testSystemProperty() throws Exception {
-    System.setProperty(SYSTEM_PROPERTY_REPOSITORY, "parameter");
-    assertRepositoryLocation("parameter");
-  }
-
-  @Test
-  public void defaultRepositoryProperty() throws Exception {
-    System.setProperty(SYSTEM_PROPERTY_DEFAULT_REPOSITORY, "parametum");
-    assertRepositoryLocation("parametum");
-  }
-
-  @Test
   public void testPreferenceWithUserHomeWildcard() throws Exception {
     anathemaPreferences.setRepositoryLocationPreference("%USER_HOME%");
     assertRepositoryLocation(getUserHomeSystemProperty());
-  }
-
-  @Test
-  public void testSystemPropertyWithUserHomeWildcard() throws Exception {
-    System.setProperty(SYSTEM_PROPERTY_REPOSITORY, "%USER_HOME%" + "/Liebaes");
-    assertRepositoryLocation(getUserHomeSystemProperty() + "/Liebaes");
-  }
-
-  @After
-  public void tearDown() throws Exception {
-    System.clearProperty(SYSTEM_PROPERTY_REPOSITORY);
-    System.clearProperty(SYSTEM_PROPERTY_DEFAULT_REPOSITORY);
   }
 }
