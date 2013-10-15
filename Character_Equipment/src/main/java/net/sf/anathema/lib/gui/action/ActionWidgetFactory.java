13279f89d892088dfcea029cbace7aada69d235d
Urs Reupke <ursreupke@gmail.com>
2013-10-15 17:28:50 +0200

    Moves single-use classes to equipment

--- Character_Equipment/src/main/java/net/sf/anathema/lib/gui/action/ActionWidgetFactory.java
+++ Character_Equipment/src/main/java/net/sf/anathema/lib/gui/action/ActionWidgetFactory.java
@@ -3,21 +3,8 @@ package net.sf.anathema.lib.gui.action;
 import net.sf.anathema.lib.control.ChangeListener;
 
 import javax.swing.JCheckBox;
-import javax.swing.JToggleButton;
 
 public class ActionWidgetFactory {
-
-  public static JToggleButton createToggleButton(final SmartToggleAction action) {
-    final JToggleButton button = new JToggleButton(action);
-    button.setSelected(action.getSelectionModel().getValue());
-    action.getSelectionModel().addChangeListener(new ChangeListener() {
-      @Override
-      public void changeOccurred() {
-        button.setSelected(action.getSelectionModel().getValue());
-      }
-    });
-    return button;
-  }
 
   public static JCheckBox createCheckBox(final SmartToggleAction action) {
     final JCheckBox checkBox = new JCheckBox(action);
