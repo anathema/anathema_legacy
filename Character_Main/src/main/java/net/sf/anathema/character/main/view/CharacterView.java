287e1a398083ec52fcd1364f4c11ca0304bd152e
Urs Reupke <ursreupke@gmail.com>
2013-10-15 16:35:55 +0200

    Removes all but one reference to Swing from Ch_Main

--- Character_Main/src/main/java/net/sf/anathema/character/main/view/CharacterView.java
+++ Character_Main/src/main/java/net/sf/anathema/character/main/view/CharacterView.java
@@ -1,6 +1,5 @@
 package net.sf.anathema.character.main.view;
 
-import net.sf.anathema.framework.swing.IView;
 import net.sf.anathema.hero.advance.overview.view.OverviewContainer;
 import net.sf.anathema.platform.fx.NodeHolder;
 
