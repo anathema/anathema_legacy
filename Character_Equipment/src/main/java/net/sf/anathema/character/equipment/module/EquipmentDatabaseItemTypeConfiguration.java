896d51654aa0e98ba2458fe0d34de99da35b960a
Urs Reupke <ursreupke@gmail.com>
2013-10-20 21:13:37 +0200

    Initializes item types as contants

--- Character_Equipment/src/main/java/net/sf/anathema/character/equipment/module/EquipmentDatabaseItemTypeConfiguration.java
+++ Character_Equipment/src/main/java/net/sf/anathema/character/equipment/module/EquipmentDatabaseItemTypeConfiguration.java
@@ -10,14 +10,10 @@ public class EquipmentDatabaseItemTypeConfiguration implements net.sf.anathema.f
 
   private static final String EQUIPMENT_DATABASE_ITEM_TYPE_ID = "EquipmentDatabase";
   private static final RepositoryConfiguration REPOSITORY_CONFIGURATION = new RepositoryConfiguration(".item", "equipment/");
-  private final IItemType type;
-
-  public EquipmentDatabaseItemTypeConfiguration() {
-    this.type = new ItemType(EQUIPMENT_DATABASE_ITEM_TYPE_ID, REPOSITORY_CONFIGURATION, false);
-  }
+  private static final IItemType ITEM_TYPE = new ItemType(EQUIPMENT_DATABASE_ITEM_TYPE_ID, REPOSITORY_CONFIGURATION, false);
 
   @Override
   public IItemType getItemType() {
-    return type;
+    return ITEM_TYPE;
   }
 }
