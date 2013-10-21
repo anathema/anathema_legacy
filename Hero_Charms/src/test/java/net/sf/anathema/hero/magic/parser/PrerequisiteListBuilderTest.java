fecc285840fd34c25557ca7295ed31d502d2dcd5
Urs Reupke <ursreupke@gmail.com>
2013-10-21 19:14:51 +0200

    Fixes Test

--- Hero_Charms/src/test/java/net/sf/anathema/hero/magic/parser/PrerequisiteListBuilderTest.java
+++ Hero_Charms/src/test/java/net/sf/anathema/hero/magic/parser/PrerequisiteListBuilderTest.java
@@ -1,7 +1,9 @@
 package net.sf.anathema.hero.magic.parser;
 
 import net.sf.anathema.character.main.magic.charm.Charm;
+import net.sf.anathema.character.main.magic.charm.CharmImpl;
 import net.sf.anathema.character.main.magic.charm.ICharmXMLConstants;
+import net.sf.anathema.character.main.magic.charm.prerequisite.CharmLearnPrerequisite;
 import net.sf.anathema.character.main.magic.charm.prerequisite.DirectCharmLearnPrerequisite;
 import net.sf.anathema.character.main.magic.parser.charms.CharmPrerequisiteList;
 import net.sf.anathema.character.main.magic.parser.charms.prerequisite.AttributePrerequisiteBuilder;
@@ -13,47 +15,80 @@ import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PrerequisiteListBuilderTest {
-
   private static Element createBasicCharmPrerequisiteXml() {
     Element prerequisiteListElement = DocumentHelper.createElement("prerequisiteListElement");
     CharmXmlTestUtils.fillBasicPrerequisites(prerequisiteListElement);
     return prerequisiteListElement;
   }
 
-  private static CharmPrerequisiteList parsePrerequisiteList(Element prerequisiteListElement) throws PersistenceException {
-    return new PrerequisiteListBuilder(new TraitPrerequisitesBuilder(), new AttributePrerequisiteBuilder(), new CharmPrerequisiteBuilder())
-            .buildPrerequisiteList(prerequisiteListElement);
-  }
+  private Element prerequisiteListElement = createBasicCharmPrerequisiteXml();
+  private Map<String, CharmImpl> charmsById = new HashMap<>();
 
   @Test
   public void testCorrectCharmWithPrerequisiteCharm() throws Exception {
-    Element prerequisiteListElement = createBasicCharmPrerequisiteXml();
     String correctId = "Solar.CorrectId";
-    prerequisiteListElement.addElement(ICharmXMLConstants.TAG_CHARM_REFERENCE).addAttribute("id", correctId);
+    createCharmWithId(correctId);
+    addCharmWithIdToXml(prerequisiteListElement, correctId);
     CharmPrerequisiteList prerequisiteList = parsePrerequisiteList(prerequisiteListElement);
-    assertThat(prerequisiteList.getCharmPrerequisites().length, is(1));
-    Charm actualParentCharm = ((DirectCharmLearnPrerequisite) prerequisiteList.getCharmPrerequisites()[0]).getDirectPredecessors()[0];
+    assertThatExpectedNumberOfPrerequisitesMatches(prerequisiteList, 1);
+    DirectCharmLearnPrerequisite prerequisite = getPrerequisite(prerequisiteList, 0);
+    prerequisite.link(charmsById);
+    Charm actualParentCharm = prerequisite.getDirectPredecessors()[0];
     assertThat(actualParentCharm.getId(), is(correctId));
   }
 
   @Test
   public void testCorrectCharmWithMultiplePrerequisiteCharms() throws Exception {
-    Element prerequisiteListElement = createBasicCharmPrerequisiteXml();
     String correctId1 = "Solar.CorrectId1";
-    prerequisiteListElement.addElement(ICharmXMLConstants.TAG_CHARM_REFERENCE).addAttribute("id", correctId1);
     String correctId2 = "Solar.CorrectId2";
-    prerequisiteListElement.addElement(ICharmXMLConstants.TAG_CHARM_REFERENCE).addAttribute("id", correctId2);
+    createCharmWithId(correctId1);
+    createCharmWithId(correctId2);
+    addCharmWithIdToXml(prerequisiteListElement, correctId1);
+    addCharmWithIdToXml(prerequisiteListElement, correctId2);
     CharmPrerequisiteList prerequisiteList = parsePrerequisiteList(prerequisiteListElement);
-    assertThat(prerequisiteList.getCharmPrerequisites().length, is(2));
-    Charm[] parentCharms = ((DirectCharmLearnPrerequisite) prerequisiteList.getCharmPrerequisites()[0]).getDirectPredecessors();
-    Charm firstActualParent = parentCharms[0];
-    Charm secondActualParent = parentCharms[1];
-    assertThat(firstActualParent.getId(), is(correctId1));
-    assertThat(secondActualParent.getId(), is(correctId2));
+    assertThatExpectedNumberOfPrerequisitesMatches(prerequisiteList, 2);
+    DirectCharmLearnPrerequisite prerequisite1 = getPrerequisite(prerequisiteList, 0);
+    DirectCharmLearnPrerequisite prerequisite2 = getPrerequisite(prerequisiteList, 1);
+    prerequisite1.link(charmsById);
+    prerequisite2.link(charmsById);
+    Charm parentCharm1 = prerequisite1.getDirectPredecessors()[0];
+    Charm parentCharm2 = prerequisite2.getDirectPredecessors()[0];
+    assertThat(parentCharm1.getId(), is(correctId1));
+    assertThat(parentCharm2.getId(), is(correctId2));
+  }
+
+  private DirectCharmLearnPrerequisite getPrerequisite(CharmPrerequisiteList prerequisiteList, int index) {
+    CharmLearnPrerequisite charmLearnPrerequisite = prerequisiteList.getCharmPrerequisites()[index];
+    return (DirectCharmLearnPrerequisite) charmLearnPrerequisite;
+  }
+
+  private CharmPrerequisiteList parsePrerequisiteList(Element prerequisiteListElement) throws PersistenceException {
+    return new PrerequisiteListBuilder(new TraitPrerequisitesBuilder(), new AttributePrerequisiteBuilder(),
+            new CharmPrerequisiteBuilder()).buildPrerequisiteList(prerequisiteListElement);
+  }
+
+  private CharmImpl createCharmWithId(String correctId) {
+    CharmImpl charm = mock(CharmImpl.class);
+    when(charm.getId()).thenReturn(correctId);
+    charmsById.put(correctId, charm);
+    return charm;
+  }
+
+  private void addCharmWithIdToXml(Element prerequisiteListElement, String correctId) {
+    prerequisiteListElement.addElement(ICharmXMLConstants.TAG_CHARM_REFERENCE).addAttribute("id", correctId);
+  }
+
+  private void assertThatExpectedNumberOfPrerequisitesMatches(CharmPrerequisiteList prerequisiteList,
+                                                              int expectedPrerequisites) {
+    assertThat(prerequisiteList.getCharmPrerequisites().length, is(expectedPrerequisites));
   }
 }
