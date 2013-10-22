529175451987bc9ea50769a6da44e4f97480ff5b
Urs Reupke <ursreupke@gmail.com>
2013-10-23 00:17:24 +0200

    Fixes error in character loading

--- Platform/src/main/java/net/sf/anathema/framework/repository/access/printname/XmlPrintNameFileAccess.java
+++ Platform/src/main/java/net/sf/anathema/framework/repository/access/printname/XmlPrintNameFileAccess.java
@@ -6,7 +6,6 @@ import net.sf.anathema.framework.view.PrintNameFile;
 import net.sf.anathema.lib.exception.AnathemaException;
 import net.sf.anathema.lib.logging.Logger;
 import net.sf.anathema.lib.xml.DocumentUtilities;
-import org.apache.commons.io.FileUtils;
 import org.dom4j.Document;
 import org.dom4j.Element;
 
@@ -17,18 +16,12 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class XmlPrintNameFileAccess implements PrintNameFileAccess {
-  // Used for backward compatibility when all fails ...
-  public static final String COMPATIBILITY_ENCODING = "ISO-8859-1";
   private static final Logger logger = Logger.getLogger(XmlPrintNameFileAccess.class);
 
-  private static final String PRINT_NAME_ATTR = "repositoryPrintName";
-  private static final String ID_ATTR = "repositoryId";
-  private static final Pattern PRINT_NAME_PATTERN = Pattern.compile(PRINT_NAME_ATTR + "=\"(.*?)\"");
-  private static final Pattern ID_PATTERN = Pattern.compile(ID_ATTR + "=\"(.*?)\"");
+  public static final String ATTRIBUTE_REPOSITORY_PRINTNAME = "repositoryPrintName";
+  public static final String ATTRIBUTE_REPOSITORY_ID = "repositoryId";
   private final IRepositoryFileResolver resolver;
 
   public XmlPrintNameFileAccess(IRepositoryFileResolver resolver) {
@@ -53,9 +46,6 @@ public class XmlPrintNameFileAccess implements PrintNameFileAccess {
   }
 
   private PrintNameFile createPrintNameFile(File file, IItemType itemType) {
-    if (itemType.getRepositoryConfiguration().isItemSavedToSingleFile()) {
-      return createSingleFilePrintNameFile(file, itemType);
-    }
     return createMultiFilePrintNameFile(file, itemType);
   }
 
@@ -84,29 +74,7 @@ public class XmlPrintNameFileAccess implements PrintNameFileAccess {
     }
   }
 
-  private PrintNameFile readPrintName(File file, IItemType itemType) throws IOException {
-    try {
-      return extractPrintnameThroughXml(file, itemType);
-    } catch (AnathemaException ex) {
-      // Fall-back to the old method
-      return extractPrintnameThroughRegularExpression(file, itemType);
-    }
-  }
-
-  private PrintNameFile extractPrintnameThroughRegularExpression(File file, IItemType itemType) throws IOException {
-    String string = FileUtils.readFileToString(file, COMPATIBILITY_ENCODING);
-    Matcher printNameMatcher = PRINT_NAME_PATTERN.matcher(string);
-    if (!printNameMatcher.find()) {
-      return null;
-    }
-    Matcher idMatcher = ID_PATTERN.matcher(string);
-    if (!idMatcher.find()) {
-      return null;
-    }
-    return new PrintNameFile(file, printNameMatcher.group(1), idMatcher.group(1), itemType);
-  }
-
-  private PrintNameFile extractPrintnameThroughXml(File file, IItemType itemType) throws FileNotFoundException {
+  private PrintNameFile readPrintName(File file, IItemType itemType) throws FileNotFoundException {
     Document doc;
     try {
       doc = DocumentUtilities.read(file.toPath());
@@ -114,8 +82,8 @@ public class XmlPrintNameFileAccess implements PrintNameFileAccess {
       return null;
     }
     Element root = doc.getRootElement();
-    String printName = root.attributeValue(PRINT_NAME_ATTR);
-    String idName = root.attributeValue(ID_ATTR);
+    String printName = root.attributeValue(ATTRIBUTE_REPOSITORY_PRINTNAME);
+    String idName = root.attributeValue(ATTRIBUTE_REPOSITORY_ID);
     if (printName == null || idName == null) {
       return null;
     }
