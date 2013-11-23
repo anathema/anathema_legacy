22c285dbd20c778f29c42cef94b69ea8c4de4de2
Urs Reupke <ursreupke@gmail.com>
2013-11-23 12:33:20 +0100

    Blocks Test class from auto-instantiation

--- Character_Equipment/src/test/java/net/sf/anathema/hero/equipment/model/DummyMaterialRules.java
+++ Character_Equipment/src/test/java/net/sf/anathema/hero/equipment/model/DummyMaterialRules.java
@@ -1,10 +1,12 @@
 package net.sf.anathema.hero.equipment.model;
 
 import net.sf.anathema.equipment.core.MagicalMaterial;
+import net.sf.anathema.framework.environment.dependencies.DoNotInstantiateAutomatically;
 import net.sf.anathema.hero.equipment.sheet.content.stats.ArtifactAttuneType;
 
 import static net.sf.anathema.hero.equipment.model.ReflectionMaterialRulesTest.ID_TEST_TYPE;
 
+@DoNotInstantiateAutomatically
 @ForCharacterType(characterType = ID_TEST_TYPE)
 public class DummyMaterialRules implements CharacterTypeMaterialRules{
 
