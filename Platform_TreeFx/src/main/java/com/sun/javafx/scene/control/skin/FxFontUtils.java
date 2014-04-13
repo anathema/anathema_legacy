cbaad9c7205d1db62ef38a5497a6a200f3a8a590
Urs Reupke <ursreupke@gmail.com>
2014-04-13 16:39:10 +0200

    Restores the ability to calculate a text's visual bounds.

--- Platform_TreeFx/src/main/java/com/sun/javafx/scene/control/skin/FxFontUtils.java
+++ Platform_TreeFx/src/main/java/com/sun/javafx/scene/control/skin/FxFontUtils.java
@@ -1,15 +1,25 @@
 package com.sun.javafx.scene.control.skin;
 
+import javafx.geometry.Bounds;
 import javafx.scene.text.Font;
-import javafx.scene.text.TextBoundsType;
+import javafx.scene.text.Text;
 
 public class FxFontUtils {
 
   public static double calculateStringWidth(Font font, String text) {
-    return Utils.computeTextWidth(font, text, 0);
+    Bounds bounds = getBounds(font, text);
+    return bounds.getWidth();
   }
 
   public static double calculateStringHeight(Font font, String text) {
-    return Utils.computeTextHeight(font, text, 0, TextBoundsType.VISUAL);
+    Bounds bounds = getBounds(font, text);
+    return bounds.getHeight();
+  }
+
+  private static Bounds getBounds(Font font, String text) {
+    Text object = new Text();
+    object.setFont(font);
+    object.setText(text);
+    return object.getLayoutBounds();
   }
 }
