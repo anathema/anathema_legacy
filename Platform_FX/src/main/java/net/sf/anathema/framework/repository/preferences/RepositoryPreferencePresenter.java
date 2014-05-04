anathema/anathema_legacy

commit 885de9eda6d35b67a23bc293394a41b36c04a5c3

Restores "show in explorer" to repo preference

Platform_FX/src/main/java/net/sf/anathema/framework/repository/preferences/RepositoryPreferencePresenter.java
@@ -24,6 +24,7 @@ public class RepositoryPreferencePresenter implements PreferencePresenter {
     initRepoDisplay();
     initBrowseForFolder();
     initResetToDefault();
+    initShowInExplorer();
   }
 
   private void initResetToDefault() {
@@ -40,18 +41,33 @@ public class RepositoryPreferencePresenter implements PreferencePresenter {
 
   private void initBrowseForFolder() {
     Tool tool = view.addTool();
-    tool.setText(environment.getString("Preferences.Repository.Browse.Button"));
-    tool.setTooltip(environment.getString("Preferences.Repository.Browse.Tooltip"));
+    tool.setText(environment.getString("Preferences.Repository.Choose.Button"));
+    tool.setTooltip(environment.getString("Preferences.Repository.Choose.Tooltip"));
     tool.setCommand(new Command() {
       @Override
       public void execute() {
-        view.selectNewRepository("Preferences.Repository.Browse.Prompt");
+        view.selectNewRepository("Preferences.Repository.Choose.Prompt");
       }
     });
     view.whenRepositoryChangeIsRequested(new ObjectValueListener<Path>() {
       @Override
       public void valueChanged(Path path) {
         model.requestChangeOfRepositoryPath(path);
+      }
+    });
+  }
+
+  private void initShowInExplorer() {
+    if (!view.canOpenInExplorer()) {
+      return;
+    }
+    Tool tool = view.addTool();
+    tool.setText(environment.getString("Preferences.Repository.Browse.Button"));
+    tool.setTooltip(environment.getString("Preferences.Repository.Browse.Tooltip"));
+    tool.setCommand(new Command() {
+      @Override
+      public void execute() {
+        view.showInExplorer(model.getRepositoryPath());
       }
     });
   }

Hero_Sheet/src/main/java/net/sf/anathema/hero/sheet/preferences/FxSheetPreferenceView.java
@@ -1,6 +1,7 @@
 package net.sf.anathema.hero.sheet.preferences;
 
 import javafx.scene.Node;
+import net.miginfocom.layout.CC;
 import net.sf.anathema.framework.preferences.elements.PreferenceView;
 import net.sf.anathema.framework.reporting.pdf.PageSize;
 import net.sf.anathema.lib.gui.AgnosticUIConfiguration;
@@ -17,7 +18,7 @@ public class FxSheetPreferenceView implements PreferenceView, NodeHolder, SheetP
   @Override
   public ObjectSelectionView<PageSize> addObjectSelectionView(String title, AgnosticUIConfiguration<PageSize> uiConfiguration) {
     ComboBoxSelectionView<PageSize> view = new ComboBoxSelectionView<>(title, uiConfiguration);
-    pane.add(view.getNode());
+    pane.add(view.getNode(), new CC().alignY("top"));
     return view;
   }
 

Platform/src/main/java/net/sf/anathema/framework/desktop/DesktopEnvironment.java
@@ -12,6 +12,10 @@ public class DesktopEnvironment {
     return isDesktopSupported();
   }
 
+  public static boolean isOpenSupported() {
+    return isDesktopSupported();
+  }
+
   public static void openOnDesktop(Path path) throws IOException {
     Desktop.getDesktop().open(path.toFile());
   }

Platform_FX/src/main/java/net/sf/anathema/framework/repository/preferences/FxRepositoryPreferenceView.java
@@ -3,6 +3,7 @@ package net.sf.anathema.framework.repository.preferences;
 import javafx.scene.Node;
 import javafx.stage.DirectoryChooser;
 import net.miginfocom.layout.CC;
+import net.sf.anathema.framework.desktop.DesktopEnvironment;
 import net.sf.anathema.framework.preferences.elements.PreferenceView;
 import net.sf.anathema.interaction.Tool;
 import net.sf.anathema.lib.control.ObjectValueListener;
@@ -15,6 +16,7 @@ import org.jmock.example.announcer.Announcer;
 import org.tbee.javafx.scene.layout.MigPane;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 
 @SuppressWarnings("UnusedDeclaration")
@@ -26,7 +28,7 @@ public class FxRepositoryPreferenceView implements PreferenceView, NodeHolder, R
   @Override
   public ITextView addRepositoryDisplay(String label) {
     FxTextView view = FxTextView.SingleLine(label);
-    pane.add(view.getNode());
+    pane.add(view.getNode(), new CC().alignX("right").alignY("top"));
     return view;
   }
 
@@ -38,7 +40,7 @@ public class FxRepositoryPreferenceView implements PreferenceView, NodeHolder, R
   @Override
   public Tool addTool() {
     FxButtonTool tool = FxButtonTool.ForAnyPurpose();
-    pane.add(tool.getNode(), new CC().alignX("right"));
+    pane.add(tool.getNode(), new CC().alignX("right").alignY("top"));
     return tool;
   }
 
@@ -56,5 +58,19 @@ public class FxRepositoryPreferenceView implements PreferenceView, NodeHolder, R
   @Override
   public void whenRepositoryChangeIsRequested(ObjectValueListener<Path> objectValueListener) {
     announcer.addListener(objectValueListener);
+  }
+
+  @Override
+  public void showInExplorer(Path repositoryPath) {
+    try {
+      DesktopEnvironment.openOnDesktop(repositoryPath);
+    } catch (IOException e) {
+      throw new RuntimeException("Please check via 'canOpenInExplorer()' before calling this method.", e);
+    }
+  }
+
+  @Override
+  public boolean canOpenInExplorer() {
+    return DesktopEnvironment.isOpenSupported();
   }
 }

Platform_FX/src/main/java/net/sf/anathema/framework/repository/preferences/RepositoryPreferenceView.java
@@ -14,4 +14,8 @@ public interface RepositoryPreferenceView {
   void selectNewRepository(String prompt);
 
   void whenRepositoryChangeIsRequested(ObjectValueListener<Path> objectValueListener);
+
+  void showInExplorer(Path repositoryPath);
+
+  boolean canOpenInExplorer();
 }

Platform_FX/src/main/resources/data/language/framework/fx/repository.properties
@@ -1,7 +1,12 @@
 Preferences.Repository=Repository
 Preferences.Repository.Label=Repository folder
+
 Preferences.Repository.Default.Button=Default
 Preferences.Repository.Default.Tooltip=Reset the storage folder to the original default.
+
+Preferences.Repository.Choose.Button=Choose
+Preferences.Repository.Choose.Tooltip=Tell Anathema where to store your data.
+Preferences.Repository.Choose.Prompt=Select a new repository folder.
+
 Preferences.Repository.Browse.Button=Browse
-Preferences.Repository.Browse.Tooltip=Tell Anathema where to store your data.
-Preferences.Repository.Browse.Prompt=Select a new repository folder.
+Preferences.Repository.Browse.Tooltip=See the directory in your system's file browser

Platform_Preferences/src/main/java/net/sf/anathema/framework/preferences/perspective/FxPreferencesNavigation.java
@@ -28,7 +28,7 @@ public class FxPreferencesNavigation extends Navigation implements PreferencesNa
         trigger.setOnAction(new EventHandler<ActionEvent>() {
           @Override
           public void handle(ActionEvent actionEvent) {
-            preferencesView.show((NodeHolder)view);
+            preferencesView.show(title, (NodeHolder)view);
           }
         });
         return view;

Platform_Preferences/src/main/java/net/sf/anathema/framework/preferences/perspective/FxPreferencesView.java
@@ -1,19 +1,26 @@
 package net.sf.anathema.framework.preferences.perspective;
 
 import javafx.scene.Node;
-import net.sf.anathema.lib.gui.layout.LayoutUtils;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import net.miginfocom.layout.CC;
+import net.miginfocom.layout.LC;
 import net.sf.anathema.platform.fx.NodeHolder;
 import org.tbee.javafx.scene.layout.MigPane;
 
 public class FxPreferencesView implements PreferencesView {
-  private final MigPane node = new MigPane(LayoutUtils.fillWithoutInsets());
+  private final MigPane node = new MigPane(new LC().fill());
 
   public Node getNode() {
     return node;
   }
 
-  public void show(NodeHolder view) {
+  public void show(String title, NodeHolder view) {
     node.getChildren().clear();
-    node.add(view.getNode());
+    Label label = new Label(title);
+    label.setStyle("-fx-font-weight: bold");
+    node.add(label, new CC().split(2));
+    node.add(new Separator(), new CC().pushX().growX().wrap());
+    node.add(view.getNode(), new CC().pushY().growY());
   }
 }

commit fcfad595ef27fdcd880d369690f71a7395d21b0b

Once again shows the repository preference screen. * Fixes an issue introduced in 885de9eda6d35b67a23bc293394a41b36c04a5c3

Platform_FX/src/main/java/net/sf/anathema/framework/repository/preferences/RepositoryPreferencePresenter.java
@@ -5,8 +5,13 @@ import net.sf.anathema.framework.preferences.elements.PreferenceModel;
 import net.sf.anathema.framework.preferences.elements.PreferencePresenter;
 import net.sf.anathema.framework.preferences.elements.PreferenceView;
 import net.sf.anathema.framework.preferences.elements.RegisteredPreferencePresenter;
+import net.sf.anathema.interaction.Command;
 import net.sf.anathema.interaction.Tool;
+import net.sf.anathema.lib.control.ChangeListener;
+import net.sf.anathema.lib.control.ObjectValueListener;
 import net.sf.anathema.lib.workflow.textualdescription.ITextView;
+
+import java.nio.file.Path;
 
 @RegisteredPreferencePresenter
 public class RepositoryPreferencePresenter implements PreferencePresenter {
@@ -26,15 +31,30 @@ public class RepositoryPreferencePresenter implements PreferencePresenter {
     Tool tool = view.addTool();
     tool.setText(environment.getString("Preferences.Repository.Default.Button"));
     tool.setTooltip(environment.getString("Preferences.Repository.Default.Tooltip"));
-    tool.setCommand(() -> model.resetToDefault());
+    tool.setCommand(new Command() {
+      @Override
+      public void execute() {
+        model.resetToDefault();
+      }
+    });
   }
 
   private void initBrowseForFolder() {
     Tool tool = view.addTool();
     tool.setText(environment.getString("Preferences.Repository.Choose.Button"));
     tool.setTooltip(environment.getString("Preferences.Repository.Choose.Tooltip"));
-    tool.setCommand(() -> view.selectNewRepository("Preferences.Repository.Choose.Prompt"));
-    view.whenRepositoryChangeIsRequested(path -> model.requestChangeOfRepositoryPath(path));
+    tool.setCommand(new Command() {
+      @Override
+      public void execute() {
+        view.selectNewRepository("Preferences.Repository.Choose.Prompt");
+      }
+    });
+    view.whenRepositoryChangeIsRequested(new ObjectValueListener<Path>() {
+      @Override
+      public void valueChanged(Path path) {
+        model.requestChangeOfRepositoryPath(path);
+      }
+    });
   }
 
   private void initShowInExplorer() {
@@ -44,13 +64,23 @@ public class RepositoryPreferencePresenter implements PreferencePresenter {
     Tool tool = view.addTool();
     tool.setText(environment.getString("Preferences.Repository.Browse.Button"));
     tool.setTooltip(environment.getString("Preferences.Repository.Browse.Tooltip"));
-    tool.setCommand(() -> view.showInExplorer(model.getRepositoryPath()));
+    tool.setCommand(new Command() {
+      @Override
+      public void execute() {
+        view.showInExplorer(model.getRepositoryPath());
+      }
+    });
   }
 
   private void initRepoDisplay() {
     final ITextView textView = view.addRepositoryDisplay(environment.getString("Preferences.Repository.Label"));
     textView.setEnabled(false);
-    model.whenLocationChanges(() -> showRepoInView(textView));
+    model.whenLocationChanges(new ChangeListener() {
+      @Override
+      public void changeOccurred() {
+        showRepoInView(textView);
+      }
+    });
     showRepoInView(textView);
   }
 