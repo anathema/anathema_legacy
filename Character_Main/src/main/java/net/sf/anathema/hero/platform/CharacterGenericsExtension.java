84237c8fa9f2fcaea99884a7b731f717ec098160
Urs Reupke <ursreupke@gmail.com>
2014-05-24 20:56:19 +0200

    Initializes the character system in a simpler way

--- Character_Main/src/main/java/net/sf/anathema/hero/platform/CharacterGenericsExtension.java
+++ Character_Main/src/main/java/net/sf/anathema/hero/platform/CharacterGenericsExtension.java
@@ -1,24 +1,29 @@
 package net.sf.anathema.hero.platform;
 
+import net.sf.anathema.character.framework.CharacterTemplateInitializer;
+import net.sf.anathema.character.framework.data.IExtensibleDataSetProvider;
 import net.sf.anathema.framework.environment.ObjectFactory;
 import net.sf.anathema.framework.environment.ResourceLoader;
 import net.sf.anathema.framework.extension.AnathemaExtension;
+import net.sf.anathema.hero.framework.DataSetInitializer;
 import net.sf.anathema.hero.framework.HeroEnvironment;
 import net.sf.anathema.hero.framework.HeroEnvironmentExtension;
-import net.sf.anathema.hero.framework.HeroEnvironmentInitializer;
+import net.sf.anathema.hero.framework.HeroEnvironmentImpl;
 import net.sf.anathema.initialization.repository.DataFileProvider;
 
 public class CharacterGenericsExtension implements HeroEnvironmentExtension, AnathemaExtension {
 
   private HeroEnvironment environment;
 
-  public void initialize(DataFileProvider dataFileProvider, ObjectFactory instantiater, ResourceLoader loader) {
-    HeroEnvironmentInitializer initializer = new HeroEnvironmentInitializer(loader, instantiater);
-    this.environment = initializer.initEnvironment(dataFileProvider);
-  }
-
   @Override
   public HeroEnvironment getEnvironment() {
     return environment;
+  }
+
+  public void initialize(DataFileProvider dataFileProvider, ObjectFactory objectFactory, ResourceLoader loader) {
+    DataSetInitializer dataSetInitializer = new DataSetInitializer(loader, objectFactory);
+    IExtensibleDataSetProvider dataSetProvider = dataSetInitializer.initializeExtensibleResources();
+    this.environment = new HeroEnvironmentImpl(dataFileProvider, objectFactory, dataSetProvider);
+    new CharacterTemplateInitializer(environment).addCharacterTemplates();
   }
 }
