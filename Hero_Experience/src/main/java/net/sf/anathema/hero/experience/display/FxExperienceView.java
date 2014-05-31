c7dcf76777a9d362bc03b995ebc128b5239723bb
Urs Reupke <ursreupke@gmail.com>
2014-05-31 12:09:22 +0200

    Right-aligns numbers in XP table

--- Hero_Experience/src/main/java/net/sf/anathema/hero/experience/display/FxExperienceView.java
+++ Hero_Experience/src/main/java/net/sf/anathema/hero/experience/display/FxExperienceView.java
@@ -1,9 +1,6 @@
 package net.sf.anathema.hero.experience.display;
 
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
@@ -36,12 +33,7 @@ public class FxExperienceView implements ExperienceView, NodeHolder {
     TableColumn<ExperiencePointEntry, String> descriptionColumn = createDescriptionColumn(properties);
     table.setEditable(true);
     table.getColumns().addAll(descriptionColumn, pointsColumn);
-    table.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<ExperiencePointEntry>() {
-      @Override
-      public void changed(ObservableValue observableValue, ExperiencePointEntry o, ExperiencePointEntry newValue) {
-        entryAnnouncer.announce().selectionChanged(newValue);
-      }
-    });
+    table.getSelectionModel().selectedItemProperty().addListener((observableValue, o, newValue) -> entryAnnouncer.announce().selectionChanged(newValue));
     MigPane totalPanel = createTotalPanel(properties);
     content.add(buttonPanel);
     content.add(table, new CC().push().grow().span());
@@ -97,23 +89,23 @@ public class FxExperienceView implements ExperienceView, NodeHolder {
   private TableColumn<ExperiencePointEntry, String> createPointsColumn(ExperienceViewProperties properties) {
     TableColumn<ExperiencePointEntry, String> pointColumn = new TableColumn<>(properties.getPointHeader());
     pointColumn.prefWidthProperty().bind(table.widthProperty().divide(4));
-    Callback<TableColumn<ExperiencePointEntry, String>, TableCell<ExperiencePointEntry, String>> cellCallback = TextFieldTableCell.forTableColumn();
-    pointColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ExperiencePointEntry, String>, ObservableValue<String>>() {
+    Callback<TableColumn<ExperiencePointEntry, String>, TableCell<ExperiencePointEntry, String>> editableCell = TextFieldTableCell.forTableColumn();
+    Callback<TableColumn<ExperiencePointEntry, String>, TableCell<ExperiencePointEntry, String>> styledEditableCell = new Callback<TableColumn<ExperiencePointEntry, String>, TableCell<ExperiencePointEntry, String>>() {
       @Override
-      public ObservableValue<String> call(TableColumn.CellDataFeatures<ExperiencePointEntry, String> features) {
-        return new SimpleStringProperty(String.valueOf(features.getValue().getExperiencePoints()));
+      public TableCell<ExperiencePointEntry, String> call(TableColumn<ExperiencePointEntry, String> experiencePointEntryStringTableColumn) {
+        TableCell<ExperiencePointEntry, String> cell = editableCell.call(experiencePointEntryStringTableColumn);
+        cell.getStyleClass().add("right-aligned");
+        return cell;
       }
-    });
-    pointColumn.setCellFactory(cellCallback);
+    };
+    pointColumn.setCellValueFactory(features -> new SimpleStringProperty(String.valueOf(features.getValue().getExperiencePoints())));
+    pointColumn.setCellFactory(styledEditableCell);
     pointColumn.setOnEditCommit(
-            new EventHandler<TableColumn.CellEditEvent<ExperiencePointEntry, String>>() {
-              @Override
-              public void handle(TableColumn.CellEditEvent<ExperiencePointEntry, String> event) {
-                ExperiencePointEntry experienceEntry = event.getRowValue();
-                Integer experiencePoints = getChangedPointValue(event, experienceEntry);
-                String description = experienceEntry.getTextualDescription().getText();
-                updateAnnouncer.announce().update(experiencePoints, description);
-              }
+            event -> {
+              ExperiencePointEntry experienceEntry = event.getRowValue();
+              Integer experiencePoints = getChangedPointValue(event, experienceEntry);
+              String description = experienceEntry.getTextualDescription().getText();
+              updateAnnouncer.announce().update(experiencePoints, description);
             }
     );
     return pointColumn;
@@ -131,22 +123,14 @@ public class FxExperienceView implements ExperienceView, NodeHolder {
     TableColumn<ExperiencePointEntry, String> descriptionColumn = new TableColumn<>(properties.getDescriptionHeader());
     descriptionColumn.prefWidthProperty().bind(table.widthProperty().multiply(3).divide(4).subtract(5));
     Callback<TableColumn<ExperiencePointEntry, String>, TableCell<ExperiencePointEntry, String>> cellCallback = TextFieldTableCell.forTableColumn();
-    descriptionColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<ExperiencePointEntry, String>, ObservableValue<String>>() {
-      @Override
-      public ObservableValue<String> call(TableColumn.CellDataFeatures<ExperiencePointEntry, String> features) {
-        return new SimpleStringProperty(features.getValue().getTextualDescription().getText());
-      }
-    });
+    descriptionColumn.setCellValueFactory(features -> new SimpleStringProperty(features.getValue().getTextualDescription().getText()));
     descriptionColumn.setCellFactory(cellCallback);
     descriptionColumn.setOnEditCommit(
-            new EventHandler<TableColumn.CellEditEvent<ExperiencePointEntry, String>>() {
-              @Override
-              public void handle(TableColumn.CellEditEvent<ExperiencePointEntry, String> event) {
-                ExperiencePointEntry experienceEntry = event.getRowValue();
-                Integer experiencePoints = experienceEntry.getExperiencePoints();
-                String description = event.getNewValue();
-                updateAnnouncer.announce().update(experiencePoints, description);
-              }
+            event -> {
+              ExperiencePointEntry experienceEntry = event.getRowValue();
+              Integer experiencePoints = experienceEntry.getExperiencePoints();
+              String description = event.getNewValue();
+              updateAnnouncer.announce().update(experiencePoints, description);
             }
     );
     return descriptionColumn;
