b10739cb78f44b9ecf169908845f302015a1c9e3
Urs Reupke <ursreupke@gmail.com>
2014-06-01 19:11:31 +0200

    Fixes saving of print state for equipment items

--- Character_Equipment/src/test/java/net/sf/anathema/character/equipment/impl/character/model/EquipmentModelTest.java
+++ Character_Equipment/src/test/java/net/sf/anathema/character/equipment/impl/character/model/EquipmentModelTest.java
@@ -1,24 +1,24 @@
 package net.sf.anathema.character.equipment.impl.character.model;
 
-import net.sf.anathema.character.equipment.dummy.DummyEquipmentObject;
-import net.sf.anathema.hero.traits.model.types.OtherTraitType;
-import net.sf.anathema.hero.dummy.models.DummyHeroConcept;
-import net.sf.anathema.hero.dummy.models.DummySpiritualTraitModel;
-import net.sf.anathema.hero.dummy.models.DummyTraitModel;
-import net.sf.anathema.hero.traits.template.Group;
-import net.sf.anathema.hero.traits.template.GroupedTraitsTemplate;
-import net.sf.anathema.hero.equipment.sheet.content.stats.weapon.IEquipmentStats;
+import net.sf.anathema.character.equipment.dummy.DummyEquipmentItem;
 import net.sf.anathema.hero.abilities.model.AbilitiesModelImpl;
 import net.sf.anathema.hero.attributes.model.AttributeModel;
 import net.sf.anathema.hero.attributes.model.AttributeModelImpl;
 import net.sf.anathema.hero.dummy.DummyHero;
 import net.sf.anathema.hero.dummy.DummyHeroEnvironment;
+import net.sf.anathema.hero.dummy.models.DummyHeroConcept;
+import net.sf.anathema.hero.dummy.models.DummySpiritualTraitModel;
+import net.sf.anathema.hero.dummy.models.DummyTraitModel;
 import net.sf.anathema.hero.equipment.EquipmentModelImpl;
+import net.sf.anathema.hero.equipment.sheet.content.stats.weapon.IEquipmentStats;
 import net.sf.anathema.hero.sheet.pdf.content.stats.StatsModelImpl;
 import net.sf.anathema.hero.specialties.model.SpecialtiesModelImpl;
 import net.sf.anathema.hero.spiritual.model.pool.EssencePoolModelImpl;
 import net.sf.anathema.hero.spiritual.template.EssencePoolTemplate;
 import net.sf.anathema.hero.traits.model.TraitModelImpl;
+import net.sf.anathema.hero.traits.model.types.OtherTraitType;
+import net.sf.anathema.hero.traits.template.Group;
+import net.sf.anathema.hero.traits.template.GroupedTraitsTemplate;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -64,10 +64,10 @@ public class EquipmentModelTest {
 
   @Test
   public void removesStatsWithoutNpe() throws Exception {
-    DummyEquipmentObject fromItem = new DummyEquipmentObject("from", "");
+    DummyEquipmentItem fromItem = new DummyEquipmentItem("from", "");
     IEquipmentStats stats = mock(IEquipmentStats.class);
     fromItem.addEquipment(stats);
-    DummyEquipmentObject toItem = new DummyEquipmentObject("to", "");
+    DummyEquipmentItem toItem = new DummyEquipmentItem("to", "");
     model.transferOptions(fromItem, toItem);
   }
 }
