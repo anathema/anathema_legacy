c2c413e193fe8319697de7bf9262f80178349059
Alessio Caiazza <nolith@abisso.org>
2014-06-11 11:00:02 +0200

    MINOR: linux: automatically detect the installation path
    Do not force users to install anathema on /usr/share/anathema
    
    This patch will use standard shell scripting to detect the real
    anathema installation folder.

--- Development_Distribution/Linux/anathema.sh
+++ Development_Distribution/Linux/anathema.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-ANATHEMA_DIR=/usr/share/anathema
+# get the absolute path of the current script
+SOURCE=$(readlink -f "$0")
+# resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it relative 
+  #   to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" 
+done
+ANATHEMA_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 REPO_DIR=${HOME}/.anathema/repository
 JAVA_BIN=
 
